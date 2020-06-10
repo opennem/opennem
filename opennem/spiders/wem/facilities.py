e39d448dd18ed2d9a1cd297868a25d4e66056000
Nik Cubrilovic <nc9@protonmail.com>
2020-06-10 15:38:47 +1000

    WEM facilities spider

---
+++ opennem/spiders/wem/facilities.py
@@ -0,0 +1,21 @@
+from scrapy import Spider
+
+from opennem.pipelines.nem import DatabaseStore, ExtractCSV
+
+
+def WemFacilities(Spider):
+    name = "au.wem.facilities"
+    start_urls = [
+        "http://data.wa.aemo.com.au/datafiles/facilities/facilities.csv"
+    ]
+    allowed_domains = ["wa.nemweb.com.au"]
+    pipelines = set([ExtractCSV, DatabaseStore,])
+
+    def parse(self, response):
+        if response:
+            yield {
+                "link": response.url,
+                "table_name": "facilities",
+                "content": response.text,
+                "type": "file",
+            }
