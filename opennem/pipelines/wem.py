dd0b300f237ba8dba405d8ed58b2a0809b0f80bb
Nik Cubrilovic <nc9@protonmail.com>
2020-06-10 15:38:38 +1000

    Add wem pipeline base

---
+++ opennem/pipelines/wem.py
@@ -0,0 +1,106 @@
+import csv
+import logging
+
+from scrapy.exceptions import DropItem
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+
+from opennem.db import db_connect
+from opennem.db.models.opennem import WemFacility, WemFacilityScada
+from opennem.utils.pipelines import check_spider_pipeline
+
+logger = logging.getLogger(__name__)
+
+TABLE_MAP = {
+    "facilities": WemFacility,
+    "facility_scada": WemFacilityScada,
+}
+
+
+class ExtractCSV(object):
+    @check_spider_pipeline
+    def process_item(self, item, spider):
+
+        if not "content" in item:
+            return item
+
+        content = item["content"]
+        del item["content"]
+
+        item["tables"] = []
+        table = {"name": None}
+
+        datacsv = csv.reader(content.split("\n"))
+
+        for row in datacsv:
+            if not row or type(row) is not list or len(row) < 1:
+                continue
+
+            record_type = row[0]
+            record = row
+            table["records"].append(record)
+
+        return item
+
+
+class DatabaseStore(object):
+    def __init__(self):
+        engine = db_connect()
+        self.session = sessionmaker(bind=engine)
+
+    @check_spider_pipeline
+    def process_item(self, item, spider):
+
+        s = self.session()
+
+        if not "tables" in item:
+            logger.info("No tables in item to process")
+            return item
+
+        tables = item["tables"]
+
+        for table in tables:
+
+            if not "name" in table:
+                logger.error("No name in table")
+                continue
+
+            table_name = table["name"]
+
+            if not table_name in TABLE_MAP:
+                logger.error(
+                    "Could not map table name to model for item {}".format(
+                        table_name
+                    )
+                )
+                continue
+
+            Model = TABLE_MAP[table_name]
+
+            if not "records" in table:
+                logger.info(
+                    "No records for item with table name {}".format(table_name)
+                )
+                continue
+
+            records = table["records"]
+
+            for record in records:
+
+                i = Model(**record)
+
+                try:
+                    s.add(i)
+                    s.commit()
+                except IntegrityError:
+                    pass
+                except Exception as e:
+                    s.rollback()
+                    logger.error("Database error: {}".format(e))
+                    logger.debug(i.__dict__)
+                finally:
+                    s.close()
+
+        s.close()
+
+        return item
