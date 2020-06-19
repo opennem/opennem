6df40d38d86ca16ed79e1d4e39170dc56f189373
Nik Cubrilovic <nc9@protonmail.com>
2020-06-19 08:41:00 +0000

    Add wrapper for separate wem models

---
+++ opennem/db/models/wem.py
@@ -0,0 +1,14 @@
+from opennem.db.models.opennem import (
+    Base,
+    WemFacility,
+    WemFacilityScada,
+    WemParticipant,
+    metadata,
+)
+
+if __name__ == "__main__":
+    from opennem.db import db_connect
+
+    engine = db_connect()
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
