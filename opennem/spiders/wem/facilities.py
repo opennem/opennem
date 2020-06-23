f3a27b20078870aec061db59de3e150aebad9ce8
Nik Cubrilovic <nc9@protonmail.com>
2020-06-23 20:42:21 +1000

    Update wem current facilities

--- opennem/spiders/wem/facilities.py
+++ opennem/spiders/wem/facilities.py
@@ -1,21 +1,19 @@
+import csv
+
 from scrapy import Spider
 
-from opennem.pipelines.nem import DatabaseStore, ExtractCSV
+from opennem.pipelines.wem import WemStoreFacility
 
 
 class WemFacilities(Spider):
     name = "au.wem.facilities"
-    start_urls = (
+    start_urls = [
         "http://data.wa.aemo.com.au/datafiles/facilities/facilities.csv"
-    )
-    allowed_domains = ["wa.nemweb.com.au"]
-    pipelines = set([ExtractCSV, DatabaseStore,])
+    ]
+    # allowed_domains = ["wa.nemweb.com.au"]
+    pipelines = set([WemStoreFacility,])
 
     def parse(self, response):
-        if response:
-            yield {
-                "link": response.url,
-                "table_name": "facilities",
-                "content": response.text,
-                "type": "file",
-            }
+        csvreader = csv.DictReader(response.text.split("\n"))
+        for row in csvreader:
+            yield row
