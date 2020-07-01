77c28d94e014fa99b308d8e419ea5760d157b6ba
Nik Cubrilovic <nc9@protonmail.com>
2020-07-02 00:41:06 +1000

    FEAT: Update wem facility scada spider to new base crawler and new pipeline

--- opennem/spiders/wem/facility_scada.py
+++ opennem/spiders/wem/facility_scada.py
@@ -2,21 +2,15 @@ import csv
 
 import scrapy
 
-from opennem.pipelines.wem import WemStoreFacilityScada
-from opennem.spiders.wem import WemHistoricSpider
+from opennem.pipelines.wem.facility_scada import WemStoreFacilityScada
+from opennem.spiders.wem import WemCurrentSpider, WemHistoricSpider
 
 
-class WemCurrentFacilityScada(scrapy.Spider):
+class WemCurrentFacilityScada(WemCurrentSpider):
     name = "au.wem.current.facility_scada"
-    start_urls = [
-        "http://data.wa.aemo.com.au/public/public-data/datafiles/facility-scada/facility-scada-2020-06.csv",
-    ]
-    pipelines_extra = set([WemStoreFacilityScada])
+    start_url = "http://data.wa.aemo.com.au/public/public-data/datafiles/facility-scada/facility-scada-{year}-{month}.csv"
 
-    def parse(self, response):
-        csvreader = csv.DictReader(response.text.split("\n"))
-        for row in csvreader:
-            yield row
+    pipelines_extra = set([WemStoreFacilityScada])
 
 
 class WemHistoricFacilityScada(WemHistoricSpider):
