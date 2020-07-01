ecc594523e5019c8b32dd74490ca5db1f4fe8956
Nik Cubrilovic <nc9@protonmail.com>
2020-07-02 01:42:43 +1000

    FEAT: Make release script auto build version and upload

--- make-release.sh
+++ make-release.sh
@@ -7,7 +7,13 @@ docker push opennem/opennem
 
 docker tag opennem/opennem opennem/opennem:$VERSION
 
+poetry build
+
 twine upload dist/*
+
+git add pyproject.toml
+
+git ci -m "v$VERSION"
 
 git tag v$VERSION
 git push -u origin master v$VERSION
