221711a59fe3f5c47552a949a02d494b4dd00791
Nik Cubrilovic <nc9@protonmail.com>
2020-07-09 16:32:05 +0000

    FIX: Off by one error in fueltech lookup map sets

--- opennem/core/fueltechs.py
+++ opennem/core/fueltechs.py
@@ -18,7 +18,7 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
 def clean_fueltech(ft):
     ft = str(ft)
 
-    ft = ft.lower().strip().replace("-", "").replace("  ", " ")
+    ft = ft.lower().strip()
 
     if ft == "":
         return None
@@ -26,7 +26,7 @@ def clean_fueltech(ft):
     return ft
 
 
-def load_facility_stations(fixture_name):
+def load_fueltech_map(fixture_name):
     fixture_path = os.path.join(DATA_PATH, fixture_name)
 
     if not os.path.isfile(fixture_path):
@@ -37,15 +37,19 @@ def load_facility_stations(fixture_name):
     with open(fixture_path) as fh:
         csvreader = csv.reader(fh)
         for line in csvreader:
+            if line[2] == "tech":
+                continue
+
             key = frozenset(map(clean_fueltech, line[:4]))
-            val = line[5]
+
+            val = line[4]
 
             fueltech_map[key] = val
 
     return fueltech_map
 
 
-FUELTECH_MAP = load_facility_stations("aemo_fueltech_map.csv")
+FUELTECH_MAP = load_fueltech_map("aemo_fueltech_map.csv")
 
 
 def lookup_fueltech(
@@ -56,7 +60,7 @@ def lookup_fueltech(
     ftd = clean_fueltech(fueltype_desc)
     ttd = clean_fueltech(techtype_desc)
 
-    lookup_set = frozenset([ft, ftd, tt, ftd])
+    lookup_set = frozenset([ft, ftd, tt, ttd])
 
     # Lookup legacy fuel tech types and map them
     if ft in LEGACY_FUELTECH_MAP.keys():
@@ -67,7 +71,7 @@ def lookup_fueltech(
 
     logger.warn(
         "Found fueltech {}, {}, {}, {} with no mapping".format(
-            ft, tt, ftd, ttd
+            ft, ftd, tt, ttd
         )
     )
 
