92dd9f24c7809f3cecfb6bd461e7a96c9d9485ec
Nik Cubrilovic <nc9@protonmail.com>
2020-07-09 16:32:05 +0000

    FIX: Remove requests cache and default names and regions in diff output

--- facility_diff.py
+++ facility_diff.py
@@ -24,11 +24,6 @@ FACILITIES_CURRENT = (
 FACILITIES_V3 = "https://s3-ap-southeast-2.amazonaws.com/data.opennem.org.au/v3/geo/au_facilities.json"
 
 
-requests_cache.install_cache(
-    REQUESTS_CACHE_PATH, expire_after=timedelta(days=60)
-)
-
-
 def normalize_states(state):
     state = state.lower()
 
@@ -56,12 +51,12 @@ def main():
     for k, v in fac_current.items():
         for duid, fac in v["duid_data"].items():
             i = [
-                v["display_name"],
+                v["display_name"] or "",
                 k,
-                normalize_regions(v["region_id"]),
+                normalize_regions(v["region_id"]) or "",
                 normalize_states(v["status"]["state"]),
                 duid,
-                fac["fuel_tech"] if "fuel_tech" in fac else None,
+                fac["fuel_tech"] if "fuel_tech" in fac else "",
                 fac["registered_capacity"]
                 if "registered_capacity" in fac
                 else None,
@@ -83,13 +78,13 @@ def main():
     for f in fac_v3:
         for fac in f["properties"]["duid_data"]:
             i = [
-                f["properties"]["name"],
+                f["properties"]["name"] or "",
                 # fac["duid"],
-                f["properties"]["station_id"],
-                normalize_regions(f["properties"]["network_region"]),
+                f["properties"]["oid"],
+                normalize_regions(f["properties"]["network_region"]) or "",
                 fac["status"].lower(),
                 fac["duid"],
-                fac["fuel_tech"] if "fuel_tech" in fac else None,
+                fac["fuel_tech"] if "fuel_tech" in fac else "",
                 fac["registered_capacity"]
                 if "registered_capacity" in fac
                 else None,
