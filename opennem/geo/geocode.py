fe4f0bce6e8adfa5e0ef89f274d6f85597d16226
Nik Cubrilovic <nc9@protonmail.com>
2020-07-12 00:59:51 +1000

    FEAT: WEM geocoder.

--- opennem/geo/geocode.py
+++ opennem/geo/geocode.py
@@ -7,20 +7,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 
 from opennem.db import db_connect
-from opennem.db.models.opennem import NemFacility
+from opennem.db.models.opennem import NemFacility, WemFacility
 from opennem.geo.google_geo import place_search
 
 logger = logging.getLogger(__name__)
 
 
-def nem_geocode():
+def nem_geocode(limit=None):
     engine = db_connect()
     session = sessionmaker(bind=engine)
     s = session()
 
     records = s.query(NemFacility).filter(NemFacility.geom == None)
 
-    limit = 50
     count = 0
     skipped = 0
     records_added = 0
@@ -62,11 +61,69 @@ def nem_geocode():
             skipped += 1
 
         count += 1
-        if count >= limit:
+        if limit and count >= limit:
             break
 
     print(
-        "Done. Added {} records. Couldn't match {}".format(
+        "NEM Done. Added {} records. Couldn't match {}".format(
+            records_added, skipped
+        )
+    )
+
+
+def wem_geocode(limit=None):
+    engine = db_connect()
+    session = sessionmaker(bind=engine)
+    s = session()
+
+    records = s.query(WemFacility).filter(WemFacility.geom == None)
+
+    count = 0
+    skipped = 0
+    records_added = 0
+
+    for r in records:
+        geo_str = "{}, WA, Australia".format(r.name)
+
+        logger.info("Encoding: {}".format(geo_str))
+
+        google_result = place_search(geo_str)
+
+        pprint(google_result)
+
+        if (
+            google_result
+            and type(google_result) is list
+            and len(google_result) > 0
+        ):
+            result = google_result[0]
+
+            # r.place_id = result["place_id"]
+
+            lat = result["geometry"]["location"]["lat"]
+            lng = result["geometry"]["location"]["lng"]
+            r.geom = "SRID=4326;POINT({} {})".format(lat, lng)
+
+            try:
+                s.add(r)
+                s.commit()
+                records_added += 1
+            except IntegrityError as e:
+                logger.error(e)
+                skipped += 1
+                pass
+            except Exception as e:
+                skipped += 1
+                logger.error("Error: {}".format(e))
+        else:
+            skipped += 1
+
+        count += 1
+        if limit and count >= limit:
+            break
+
+    print(
+        "WEM Done. Added {} records. Couldn't match {}".format(
             records_added, skipped
         )
     )
@@ -74,3 +131,4 @@ def nem_geocode():
 
 if __name__ == "__main__":
     nem_geocode()
+    wem_geocode()
