cb1ee677370c6ba4a632c27db238e6e0d383e961
Nik Cubrilovic <nc9@protonmail.com>
2020-07-15 20:03:49 +1000

    FIX: Improve logging in initdb process

--- opennem/db/initdb.py
+++ opennem/db/initdb.py
@@ -3,9 +3,19 @@ from opennem.db.models.bom import metadata as metadata_bom
 from opennem.db.models.opennem import WemFacilityScada, metadata
 
 if __name__ == "__main__":
-    engine = db_connect()
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
+    print("Initdb.")
 
+    engine = db_connect()
+    print("Connected.")
+
+    print("Opennem init.")
+    metadata.drop_all(engine)
+    print("Creating....")
+    metadata.create_all(engine)
+    print("Done.")
+
+    print("BOM init.")
     metadata_bom.drop_all(engine)
+    print("Creating....")
     metadata_bom.create_all(engine)
+    print("Done.")
