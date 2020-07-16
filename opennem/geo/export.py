0bca985e840603e24e79e3f67599c708ec2f2130
Nik Cubrilovic <nc9@protonmail.com>
2020-07-16 22:52:31 +1000

    FIX: Updated geojson export fields and format

--- opennem/geo/export.py
+++ opennem/geo/export.py
@@ -69,8 +69,6 @@ def wem_export():
                 current_station_code = row[9]
 
                 if row[11]:
-                    # geom = json.loads(row[11])
-                    # print(row[11])
                     f.geometry = Point((row[15], row[14]))
 
                 f.properties = {
@@ -85,7 +83,7 @@ def wem_export():
             f.properties["duid_data"].append(
                 {
                     "duid": row[0],
-                    "comissioned": row[2].isoformat()
+                    "comissioned_date": row[2].isoformat()
                     if type(row[2]) is datetime
                     else str(row[2]),
                     "fuel_tech": row[5],
@@ -121,12 +119,14 @@ def nem_export():
             ws.postcode,
             ST_X(ws.geom),
             ST_Y(ws.geom),
-            ST_AsText(ws.geom)
+            ST_AsText(ws.geom),
+            ws.name_clean,
+            ws.id
         from nem_station ws
-        right join nem_facility wf on wf.station_id = ws.id
+        join nem_facility wf on wf.station_id = ws.id
         join fueltech f on f.code = wf.fueltech_id
         join facility_status fs on fs.code = wf.status_id
-        order by station_code asc
+        order by ws.id asc
     """
     query = __sql.format()
     features = []
@@ -139,13 +139,13 @@ def nem_export():
         rows = c.execute(query)
 
         for row in rows:
-            if current_station_code != row[9]:
+            if current_station_code != row[18]:
 
                 if f:
                     features.append(f)
 
                 f = Feature()
-                current_station_code = row[9]
+                current_station_code = row[18]
 
                 if row[11]:
                     f.geometry = Point((row[15], row[14]))
@@ -155,7 +155,7 @@ def nem_export():
                     "station_code": row[9],
                     "state": row[12],
                     "postcode": row[13],
-                    "name": row[4],
+                    "name": row[17] or row[4],
                     "duid_data": [],
                 }
 
@@ -163,13 +163,13 @@ def nem_export():
                 {
                     "duid": row[0],
                     "fuel_tech": row[5],
-                    "comissioned": row[2].isoformat()
+                    "commissioned_date": row[2].isoformat()
                     if type(row[2]) is datetime
                     else str(row[2]),
                     "fuel_tech_label": row[6],
                     "status": row[7],
                     "status_label": row[8],
-                    "registered_capacity": float(row[1]),
+                    "registered_capacity": float(row[1]) if row[1] else None,
                 }
             )
 
@@ -305,15 +305,15 @@ def nem_additionals():
 if __name__ == "__main__":
     wf = wem_export()
     nf = nem_export()
-    wf_a = wem_additionals()
-    nf_a = nem_additionals()
+    # wf_a = wem_additionals()
+    # nf_a = nem_additionals()
 
     crs = {
         "type": "name",
         "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"},
     }
 
-    geoj = FeatureCollection(nf + wf + wf_a + nf_a, crs=crs, name="opennem")
+    geoj = FeatureCollection(nf + wf, crs=crs, name="opennem")
 
     geoj["name"] = "nem_facilities"
     geoj["crs"] = crs
