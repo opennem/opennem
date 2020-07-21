d181eba943b1c285b2a79966a51f3874296fc907
Nik Cubrilovic <nc9@protonmail.com>
2020-07-22 08:22:55 +1000

    FEAT: Fueltechs map all from CSV for AEMO

--- opennem/core/fueltechs.py
+++ opennem/core/fueltechs.py
@@ -1,4 +1,6 @@
+import csv
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -10,55 +12,7 @@ LEGACY_FUELTECH_MAP = {
 }
 
 
-FUELTECH_MAP = {
-    "wind": "wind",
-    "solar": "solar_utility",
-    "gas": "gas_ccgt",
-    "landfill gas": "bioenergy_biogas",
-    "biomass": "bioenergy_biomass",
-    "distillate": "distillate",
-    "black coal": "coal_black",
-    "brown coal": "coal_brown",
-    "water": "hydro",
-    "diesel": "distillate",
-    "wind": "wind",
-    "solar": "solar_utility",
-    "gas": "gas_ccgt",
-    "coal seam methane": "gas_wcmg",
-    "landfill gas": "bioenergy_biogas",
-    "biogas": "bioenergy_biogas",
-    "biomass": "bioenergy_biomass",
-    "coal": "coal_black",
-    "distillate": "distillate",
-    "waste coal mine gas": "gas_wcmg",
-    "battery storage": "battery_discharging",
-    "landfill methane / landfill": "bioenergy_biomass",
-    "municipal solid waste": "bioenergy_biomass",
-    "landfill methane / landfill gas": "bioenergy_biogas",
-    "bagasse": "bioenergy_biomass",
-    "coal seam methane": "bioenergy_biogas",
-    "green and air dried wood": "bioenergy_biogas",
-    "renewable/ biomass / waste": "bioenergy_biomass",
-    "storage battery": "battery_discharging",
-    "fuel oil": {"turbine - ocgt": "distillate"},
-    "kerosene": "distillate",
-    "storage": {"battery": "battery_discharging"},
-    "natural gas": {
-        "open cycle gas turbines (ocgt)": "gas_ocgt",
-        "compression reciprocating engine": "gas_wcmg",
-        "combined cycle gas turbine (ccgt)": "gas_ccgt",
-    },
-    "natural gas / diesel": {
-        "open cycle gas turbines (ocgt)": "gas_ocgt",
-        "compression reciprocating engine": "gas_wcmg",
-        "combined cycle gas turbine (ccgt)": "gas_ccgt",
-    },
-    "other": {
-        "solar pv fixed": "solar_utility",
-        "storage battery": "battery_discharging",
-        "wind turbine onshore": "wind",
-    },
-}
+DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
 
 
 def clean_fueltech(ft):
@@ -72,6 +26,28 @@ def clean_fueltech(ft):
     return ft
 
 
+def load_facility_stations(fixture_name):
+    fixture_path = os.path.join(DATA_PATH, fixture_name)
+
+    if not os.path.isfile(fixture_path):
+        raise Exception("Not a file: {}".format(fixture_path))
+
+    fueltech_map = {}
+
+    with open(fixture_path) as fh:
+        csvreader = csv.reader(fh)
+        for line in csvreader:
+            key = frozenset(map(line[:4], clean_fueltech))
+            val = line[5]
+
+            fueltech_map[key] = val
+
+    return fueltech_map
+
+
+FUELTECH_MAP = load_facility_stations("aemo_fueltech_map.csv")
+
+
 def lookup_fueltech(
     fueltype, techtype=None, fueltype_desc=None, techtype_desc=None
 ):
@@ -80,60 +56,19 @@ def lookup_fueltech(
     ftd = clean_fueltech(fueltype_desc)
     ttd = clean_fueltech(techtype_desc)
 
+    lookup_set = frozenset([ft, ftd, tt, ftd])
+
     # Lookup legacy fuel tech types and map them
     if ft in LEGACY_FUELTECH_MAP.keys():
         return LEGACY_FUELTECH_MAP[ft]
 
-    if ftd and ftd in FUELTECH_MAP.keys():
-        return FUELTECH_MAP[ftd]
+    if lookup_set in FUELTECH_MAP:
+        return FUELTECH_MAP[lookup_set]
 
-    if tt and tt in FUELTECH_MAP.keys():
-        if type(FUELTECH_MAP[tt]) is str:
-            return FUELTECH_MAP[tt]
-
-        if type(FUELTECH_MAP[tt]) is dict and ftd in FUELTECH_MAP[tt]:
-            return FUELTECH_MAP[tt][ftd]
-
-    if ttd and ttd in FUELTECH_MAP.keys():
-        if type(FUELTECH_MAP[ttd]) is str:
-            return FUELTECH_MAP[ttd]
-
-        if type(FUELTECH_MAP[ttd]) is dict and ftd in FUELTECH_MAP[ttd]:
-            return FUELTECH_MAP[ttd][ftd]
-
-    # Lookup others
-    if not ft in FUELTECH_MAP.keys():
-        logger.warn(
-            "Found fueltech {}, {}, {}, {} with no mapping".format(
-                ft, tt, ftd, ttd
-            )
+    logger.warn(
+        "Found fueltech {}, {}, {}, {} with no mapping".format(
+            ft, tt, ftd, ttd
         )
-        return None
+    )
 
-    lookup = FUELTECH_MAP[ft]
-
-    if type(lookup) is dict:
-        if not tt:
-            raise Exception(
-                "Require tech type for lookup key {}".format(fueltype)
-            )
-
-        if tt in lookup.keys():
-            return lookup[tt]
-
-        logger.warn(
-            "Found fueltech {}, {}, {}, {} with no mapping".format(
-                ft, tt, ftd, ttd
-            )
-        )
-
-        return None
-
-    return lookup
-
-
-def nemri_fueltech_lookup():
-    """
-        NEM RI fueltech lookup method
-    """
-    pass
+    return None
