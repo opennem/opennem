1b3768662ec3ee162a5027290e829692f3445610
Nik Cubrilovic <nc9@protonmail.com>
2020-07-22 10:35:19 +1000

    FEAT: Patch records with opennem

---
+++ opennem/core/patches.py
@@ -0,0 +1,35 @@
+# GULLRWF2 -> Biala
+import json
+from datetime import date, datetime
+from pprint import pprint
+
+from sqlalchemy.orm import sessionmaker
+
+from opennem.db import db_connect
+from opennem.db.models.opennem import metadata
+
+engine = db_connect()
+session = sessionmaker(bind=engine)
+
+
+def patches():
+
+    sqls = [
+        "update wem_facility set capacity_credits = 2.0 where code = 'GOSNELLS'",
+        "update wem_facility set capacity_credits = 1.1 where code = 'ATLAS'",
+        "update nem_facility set station_id = null where code = 'GULLRWF2'",
+        "update nem_facility set code = 'GULLRWF2' where name_clean = 'Biala'",
+        "update nem_facility set station_id = (select id from nem_station where name = 'Wivenhoe Small') where code ='WIVENSH'",
+        "update nem_station set name_clean = 'Wivenhoe Mini' where name = 'Wivenhoe Small'",
+        "update nem_facility set fueltech_id = 'pumps' where code in ('PUMP2', 'PUMP1')",
+    ]
+
+    with engine.connect() as c:
+
+        for query in sqls:
+            rows = c.execute(query)
+            pprint(rows)
+
+
+if __name__ == "__main__":
+    patches()
