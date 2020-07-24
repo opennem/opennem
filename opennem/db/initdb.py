215ced84183dbd418590848cf43ae6abc9af8e23
Nik Cubrilovic <nc9@protonmail.com>
2020-07-24 11:19:24 +1000

    META: Logging in initdb

--- opennem/db/initdb.py
+++ opennem/db/initdb.py
@@ -3,19 +3,14 @@ from opennem.db.models.bom import metadata as metadata_bom
 from opennem.db.models.opennem import WemFacilityScada, metadata
 
 if __name__ == "__main__":
-    print("Initdb.")
-
     engine = db_connect()
-    print("Connected.")
 
-    print("Opennem init.")
     metadata.drop_all(engine)
-    print("Creating....")
     metadata.create_all(engine)
-    print("Done.")
+    print("Created opennem schema.")
 
-    print("BOM init.")
     metadata_bom.drop_all(engine)
-    print("Creating....")
+    print("Creating bom schema ....")
     metadata_bom.create_all(engine)
-    print("Done.")
+
+    print("Done initdb.")
