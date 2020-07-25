0a42d2b12a201a735019877ea85fd7b6f1eb15d4
Nik Cubrilovic <nc9@protonmail.com>
2020-07-25 14:24:04 +0000

    FEAT: Init opennem schema

--- opennem/db/initdb.py
+++ opennem/db/initdb.py
@@ -1,16 +1,16 @@
 from opennem.db import db_connect
 from opennem.db.models.bom import metadata as metadata_bom
-from opennem.db.models.opennem import WemFacilityScada, metadata
+from opennem.db.models.opennem import metadata as metadata_opennem
 
 if __name__ == "__main__":
     engine = db_connect()
 
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
+    metadata_opennem.drop_all(engine)
+    metadata_opennem.create_all(engine)
     print("Created opennem schema.")
 
     metadata_bom.drop_all(engine)
-    print("Creating bom schema ....")
     metadata_bom.create_all(engine)
+    print("Created bom schema ....")
 
     print("Done initdb.")
