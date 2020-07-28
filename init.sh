76a60c4aeb841d1f5800d79adb9843c585d56aef
Nik Cubrilovic <nc9@protonmail.com>
2020-07-28 12:31:16 +1000

    DEV: Update init

--- init.sh
+++ init.sh
@@ -1,33 +1,55 @@
-# init db
+# # init db
 python -m opennem.db.initdb
 
-# load required fixtures
+# # load required fixtures
 python -m opennem.db.load_fixtures
+python -m opennem.db.load_facilitymap
 
-# load facilities from wem and nem
-scrapy crawl au.wem.facilities
 
-# merge with current facility mapping data (will import nem)
-python db_test.py
+# # load participants
+scrapy crawl -L ERROR au.wem.participant
+scrapy crawl -L ERROR au.wem.live.participant
 
-# bom data
-scrapy crawl bom.capitals.perth
-scrapy crawl bom.capitals.sydney
-scrapy crawl bom.capitals.melbourne
-scrapy crawl bom.capitals.brisbane
+# # load facilities from wem and nem
+scrapy crawl -L INFO au.nem.facilities.rel
+scrapy crawl -L INFO au.nem.facilities.gi
 
-# wem current data
-scrapy crawl au.wem.current.balancing_summary
-scrapy crawl au.wem.current.facility_scada
+scrapy crawl -L ERROR au.wem.facilities
+scrapy crawl -L ERROR au.wem.live.facilities
 
-# wem archive data
-scrapy crawl au.wem.archive.balancing_summary
-scrapy crawl au.wem.archive.facility_scada
 
-# nem current data
-scrapy crawl au.nem.current.dispatch_is
-scrapy crawl au.nem.current.dispatch_scada
+# # merge with current facility mapping data (will import nem)
+# # python db_test.py
 
-# nem archive data
-scrapy crawl au.nem.archive.dispatch_is
-scrapy crawl au.nem.archive.dispatch_scada
+# # bom data
+# # scrapy crawl -L ERROR bom.capitals.perth
+# # scrapy crawl -L ERROR bom.capitals.sydney
+# # scrapy crawl -L ERROR bom.capitals.melbourne
+# # scrapy crawl -L ERROR bom.capitals.brisbane
+
+# # wem current data
+# # scrapy crawl -L ERROR au.wem.current.balancing_summary
+# # scrapy crawl -L ERROR au.wem.current.facility_scada
+
+# # wem archive data
+# # scrapy crawl -L ERROR au.wem.archive.balancing_summary
+# # scrapy crawl -L ERROR au.wem.archive.facility_scada
+
+# # nem current data
+# # scrapy crawl au.nem.current.dispatch_is
+# # scrapy crawl au.nem.current.dispatch_scada
+
+# # nem archive data
+# # scrapy crawl au.nem.archive.dispatch_is
+# # scrapy crawl au.nem.archive.dispatch_scada
+
+# # python -m opennem.api.exporter
+# #
+
+# python -m opennem.geo.geocode
+
+python -m opennem.core.patches
+
+python -m opennem.geo.export
+
+python facility_diff.py
