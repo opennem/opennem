6233e3d24454dce9f463e8d9a6c7cd79dfcb0003
Nik Cubrilovic <nc9@protonmail.com>
2020-07-28 11:11:19 +1000

    FEAT: Group RI by clean name and sort

--- opennem/pipelines/nem/registration_exemption.py
+++ opennem/pipelines/nem/registration_exemption.py
@@ -1,19 +1,29 @@
 from itertools import groupby
 
+from opennem.core.normalizers import station_name_cleaner
 from opennem.utils.pipelines import check_spider_pipeline
 
 
 class RegistrationExemptionGrouperPipeline(object):
     @check_spider_pipeline
-    def parse_item(self, item, spider=None):
+    def process_item(self, item, spider=None):
         if not "generators" in item:
             raise Exception("No generators found in item pipeline failed")
 
         generators = item["generators"]
 
+        # Add clean station names
+        generators = [
+            {**i, "name": station_name_cleaner(i["station_name"])}
+            for i in generators
+        ]
+
+        # sort by name
+        generators = sorted(generators, key=lambda k: k["name"])
+
         generators_grouped = {}
 
-        for k, v in groupby(generators, key=lambda v: v["station_name"]):
+        for k, v in groupby(generators, key=lambda v: v["name"]):
             if not k in generators_grouped:
                 generators_grouped[k] = []
 
