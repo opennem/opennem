66cf041c95ef75871824d03f2a3c16fa7cea0608
Nik Cubrilovic <nc9@protonmail.com>
2020-07-29 12:15:56 +1000

    FEAT: Unit tests for capacity cleaner

---
+++ tests/test_capacity_cleaner.py
@@ -0,0 +1,38 @@
+from opennem.core.normalizers import clean_capacity
+
+
+class TestCapacityCleaner(object):
+    def test_capacity_blank(self):
+        subject = clean_capacity("")
+        assert type(subject) is type(None), "Blank string should be nonetype"
+        assert subject == None, "Blank capacity should be none"
+
+    def test_capacity_string_excel_blank(self):
+        subject = clean_capacity("-")
+        assert type(subject) is type(None), "Blank string should be nonetype"
+        assert subject == None, "Blank capacity should be none"
+
+    def test_capacity_zero_string(self):
+        subject = clean_capacity("0")
+        assert type(subject) is float, "Capacity should be a float"
+        assert subject == 0.0, "0 string capacity should be float 0"
+
+    def test_capacity_zero(self):
+        subject = clean_capacity(0)
+        assert type(subject) is float, "Capacity should be a float"
+        assert subject == 0.0, "0 string capacity should be float 0"
+
+    def test_capacity_zero__string_padded(self):
+        subject = clean_capacity("0  ")
+        assert type(subject) is float, "Capacity should be a float"
+        assert subject == 0.0, "0 string capacity should be float 0"
+
+    def test_capacity_postive_value(self):
+        subject = clean_capacity("1.1")
+        assert type(subject) is float, "Capacity should be a float"
+        assert subject == 1.1, "0 string capacity should be float 0"
+
+    def test_capacity_rounding(self):
+        subject = clean_capacity("1.00000001")
+        assert type(subject) is float, "Capacity should be a float"
+        assert subject == 1.0, "0 string capacity should be rounded to 1"
