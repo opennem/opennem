439f6f82f8fb83b8744ed10f5a1fbc6801c7ba74
Nik Cubrilovic <nc9@protonmail.com>
2020-07-29 21:05:32 +1000

    FEAT: Update unit code for id to go after alias and tests

--- opennem/core/unit_codes.py
+++ opennem/core/unit_codes.py
@@ -11,13 +11,13 @@ def get_unit_code(duid: str, unit: UnitSchema) -> str:
     """
 
     duid_clean = normalize_duid(duid)
-
+    unit_id = None
     # @TODO - check if we can strip, probably not a good idea
 
-    # if not duid_clean.endswith(unit.id):
-    # duid_clean += str(unit.id)
+    if not duid_clean.endswith(str(unit.id)):
+        unit_id = str(unit.id)
 
-    components = [duid_clean, unit.alias, unit.id]
+    components = [duid_clean, unit.alias, unit_id]
 
     # empty out None's
     components = [i for i in components if i]
