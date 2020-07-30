bc934614a0abf43178a6ca5d8cbefc62fa5a03b4
Nik Cubrilovic <nc9@protonmail.com>
2020-07-30 12:05:27 +1000

    FEAT: Local file exporter module

---
+++ opennem/exporter/local.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+LOCAL_DIR_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "data")
+
+if not os.path.exists(LOCAL_DIR_PATH):
+    raise Exception("Folder {} does not exist".format(LOCAL_DIR_PATH))
+
+
+def write_to_local(file_path, data):
+    save_file_path = os.path.join(LOCAL_DIR_PATH, file_path)
+
+    with open(save_file_path, "w") as fh:
+        fh.write(data)
+
+    logger.info("Wrote {} to {}".format(len(data), save_file_path))
