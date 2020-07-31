49bb53ee11fad2c37cf1305b2b1dc87b6574e172
Nik Cubrilovic <nc9@protonmail.com>
2020-07-31 13:04:59 +1000

    FEAT: WEM facility code parser and unit tests

---
+++ opennem/core/facility_code.py
@@ -0,0 +1,32 @@
+"""
+    Parse facility codes facility ids
+
+"""
+
+WEM_CODE_SUFFIXES = ["plant"]
+
+
+def parse_wem_facility_code(facility_code: str) -> str:
+    """
+        Parses WEM facility codes into station_code and facility_code
+
+        If the suffix is in the map include it
+
+        ex. PINJAR_GT7 becomes PINJAR and PINJAR_GT7
+    """
+
+    if not type(facility_code) is str:
+        raise Exception("Invalid facilty code: {}".format(facility_code))
+
+    if not facility_code:
+        # Exception?
+        return None
+
+    comp = facility_comp = facility_code.split("_")
+
+    num_comp = len(comp)
+
+    if comp[num_comp - 1].lower() not in WEM_CODE_SUFFIXES:
+        facility_comp = comp[: num_comp - 1]
+
+    return "_".join(facility_comp)
---
+++ tests/test_wem_facility_codes.py
@@ -0,0 +1,25 @@
+from opennem.core.facility_code import parse_wem_facility_code
+
+
+class TestWemFacilityCode(object):
+    def test_pinjar(self):
+        code = "PINJAR_GT7"
+        facility_code = parse_wem_facility_code(code)
+
+        assert facility_code == "PINJAR", "Facility code is Pinjar"
+
+    def test_west_kalgoorlie(self):
+        code = "WEST_KALGOORLIE_GT2"
+        facility_code = parse_wem_facility_code(code)
+
+        assert (
+            facility_code == "WEST_KALGOORLIE"
+        ), "Facility code is WEST_KALGOORLIE"
+
+    def test_bridgerown(self):
+        code = "BRIDGETOWN_BIOMASS_PLANT"
+        facility_code = parse_wem_facility_code(code)
+
+        assert (
+            facility_code == "BRIDGETOWN_BIOMASS_PLANT"
+        ), "Bridgetown remains the same"
