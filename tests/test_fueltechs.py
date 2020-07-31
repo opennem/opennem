8d030ed8bb5d10547ef571fc4640c063e19c9f92
Nik Cubrilovic <nc9@protonmail.com>
2020-07-31 11:39:40 +1000

    FEAT: Fueltech unit tests

---
+++ tests/test_fueltechs.py
@@ -0,0 +1,34 @@
+from opennem.core.fueltechs import lookup_fueltech
+
+
+class TestFueltechMap(object):
+    def test_bairnsdale(self):
+        subj = lookup_fueltech(
+            "Fossil",
+            "Natural Gas",
+            "Combustion",
+            "Open Cycle Gas turbines (OCGT)",
+        )
+
+        assert subj == "gas_ocgt", "Bairnsdale is gas_ocgt"
+
+    def test_rel_hydro(self):
+        subj = lookup_fueltech(
+            "Hydro", "Water", "Renewable", "Hydro - Gravity"
+        )
+
+        assert subj == "hydro", "REL hydro fueltech test"
+
+    def test_battery_storage(self):
+        subj = lookup_fueltech("Battery storage", "Grid", "Storage", "Battery")
+
+        assert (
+            subj == "battery_discharging"
+        ), "Fueltech battery storage generator"
+
+    def test_battery_load(self):
+        subj = lookup_fueltech(None, None, "Storage", "Battery", "load")
+
+        assert (
+            subj == "battery_charging"
+        ), "Load storage battery is battery discharging"
