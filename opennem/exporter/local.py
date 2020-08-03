83bbbd77baa1d912943bf83c3a5a4c01ed992e82
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 10:35:11 +1000

    FEAT: Support file handle streams in export outputs

--- opennem/exporter/local.py
+++ opennem/exporter/local.py
@@ -1,3 +1,4 @@
+import io
 import logging
 import os
 
@@ -11,6 +12,9 @@ if not os.path.exists(LOCAL_DIR_PATH):
 
 def write_to_local(file_path, data):
     save_file_path = os.path.join(LOCAL_DIR_PATH, file_path)
+
+    if type(data) is io.StringIO:
+        data = data.getvalue()
 
     with open(save_file_path, "w") as fh:
         fh.write(data)
