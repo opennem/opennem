64b79d05695c4e7bc0517d998ddebc7d7af04d99
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 11:09:42 +1000

    FEAT: opennem json export and API format

---
+++ opennem/exporter/onjson.py
@@ -0,0 +1,42 @@
+import json
+
+from opennem.api.stations import get_stations
+from opennem.exporter.encoders import OpenNEMJSONEncoder
+
+
+def stations_json_records():
+    stations = get_stations()
+
+    records = []
+
+    for station in stations:
+        for facility in station.facilities:
+            rec = {
+                "name": station.name,
+                # "oid": station.oid,
+                "ocode": station.ocode,
+                "code": station.code,
+                "network": facility.network_code,
+                "region": facility.network_region,
+                "status": facility.status_id,
+                "fueltech": facility.fueltech_id,
+                "unit_id": facility.unit_id,
+                "unit_num": facility.unit_number,
+                "unit_cap": facility.capacity_aggregate,
+                "station_cap_agg": station.capacity_aggregate,
+                "station_cap_registered": station.capacity_registered,
+                "added_by": facility.created_by,
+                "updated_by": facility.created_by,
+            }
+            records.append(rec)
+
+    return records
+
+
+def stations_json_serialize():
+
+    json_records = stations_json_records()
+
+    json_serialized = json.dumps(json_records, cls=OpenNEMJSONEncoder)
+
+    return json_serialized
