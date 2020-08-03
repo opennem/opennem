c326e9ddc5bc536510fd6c02961b086a7db94f7d
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 10:31:52 +1000

    FEAT: Add stations csv to local runner

--- opennem/exporter/runner.py
+++ opennem/exporter/runner.py
@@ -1,4 +1,5 @@
 from opennem.exporter.aws import write_to_s3
+from opennem.exporter.csv import stations_csv_serialize
 from opennem.exporter.geojson import stations_geojson_serialize
 from opennem.exporter.local import write_to_local
 
@@ -15,6 +16,13 @@ def stations_geojson_to_local():
     write_to_local("stations.json", stations_geojson)
 
 
+def stations_csv_to_local():
+    stations_csv = stations_csv_serialize()
+
+    write_to_local("stations.csv", stations_csv)
+
+
 if __name__ == "__main__":
     stations_geojson_to_s3()
     stations_geojson_to_local()
+    stations_csv_to_local()
