fc73b1a9e7fb14a037a4acb5a39fe682e761e7c1
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 11:09:54 +1000

    FEAT: Update runner with opennem json export format

--- opennem/exporter/runner.py
+++ opennem/exporter/runner.py
@@ -2,6 +2,7 @@ from opennem.exporter.aws import write_to_s3
 from opennem.exporter.csv import stations_csv_serialize
 from opennem.exporter.geojson import stations_geojson_serialize
 from opennem.exporter.local import write_to_local
+from opennem.exporter.onjson import stations_json_serialize
 
 
 def stations_geojson_to_s3():
@@ -13,7 +14,7 @@ def stations_geojson_to_s3():
 def stations_geojson_to_local():
     stations_geojson = stations_geojson_serialize()
 
-    write_to_local("stations.goejson", stations_geojson)
+    write_to_local("stations.geojson", stations_geojson)
 
 
 def stations_csv_to_local():
@@ -22,7 +23,14 @@ def stations_csv_to_local():
     write_to_local("stations.csv", stations_csv)
 
 
+def stations_json_to_local():
+    stations_json = stations_json_serialize()
+
+    write_to_local("stations.json", stations_json)
+
+
 if __name__ == "__main__":
     stations_geojson_to_s3()
     stations_geojson_to_local()
     stations_csv_to_local()
+    stations_json_to_local()
