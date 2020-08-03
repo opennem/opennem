f21f75f27ed2e7d53aac252452eeda29587b5310
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 11:05:29 +1000

    FEAT: Opennem JSON encoder

--- opennem/exporter/encoders.py
+++ opennem/exporter/encoders.py
@@ -5,7 +5,16 @@ from datetime import datetime
 from geojson import GeoJSONEncoder
 
 
-class OpenNEMGeoJSONEncoder(GeoJSONEncoder):
+class OpenNEMJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return super(OpenNEMGeoJSONEncoder, self).default(o)
+
+
+class OpenNEMGeoJSONEncoder(GeoJSONEncoder, OpenNEMJSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             return float(o)
