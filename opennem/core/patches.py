03e20e2246464fe7bb83da89afc1198fb102c61e
Nik Cubrilovic <nc9@protonmail.com>
2020-08-03 11:21:53 +1000

    FIX: Update WEM patches

--- opennem/core/patches.py
+++ opennem/core/patches.py
@@ -15,15 +15,16 @@ session = sessionmaker(bind=engine)
 def patches():
 
     sqls = [
-        "update wem_facility set capacity_maximum = 2.0 where code = 'GOSNELLS'",
-        "update wem_facility set capacity_maximum = 1.1 where code = 'ATLAS'",
-        "update nem_facility set station_id = null where code = 'GULLRWF2'",
-        "update nem_facility set code = 'GULLRWF2' where name_clean = 'Biala'",
-        "update nem_facility set station_id = (select id from nem_station where name = 'Wivenhoe Small Hydro') where code ='WIVENSH'",
-        "update nem_station set name_clean = 'Wivenhoe Mini' where name_clean = 'Wivenhoe Small'",
-        "update nem_facility set fueltech_id = 'pumps' where code in ('PUMP2', 'PUMP1')",
-        "update nem_facility set station_id = null where name='Crookwell' and code is null",
-        "update nem_facility set station_id = null where name='Pioneer Sugar Mill' and code is null",
+        "update facility set capacity_registered = 2.0, unit_capacity = 2.0 where code = 'GOSNELLS'",
+        "update facility set capacity_registered = 1.1, unit_capacity = 1.1  where code = 'ATLAS'",
+        # code GULLRWF2_74 -> Biala
+        # "update facility set station_id = null where code = 'GULLRWF2'",
+        # "update facility set code = 'GULLRWF2' where name_clean = 'Biala'",
+        "update facility set station_id = (select id from station where name = 'Wivenhoe Small Hydro') where code ='WIVENSH'",
+        "update station set name_clean = 'Wivenhoe Mini' where name_clean = 'Wivenhoe Small'",
+        "update facility set fueltech_id = 'pumps' where code in ('PUMP2', 'PUMP1')",
+        "update facility set station_id = null where name='Crookwell' and code is null",
+        "update facility set station_id = null where name='Pioneer Sugar Mill' and code is null",
     ]
 
     with engine.connect() as c:
