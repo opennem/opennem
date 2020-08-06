3cdfb9ade0049deb600598c8472ffc3b9553350b
Nik Cubrilovic <nc9@protonmail.com>
2020-08-06 15:55:12 +1000

    FEAT: duid map and retired duids

---
+++ opennem/core/facility_duid_map.py
@@ -0,0 +1,17 @@
+from opennem.core import load_data_json
+
+FACILITY_DUID_MAP = load_data_json("facility_duid_map.json")
+
+
+def facility_duid_map(duid: str) -> str:
+    """
+
+
+    """
+    if not type(FACILITY_DUID_MAP) is dict:
+        raise Exception("Facility duid map invalid data type")
+
+    if duid in FACILITY_DUID_MAP:
+        return FACILITY_DUID_MAP[duid]
+
+    return duid
---
+++ tests/test_facility_duid_map.py
@@ -0,0 +1,9 @@
+from opennem.core.facility_duid_map import facility_duid_map
+
+
+class TestFacilityDuidMap(object):
+    def test_gordon(self):
+        subject = "GORDON1"
+        value = facility_duid_map(subject)
+
+        assert value == "GORDON", "GORDON1 becomes GORDON as they merged"
