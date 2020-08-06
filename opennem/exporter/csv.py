84b140020be2c6cb4d4f9356acb20797855494da
Nik Cubrilovic <nc9@protonmail.com>
2020-08-06 16:37:25 +1000

    FEAT: Skip retired duids

--- opennem/exporter/csv.py
+++ opennem/exporter/csv.py
@@ -2,6 +2,7 @@ import csv
 from io import StringIO
 
 from opennem.api.stations import get_stations
+from opennem.core.facility_duid_map import duid_is_retired
 
 
 def stations_csv_records():
@@ -15,6 +16,9 @@ def stations_csv_records():
                 continue
 
             if facility.status_id is None:
+                continue
+
+            if duid_is_retired(facility.code):
                 continue
 
             rec = {
