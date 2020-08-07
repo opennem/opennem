32253f7fd70ffef92d08c868fdd90695a42ed74a
Nik Cubrilovic <nc9@protonmail.com>
2020-08-07 10:17:05 +1000

    FEAT: station_duid_map to map duids to stations and data source

---
+++ opennem/core/station_duid_map.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from opennem.core import load_data_json
+
+STATION_DUID_MAP = load_data_json("facility_duid_map.json")
+
+
+def facility_map_station(
+    duid: str, default_station: Optional[str] = None
+) -> Optional[str]:
+    """
+        Maps duid -> station code
+
+    """
+    if not type(STATION_DUID_MAP) is dict:
+        raise Exception("Station duid map invalid data type")
+
+    if duid in STATION_DUID_MAP:
+        return STATION_DUID_MAP[duid]
+
+    return default_station
