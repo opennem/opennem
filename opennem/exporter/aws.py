4b27dc7b469f2359f1bdae0964d49b978118db82
Nik Cubrilovic <nc9@protonmail.com>
2020-08-09 14:59:46 +1000

    FEAT: Types and returns in s3 write

--- opennem/exporter/aws.py
+++ opennem/exporter/aws.py
@@ -12,14 +12,20 @@ UPLOAD_ARGS = {
 }
 
 
-def write_to_s3(file_path, data):
+def write_to_s3(file_path: str, data: str) -> int:
+    """
+        Write data to an s3 path
+    """
     s3_save_path = os.path.join(S3_EXPORT_DEFAULT_BUCKET, file_path)
+    write_count = 0
 
     with open(
         s3_save_path,
         "w",
         transport_params=dict(multipart_upload_kwargs=UPLOAD_ARGS),
     ) as fh:
-        fh.write(data)
+        write_count = fh.write(data)
 
     logger.info("Wrote {} to {}".format(len(data), s3_save_path))
+
+    return write_count
