1db3809532ec9c3946979f17f55f7a5f3e4acefb
Nik Cubrilovic <nc9@protonmail.com>
2020-08-13 14:14:35 +1000

    FEAT: API async get_database_session

--- opennem/api/__init__.py
+++ opennem/api/__init__.py
@@ -1,32 +1,56 @@
-from typing import Optional
+from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from pydantic import BaseModel
+from sqlalchemy.orm import Session, sessionmaker
 
 from opennem.controllers.stations import (
     create_station,
     get_station,
     get_stations,
 )
-from opennem.schema import OpennemStationSubmission
+from opennem.db import db_connect
+from opennem.db.models.opennem import Facility, Station
+from opennem.schema.opennem import StationSchema, StationSubmission
 
 app = FastAPI()
 
+engine = db_connect()
+session = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
-@app.get("/stations")
+
+def get_database_session():
+    try:
+        s = session()
+        yield s
+    finally:
+        s.close()
+
+
+@app.get(
+    "/stations", response_model=List[StationSchema],
+)
 def stations(
-    name: Optional[str] = None, limit: Optional[int] = None, page: int = 1
-):
-    stations = get_stations(name=name, limit=limit, page=page)
+    db: Session = Depends(get_database_session),
+    name: Optional[str] = None,
+    limit: Optional[int] = None,
+    page: int = 1,
+) -> List[StationSchema]:
+    stations = get_stations(db, name=name, limit=limit, page=page)
 
-    return {"stations": [i for i in stations]}
+    return stations
 
 
 @app.get("/station/{station_id}")
-def station(station_id: int):
-    return get_station(station_id)
+def station(
+    session: Session = Depends(get_database_session), station_id: int = None
+):
+    return get_station(session, station_id)
 
 
 @app.post("/station")
-def station_create(station: OpennemStationSubmission):
+def station_create(
+    session: Session = Depends(get_database_session),
+    station: StationSubmission = None,
+):
     return True
