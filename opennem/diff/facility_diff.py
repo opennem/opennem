b97ddf465bcf434766e44794f131e53f48873066
Nik Cubrilovic <nc9@protonmail.com>
2020-08-19 15:59:03 +1000

    FEAT: Markdown report for diff

--- opennem/diff/facility_diff.py
+++ opennem/diff/facility_diff.py
@@ -38,10 +38,10 @@ def normalize_regions(region):
     return region
 
 
-markdown_report = ""
+markdown_report = []
 
 
-def run_diff():
+def get_maps():
     logger.info("Running facility diff")
 
     with open("opennem/db/fixtures/facility_registry.json") as fh:
@@ -81,7 +81,9 @@ def run_diff():
                 # fac["duid"],
                 f["properties"]["oid"],
                 f["properties"]["station_code"] or "",
-                normalize_regions(f["properties"]["network_region"]) or "",
+                normalize_regions(f["properties"]["network_region"])
+                if "network_region" in f["properties"]
+                else "",
                 fac["status"].lower(),
                 fac["duid"] or "",
                 fac["fuel_tech"] if "fuel_tech" in fac else "",
@@ -92,6 +94,45 @@ def run_diff():
             fac_v3_remapped.append(i)
 
     fac_v3_remapped = sorted(fac_v3_remapped, key=itemgetter(3, 0, 5, 7))
+
+    return fac_current_remapped, fac_v3_remapped
+
+
+def add(line, check=False):
+    if type(line) is list:
+        for l in line:
+            if check:
+                l = " * {}".format(l)
+            markdown_report.append(l)
+    else:
+        if check:
+            line = " * {}".format(line)
+        markdown_report.append(line)
+
+
+def run_diff():
+    current, v3 = get_maps()
+    current_stations = list(set([i[1] for i in current]))
+    v3_stations = list(set([i[2] for i in v3]))
+    current_stations_names = list(set([i[0] for i in current]))
+    v3_stations_names = list(set([i[0] for i in v3]))
+
+    add("# Opennem Facility Diff Report")
+    add("## Overview ")
+    add(" * Current Stations: {}".format(len(current_stations)))
+    add(" * v3 Stations: {}".format(len(v3_stations)))
+    add(" * Current Facilities: {}".format(len(current)))
+    add(" * v3 Facilities: {}".format(len(v3)))
+    add("")
+    add(" ## Station codes in current not in v3")
+    add("")
+    add(list(set(current_stations) - set(v3_stations)), True)
+    add(" ## Stations in current not in v3")
+    add("")
+    add(list(set(current_stations_names) - set(v3_stations_names)), True)
+
+    with open("data/diff_report.md", "w") as fh:
+        fh.write("\n".join(markdown_report))
 
 
 if __name__ == "__main__":
