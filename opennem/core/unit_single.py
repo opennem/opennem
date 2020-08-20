5c07058f02bf70feefaaf2d1fc7cc8bed1ccd760
Nik Cubrilovic <nc9@protonmail.com>
2020-08-20 16:22:11 +1000

    FIX: Use core data loader in single unit checker

--- opennem/core/unit_single.py
+++ opennem/core/unit_single.py
@@ -1,27 +1,12 @@
 import json
 import os
 
-DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
+from opennem.core import load_data_json
+
+SINGLE_UNITS = load_data_json("single_units.json")
 
 
-def load_single_units(fixture_name):
-    fixture_path = os.path.join(DATA_PATH, fixture_name)
-
-    if not os.path.isfile(fixture_path):
-        raise Exception("Not a file: {}".format(fixture_path))
-
-    fixture = None
-
-    with open(fixture_path) as fh:
-        fixture = json.load(fh)
-
-    return fixture
-
-
-SINGLE_UNITS = load_single_units("single_units.json")
-
-
-def facility_unit_numbers_are_single(facility_duid):
+def facility_unit_numbers_are_single(facility_duid: str) -> bool:
     """
         These units have unit numbers that are single units rather than ranges.
 
