11a6fa173f44dc2a3374d76ab359b86941ccca8d
Nik Cubrilovic <nc9@protonmail.com>
2020-08-25 09:39:28 +1000

    FEAT: Bind opennem mms import to cli

--- opennem/cli.py
+++ opennem/cli.py
@@ -5,6 +5,7 @@ import click
 from scrapy.utils.python import garbage_collect
 
 from opennem.diff.facility_diff import run_diff
+from opennem.importer.mms import run_import_mms
 from opennem.importer.opennem import run_opennem_import
 
 logger = logging.getLogger("opennem.cli")
@@ -26,14 +27,27 @@ def diff():
     run_diff()
 
 
-@click.command()
+@click.group()
 def cmd_import():
+    pass
+
+
+@click.command()
+def cmd_import_opennem():
     run_opennem_import()
+
+
+@click.command()
+def cmd_import_mms():
+    run_import_mms()
 
 
 main.add_command(crawl)
 main.add_command(diff)
 main.add_command(cmd_import, name="import")
+
+cmd_import.add_command(cmd_import_opennem, name="opennem")
+cmd_import.add_command(cmd_import_mms, name="mms")
 
 if __name__ == "__main__":
     try:
