89a359abdb03ed7c3304218e901b474f979f2cca
Nik Cubrilovic <nc9@protonmail.com>
2020-08-25 09:39:46 +1000

    FIX: Typing imports fix

--- opennem/diff/facility_diff.py
+++ opennem/diff/facility_diff.py
@@ -14,6 +14,7 @@ from datetime import timedelta
 from itertools import chain
 from operator import itemgetter
 from pprint import pprint
+from typing import List
 
 from mdutils.mdutils import MdUtils
 
