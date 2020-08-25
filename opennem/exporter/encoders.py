04fe628542a1cf145e3bf8f9e953ef802aba32a1
Nik Cubrilovic <nc9@protonmail.com>
2020-08-25 14:27:05 +1000

    FEAT: Stringify dispatch_type_string in json encoder

--- opennem/exporter/encoders.py
+++ opennem/exporter/encoders.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 
 from geojson import GeoJSONEncoder
+from opennem.core.dispatch_type import DispatchType, dispatch_type_string
 
 
 class OpenNEMJSONEncoder(json.JSONEncoder):
@@ -20,4 +21,6 @@ class OpenNEMGeoJSONEncoder(GeoJSONEncoder, OpenNEMJSONEncoder):
             return float(o)
         if isinstance(o, datetime):
             return o.isoformat()
+        if isinstance(o, DispatchType):
+            return dispatch_type_string(o)
         return super(OpenNEMGeoJSONEncoder, self).default(o)
