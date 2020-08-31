705a334ae742888d45ca3c8bb2a7f2054c464e25
Nik Cubrilovic <nc9@protonmail.com>
2020-08-31 11:09:18 +1000

    FEAT: Cli hookup registry import

--- opennem/importer/all.py
+++ opennem/importer/all.py
@@ -1,9 +1,11 @@
-from opennem.importer.aemo_gi import run_import_nem_gi
-from opennem.importer.aemo_rel import run_import_nem_rel
-from opennem.importer.mms import run_import_mms
+from opennem.importer.aemo_gi import gi_export
+from opennem.importer.aemo_rel import rel_export
+from opennem.importer.mms import mms_export
+from opennem.importer.registry import registry_export
 
 
 def run_all():
-    run_import_mms()
-    run_import_nem_rel()
-    run_import_nem_gi()
+    mms_export()
+    rel_export()
+    gi_export()
+    registry_export()
