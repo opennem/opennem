68e56c26577b8c89109af63bffb4111114122dd9
Nik Cubrilovic <nc9@protonmail.com>
2020-09-01 09:13:03 +1000

    FEAT: Exporter based on JSON schemas

--- opennem/exporter/csv.py
+++ opennem/exporter/csv.py
@@ -1,58 +1,56 @@
 import csv
 from io import StringIO
+from typing import List
 
 from opennem.controllers.stations import get_stations
 from opennem.core.facility_duid_map import duid_is_retired
 from opennem.db import get_database_session
+from opennem.schema.opennem import StationSchema
 
 
-def stations_csv_records():
-    db = get_database_session()
-    stations = get_stations(db)
-
+def stations_csv_records(stations: List[StationSchema]):
     records = []
 
     for station in stations:
         for facility in station.facilities:
-            if facility.fueltech_id is None:
+            if facility.fueltech is None:
                 continue
 
-            if facility.status_id is None:
+            if facility.status is None:
                 continue
 
             if duid_is_retired(facility.code):
                 continue
 
-            if facility.active == False:
+            if facility.active is False:
                 continue
 
             rec = {
                 "name": station.name,
-                # "oid": station.oid,
-                "ocode": station.ocode,
-                "code": facility.duid,
+                "oid": station.oid(),
+                "ocode": station.ocode(),
+                "code": facility.code,
                 "region": facility.network_region,
-                "status": facility.status_id,
-                "fueltech": facility.fueltech_id,
+                "status": facility.status.code,
+                "fueltech": facility.fueltech.code,
                 "unit_id": facility.unit_id,
                 "unit_num": facility.unit_number,
-                "unit_cap": facility.capacity_aggregate,
-                "station_cap_agg": station.capacity_aggregate,
+                # "unit_cap": facility.capacity_aggregate,
+                # "station_cap_agg": station.capacity_aggregate,
                 "station_cap_registered": station.capacity_registered,
-                "added_by": facility.created_by,
-                "updated_by": facility.updated_by,
+                "craeted_by": facility.created_by,
             }
             records.append(rec)
 
     return records
 
 
-def stations_csv_serialize(csv_stream=None):
+def stations_csv_serialize(stations, csv_stream=None):
 
     if not csv_stream:
         csv_stream = StringIO()
 
-    csv_records = stations_csv_records()
+    csv_records = stations_csv_records(stations)
 
     csv_fieldnames = csv_records[0].keys()
 
--- opennem/exporter/runner.py
+++ opennem/exporter/runner.py
@@ -1,41 +1,71 @@
+import json
+from typing import List
+
+from pydantic import ValidationError
+
 from opennem.exporter.aws import write_to_s3
 from opennem.exporter.csv import stations_csv_serialize
 from opennem.exporter.geojson import stations_geojson_serialize
 from opennem.exporter.local import write_to_local
 from opennem.exporter.onjson import stations_json_serialize
+from opennem.importer.all import run_all
+from opennem.importer.opennem import opennem_import
+from opennem.schema.opennem import StationSchema
 from opennem.utils.log_config import logging
 
 logger = logging.getLogger("opennem.exporter")
 
 
-def stations_geojson_to_s3():
-    stations_geojson = stations_geojson_serialize()
+def load_stations():
+    with open("data/opennem.json") as fh:
+        __data = json.load(fh)
+
+    stations: List[StationSchema] = []
+
+    for i in __data.values():
+        try:
+            stations.append(StationSchema(**i))
+        except ValidationError as e:
+            logger.error(
+                "Error with record: {} {}: {}".format(i["code"], i["name"], e)
+            )
+
+    return stations
+
+
+def stations_geojson_to_s3(stations: List[StationSchema]):
+    stations_geojson = stations_geojson_serialize(stations)
 
     facilities_path = "geo/au_facilities.json"
 
-    file_length = write_to_s3("geo/au_facilities.json", stations_geojson)
+    file_length = write_to_s3(facilities_path, stations_geojson)
+
+    return file_length
 
 
-def stations_geojson_to_local():
-    stations_geojson = stations_geojson_serialize()
+def stations_geojson_to_local(stations: List[StationSchema]):
+    stations_geojson = stations_geojson_serialize(stations)
 
     write_to_local("stations.geojson", stations_geojson)
 
 
-def stations_csv_to_local():
-    stations_csv = stations_csv_serialize()
+def stations_csv_to_local(stations: List[StationSchema]):
+    stations_csv = stations_csv_serialize(stations)
 
     write_to_local("stations.csv", stations_csv)
 
 
-def stations_json_to_local():
-    stations_json = stations_json_serialize()
+def stations_json_to_local(stations: List[StationSchema]):
+    stations_json = stations_json_serialize(stations)
 
     write_to_local("stations.json", stations_json)
 
 
 if __name__ == "__main__":
-    stations_geojson_to_s3()
-    stations_geojson_to_local()
-    stations_csv_to_local()
-    stations_json_to_local()
+    run_all()
+    stations = opennem_import()
+
+    stations_geojson_to_s3(stations.copy())
+    stations_geojson_to_local(stations.copy())
+    stations_csv_to_local(stations.copy())
+    stations_json_to_local(stations.copy())
