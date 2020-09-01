acb77db387932831b058f150244514a72167ffc6
Nik Cubrilovic <nc9@protonmail.com>
2020-09-01 16:11:00 +1000

    FEAT: capacity_cleaner more unit tests and type comparisons

--- tests/test_capacity_cleaner.py
+++ tests/test_capacity_cleaner.py
@@ -1,31 +1,38 @@
 from opennem.core.normalizers import clean_capacity
 
 
+def is_none(subject) -> bool:
+    return isinstance(subject, type(None))
+
+
 class TestCapacityCleaner(object):
     def test_capacity_blank(self):
         subject = clean_capacity("")
-        assert type(subject) is type(None), "Blank string should be nonetype"
-        assert subject == None, "Blank capacity should be none"
+        assert is_none(subject), "Blank string should return None"
 
     def test_capacity_none(self):
         subject = clean_capacity(None)
-        assert type(subject) is type(None), "None string should be nonetype"
-        assert subject == None, "None capacity should be none"
+        assert is_none(subject), "None should return None"
 
     def test_capacity_string_excel_blank(self):
         subject = clean_capacity("-")
-        assert type(subject) is type(None), "Blank string should be nonetype"
-        assert subject == None, "Blank capacity should be none"
+        assert is_none(subject), "Blank string should return None"
 
     def test_capacity_zero_string(self):
         subject = clean_capacity("0")
-        assert type(subject) is float, "Capacity should be a float"
+        assert isinstance(subject, float), "Capacity should be a float"
         assert subject == 0.0, "0 string capacity should be float 0"
 
     def test_capacity_zero(self):
         subject = clean_capacity(0)
-        assert type(subject) is float, "Capacity should be a float"
+        assert isinstance(subject, float), "Capacity should be a float"
         assert subject == 0.0, "0 string capacity should be float 0"
+
+    def test_capacity_float(self):
+        value = 204.4
+        subject = clean_capacity(value)
+
+        assert value == subject, "Floats return floats"
 
     def test_capacity_zero__string_padded(self):
         subject = clean_capacity("0  ")
@@ -37,7 +44,7 @@ class TestCapacityCleaner(object):
         assert type(subject) is float, "Capacity should be a float"
         assert subject == 1.1, "0 string capacity should be float 0"
 
-    def test_capacity_rounding(self):
+    def test_capacity_string_rounding(self):
         subject = clean_capacity("1.00000001")
         assert type(subject) is float, "Capacity should be a float"
         assert subject == 1.0, "0 string capacity should be rounded to 1"
