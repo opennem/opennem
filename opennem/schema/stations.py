5acafe6d61fbcd5756b11a84089ea69198386470
Nik Cubrilovic <nc9@protonmail.com>
2020-09-03 13:17:42 +0000

    FEAT: Convert stationset to key on code rather than id

--- opennem/schema/stations.py
+++ opennem/schema/stations.py
@@ -52,13 +52,13 @@ class StationSet(UserList):
         pass
 
     def add(self, station: StationSchema):
-        if not station.id:
-            raise Exception("Require a station id")
+        if not station.code:
+            raise Exception("Require a station code")
 
-        _key = station.id
+        _key = station.code
 
-        if self.get(_key):
-            raise Exception("Duplicate id {}".format(_key))
+        if self.get_code(_key):
+            raise Exception("Duplicate code {}".format(_key))
 
         self.data.append(station)
 
