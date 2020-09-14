91a660bac7aba98ae173dd0c4273f7421187677d
Nik Cubrilovic <nc9@protonmail.com>
2020-09-14 08:57:55 +0000

    FEAT: Comparator supports both base schemas and db schemas and has more unit test coverage

--- opennem/importer/comparator.py
+++ opennem/importer/comparator.py
@@ -1,17 +1,34 @@
-from typing import Any, List
+from typing import Any, List, Union
+
+from dictalchemy.utils import asdict
+from pydantic import BaseModel
+
+from opennem.db.models.opennem import BaseModel as BaseDbModel
 
 
-def compare_record(subject: object, target: object, fields: List[str]) -> bool:
+def compare_record_differs(
+    subject: object, target: object, fields: Union[str, List[str]]
+) -> bool:
+    """
+        Compare two objects or types returns True if they're the same
+
+    """
+
+    if not type(fields) is list:
+        fields = [fields]
 
     comparator_results = []
 
     for field in fields:
         subject_value = field_getter(subject, field)
-        target_value = field_getter(subject, field)
+        target_value = field_getter(target, field)
 
-        result = subject_value == target_value
+        result = subject_value != target_value
 
+        print(field, subject_value, target_value, result)
         comparator_results.append(result)
+
+    print(comparator_results)
 
     return all(comparator_results)
 
@@ -20,6 +37,28 @@ def compare_field(subject: Any, target: Any) -> bool:
     pass
 
 
-def field_getter(subject: object, field_name: str):
+def field_getter(subject: object, field_name: str, default: any = None):
+    """
+        Gets a field value from a dict or object
+
+    """
+    value = None
+
+    if type(subject) is dict and field_name in subject:
+        value = subject.get(field_name, default)
+
     if hasattr(subject, field_name):
-        return getattr(subject, field_name, None)
+        value = getattr(subject, field_name, default)
+
+    if isinstance(value, BaseModel):
+        value: dict = value.dict()
+
+    if isinstance(value, BaseDbModel):
+        value: dict = asdict(
+            value, exclude=["created_by", "created_at", "updated_at"]
+        )
+
+    if value:
+        return value
+
+    return default
--- tests/test_record_comparator.py
+++ tests/test_record_comparator.py
@@ -1,5 +1,5 @@
 from opennem.db.models.opennem import Facility, FacilityStatus, FuelTech
-from opennem.importer.comparator import compare_record
+from opennem.importer.comparator import compare_record_differs
 from opennem.schema.opennem import (
     FacilitySchema,
     FacilityStatusSchema,
@@ -8,7 +8,14 @@ from opennem.schema.opennem import (
 
 
 class TestRecordComparator(object):
-    def test_fueltech(self):
+    models = {}
+    schemas = {}
+
+    @classmethod
+    def setup_class(cls):
+        """
+            Load the stations fixture for this test suite
+        """
         fueltech_record_dict = {
             "code": "coal_black",
             "label": "Black Coal",
@@ -19,7 +26,6 @@ class TestRecordComparator(object):
 
         facility_record_dict = {
             "code": "TEST",
-            # "network_region": {"code": "NEM", "country": "au", "label": "NEM"},
         }
 
         db_fueltech = FuelTech(**fueltech_record_dict)
@@ -35,8 +41,45 @@ class TestRecordComparator(object):
             fueltech=schema_fueltech,
             status=schema_status,
         )
-        schema_facility.fueltech = schema_fueltech
+
+        cls.models["facility_plain"] = db_facility
+        cls.schemas["facility_plain"] = schema_facility
+
+    def test_none(self):
+        subject = {
+            "field": True,
+        }
+
+        target = {"field": None}
 
         assert (
-            compare_record(schema_facility, db_facility, "fueltech") is True
+            compare_record_differs(subject, target, "field") is True
+        ), "No field means it changed"
+        assert (
+            compare_record_differs(subject, {}, "field") is True
+        ), "Empty means it changed"
+
+    def test_boolean(self):
+        subject = {
+            "field": True,
+        }
+
+        target = {"field": True}
+
+        target_false = {"field": False}
+
+        assert (
+            compare_record_differs(subject, target, "field") is False
+        ), "Fields are both true"
+        assert (
+            compare_record_differs(subject, target_false, "field") is True
+        ), "Fields True and False so differ"
+
+    def test_fueltech(self):
+        subject_schema = self.schemas.get("facility_plain")
+        target_model = self.models.get("facility_plain")
+
+        assert (
+            compare_record_differs(subject_schema, target_model, "fueltech")
+            is False
         ), "Fueltechs are identical"
