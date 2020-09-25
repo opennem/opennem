125430186398f22aa503ab9ef7b24535f4847c0a
Nik Cubrilovic <nc9@protonmail.com>
2020-09-26 01:13:18 +1000

    FEAT: Stations as module with lighter schemas for list outputs and more

--- opennem/api/app.py
+++ opennem/api/app.py
@@ -1,13 +1,13 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List
 
-import pytz
 from fastapi import Depends, FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from starlette import status
 
 from opennem.api.locations import router as locations_router
+from opennem.api.station.router import router as station_router
 from opennem.api.stats.router import router as stats_router
 from opennem.core.dispatch_type import DispatchType
 from opennem.core.time import human_to_interval
@@ -33,16 +33,13 @@ from .schema import (
     FueltechResponse,
     RevisionModification,
     RevisionModificationResponse,
-    StationIDList,
-    StationModification,
-    StationResponse,
-    UpdateResponse,
 )
 
 app = FastAPI(title="OpenNEM", debug=True, version="3.0.0-alpha.3")
 
 app.include_router(stats_router, tags=["Stats"], prefix="/stats")
 app.include_router(locations_router, tags=["Locations"], prefix="/locations")
+app.include_router(station_router, tags=["Stations"], prefix="/station")
 
 
 origins = [
@@ -60,224 +57,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.get(
-    "/stations",
-    response_model=List[StationSchema],
-    description="Get a list of all stations",
-)
-def stations(
-    session: Session = Depends(get_database_session),
-    facilities_include: Optional[bool] = Query(
-        False, description="Include facilities in records"
-    ),
-    revisions_include: Optional[bool] = Query(
-        False, description="Include revisions in records"
-    ),
-    history_include: Optional[bool] = Query(
-        False, description="Include history in records"
-    ),
-    only_approved: Optional[bool] = Query(
-        False, description="Only show approved stations not those pending"
-    ),
-    name: Optional[str] = None,
-    limit: Optional[int] = None,
-    page: int = 1,
-) -> List[StationSchema]:
-    stations = (
-        session.query(Station)
-        .join(Location)
-        .enable_eagerloads(True)
-        #
-        # .group_by(Station.code)
-        # .filter(Facility.fueltech_id.isnot(None))
-        # .filter(Facility.status_id.isnot(None))
-    )
-
-    if facilities_include:
-        stations = stations.outerjoin(
-            Facility, Facility.station_id == Station.id
-        ).outerjoin(FuelTech, Facility.fueltech_id == FuelTech.code)
-
-    if revisions_include:
-        stations = stations.outerjoin(
-            Revision, Revision.station_id == Station.id
-        )
-
-    if only_approved:
-        stations = stations.filter(Station.approved == True)
-
-    if name:
-        stations = stations.filter(Station.name.like("%{}%".format(name)))
-
-    stations = stations.order_by(
-        # Facility.network_region,
-        Station.name,
-        # Facility.network_code,
-        # Facility.code,
-    )
-
-    stations = stations.all()
-
-    return stations
-
-
-@app.get(
-    "/station/ids",
-    response_model=List[StationIDList],
-    description="Get a list of station ids for dropdowns",
-)
-def station_ids(
-    session: Session = Depends(get_database_session),
-    only_approved: Optional[bool] = Query(
-        True, description="Only show approved stations not those pending"
-    ),
-) -> List[StationIDList]:
-    stations = session.query(Station).join(Station.location)
-
-    if only_approved:
-        stations = stations.filter(Station.approved == True)
-
-    stations = stations.order_by(Station.id,)
-
-    stations = stations.all()
-
-    return stations
-
-
-@app.get(
-    "/station/{station_code:path}",
-    response_model=StationSchema,
-    description="Get a single station by code",
-)
-def station(
-    session: Session = Depends(get_database_session),
-    station_code: str = None,
-    only_generators: bool = Query(True, description="Show only generators"),
-    power_include: Optional[bool] = Query(
-        False, description="Include last week of power output"
-    ),
-    revisions_include: Optional[bool] = Query(
-        False, description="Include revisions in records"
-    ),
-    history_include: Optional[bool] = Query(
-        False, description="Include history in records"
-    ),
-):
-    station = (
-        session.query(Station)
-        .join(Facility)
-        .filter(Station.code == station_code)
-    )
-
-    if only_generators:
-        station = station.filter(
-            Facility.dispatch_type == DispatchType.GENERATOR
-        )
-
-    station = station.one_or_none()
-
-    if not station:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Station not found"
-        )
-
-    if revisions_include:
-        revisions = session.query(Revision).all()
-
-        station.revisions = list(
-            filter(
-                lambda rev: rev.schema == "station"
-                and rev.code == station.code,
-                revisions,
-            )
-        )
-
-        for facility in station.facilities:
-            facility.revisions = list(
-                filter(
-                    lambda rev: rev.schema == "facility"
-                    and rev.code == facility.code,
-                    revisions,
-                )
-            )
-
-    if power_include:
-
-        pass
-
-    return station
-
-
-@app.get(
-    "/station/history",
-    name="station all history",
-    description="""Get history for all stations""",
-    response_model=List[StationSchema],
-)
-def stations_history(session: Session = Depends(get_database_session)):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
-
-
-@app.get(
-    "/station/history/{station_code}",
-    name="station history",
-    description="""Get history for a station""",
-    response_model=StationSchema,
-)
-def station_history(
-    station_code: str, session: Session = Depends(get_database_session)
-):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
-
-
-@app.get(
-    "/station",
-    name="station lookup",
-    description="""Lookup station by code or network_code. \
-        Require one of code or network_code""",
-    response_model=List[StationSchema],
-)
-def station_lookup(
-    session: Session = Depends(get_database_session),
-    station_code: Optional[str] = Query(
-        None, description="Code of the station to lookup"
-    ),
-    network_code: Optional[str] = Query(
-        None, description="The network code to lookup"
-    ),
-) -> List[StationSchema]:
-    if not station_code and not network_code:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-    station = session.query(Station)
-
-    if station_code:
-        station = station.filter_by(code=station_code)
-
-    if network_code:
-        station = station.filter_by(network_name=network_code)
-
-    stations = station.all()
-
-    if not stations:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-
-    return stations
-
-
-@app.post(
-    "/stations",
-    name="station",
-    # response_model=StationSchema,
-    description="Create a station",
-)
-def station_create(
-    session: Session = Depends(get_database_session),
-    # station: StationSubmission = None,
-):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
 
 
 @app.get(
@@ -334,42 +113,6 @@ def revision(
         )
 
     return revision
-
-
-@app.put("/station/{station_id}", name="Station update")
-def station_update(
-    station_id: int,
-    data: StationModification = {},
-    session: Session = Depends(get_database_session),
-) -> dict:
-    station = session.query(Station).get(station_id)
-
-    if not station:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Station not found"
-        )
-
-    if data.modification == "approve":
-
-        if station.approved is True:
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail="Station already approved",
-            )
-
-        station.approved = True
-        station.approved_at = datetime.now()
-        station.approved_by = "opennem.admin"
-
-    if data.modification == "reject":
-        station.approved = False
-
-    session.add(station)
-    session.commit()
-
-    response = UpdateResponse(success=True, record=station)
-
-    return response
 
 
 @app.put("/facility/{facility_id}", name="Facility update")
--- opennem/api/schema.py
+++ opennem/api/schema.py
@@ -22,30 +22,6 @@ class ApiBase(BaseModel):
         validate_assignment = True
 
 
-# all records have an id
-class UpdateRecordBase(BaseModel):
-    id: int
-
-    approved: bool = False
-    approved_by: Optional[str]
-    approved_at: Optional[datetime]
-
-
-class StationResponse(BaseModel):
-    success: bool = False
-    record_num: int
-    records: Optional[List[StationSchema]]
-
-
-class StationModificationTypes(str, Enum):
-    approve = "approve"
-
-
-class StationModification(ApiBase):
-    comment: Optional[str] = Field(None)
-    modification: StationModificationTypes
-
-
 class RevisionModificationTypes(str, Enum):
     approve = "approve"
     reject = "reject"
@@ -62,11 +38,6 @@ class RevisionModificationResponse(ApiBase):
     target: Optional[Union[FacilitySchema, StationSchema, LocationSchema]]
 
 
-class UpdateResponse(ApiBase):
-    success: bool = False
-    record: UpdateRecordBase
-
-
 class FueltechResponse(ApiBase):
     success: bool = True
 
@@ -76,13 +47,3 @@ class FueltechResponse(ApiBase):
 class RevisionUpdate(ApiBase):
     pass
 
-
-class StationIDListLocation(ApiBase):
-    state: str
-    country: str = "au"
-
-
-class StationIDList(ApiBase):
-    id: int
-    name: str
-    location: StationIDListLocation
