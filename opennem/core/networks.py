f7b67204f89d475018136928e11a74ae9387cde6
Nik Cubrilovic <nc9@protonmail.com>
2020-09-30 15:45:47 +1000

    FEAT: network_from_network_region

--- opennem/core/networks.py
+++ opennem/core/networks.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from opennem.schema.opennem import NetworkNEM, NetworkSchema, NetworkWEM
 
 
@@ -8,8 +10,14 @@ def network_from_state(state: str) -> NetworkSchema:
     return NetworkNEM
 
 
-def network_from_network_region(network_region: str) -> NetworkSchema:
+def network_from_network_region(
+    network_region: str,
+) -> Optional[NetworkSchema]:
+    network_region = network_region.upper()
+
     if network_region in ["WEM"]:
         return NetworkWEM
+    if network_region in ["WEM"]:
+        return NetworkNEM
 
-    return NetworkNEM
+    return None
