32abede4c0f49912df683118c53bd8268201277a
Nik Cubrilovic <nc9@protonmail.com>
2020-09-30 15:53:29 +1000

    FIX: query formating in energy validate network_code

--- opennem/api/stats/queries.py
+++ opennem/api/stats/queries.py
@@ -5,19 +5,21 @@ def energy_year_network(network_code: str = "WEM", year: int = None) -> str:
     if not year:
         year = datetime.today().year
 
+    network_code = network_code.upper()
+
     return """
         select
             date_trunc('day', fs.trading_interval) AS trading_day,
             max(fs.eoi_quantity) as energy_output,
-            f.fueltech_id
+            f.fueltech_id as fueltech
         from facility_scada fs
         left join facility f on fs.facility_code = f.code
         where
             f.fueltech_id is not null
             and extract('year' from fs.trading_interval) = {year}
-            and fs.network_id = '{network_code'
+            and fs.network_id = '{network_code}'
         group by 1, f.fueltech_id
         order by 1 desc, 2 asc
     """.format(
-        year, network_code
+        year=year, network_code=network_code
     )
