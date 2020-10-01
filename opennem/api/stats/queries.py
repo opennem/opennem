b737708854ebbe6462b8c523981131022cb1bf4e
Nik Cubrilovic <nc9@protonmail.com>
2020-10-01 12:46:58 +1000

    FIX: Normalize time series joins to UTC

--- opennem/api/stats/queries.py
+++ opennem/api/stats/queries.py
@@ -11,33 +11,41 @@ def duid_in_case(facility_codes: List[str]) -> str:
 
 
 def wem_energy_facility(
-    facility_codes: List[str], interval: str = "day", period: str = "7d"
+    facility_codes: List[str],
+    network_code: str,
+    interval: str = "day",
+    period: str = "7d",
 ) -> str:
+
+    network_code = network_code.upper()
 
     __query = """
         with intervals as (
             select generate_series(
-                date_trunc('day', now()) - '7 day'::interval,
-                date_trunc('day', now()),
+                date_trunc('day', now() AT TIME ZONE 'UTC') - '7 day'::interval,
+                date_trunc('day', now() AT TIME ZONE 'UTC'),
                 '1 day'::interval
-            ) as interval
+            )::date as interval
         )
 
         select
             i.interval AS trading_day,
             fs.facility_code as facility_code,
-            sum(fs.eoi_quantity) as energy_output
+            coalesce(sum(fs.eoi_quantity), NULL) as energy_output
         from intervals i
-        full join facility_scada fs on date_trunc('day', fs.trading_interval) = i.interval
+        left join facility_scada fs on date_trunc('day', fs.trading_interval AT TIME ZONE 'UTC')::date = i.interval
         where
             fs.facility_code in ({facility_codes_parsed})
-            and fs.trading_interval > now() - '7 day'::interval
-            and fs.network_id = 'WEM'
+            and fs.trading_interval > now() AT TIME ZONE 'UTC' - '7 day'::interval
+            and fs.network_id = '{network_code}'
         group by 1, 2
-        order by 1 asc, 2 asc
+        order by 2 asc, 1 asc
     """
 
-    query = __query.format(facility_codes_parsed=duid_in_case(facility_codes))
+    query = __query.format(
+        facility_codes_parsed=duid_in_case(facility_codes),
+        network_code=network_code,
+    )
 
     return query
 
