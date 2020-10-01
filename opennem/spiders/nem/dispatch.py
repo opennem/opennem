2b7a7b8a0288f6c948dce1340f5bd4f58aaa272a
Nik Cubrilovic <nc9@protonmail.com>
2020-10-01 10:23:50 +1000

    FEAT: Separate nightly dispatch crawler + pipeline

---
+++ opennem/spiders/nem/dispatch.py
@@ -0,0 +1,27 @@
+from opennem.pipelines.nem.opennem import NemwebUnitScadaOpenNEMStorePipeline
+from opennem.spiders.nemweb import NemwebSpider
+
+
+class NemwebCurrentDispatch(NemwebSpider):
+    name = "au.nem.current.dispatch"
+    start_url = "http://nemweb.com.au/Reports/Current/Next_Day_Dispatch/"
+
+    # only get most recent
+    limit = 1
+
+    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline,])
+
+
+class NemwebArchiveDispatch(NemwebSpider):
+    name = "au.nem.archive.dispatch"
+    start_urls = ["http://nemweb.com.au/Reports/Current/Next_Day_Dispatch/"]
+    limit = 0
+
+    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline,])
+
+    # Archives tend to contain large zips of embedded zips so throttle
+    # to limit memory use
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 1,
+        "CONCURRENT_ITEMS": 1,
+    }
