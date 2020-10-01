d3371c32d3f9d0ce051b8f72121f3670f5ebd56f
Nik Cubrilovic <nc9@protonmail.com>
2020-10-01 10:26:35 +1000

    FEAT: duid code list to in query

--- opennem/api/stats/queries.py
+++ opennem/api/stats/queries.py
@@ -1,4 +1,14 @@
 from datetime import datetime
+from typing import List
+
+from opennem.core.normalizers import normalize_duid
+
+
+def duid_in_case(facility_codes: List[str]) -> str:
+    return ",".join(
+        ["'{}'".format(i) for i in map(normalize_duid, facility_codes)]
+    )
+
 
 
 def energy_year_network(network_code: str = "WEM", year: int = None) -> str:
