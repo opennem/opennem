624af5f056f9187f3e70d67fb6d3144af55b35b5
Nik Cubrilovic <nc9@protonmail.com>
2020-10-02 17:34:28 +1000

    FEAT: Update BOMBase spider to support station_ids

--- opennem/spiders/bom/bom_base.py
+++ opennem/spiders/bom/bom_base.py
@@ -17,11 +17,24 @@ class BomJSONObservationSpider(scrapy.Spider):
         if not type(json_response) is dict:
             raise Exception("invalid response from bom")
 
-        if not "observations" in json_response:
+        if "observations" not in json_response:
             raise Exception("invalid response from bom")
 
-        if not "data" in json_response["observations"]:
+        if "data" not in json_response["observations"]:
             raise Exception("invalid response from bom")
+
+        station_id = None
+
+        if "station_id" in response.meta:
+            station_id = response.meta["station_id"]
+
+        elif hasattr(self, "station_id"):
+            station_id = self.station_id
+
+        if not station_id:
+            raise Exception("No station id for this scrape can't join")
 
         for i in json_response["observations"]["data"]:
+            i["station_id"] = station_id
+
             yield i
