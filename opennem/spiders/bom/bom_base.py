4bad7be4b6a430916d4f3e37ae53f4c8a2f21d95
Nik Cubrilovic <nc9@protonmail.com>
2020-10-07 10:22:08 +1100

    FIX: Update BOMBase spider

--- opennem/spiders/bom/bom_base.py
+++ opennem/spiders/bom/bom_base.py
@@ -7,8 +7,12 @@ from opennem.pipelines.bom import StoreBomObservation
 
 class BomJSONObservationSpider(scrapy.Spider):
     allowed_domains = ["bom.gov.au"]
+
     start_urls = ["http://reg.bom.gov.au/fwo/IDN60901/IDN60901.94767.json"]
+
     pipelines = set([StoreBomObservation])
+
+    station_id: str = None
 
     def parse(self, response):
         json_response = json.loads(response.text)
@@ -27,7 +31,7 @@ class BomJSONObservationSpider(scrapy.Spider):
         if "station_id" in response.meta:
             station_id = response.meta["station_id"]
 
-        elif hasattr(self, "station_id"):
+        elif hasattr(self, "station_id") and self.station_id:
             station_id = self.station_id
 
         if not station_id:
