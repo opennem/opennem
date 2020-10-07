9aa127a7fc093c0eb96b950f3fc41b44d63847f3
Nik Cubrilovic <nc9@protonmail.com>
2020-10-07 11:42:26 +1100

    FEAT: Migration add network_region text column

---
+++ opennem/db/migrations/versions/911efdc0c77f_balancing_summary_add_network_region_.py
@@ -0,0 +1,30 @@
+"""Balancing summary add network_region column
+
+Revision ID: 911efdc0c77f
+Revises: 8bf30c37cb85
+Create Date: 2020-10-07 11:16:51.578193
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "911efdc0c77f"
+down_revision = "8bf30c37cb85"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "balancing_summary",
+        sa.Column("network_region", sa.Text(), nullable=True),
+    )
+    op.execute("COMMIT")
+    op.execute(
+        "update balancing_summary set network_region='WEM' where network_id='WEM'"
+    )
+
+
+def downgrade():
+    op.drop_column("balancing_summary", "network_region")
