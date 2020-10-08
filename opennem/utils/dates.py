3fb8e4bdc47f4c61dfe11a69fc5bc05e12ab516b
Nik Cubrilovic <nc9@protonmail.com>
2020-10-09 00:58:46 +1100

    FEAT: New dates module with parser and unit tests

---
+++ opennem/utils/dates.py
@@ -0,0 +1,42 @@
+import logging
+from datetime import datetime
+from typing import Optional, Union
+
+from dateutil.parser import ParserError, parse
+
+from opennem.core.networks import NetworkSchema
+from opennem.utils.timezone import UTC, is_aware, make_aware
+
+logger = logging.getLogger(__name__)
+
+
+def parse_date(
+    date_str: Union[str, datetime],
+    network: Optional[NetworkSchema] = None,
+    dayfirst: bool = True,
+    is_utc: bool = False,
+) -> Optional[datetime]:
+    dt_return = None
+
+    if isinstance(date_str, datetime):
+        dt_return = date_str
+
+    elif isinstance(date_str, str):
+        try:
+            dt_return = parse(date_str, dayfirst=dayfirst)
+        except ParserError:
+            raise ValueError("Invalid date string passed")
+
+    else:
+        raise ValueError("Require a datetime or string object to parse date")
+
+    if network:
+        tz = network.get_timezone()
+
+        if tz:
+            dt_return = make_aware(dt_return, timezone=tz)
+
+    if is_utc:
+        dt_return = make_aware(dt_return, timezone=UTC)
+
+    return dt_return
---
+++ tests/test_dates.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+
+import pytest
+import pytz
+from pytz import timezone
+
+from opennem.utils.dates import parse_date
+from opennem.utils.timezone import is_aware
+
+UTC = pytz.utc
+
+
+class TestUtilDates(object):
+    def test_date_none(self):
+        with pytest.raises(ValueError) as excinfo:
+            parse_date(None)
+
+        assert "Require a datetime or string" in str(
+            excinfo
+        ), "Empty string raises exception"
+
+    def test_date_empty(self):
+        with pytest.raises(ValueError) as excinfo:
+            parse_date("")
+
+        assert "Invalid date string passed" in str(
+            excinfo
+        ), "Empty string raises exception"
+
+    def test_wem_date_comissioned(self):
+        subject = parse_date("1/11/08 0:00")
+        comparator = datetime(2008, 11, 1, 0, 0, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_nem_dispatch_interval_amiguous(self):
+        subject = parse_date("1/9/19 4:00")
+        comparator = datetime(2019, 9, 1, 4, 0, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_nem_dispatch_interval(self):
+        subject = parse_date("30/9/19 4:00")
+        comparator = datetime(2019, 9, 30, 4, 0, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_nem_dispatch_scada_interval(self):
+        subject = parse_date("2020/06/01 21:35:00", dayfirst=False)
+        comparator = datetime(2020, 6, 1, 21, 35, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_nem_excel_formatted(self):
+        subject = parse_date("27/9/2019  2:55:00 pm")
+        comparator = datetime(2019, 9, 27, 14, 55, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_bom_date(self):
+        subject = parse_date("20201008133000", dayfirst=False)
+        comparator = datetime(2020, 10, 8, 13, 30, 0, 0)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is False, "Date has no timezone info"
+
+    def test_bom_date_utc(self):
+        subject = parse_date("20201008133000", dayfirst=False, is_utc=True)
+        comparator = datetime(2020, 10, 8, 13, 30, 0, 0, tzinfo=UTC)
+
+        assert subject == comparator, "Parses date correctly"
+        assert is_aware(subject) is True, "Date has timezone info"
