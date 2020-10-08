dc5d63fa0c2ee41881b59ffde722cb6813825819
Nik Cubrilovic <nc9@protonmail.com>
2020-10-09 01:00:53 +1100

    FEAT: New network schema with timezone offsets

--- opennem/schema/network.py
+++ opennem/schema/network.py
@@ -1,4 +1,9 @@
-from pytz import timezone
+from datetime import timezone
+from typing import Optional
+
+from pydantic import Field
+
+from opennem.utils.timezone import get_current_timezone, get_fixed_timezone
 
 from .core import BaseConfig
 
@@ -7,25 +12,39 @@ class NetworkSchema(BaseConfig):
     code: str
     country: str
     label: str
-    timezone: str
-    interval_size: int
+    timezone: Optional[str] = Field(None, description="Network timezone")
+    timezone_database: Optional[str] = Field(
+        None, description="Database timezone format"
+    )
+    offset: Optional[int] = Field(
+        None, description="Network time offset in minutes"
+    )
+    interval_size: int = Field(
+        ..., description="Size of network interval in minutes"
+    )
 
     def get_timezone(self) -> timezone:
-        return timezone(self.timezone)
+        if self.offset:
+            return get_fixed_timezone(self.offset)
+        if self.timezone:
+            return timezone(self.timezone)
+        return get_current_timezone()
 
 
 NetworkNEM = NetworkSchema(
     code="NEM",
     label="NEM",
     country="au",
-    timezone="Australia/Sydney",
+    timezone_database="AEST",
+    offset=600,
     interval_size=5,
 )
 NetworkWEM = NetworkSchema(
     code="WEM",
     label="WEM",
     country="au",
-    timezone="Australia/Perth",
+    timezone="AWST",
+    offset=480,
     interval_size=30,
 )
 
