c33bb15a7e59bc2abfb6385f770476d6384a3320
Nik Cubrilovic <nc9@protonmail.com>
2020-10-08 22:33:21 +1100

    FEAT: network_from_network_code and network_from_state

--- opennem/core/networks.py
+++ opennem/core/networks.py
@@ -1,18 +1,19 @@
-from typing import Optional
-
 from opennem.schema.opennem import NetworkNEM, NetworkSchema, NetworkWEM
 
 
 def network_from_state(state: str) -> NetworkSchema:
-    if state.trim.upper() in ["WA"]:
+    state = state.upper().strip()
+
+    if state in ["WA"]:
         return NetworkWEM
 
-    return NetworkNEM
+    if state in ["QLD", "NSW", "VIC", "ACT", "TAS", "SA", "NT"]:
+        return NetworkNEM
+
+    raise Exception("Unknown network {}".format(state))
 
 
-def network_from_network_region(
-    network_region: str,
-) -> Optional[NetworkSchema]:
+def network_from_network_region(network_region: str,) -> NetworkSchema:
     network_region = network_region.upper()
 
     if network_region in ["WEM", "WA1"]:
@@ -20,4 +21,16 @@ def network_from_network_region(
     if network_region in ["NEM", "NSW1", "QLD1", "SA1", "VIC1", "TAS1"]:
         return NetworkNEM
 
-    raise Exception("Uknownn network {}".format(network_region))
+    raise Exception("Unknown network {}".format(network_region))
+
+
+def network_from_network_code(network_code: str) -> NetworkSchema:
+    network_code = network_code.upper().strip()
+
+    if network_code in ["WEM"]:
+        return NetworkWEM
+
+    if network_code in ["NEM"]:
+        return NetworkNEM
+
+    raise Exception("Unknown network {}".format(network_code))
