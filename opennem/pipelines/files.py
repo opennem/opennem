fdb844c93e67cef4a5df1be4a8053f365d319ca3
Nik Cubrilovic <nc9@protonmail.com>
2020-10-09 11:41:45 +1100

    FIX: Only read file handle if return successful in embedded zip handler

--- opennem/pipelines/files.py
+++ opennem/pipelines/files.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
+from requests import RequestException
+
 from opennem.utils.handlers import open
 from opennem.utils.pipelines import check_spider_pipeline
-from requests import RequestException
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,7 @@ class LinkExtract(object):
 
     @check_spider_pipeline
     def process_item(self, item, spider):
-        if not "link" in item:
+        if "link" not in item:
             return item
 
         url = item["link"]
@@ -31,9 +32,10 @@ class LinkExtract(object):
         except Exception as e:
             logger.error("Error: {}".format(e))
 
-        content = fh.read()
+        if fh:
+            content = fh.read()
 
-        item["content"] = content
-        item["extension"] = file_extension
+            item["content"] = content
+            item["extension"] = file_extension
 
         return item
