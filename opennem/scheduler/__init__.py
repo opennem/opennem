cdf63d39038401b207a9a1228bc8d7165719c9ce
Nik Cubrilovic <nc9@protonmail.com>
2020-10-10 15:06:24 +1100

    FEAT: run wem_run_all every 10 minutes

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -1,11 +1,11 @@
 from huey import RedisHuey, crontab
 
-# from opennem.api.exporter import wem_run_all
+from opennem.api.exporter import wem_run_all
 from opennem.settings import settings
 
 huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
 
 
-# @huey.periodic_task(crontab(minute="*/10"))
-# def wem_export_task():
-#     wem_run_all()
+@huey.periodic_task(crontab(minute="*/10"))
+def wem_export_task():
+    wem_run_all()
