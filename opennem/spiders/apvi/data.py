51f1e9a1b56a5ae2a962916bd9f21b559d027597
Nik Cubrilovic <nc9@protonmail.com>
2020-10-12 15:15:43 +1100

    FEAT: au.apvi.historic.data - get 90 days

--- opennem/spiders/apvi/data.py
+++ opennem/spiders/apvi/data.py
@@ -7,6 +7,10 @@ from opennem.utils.dates import date_series, get_date_component
 
 APVI_DATA_URI = "https://pv-map.apvi.org.au/data"
 
+TODAY = datetime.now().date()
+YESTERDAY = TODAY - timedelta(days=1)
+MONTH_AGO = TODAY - timedelta(days=29)
+
 
 class APVIDataSpiderBase(scrapy.Spider):
 
@@ -27,9 +31,17 @@ class APVIDataCurrentSpider(APVIDataSpiderBase):
     name = "au.apvi.current.data"
 
     def start_requests(self):
-        yesterday = datetime.now().date() - timedelta(days=1)
+        for date in date_series(YESTERDAY):
+            yield scrapy.FormRequest(
+                APVI_DATA_URI, formdata={"day": date.strftime("%Y-%m-%d")}
+            )
 
-        for date in date_series(yesterday):
+
+class APVIDataHistoricSpider(APVIDataSpiderBase):
+    name = "au.apvi.historic.data"
+
+    def start_requests(self):
+        for date in date_series(MONTH_AGO, length=90):
             yield scrapy.FormRequest(
                 APVI_DATA_URI, formdata={"day": date.strftime("%Y-%m-%d")}
             )
