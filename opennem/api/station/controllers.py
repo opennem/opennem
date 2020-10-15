9da89736cb651db9f9d2dbec39605e66b14999c4
Nik Cubrilovic <nc9@protonmail.com>
2020-10-15 14:07:02 +1100

    FEAT: Move controllers to API and update Stations controller

---
+++ opennem/api/station/controllers.py
@@ -0,0 +1,44 @@
+import decimal
+from typing import List, Optional
+
+from sqlalchemy.orm.session import Session
+
+from opennem.db.models.opennem import Facility, Station
+
+
+def get_stations(
+    session: Session,
+    only_approved: bool = True,
+    name: Optional[str] = None,
+    limit: Optional[int] = None,
+    page: int = 1,
+) -> List[Station]:
+    """
+        API controller that gets all stations sorted and joined
+
+    """
+
+    stations = (
+        session.query(Station)
+        .join(Station.facilities)
+        .join(Facility.fueltech)
+        .filter(Facility.fueltech_id.isnot(None))
+        .filter(Facility.status_id.isnot(None))
+    )
+
+    if name:
+        stations = stations.filter(Station.name.like("%{}%".format(name)))
+
+    if only_approved:
+        stations = stations.filter(Station.approved == True)
+
+    stations = stations.order_by(
+        Facility.network_region,
+        Station.name,
+        Facility.network_code,
+        Facility.code,
+    )
+
+    stations = stations.all()
+
+    return stations
