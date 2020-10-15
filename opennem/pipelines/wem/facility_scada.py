6d02149373a36ac310d04094748304035d0503fb
Nik Cubrilovic <nc9@protonmail.com>
2020-10-15 20:10:18 +1100

    FEAT: use generator for facility_scada to speed up inserts

--- opennem/pipelines/wem/facility_scada.py
+++ opennem/pipelines/wem/facility_scada.py
@@ -15,6 +15,36 @@ from opennem.utils.pipelines import check_spider_pipeline
 logger = logging.getLogger(__name__)
 
 
+def generate_records(csvreader, spider):
+    for row in csvreader:
+        trading_interval = parse_date(
+            row["Trading Interval"], network=NetworkWEM, dayfirst=False
+        )
+        facility_code = normalize_duid(row["Facility Code"])
+
+        generated = 0.0
+        generated_field = "EOI Quantity (MW)"
+
+        if generated_field in row:
+            generated = clean_float(row[generated_field])
+
+        energy = 0.0
+        energy_field = "Energy Generated (MWh)"
+
+        if energy_field in row:
+            energy = clean_float(row[energy_field])
+
+        yield {
+            "created_by": spider.name,
+            # "updated_by": None,
+            "network_id": "WEM",
+            "trading_interval": trading_interval,
+            "facility_code": facility_code,
+            "generated": generated,
+            "eoi_quantity": energy,
+        }
+
+
 class WemStoreFacilityScada(DatabaseStoreBase):
     @check_spider_pipeline
     def process_item(self, item, spider=None):
@@ -24,54 +54,36 @@ class WemStoreFacilityScada(DatabaseStoreBase):
 
         csvreader = csv.DictReader(item["content"].split("\n"))
 
-        records_to_store = []
-        primary_keys = []
+        records_stored = 0
 
-        for row in csvreader:
-            trading_interval = parse_date(
-                row["Trading Interval"], network=NetworkWEM, dayfirst=False
+        for record in generate_records(csvreader, spider):
+            stmt = insert(FacilityScada).values(record)
+            stmt.bind = engine
+            stmt = stmt.on_conflict_do_update(
+                constraint="facility_scada_pkey",
+                set_={
+                    # "updated_by": stmt.excluded.created_by,
+                    "eoi_quantity": stmt.excluded.eoi_quantity,
+                    "generated": stmt.excluded.generated,
+                },
             )
-            facility_code = normalize_duid(row["Facility Code"])
 
-            __key = (trading_interval, facility_code)
-
-            if __key not in primary_keys:
-                records_to_store.append(
-                    {
-                        "created_by": spider.name,
-                        # "updated_by": None,
-                        "network_id": "WEM",
-                        "trading_interval": trading_interval,
-                        "facility_code": facility_code,
-                        "generated": row["EOI Quantity (MW)"] or None,
-                        "eoi_quantity": row["Energy Generated (MWh)"] or None,
-                    }
-                )
-                primary_keys.append(__key)
-
-        # free
-        primary_keys = None
-
-        stmt = insert(FacilityScada).values(records_to_store)
-        stmt.bind = engine
-        stmt = stmt.on_conflict_do_update(
-            constraint="facility_scada_pkey",
-            set_={
-                # "updated_by": stmt.excluded.created_by,
-                "eoi_quantity": stmt.excluded.eoi_quantity,
-                "generated": stmt.excluded.generated,
-            },
-        )
+            try:
+                session.execute(stmt)
+                records_stored += 1
+            except Exception as e:
+                logger.error("Error: {}".format(e))
+            finally:
+                session.close()
 
         try:
-            session.execute(stmt)
             session.commit()
         except Exception as e:
             logger.error("Error: {}".format(e))
         finally:
             session.close()
 
-        return len(records_to_store)
+        return records_stored
 
 
 class WemStoreLiveFacilityScada(DatabaseStoreBase):
@@ -136,7 +148,7 @@ class WemStoreLiveFacilityScada(DatabaseStoreBase):
                         "network_id": "WEM",
                         "trading_interval": interval,
                         "facility_code": facility_code,
-                        "generated": val,
+                        "eoi_quantity": val,
                     }
                 )
 
@@ -146,7 +158,7 @@ class WemStoreLiveFacilityScada(DatabaseStoreBase):
             constraint="facility_scada_pkey",
             set_={
                 # "updated_by": stmt.excluded.created_by,
-                "generated": stmt.excluded.generated,
+                "eoi_quantity": stmt.excluded.eoi_quantity,
             },
         )
 
@@ -172,7 +184,6 @@ class WemStoreFacilityIntervals(object):
         csvreader = csv.DictReader(item["content"].split("\n"))
 
         records_to_store = []
-        last_asat = None
 
         for row in csvreader:
             trading_interval = parse_date(
@@ -208,6 +219,7 @@ class WemStoreFacilityIntervals(object):
             set_={
                 # "updated_by": stmt.excluded.created_by,
                 "generated": stmt.excluded.generated,
+                "eoi_quantity": stmt.excluded.eoi_quantity,
             },
         )
 
