a7b40d2949a31adf2f5d2a20ccd8396e098d65be
Nik Cubrilovic <nc9@protonmail.com>
2020-10-15 15:51:13 +1100

    FEAT: AWS exporter settings, content type

--- opennem/exporter/aws.py
+++ opennem/exporter/aws.py
@@ -1,28 +1,30 @@
 import logging
 import os
+from typing import Dict
 
 from smart_open import open
 
+from opennem.settings import settings
+
 logger = logging.getLogger(__name__)
 
-S3_EXPORT_DEFAULT_BUCKET = "s3://data.opennem.org.au/v3/"
 
-UPLOAD_ARGS = {
-    "ContentType": "application/json",
-}
+def get_upload_args(content_type: str = "application/json") -> Dict:
+    return {"ContentType": content_type}
 
 
-def write_to_s3(file_path: str, data: str) -> int:
+def write_to_s3(file_path: str, data: str, content_type: str = None) -> int:
     """
         Write data to an s3 path
     """
-    s3_save_path = os.path.join(S3_EXPORT_DEFAULT_BUCKET, file_path)
+    s3_save_path = os.path.join(settings.s3_bucket_path, file_path)
     write_count = 0
+    upload_args = get_upload_args(content_type)
 
     with open(
         s3_save_path,
         "w",
-        transport_params=dict(multipart_upload_kwargs=UPLOAD_ARGS),
+        transport_params=dict(multipart_upload_kwargs=upload_args),
     ) as fh:
         write_count = fh.write(data)
 
