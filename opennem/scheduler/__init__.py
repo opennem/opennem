faa40cce0c48e7b6b306ec5fc82e6459c2de5415
Nik Cubrilovic <nc9@protonmail.com>
2020-10-15 16:01:13 +1100

    FEAT: scheduler add geojson exporter

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -1,6 +1,7 @@
 from huey import RedisHuey, crontab
 
 from opennem.api.exporter import wem_run_all
+from opennem.exporter.geojson import export_facility_geojson
 from opennem.settings import settings
 
 huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
@@ -9,3 +10,8 @@ huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
 @huey.periodic_task(crontab(minute="*/10"))
 def wem_export_task():
     wem_run_all()
+
+
+@huey.periodic_task(crontab(minute="*/5"))
+def export_geojson():
+    export_facility_geojson()
