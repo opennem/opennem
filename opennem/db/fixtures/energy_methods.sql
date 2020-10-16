9add885476ec0fc700dba03baf4742c1cf0104ec
Nik Cubrilovic <nc9@protonmail.com>
2020-10-16 16:57:17 +1100

    FIX: Clean up energy function names, vars and add note

--- opennem/db/fixtures/energy_methods.sql
+++ opennem/db/fixtures/energy_methods.sql
@@ -3,9 +3,15 @@
 -- Postgres functions and aggregate for calculate energy
 -- any feedback improvements please contribute
 
+
+-- Example query used with timescaledb buckets:
+--
+-- @NOTE https://gist.github.com/nc9/9679b9f025bb360910bb3cc49173818d
+
+
 drop aggregate if exists energy_total (numeric, int);
 drop function if exists agg_power (agg_state point, el numeric, interval_size_minutes int);
-drop function if exists grt_finalfunc;
+drop function if exists onenergy_finalfunc;
 
 create function agg_power(agg_state point, el numeric, interval_size_minutes int)
 returns point
@@ -13,11 +19,11 @@ immutable
 language plpgsql
 as $$
 declare
-  current_sum numeric = 0.0;
-  num_elements int = 0;
-  power_hour float = 0;
-  n float = 10;
-  w int = 0;
+  current_sum numeric := 0.0;
+  num_elements int := 0;
+  power_hour float := 0;
+  n float := 10;
+  w int := 0;
 begin
  	num_elements = agg_state[1] + 1;
 
@@ -31,14 +37,14 @@ begin
 		w := 4;
 	END IF;
 
-	power_hour = el / (60 / interval_size_minutes);
- 	current_sum = agg_state[0] + (w * power_hour);
+	power_hour := el / (60 / interval_size_minutes);
+ 	current_sum := agg_state[0] + (w * power_hour);
 
  	return point(current_sum, num_elements);
 end;
 $$;
 
-create function grt_finalfunc(agg_state point)
+create function onenergy_finalfunc(agg_state point)
 returns float8
 immutable
 strict
@@ -46,21 +52,21 @@ language plpgsql
 as $$
 declare
   result_int numeric = 0.0;
-  a float = 0;
-  b float = 1;
-  n float = 10;
-  h float = (b - a) / n;
+  a float := 0;
+  b float := 1;
+  n float := 10;
+  h float := (b - a) / n;
 begin
-	result_int = h * agg_state[0] / 3 * 10;
+	result_int := h * agg_state[0] / 3 * 10;
 
   	return round(result_int, 2);
 end;
 $$;
 
-create aggregate energy_total (numeric, int)
+create aggregate on_energy_sum (numeric, int)
 (
     sfunc = agg_power,
     stype = point,
-    finalfunc = grt_finalfunc,
+    finalfunc = onenergy_finalfunc,
     initcond = '(0,0)'
 );
