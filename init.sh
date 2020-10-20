cd88a1501a3a8421627da65d43ba4c156d71b790
Nik Cubrilovic <git@nikcub.me>
2020-10-20 17:31:23 +1100

    FIX: Update init script to init rooftops and wem pulse data

--- init.sh
+++ init.sh
@@ -14,12 +14,15 @@ scrapy crawl -L ERROR au.wem.live.facilities
 scrapy crawl -L ERROR bom.capitals
 
 # rooftop
+python -m opennem.importer.apvi
 scrapy crawl -L ERROR au.apvi.current.data
 
 # stats data
 scrapy crawl -L ERROR au.wem.live.facility_intervals
+scrapy crawl -L ERROR au.wem.live.pulse
 scrapy crawl -L ERROR au.wem.current.facility_scada
 scrapy crawl -L ERROR au.wem.current.balancing_summary
+
 scrapy crawl -L ERROR au.nem.current.dispatch_scada
 scrapy crawl -L ERROR au.nem.current.dispatch
 scrapy crawl -L ERROR au.nem.current.dispatch_actual_gen
