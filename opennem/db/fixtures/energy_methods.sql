fc78c82083bcb1d3281f88d78ba07a138cdc6707
Nik Cubrilovic <git@nikcub.me>
2020-10-20 19:36:41 +1100

    FEAT: Update on_energy_sum energy calc sql function

--- opennem/db/fixtures/energy_methods.sql
+++ opennem/db/fixtures/energy_methods.sql
@@ -13,60 +13,91 @@ drop aggregate if exists energy_total (numeric, int);
 drop function if exists agg_power (agg_state point, el numeric, interval_size_minutes int);
 drop function if exists onenergy_finalfunc;
 
-create function agg_power(agg_state point, el numeric, interval_size_minutes int)
-returns point
+drop aggregate if exists on_energy_sum (numeric, text);
+drop function if exists agg_power (agg_state numeric[], el numeric, text);
+drop function if exists onenergy_finalfunc (agg_state numeric[]);
+
+create function agg_power(agg_state numeric[], el numeric, text)
+returns numeric[]
 immutable
 language plpgsql
 as $$
 declare
   current_sum numeric := 0.0;
   num_elements int := 0;
+  intterval_size int := 0;
   power_hour float := 0;
   n float := 10;
-  w int := 0;
+
+  new_el text[];
 begin
- 	num_elements = agg_state[1] + 1;
 
- 	IF num_elements = 1 then
-		w := 1;
-	elsif num_elements = (n + 1) then
-		w := 1;
-	elsif (num_elements % 2) = 1 then
-		w := 2;
-	else
-		w := 4;
-	END IF;
+	agg_state := agg_state || el;
 
-	power_hour := el / (60 / interval_size_minutes);
- 	current_sum := agg_state[0] + (w * power_hour);
-
- 	return point(current_sum, num_elements);
+	return agg_state;
 end;
 $$;
 
-create function onenergy_finalfunc(agg_state point)
+create function onenergy_finalfunc(agg_state numeric[])
 returns float8
 immutable
 strict
 language plpgsql
 as $$
 declare
-  result_int numeric = 0.0;
+  result_int numeric;
+  divis numeric;
+  bucket_size_minutes int := 0;
+  m numeric;
+  cur_index int := 1;
+  current_sum numeric := 0;
+  power_interval numeric;
+  num_intervals int;
   a float := 0;
   b float := 1;
   n float := 10;
+  w int := 0;
   h float := (b - a) / n;
 begin
-	result_int := h * agg_state[0] / 3 * 10;
+	bucket_size_minutes := extract(epoch FROM '1 day'::interval) / 60;
+
+	num_intervals := array_length(agg_state, 1);
+
+	FOREACH m IN ARRAY agg_state
+ 	LOOP
+		IF cur_index = 1 then
+			w := 1;
+		elsif cur_index = (n + 1) then
+			w := 1;
+		elsif (cur_index % 2) = 1 then
+			w := 2;
+		else
+			w := 4;
+		END IF;
+
+		cur_index := cur_index + 1;
+
+		divis := (60 / (bucket_size_minutes / num_intervals));
+
+		if m > 0 and divis > 0 then
+			power_interval = m / divis;
+		else
+			power_interval = 0;
+		end if;
+
+ 		current_sum = current_sum + (w * power_interval);
+	END LOOP;
+
+	result_int := h * current_sum / 6 * 10;
 
   	return round(result_int, 2);
 end;
 $$;
 
-create aggregate on_energy_sum (numeric, int)
+create aggregate on_energy_sum (numeric, bucket_size text)
 (
     sfunc = agg_power,
-    stype = point,
+    stype = numeric[],
     finalfunc = onenergy_finalfunc,
-    initcond = '(0,0)'
+    initcond = '{}'
 );
