03010ef6ea80cd26148b9a7d07a3f7a81dbc53e5
Nik Cubrilovic <git@nikcub.me>
2020-10-21 17:35:14 +1100

    FEAT: support variable interval sizes in on_energy_sum and new sum method

--- opennem/db/fixtures/energy_methods.sql
+++ opennem/db/fixtures/energy_methods.sql
@@ -1,95 +1,68 @@
--- OpenNEM energy aggregate functions
---
--- Postgres functions and aggregate for calculate energy
--- any feedback improvements please contribute
+drop aggregate if exists on_energy_sum (numeric, text) cascade;
 
+drop type if exists tenergy cascade;
+CREATE TYPE tenergy AS (
+    items       numeric[],
+    bucket_size_minutes    int
+);
 
--- Example query used with timescaledb buckets:
---
--- @NOTE https://gist.github.com/nc9/9679b9f025bb360910bb3cc49173818d
+drop function if exists agg_power(tenergy, numeric, text);
 
-
-drop aggregate if exists energy_total (numeric, int);
-drop function if exists agg_power (agg_state point, el numeric, interval_size_minutes int);
-drop function if exists onenergy_finalfunc;
-
-drop aggregate if exists on_energy_sum (numeric, text);
-drop function if exists agg_power (agg_state numeric[], el numeric, text);
-drop function if exists onenergy_finalfunc (agg_state numeric[]);
-
-create function agg_power(agg_state numeric[], el numeric, text)
-returns numeric[]
+create function agg_power(agg_state tenergy, el numeric, bucket_interval text)
+returns tenergy
 immutable
 language plpgsql
 as $$
 begin
-
-	agg_state := agg_state || el;
+	agg_state.bucket_size_minutes := extract(epoch FROM bucket_interval::interval) / 60;
+	agg_state.items := agg_state.items || el;
 
 	return agg_state;
 end;
 $$;
 
-create function onenergy_finalfunc(agg_state numeric[])
-returns float8
+drop function if exists onenergy_finalfunc (agg_state tenergy) cascade;
+
+create function onenergy_finalfunc(agg_state tenergy)
+returns numeric
 immutable
-strict
 language plpgsql
 as $$
 declare
   result_int numeric;
-  divis numeric;
-  bucket_size_minutes int := 0;
-  m numeric;
-  cur_index int := 1;
-  current_sum numeric := 0;
-  power_interval numeric;
   num_intervals int;
-  a float := 0;
-  b float := 1;
-  n float := 10;
-  w int := 0;
-  h float := (b - a) / n;
+  poly text = '';
+  poly_array text[] = '{}';
+  interval_size_minutes int;
+  y int := 0;
+  m int;
 begin
-	bucket_size_minutes := extract(epoch FROM '1 day'::interval) / 60;
+	num_intervals := array_length(agg_state.items, 1);
+	interval_size_minutes := agg_state.bucket_size_minutes / num_intervals;
 
-	num_intervals := array_length(agg_state, 1);
+	poly_array = poly_array || format('%s %s', 0, 0);
 
-	FOREACH m IN ARRAY agg_state
- 	LOOP
-		IF cur_index = 1 then
-			w := 1;
-		elsif cur_index = (n + 1) then
-			w := 1;
-		elsif (cur_index % 2) = 1 then
-			w := 2;
-		else
-			w := 4;
-		END IF;
-
-		cur_index := cur_index + 1;
-
-		divis := (60 / (bucket_size_minutes / num_intervals));
-
-		if m > 0 and divis > 0 then
-			power_interval = m / divis;
-		else
-			power_interval = 0;
-		end if;
-
- 		current_sum = current_sum + (w * power_interval);
+	FOREACH m IN ARRAY agg_state.items
+	LOOP
+		poly_array = poly_array || format('%s %s', y, m);
+		y := y + interval_size_minutes;
 	END LOOP;
 
-	result_int := h * current_sum / 6 * 10;
+	poly_array = poly_array || format('%s %s', agg_state.bucket_size_minutes, 0);
+	poly_array = poly_array || format('%s %s', 0, 0);
 
-  	return round(result_int, 2);
+	poly := format('POLYGON((%s))', array_to_string(poly_array, ', '));
+
+	result_int := (ST_AREA(poly::geometry) / 60)::numeric;
+
+  	return result_int;
 end;
 $$;
 
-create aggregate on_energy_sum (numeric)
+create aggregate on_energy_sum (numeric, bucket_size text)
 (
     sfunc = agg_power,
-    stype = numeric[],
+    stype = tenergy,
     finalfunc = onenergy_finalfunc,
-    initcond = '{}'
+    initcond = '({}, 0)'
 );
