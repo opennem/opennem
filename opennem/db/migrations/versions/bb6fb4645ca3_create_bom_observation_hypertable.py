7558a85b84c8c5f689d26b71bafffeacc121fd8d
Nik Cubrilovic <git@nikcub.me>
2020-10-21 14:21:04 +1100

    FEAT: create bom_observation hypertable

---
+++ opennem/db/migrations/versions/bb6fb4645ca3_create_bom_observation_hypertable.py
@@ -0,0 +1,24 @@
+"""Create bom_observation hypertable
+
+Revision ID: bb6fb4645ca3
+Revises: 4ee58f229572
+Create Date: 2020-10-21 14:20:02.009394
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "bb6fb4645ca3"
+down_revision = "4ee58f229572"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        "select create_hypertable('bom_observation', 'observation_time', if_not_exists => TRUE, migrate_data => TRUE, chunk_time_interval => INTERVAL '1 week')"
+    )
+
+
+def downgrade():
+    pass
