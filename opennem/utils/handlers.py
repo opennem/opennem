665e21caad936c364f63e720924c9bfedd15832c
Nik Cubrilovic <git@nikcub.me>
2020-10-25 17:32:23 +1100

    FEAT: ZIP_LIMIT in zip processing

--- opennem/utils/handlers.py
+++ opennem/utils/handlers.py
@@ -74,15 +74,24 @@ def chain_streams(streams, buffer_size=io.DEFAULT_BUFFER_SIZE):
     return io.BufferedReader(ChainStream(), buffer_size=buffer_size)
 
 
+ZIP_LIMIT = 0
+
+
 def _handle_zip(file_obj, mode):
     with zipfile.ZipFile(file_obj) as zf:
         if len(zf.namelist()) == 1:
             return zf.open(zf.namelist()[0])
 
         c = []
+        stream_count = 0
+
         for filename in zf.namelist():
             if filename.endswith(".zip"):
+                if ZIP_LIMIT > 0 and stream_count >= ZIP_LIMIT:
+                    continue
+
                 c.append(_handle_zip(zf.open(filename), mode))
+                stream_count += 1
             else:
                 c.append(zf.open(filename))
 
