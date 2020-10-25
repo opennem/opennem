4c61d821db8e3ff9d5e95efcafd5023382bbb60e
Nik Cubrilovic <git@nikcub.me>
2020-10-25 17:39:22 +1100

    FEAT: run all every 6 hours

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -12,7 +12,7 @@ def schedule_wem_export_task():
     wem_run_all()
 
 
-@huey.periodic_task(crontab(hour="*/12"))
+@huey.periodic_task(crontab(hour="*/6"))
 def schedule_wem_export_all():
     wem_export_all()
 
