30025f15e7decebe1f9bcdbb0125872e260d3e30
Nik Cubrilovic <git@nikcub.me>
2020-10-28 03:25:24 +1100

    FIX: sentry from settings env

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -1,13 +1,15 @@
 import sentry_sdk
-from pkg_resources import get_distribution
 
 from opennem.settings import settings
+
+# from pkg_resources import get_distribution
+
 
 # VERSION = get_distribution("opennem").version
 # __version__ = VERSION
 
 
-if settings.env != "development":
+if settings.debug is False:
     sentry_sdk.init(
         settings.sentry_url, traces_sample_rate=1.0, environment=settings.env
     )
