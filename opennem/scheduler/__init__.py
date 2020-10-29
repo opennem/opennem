acac556dda61f2e3e9d70469be01e1700c48cbbf
Nik Cubrilovic <git@nikcub.me>
2020-10-29 12:29:42 +1100

    FEAT: break out most recently daily grouping in schedule and run more often

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -2,9 +2,9 @@ from huey import RedisHuey, crontab
 
 from opennem.api.export.tasks import (
     au_export_power,
+    wem_export_daily,
     wem_export_monthly,
     wem_export_power,
-    wem_export_years,
 )
 from opennem.exporter.geojson import export_facility_geojson
 from opennem.settings import settings
@@ -17,14 +17,19 @@ def schedule_wem_export_task():
     wem_export_power()
 
 
-@huey.periodic_task(crontab(minute="*/15"))
+@huey.periodic_task(crontab(minute="*/30"))
+def schedule_wem_export_daily_most_recent():
+    wem_export_daily(limit=1)
+
+
+@huey.periodic_task(crontab(hour="*/4"))
+def schedule_wem_export_years():
+    wem_export_daily()
+
+
+@huey.periodic_task(crontab(hour="*/6"))
 def schedule_wem_export_all():
     wem_export_monthly()
-
-
-@huey.periodic_task(crontab(minute="*/15"))
-def schedule_wem_export_years():
-    wem_export_years()
 
 
 @huey.periodic_task(crontab(minute="*/5"))
