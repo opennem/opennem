9c0d9636b2880cabd7e843f37082e086286722c6
Nik Cubrilovic <git@nikcub.me>
2020-11-02 22:18:43 +1100

    FIX: adjust schedule intervals

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -12,7 +12,7 @@ from opennem.settings import settings
 huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
 
 
-@huey.periodic_task(crontab(minute="*/5"))
+@huey.periodic_task(crontab(minute="*/5",))
 def schedule_wem_export_task():
     wem_export_power()
 
@@ -22,17 +22,17 @@ def schedule_wem_export_daily_most_recent():
     wem_export_daily(limit=1)
 
 
-@huey.periodic_task(crontab(hour="*/2"))
+@huey.periodic_task(crontab(hour="*/12"))
 def schedule_wem_export_years():
     wem_export_daily()
 
 
-@huey.periodic_task(crontab(hour="*/2"))
+@huey.periodic_task(crontab(hour="*/12"))
 def schedule_wem_export_all():
     wem_export_monthly()
 
 
-@huey.periodic_task(crontab(minute="*/5"))
+@huey.periodic_task(crontab(minute="*/30"))
 def schedule_export_geojson():
     export_facility_geojson()
 
