d8662e001df5c629f2a9a891d43c761275f47ed9
Nik Cubrilovic <git@nikcub.me>
2020-11-10 22:46:52 +1100

    FIX: Return none on no bom station for network_region if no match (fail gracefully)

--- opennem/core/network_region_bom_station_map.py
+++ opennem/core/network_region_bom_station_map.py
@@ -2,12 +2,14 @@
     Map network regions to BOM stations
 """
 
+from typing import Optional
+
 from opennem.core.loader import load_data
 
 NETWORK_REGION_BOM_STATION = load_data("network_region_bom_station_map.json")
 
 
-def get_network_region_weather_station(region_code: str) -> str:
+def get_network_region_weather_station(region_code: str) -> Optional[str]:
     """
         Maps a network region to a weather station id
 
@@ -18,6 +20,4 @@ def get_network_region_weather_station(region_code: str) -> str:
     if region_code in NETWORK_REGION_BOM_STATION:
         return NETWORK_REGION_BOM_STATION[region_code]
 
-    raise Exception(
-        "Weather station not found for region: {}".format(region_code)
-    )
+    return None
