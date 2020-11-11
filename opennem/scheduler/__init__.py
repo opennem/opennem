f737c3c19ee963a76b4325d8a245d4f857ee5bba
Nik Cubrilovic <git@nikcub.me>
2020-11-12 01:42:23 +1100

    FEAT: tune tasks

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -18,41 +18,48 @@ huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
 # export tasks
 
 
-@huey.periodic_task(crontab(minute="*/5",))
-def schedule_wem_export_task():
+@huey.periodic_task(crontab(minute="*/5"))
+@huey.lock_task("schedule_live_tasks")
+def schedule_live_tasks():
     wem_export_power()
     export_power(PriorityType.live)
 
 
 @huey.periodic_task(crontab(hour="*/1"))
-def schedule_wem_export_daily_most_recent():
+@huey.lock_task("schedule_hourly_tasks")
+def schedule_hourly_tasks():
     wem_export_daily(limit=1)
     export_energy(PriorityType.daily, latest=True)
 
 
 @huey.periodic_task(crontab(hour="*/12"))
-def schedule_wem_export_years():
+@huey.lock_task("schedule_daily_tasks")
+def schedule_daily_tasks():
     wem_export_daily()
     export_energy(PriorityType.daily)
 
 
 @huey.periodic_task(crontab(hour="*/12"))
+@huey.lock_task("schedule_wem_export_all")
 def schedule_wem_export_all():
     wem_export_monthly()
     export_energy(PriorityType.monthly)
 
 
 @huey.periodic_task(crontab(minute="*/30"))
+@huey.lock_task("schedule_export_geojson")
 def schedule_export_geojson():
     export_facility_geojson()
 
 
 @huey.periodic_task(crontab(minute="*/30"))
-def schedule_export_au():
+@huey.lock_task("schedule_export_metadata")
+def schedule_export_metadata():
     export_metadata()
 
 
 # monitoring tasks
 @huey.periodic_task(crontab(minute="*/5"))
+@huey.lock_task("monitor_wem_interval")
 def monitor_wem_interval():
     get_wem_interval_delay()
