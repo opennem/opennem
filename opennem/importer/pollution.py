048c26b22ccf3d4867669686a5bf8d461f61920f
Nik Cubrilovic <git@nikcub.me>
2020-11-13 03:08:36 +1100

    FEAT: pollution values importer

---
+++ opennem/importer/pollution.py
@@ -0,0 +1,36 @@
+from opennem.db import SessionLocal
+from opennem.db.models.opennem import Facility
+from opennem.importer.mms import mms_import
+
+
+def import_pollution():
+    mms = mms_import()
+
+    facility_poll_map = {}
+
+    for station in mms:
+        for facility in station.facilities:
+            if facility.emissions_factor_co2:
+                facility_poll_map[
+                    facility.code
+                ] = facility.emissions_factor_co2
+
+    session = SessionLocal()
+
+    for fac_code, pol_value in facility_poll_map.items():
+        facility = (
+            session.query(Facility).filter_by(code=fac_code).one_or_none()
+        )
+
+        if not facility:
+            print("could not find facility: {}".format(fac_code))
+            continue
+
+        facility.emissions_factor_co2 = pol_value
+        session.add(facility)
+
+    session.commit()
+
+
+if __name__ == "__main__":
+    import_pollution()
