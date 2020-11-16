f579f44622990089dd457257b29b7cb472305fc1
Nik Cubrilovic <git@nikcub.me>
2020-11-16 17:11:40 +1100

    FIX: settings type not Optional

--- opennem/settings/__init__.py
+++ opennem/settings/__init__.py
@@ -29,10 +29,6 @@ MODULE_DIR = os.path.dirname(__file__)
 
 ENV = os.getenv("ENV", default="development")
 
-# settings default loader
-
-settings: Optional[OpennemSettings] = None
-
 _env_file = load_env_file(ENV)
 
 # @TODO add logging
@@ -40,4 +36,4 @@ if _env_file:
     load_dotenv(dotenv_path=_env_file, override=True)
 
 # @NOTE don't use pydantics env file support since it doesn't support multiple
-settings = OpennemSettings()
+settings: OpennemSettings = OpennemSettings()
