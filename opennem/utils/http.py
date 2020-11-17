23b5b5b8bdca0c2ba6d7ebd7eb338bf13c1562f5
Nik Cubrilovic <git@nikcub.me>
2020-11-17 16:07:17 +1100

    FEAT: http module with global session, retries strategy, failures and timeout settings

---
+++ opennem/utils/http.py
@@ -0,0 +1,40 @@
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+DEFAULT_TIMEOUT = 30
+
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = DEFAULT_TIMEOUT
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
+
+
+retry_strategy = Retry(
+    total=3,
+    backoff_factor=1,
+    status_forcelist=[403, 429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"],
+)
+
+
+http = requests.Session()
+
+adapter_timeout = TimeoutHTTPAdapter()
+http.mount("https://", adapter_timeout)
+http.mount("http://", adapter_timeout)
+
+
+adapter_retry = HTTPAdapter(max_retries=retry_strategy)
+http.mount("https://", adapter_retry)
+http.mount("http://", adapter_retry)
