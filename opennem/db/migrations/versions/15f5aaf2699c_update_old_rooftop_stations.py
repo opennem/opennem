6f32c5aab2edfbf54bfe88ad20431cf195d12746
Nik Cubrilovic <git@nikcub.me>
2020-11-17 15:22:18 +1100

    FEAT: rooftop database migration

---
+++ opennem/db/migrations/versions/15f5aaf2699c_update_old_rooftop_stations.py
@@ -0,0 +1,104 @@
+# pylint: disable=no-member
+"""
+Update old rooftop stations
+
+Revision ID: 15f5aaf2699c
+Revises: 0716d393ff9d
+Create Date: 2020-11-17 15:05:40.037459
+
+"""
+import geoalchemy2
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "15f5aaf2699c"
+down_revision = "0716d393ff9d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_TAS' where code = 'ROOFTOP_TAS'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_QLD' where code = 'ROOFTOP_QLD'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_NSW' where code = 'ROOFTOP_NSW'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_VIC' where code = 'ROOFTOP_VIC'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_SA' where code = 'ROOFTOP_SA'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_WA' where code = 'ROOFTOP_WA'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_APVI_NT' where code = 'ROOFTOP_NT'"
+    )
+
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_TAS' where facility_code = 'ROOFTOP_TAS'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_QLD' where facility_code = 'ROOFTOP_QLD'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_NSW' where facility_code = 'ROOFTOP_NSW'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_VIC' where facility_code = 'ROOFTOP_VIC'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_SA' where facility_code = 'ROOFTOP_SA'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_WA' where facility_code = 'ROOFTOP_WA'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_APVI_NT' where facility_code = 'ROOFTOP_NT'"
+    )
+
+
+def downgrade():
+    op.execute(
+        "update facility set code = 'ROOFTOP_TAS' where code = 'ROOFTOP_APVI_TAS'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_QLD' where code = 'ROOFTOP_APVI_QLD'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_VIC' where code = 'ROOFTOP_APVI_VIC'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_SA' where code = 'ROOFTOP_APVI_SA'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_WA' where code = 'ROOFTOP_APVI_WA'"
+    )
+    op.execute(
+        "update facility set code = 'ROOFTOP_NT' where code = 'ROOFTOP_APVI_NT'"
+    )
+
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_TAS' where facility_code = 'ROOFTOP_APVI_TAS'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_QLD' where facility_code = 'ROOFTOP_APVI_QLD'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_VIC' where facility_code = 'ROOFTOP_APVI_VIC'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_SA' where facility_code = 'ROOFTOP_APVI_SA'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_WA' where facility_code = 'ROOFTOP_APVI_WA'"
+    )
+    op.execute(
+        "update facility_scada set facility_code = 'ROOFTOP_NT' where facility_code = 'ROOFTOP_APVI_NT'"
+    )
