1cb8bd685e03eaef1a7a964c8a38df9569b11dad
Nik Cubrilovic <git@nikcub.me>
2020-11-17 15:04:38 +1100

    FEAT: update rooftop stations with AEMO support

--- opennem/importer/apvi.py
+++ opennem/importer/apvi.py
@@ -8,14 +8,23 @@ logger = logging.getLogger(__name__)
 
 ROOFTOP_CODE = "ROOFTOP"
 
+
 STATE_NETWORK_REGION_MAP = [
+    # APVI
     {"state": "NSW", "network": "APVI", "network_region": "NSW1"},
     {"state": "QLD", "network": "APVI", "network_region": "QLD1"},
     {"state": "VIC", "network": "APVI", "network_region": "VIC1"},
     {"state": "TAS", "network": "APVI", "network_region": "TAS1"},
     {"state": "SA", "network": "APVI", "network_region": "SA1"},
     {"state": "NT", "network": "APVI", "network_region": "NT1"},
-    {"state": "WA", "network": "WEM", "network_region": "WEM"},
+    {"state": "WA", "network": "APVI", "network_region": "WEM"},
+    # AEMO
+    {"state": "NSW", "network": "NEM", "network_region": "NSW1"},
+    {"state": "QLD", "network": "NEM", "network_region": "QLD1"},
+    {"state": "VIC", "network": "NEM", "network_region": "VIC1"},
+    {"state": "TAS", "network": "NEM", "network_region": "TAS1"},
+    {"state": "SA", "network": "NEM", "network_region": "SA1"},
+    {"state": "NT", "network": "NEM", "network_region": "NT1"},
 ]
 
 
@@ -24,8 +33,10 @@ def rooftop_facilities():
     session = SessionLocal()
 
     for state_map in STATE_NETWORK_REGION_MAP:
-        state_rooftop_code = "{}_{}".format(
-            ROOFTOP_CODE, state_map["state"].upper()
+        state_rooftop_code = "{}_{}_{}".format(
+            ROOFTOP_CODE,
+            state_map["network"].upper(),
+            state_map["state"].upper(),
         )
 
         rooftop_station = (
@@ -45,8 +56,8 @@ def rooftop_facilities():
             state_map["state"]
         )
         rooftop_station.approved = True
-        rooftop_station.approved_by = "opennem.importer.apvi"
-        rooftop_station.created_by = "opennem.importer.apvi"
+        rooftop_station.approved_by = "opennem.importer.rooftop"
+        rooftop_station.created_by = "opennem.importer.rooftop"
 
         if not rooftop_station.location:
             rooftop_station.location = Location(state=state_map["state"])
@@ -67,8 +78,8 @@ def rooftop_facilities():
         rooftop_fac.status_id = "operating"
         rooftop_fac.active = True
         rooftop_fac.dispatch_type = DispatchType.GENERATOR
-        rooftop_fac.approved_by = "opennem.importer.apvi"
-        rooftop_fac.created_by = "opennem.importer.apvi"
+        rooftop_fac.approved_by = "opennem.importer.rooftop"
+        rooftop_fac.created_by = "opennem.importer.rooftop"
 
         rooftop_station.facilities.append(rooftop_fac)
         session.add(rooftop_fac)
