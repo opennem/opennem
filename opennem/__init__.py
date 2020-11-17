0a83e2a51179645908440fb1fbca50f822e20903
Nik Cubrilovic <git@nikcub.me>
2020-11-17 14:16:05 +1100

    FEAT: sentry sqlalchemy and redis plugins

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -1,16 +1,13 @@
 import sentry_sdk
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from opennem.settings import settings
 
-# from pkg_resources import get_distribution
-
-
-# VERSION = get_distribution("opennem").version
-# __version__ = VERSION
-
-
 if settings.debug is False:
     sentry_sdk.init(
-        settings.sentry_url, traces_sample_rate=1.0, environment=settings.env
+        settings.sentry_url,
+        traces_sample_rate=1.0,
+        environment=settings.env,
+        integrations=[RedisIntegration(), SqlalchemyIntegration()],
     )
-
