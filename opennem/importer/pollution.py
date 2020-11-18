7cce3256a7403971e6823090ea4c6542f195a085
Nik Cubrilovic <git@nikcub.me>
2020-11-18 15:23:02 +1100

    FEAT: wem pollution importer from CER

--- opennem/importer/pollution.py
+++ opennem/importer/pollution.py
@@ -1,11 +1,16 @@
 import csv
+import logging
 
+from opennem.core.normalizers import station_name_cleaner
 from opennem.db import SessionLocal
-from opennem.db.models.opennem import Facility
+from opennem.db.models.opennem import Facility, Station
 from opennem.importer.mms import mms_import
 from opennem.utils.http import http
+from opennem.utils.mime import decode_bytes
 
 CER_CSV = "http://www.cleanenergyregulator.gov.au/DocumentAssets/Documents/Greenhouse%20and%20energy%20information%20for%20designated%20generation%20facilities%202018-19%20.csv"
+
+logger = logging.getLogger(__name__)
 
 
 def import_pollution_mms():
@@ -46,7 +51,41 @@ def import_pollution_mms():
 
 
 def import_pollution_cer():
-    pass
+    session = SessionLocal()
+
+    r = http.get(CER_CSV)
+
+    if not r.ok:
+        logger.error("response failed: {}".format(CER_CSV))
+        return False
+
+    csv_content = decode_bytes(r.content).splitlines()
+    csvreader = csv.DictReader(csv_content)
+
+    swis_records = list(
+        filter(
+            lambda r: r["Grid"] == "SWIS"
+            and r["Emission Intensity (t CO2-e/ MWh)"] != "0",
+            csvreader,
+        )
+    )
+
+    for rec in swis_records:
+        station_name = station_name_cleaner(rec["Facility Name"])
+
+        lookup = (
+            session.query(Station).filter(Station.name == station_name).all()
+        )
+
+        if lookup:
+            station = lookup.pop()
+            print(
+                "Found {} with code {} and {} facilities".format(
+                    station.name, station.code, len(station.facilities)
+                )
+            )
+        else:
+            print("Could not find {}".format(station_name))
 
 
 if __name__ == "__main__":
