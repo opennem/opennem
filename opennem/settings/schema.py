64fd6842bbc4c558015cb186a9051102b893efc0
Nik Cubrilovic <git@nikcub.me>
2020-11-23 18:44:11 +1100

    FIX: lint settings

--- opennem/settings/schema.py
+++ opennem/settings/schema.py
@@ -8,10 +8,12 @@ from scrapy.utils.project import get_project_settings
 
 class OpennemSettings(BaseSettings):
     env: str = "development"
+
     db_url: PostgresDsn = (
         "postgresql://opennem:opennem@127.0.0.1:15433/opennem"
     )
     cache_url: RedisDsn = "redis://127.0.0.1"
+
     sentry_url: Optional[str]
 
     google_places_api_key: Optional[str] = None
