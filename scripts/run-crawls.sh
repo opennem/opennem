ac658445528ee8b9b3f83f93f03a084210cd8ca0
Nik Cubrilovic <git@nikcub.me>
2020-11-26 01:51:48 +1100

    FEAT: update run-crawls script for dev

--- scripts/run-crawls.sh
+++ scripts/run-crawls.sh
@@ -1,13 +1,21 @@
 set -euxo pipefail
 
+# apvi
 scrapy crawl -L ERROR au.apvi.current.data
 
-# stats data
+# stats data wem
 scrapy crawl -L ERROR au.wem.live.facility_intervals
 scrapy crawl -L ERROR au.wem.live.pulse
 scrapy crawl -L ERROR au.wem.current.facility_scada
 scrapy crawl -L ERROR au.wem.current.balancing_summary
 
+# nem
 scrapy crawl -L ERROR au.nem.current.dispatch_scada
 scrapy crawl -L ERROR au.nem.current.dispatch
 scrapy crawl -L ERROR au.nem.current.dispatch_actual_gen
+scrapy crawl -L ERROR au.nem.current.rooftop
+
+
+# bom
+scrapy crawl bom.all
+scrapy crawl bom.capitals
