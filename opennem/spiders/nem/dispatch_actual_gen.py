cb9201da190d3f1b7311f860b6140c8b7a98b464
Nik Cubrilovic <git@nikcub.me>
2020-11-26 01:37:49 +1100

    FEAT: dispatch actual gen uses bulk inserters

--- opennem/spiders/nem/dispatch_actual_gen.py
+++ opennem/spiders/nem/dispatch_actual_gen.py
@@ -1,3 +1,5 @@
+from opennem.pipelines.bulk_insert import BulkInsertPipeline
+from opennem.pipelines.csv import RecordsToCSVPipeline
 from opennem.pipelines.nem.opennem import NemwebUnitScadaOpenNEMStorePipeline
 from opennem.spiders.nemweb import NemwebSpider
 
@@ -8,7 +10,13 @@ class NemwebDispatchActualGenLatest(NemwebSpider):
 
     limit = 1
 
-    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
 
 class NemwebDispatchActualGenCurrent(NemwebSpider):
@@ -18,7 +26,13 @@ class NemwebDispatchActualGenCurrent(NemwebSpider):
     limit = 0
     skip = 1
 
-    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
 
 class NemwebDispatchActualGenArchive(NemwebSpider):
@@ -26,7 +40,13 @@ class NemwebDispatchActualGenArchive(NemwebSpider):
     start_url = "http://www.nemweb.com.au/Reports/ARCHIVE/Next_Day_Actual_Gen/"
     limit = 0
 
-    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
     # Archives tend to contain large zips of embedded zips so throttle
     # to limit memory use
