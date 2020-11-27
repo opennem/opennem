ca5bae9a1c30404509be0352c60f97deed455763
Nik Cubrilovic <git@nikcub.me>
2020-11-27 23:43:48 +1100

    FEAT: get_interval_by_size method

--- opennem/core/time.py
+++ opennem/core/time.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from opennem.core.loader import load_data
 from opennem.schema.time import TimeInterval, TimePeriod
@@ -29,6 +29,21 @@ PERIODS = load_periods()
 PERIODS_SUPPORTED = [i.period_human for i in PERIODS]
 
 
+def get_interval_by_size(interval_size: int) -> Optional[TimeInterval]:
+    """
+    Get an interval by size
+
+    """
+    interval_lookup = list(
+        filter(lambda x: x.interval == interval_size, INTERVALS)
+    )
+
+    if interval_lookup:
+        return interval_lookup.pop()
+
+    raise Exception("Invalid interval {} not mapped".format(interval_size))
+
+
 def get_interval(interval_human: str) -> TimeInterval:
     interval_lookup = list(
         filter(lambda x: x.interval_human == interval_human, INTERVALS)
@@ -49,4 +64,3 @@ def get_period(period_human: str) -> TimePeriod:
         return period_lookup.pop()
 
     raise Exception("Invalid interval {} not mapped".format(period_human))
-
