9b75455ed4335082baeb814371c663333320bd7a
Nik Cubrilovic <git@nikcub.me>
2020-11-29 17:10:22 +1100

    FIX: write_to_local parameter types

--- opennem/exporter/local.py
+++ opennem/exporter/local.py
@@ -1,14 +1,18 @@
 import io
 import logging
+from io import BytesIO, StringIO
 from os import makedirs
 from pathlib import Path
+from typing import Union
 
 from opennem.settings import settings
 
 logger = logging.getLogger(__name__)
 
 
-def write_to_local(file_path, data) -> int:
+def write_to_local(
+    file_path: str, data: Union[StringIO, bytes, BytesIO, str]
+) -> int:
     save_folder = settings.static_folder_path
 
     save_file_path = Path(save_folder) / file_path.lstrip("/")
