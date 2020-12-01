12a0a27a158b0afb4f0df9bddf3b4ab23baa8e8c
Nik Cubrilovic <git@nikcub.me>
2020-12-02 02:03:33 +1100

    FEAT: new database index on AEST/AWST on facility_scada on day truncs

---
+++ opennem/db/migrations/versions/d7c22e0add08_indexes_for_day_truncs_in_nem_and_wem_.py
@@ -0,0 +1,38 @@
+# pylint: disable=no-member
+"""
+Indexes for day truncs in NEM and WEM timezones
+
+Revision ID: d7c22e0add08
+Revises: df7308f58ee2
+Create Date: 2020-12-02 02:00:14.521669
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "d7c22e0add08"
+down_revision = "df7308f58ee2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_index(
+        "idx_facility_scada_trading_interval_perth_day",
+        "facility_scada",
+        [sa.text("date_trunc('day', trading_interval AT TIME ZONE 'AWST')")],
+        postgresql_concurrently=True,
+    )
+
+    op.create_index(
+        "idx_facility_scada_trading_interval_sydney_day",
+        "facility_scada",
+        [sa.text("date_trunc('day', trading_interval AT TIME ZONE 'AEST')")],
+        postgresql_concurrently=True,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index("idx_facility_scada_trading_interval_perth_day")
+    op.drop_index("idx_facility_scada_trading_interval_sydney_day")
