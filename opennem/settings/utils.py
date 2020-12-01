fc88f0789e5c194bc3270a5da79e5f9b52681832
Nik Cubrilovic <git@nikcub.me>
2020-12-01 18:10:24 +1100

    FIX: multiple env file support, check if exist

--- opennem/settings/utils.py
+++ opennem/settings/utils.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 from urllib.parse import urlparse
 
 BASE_ENV_FILE_NAME = ".env"
@@ -8,9 +8,9 @@ BASE_ENV_FILE_NAME = ".env"
 
 def replace_database_in_url(db_url, db_name):
     """
-        replaces the database portion of a database connection URL with db_name
+    replaces the database portion of a database connection URL with db_name
 
-        @param db_name database name to replace with
+    @param db_name database name to replace with
     """
 
     db_url_parsed = urlparse(db_url)
@@ -19,16 +19,30 @@ def replace_database_in_url(db_url, db_name):
     return db_url_parsed.geturl()
 
 
-def load_env_file(environment: str) -> Optional[Path]:
-    environment_suffix = environment.strip().lower()
+# cache cwd
+_cwd = Path(os.getcwd())
 
-    environment_env_file = "{}.{}".format(
-        BASE_ENV_FILE_NAME, environment_suffix
-    )
 
-    env_path = Path(os.getcwd()) / Path(environment_env_file)
+def _is_env_file(env_file_path: str) -> bool:
+    """
+    Checks if env file exists in current working directory
+    """
+    return (_cwd / Path(env_file_path)).is_file()
 
-    if env_path.is_file():
-        return env_path
 
-    return None
+def load_env_file(environment: Optional[str]) -> List[str]:
+    """
+    Returns a list of env files to load
+    """
+    env_files = [BASE_ENV_FILE_NAME]
+
+    if environment:
+        environment_suffix = environment.strip().lower()
+
+        env_files.append(
+            "{}.{}".format(BASE_ENV_FILE_NAME, environment_suffix)
+        )
+
+    env_files_exist = [str(i) for i in env_files if _is_env_file(i)]
+
+    return env_files_exist
