1c02ddfd27816dd2ef03761fd01f101125b8236a
Nik Cubrilovic <git@nikcub.me>
2020-12-01 17:08:52 +1100

    FIX: check for sentry in opennem main

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -4,7 +4,7 @@ from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 from opennem.settings import settings
 
-if settings.debug is False:
+if settings.sentry_enabled and settings.debug is False:
     sentry_sdk.init(
         settings.sentry_url,
         traces_sample_rate=1.0,
