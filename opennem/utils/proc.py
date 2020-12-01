4e4dd2d4d71141bd5dc6dd4259775ddb2b2f4771
Nik Cubrilovic <git@nikcub.me>
2020-12-01 18:34:51 +1100

    FEAT: detect running as scrapy and proc module

---
+++ opennem/utils/proc.py
@@ -0,0 +1,19 @@
+import sys
+from pathlib import Path
+
+
+def running_as_scrapy() -> bool:
+    """
+    Detect if we're running as scrapy
+    """
+
+    # running as lib
+    if not sys.argv or len(sys.argv) < 1:
+        return False
+
+    _cl = sys.argv[0]
+
+    if Path(_cl).name in ["scrapy"]:
+        return True
+
+    return False
