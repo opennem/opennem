03ae62c719bb1439477f6c71c828325978997adb
Nik Cubrilovic <git@nikcub.me>
2020-12-01 17:21:21 +1100

    FEAT: add python version check in core module

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -1,3 +1,11 @@
+import sys
+
+# Check minimum required Python version
+if sys.version_info < (3, 7):
+    print("OpenNEM %s requires Python 3.7 or greater")
+    sys.exit(1)
+
+
 # setup sentry
 from opennem.settings import settings
 from opennem.utils.sentry import setup_sentry
