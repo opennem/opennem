7755d9d2a3bf5831b0770fdefb64edf4ee54c69b
Nik Cubrilovic <git@nikcub.me>
2020-12-01 19:13:52 +1100

    FEAT: aemo_wem_live_interval

--- opennem/monitors/aemo_intervals.py
+++ opennem/monitors/aemo_intervals.py
@@ -1,64 +1,34 @@
 import logging
 from datetime import datetime, timedelta
 
-import requests
-
-from opennem.core.networks import network_from_network_code
 from opennem.monitors.aemo_wem_live_intervals import (
     get_aemo_wem_live_facility_intervals_recent_date,
 )
 from opennem.notifications.slack import slack_message
+from opennem.schema.network import NetworkWEM
 from opennem.settings import settings
-from opennem.utils.dates import chop_microseconds, parse_date
+from opennem.utils.dates import chop_microseconds
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("opennem.monitors.aemo")
 
 
-def get_wem_interval_delay() -> bool:
-    resp = requests.get(
-        "https://data.opennem.org.au/v3/stats/au/WEM/power/7d.json"
-    )
-
-    if resp.status_code != 200:
-        logger.error("Error retrieving wem power")
-        return False
-
-    resp_json = resp.json()
-
-    if "data" not in resp_json:
-        logger.error("Error retrieving wem power: malformed response")
-        return False
-
-    data = resp_json["data"]
-    network_code = resp_json["code"]
-
-    network = network_from_network_code(network_code)
-
-    fueltech_data = data.pop()
-
-    history_end_date = fueltech_data["history"]["last"]
-
-    history_date = parse_date(history_end_date, dayfirst=False)
-
-    if not history_date:
-        logger.error("Could not read history date for wem intervals")
-        return False
+def aemo_wem_live_interval() -> bool:
+    """
+    Monitors the delay from the AEMO live scada data on the portal
+    """
+    network = NetworkWEM
 
     now_date = datetime.now().astimezone(network.get_timezone())
 
     live_most_recent = get_aemo_wem_live_facility_intervals_recent_date()
 
-    time_delta = chop_microseconds(now_date - history_date)
-
-    if time_delta > timedelta(hours=3):
-        slack_message(
-            "*WARNING*: WEM live interval delay on {} currently: {}\n\nFeed time: {}\nCurrent time: {}\n".format(
-                settings.env, time_delta, history_date, now_date
-            )
-        )
-
     live_delta = chop_microseconds(now_date - live_most_recent)
 
+    logger.debug(
+        "Live time: {},  delay: {}".format(live_most_recent, live_delta)
+    )
+
+    # @TODO move the minutes into settings
     if live_delta > timedelta(minutes=90):
         slack_message(
             "*WARNING*: AEMO Live intervals for WEM on {} curently delayed by {}\n\nAEMO feed most recent: {}".format(
@@ -71,4 +41,4 @@ def get_wem_interval_delay() -> bool:
 
 
 if __name__ == "__main__":
-    delay = get_wem_interval_delay()
+    delay = aemo_wem_live_interval()
