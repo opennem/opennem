d666463f6e52b3bbf590b0499565bdc95ba038b1
Nik Cubrilovic <git@nikcub.me>
2020-12-03 14:23:52 +1100

    FEAT: dispatch_is latest spider + bulk insert pipeline to all dispatch_is

--- opennem/spiders/nem/dispatch_is.py
+++ opennem/spiders/nem/dispatch_is.py
@@ -1,5 +1,24 @@
+from opennem.pipelines.bulk_insert import BulkInsertPipeline
+from opennem.pipelines.csv import RecordsToCSVPipeline
 from opennem.pipelines.nem.opennem import NemwebUnitScadaOpenNEMStorePipeline
 from opennem.spiders.nemweb import NemwebSpider
+
+
+class NemwebLatestDispatchIS(NemwebSpider):
+    name = "au.nem.latest.dispatch_is"
+    start_url = "http://nemweb.com.au/Reports/Current/DispatchIS_Reports/"
+    limit = 2
+
+    # only get most recent
+    # limit = 1
+
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
 
 class NemwebCurrentDispatchIS(NemwebSpider):
@@ -7,7 +26,13 @@ class NemwebCurrentDispatchIS(NemwebSpider):
     start_url = "http://nemweb.com.au/Reports/Current/DispatchIS_Reports/"
     limit = 0
 
-    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
 
 class NemwebArchiveDispatchIS(NemwebSpider):
@@ -15,7 +40,13 @@ class NemwebArchiveDispatchIS(NemwebSpider):
     start_url = "http://nemweb.com.au/Reports/Archive/DispatchIS_Reports/"
     limit = 0
 
-    pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
 
     # Archives tend to contain large zips of embedded zips so throttle
     # to limit memory use
