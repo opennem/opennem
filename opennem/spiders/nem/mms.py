804e192e99fdd04a090e790010d3758e43c87ab4
Nik Cubrilovic <git@nikcub.me>
2020-12-06 00:57:43 +1100

    FIX: lint errors and remove MMS old spider

--- opennem/spiders/nem/mms.py
+++ opennem/spiders/nem/mms.py
@@ -1,9 +1,6 @@
-import scrapy
-
 from opennem.pipelines.nem import (
     ExtractCSV,
     ReadStringHandle,
-    TableRecordSplitter,
     UnzipSingleFilePipeline,
 )
 from opennem.pipelines.nem.mms import (
@@ -17,16 +14,7 @@ from opennem.pipelines.nem.mms import (
 from opennem.pipelines.nem.mms_dudetailsummary import (
     AEMOMMSDudetailSummaryGrouper,
 )
-from opennem.spiders.dirlisting import DirlistingSpider
 from opennem.spiders.nem import NemSingleMMSSpider
-
-
-class NemArchiveMMSSpider(DirlistingSpider):
-    name = "au.nem.archive.mms"
-
-    start_urls = [
-        "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/2020/MMSDM_2020_06/MMSDM_Historical_Data_SQLLoader/DATA/"
-    ]
 
 
 class NemMMSStationSpider(NemSingleMMSSpider):
@@ -53,7 +41,7 @@ class NemMMSParticipantSpider(NemSingleMMSSpider):
 class NemMMSDudeTailSpider(NemSingleMMSSpider):
     name = "au.nem.mms.dudetail"
 
-    pipelines_extra = set([NemStoreMMSDudetail,])
+    pipelines_extra = set([NemStoreMMSDudetail])
     url = "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/2020/MMSDM_2020_06/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DUDETAIL_202006010000.zip"
 
 
@@ -71,12 +59,12 @@ class NemMMSDudeTailSummarySpider(NemSingleMMSSpider):
         ]
     )
 
-    pipelines_extra = set([NemStoreMMSDudetailSummary,])
+    pipelines_extra = set([NemStoreMMSDudetailSummary])
     url = "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/2020/MMSDM_2020_06/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_DUDETAILSUMMARY_202006010000.zip"
 
 
 class NemMMSStaduAlloc(NemSingleMMSSpider):
     name = "au.nem.mms.statdualloc"
 
-    pipelines_extra = set([NemStoreMMSStatdualloc,])
+    pipelines_extra = set([NemStoreMMSStatdualloc])
     url = "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/2020/MMSDM_2020_06/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_STADUALLOC_202006010000.zip"
