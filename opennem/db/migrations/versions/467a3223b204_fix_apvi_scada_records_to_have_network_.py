5395715d13df60349f12d09a050d4f0633765ce8
Nik Cubrilovic <git@nikcub.me>
2020-12-07 16:37:28 +1100

    FIX: Database migration Fix APVI scada records to have network APVI

---
+++ opennem/db/migrations/versions/467a3223b204_fix_apvi_scada_records_to_have_network_.py
@@ -0,0 +1,26 @@
+# pylint: disable=no-member
+"""
+Fix APVI scada records to have network APVI
+
+Revision ID: 467a3223b204
+Revises: bfb98983b92c
+Create Date: 2020-12-07 16:37:00.632028
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "467a3223b204"
+down_revision = "bfb98983b92c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        "update facility_scada set network_id='APVI' where facility_code like 'ROOFTOP_APVI_%';"
+    )
+
+
+def downgrade() -> None:
+    pass
