2dcfd3d041911999e321439ce0b5952340df0fc0
Nik Cubrilovic <git@nikcub.me>
2020-12-09 15:40:23 +1100

    FEAT: more scrapyd queeu features in script

--- scripts/spider_queue.py
+++ scripts/spider_queue.py
@@ -4,6 +4,7 @@
 
 """
 import logging
+from typing import List
 from urllib.parse import urljoin
 
 import requests
@@ -17,13 +18,72 @@ SCRAPYD_SERVER = "https://scrapyd.dev.opennem.org.au/"
 logger = logging.getLogger("spider_queue")
 
 
-def get_spiders():
-    settings = project.get_project_settings()
-    spider_loader = spiderloader.SpiderLoader.from_settings(settings)
+def get_spiders() -> List[str]:
+    scrapy_settings = project.get_project_settings()
+    spider_loader = spiderloader.SpiderLoader.from_settings(scrapy_settings)
     spiders = spider_loader.list()
     classes = [spider_loader.load(name) for name in spiders]
 
     return [str(i.name) for i in classes]
+
+
+def get_jobs() -> List[str]:
+    job_url = urljoin(SCRAPYD_SERVER, "listjobs.json?project=opennem")
+
+    jobs = requests.get(job_url).json()
+
+    return jobs
+
+
+def cancel_job(id: str) -> bool:
+    cancel_job_url = urljoin(SCRAPYD_SERVER, "cancel.json")
+
+    r = requests.post(cancel_job_url, data={"project": "opennem", "job": id})
+
+    resp = r.json()
+
+    if resp["status"] == "error":
+        logger.error("Error: {}".format(resp["message"]))
+        return False
+
+    logger.info("Cancelled job: {}".format(resp["jobid"]))
+
+    return True
+
+
+def add_spider(spider_name: str) -> bool:
+    schedule_url = urljoin(SCRAPYD_SERVER, "schedule.json")
+
+    r = requests.post(
+        schedule_url, data={"project": "opennem", "spider": spider_name}
+    )
+
+    if not r.ok:
+        logger.error("Error: {}".format(r.status_code))
+        return False
+
+    resp = r.json()
+
+    if resp["status"] == "error":
+        logger.error("Error: {}".format(resp["message"]))
+        return False
+
+    logger.info(
+        "Queued spider {} with task: {}".format(spider_name, resp["jobid"])
+    )
+
+    return True
+
+
+def cancel_pending():
+    jobs = get_jobs()
+
+    pending_jobs = jobs["pending"]
+
+    for job in pending_jobs:
+        job_id = job["id"]
+        logger.info("Cancelling {}".format(job_id))
+        cancel_job(job_id)
 
 
 def add_spider(spider_name: str) -> bool:
@@ -53,13 +113,9 @@ def add_spider(spider_name: str) -> bool:
 def queue_spider():
     spiders = get_spiders()
 
-    # print(spiders)
-
     for s in spiders:
-        if "historic" in s:
+        if "archive" in s and s != "au.mms.archive":
             add_spider(s)
-
-    # add_spider("au.nem.latest.price")
 
 
 if __name__ == "__main__":
