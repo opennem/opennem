47bf55568d8055932bd3979da29f470be7256136
Nik Cubrilovic <git@nikcub.me>
2020-12-10 00:31:06 +1100

    FIX: import Any type

--- opennem/api/tasks/router.py
+++ opennem/api/tasks/router.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 from fastapi import APIRouter, HTTPException, Query
 from sqlalchemy.orm import Session
