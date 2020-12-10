c2661e7e9f7c7c3538972ae8c83eb89b3c78409f
Nik Cubrilovic <git@nikcub.me>
2020-12-10 11:54:51 +1100

    FIX: MMS crawler handle content decode

--- opennem/spiders/aemo/mms.py
+++ opennem/spiders/aemo/mms.py
@@ -1,5 +1,7 @@
+import logging
 from datetime import datetime
 from io import BytesIO
+from typing import Dict, Generator
 from zipfile import ZipFile
 
 import scrapy
@@ -12,13 +14,15 @@ from opennem.utils.dates import get_date_component, month_series
 from opennem.utils.handlers import _handle_zip, chain_streams
 from opennem.utils.mime import decode_bytes, mime_from_content, mime_from_url
 
+logger = logging.getLogger(__name__)
+
 MMS_URL = "http://nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/{year}/MMSDM_{year}_{month}/MMSDM_Historical_Data_SQLLoader/DATA/PUBLIC_DVD_{table}_{year}{month}010000.zip"
 
 
 class MMSArchiveSingleSpider(scrapy.Spider):
     name = "au.mms.archive"
 
-    tables = ["DISPATCH_UNIT_SCADA", "DISPATCH_PRICE"]
+    tables = ["DISPATCH_UNIT_SCADA", "_DISPATCHPRICE"]
 
     pipelines = set(
         [
@@ -29,9 +33,13 @@ class MMSArchiveSingleSpider(scrapy.Spider):
         ]
     )
 
-    def start_requests(self):
+    def start_requests(self) -> Generator[scrapy.Request, None, None]:
         start_month = datetime(2019, 10, 1)
         end_month = datetime(2010, 1, 1)
+
+        start_month = datetime(2016, 8, 1)
+        end_month = datetime(2016, 8, 1)
+
         for date in month_series(start_month, end_month):
             for table in self.tables:
                 url_params = {
@@ -44,7 +52,7 @@ class MMSArchiveSingleSpider(scrapy.Spider):
 
                 yield scrapy.Request(req_url)
 
-    def parse(self, response):
+    def parse(self, response) -> Generator[Dict, None, None]:
         content = None
 
         file_mime = mime_from_content(response.body)
@@ -70,6 +78,10 @@ class MMSArchiveSingleSpider(scrapy.Spider):
                 content = chain_streams(c).read()
         else:
             content = response.body.getvalue()
+
+        if not content:
+            logger.info("No content from scrapy request")
+            return None
 
         content = decode_bytes(content)
 
