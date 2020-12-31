d4a0f376592d1e4e5e9accc605a40b3bf5dd9a9a
Nik Cubrilovic <git@nikcub.me>
2020-12-31 12:03:09 +1100

    FEAT: net_flows method update to separate out imports/exports

--- opennem/core/flows.py
+++ opennem/core/flows.py
@@ -1,11 +1,13 @@
 from itertools import groupby
 from operator import attrgetter
-from typing import List
+from typing import Dict, List
 
 from opennem.api.stats.schema import DataQueryResult, RegionFlowResult
 
 
-def net_flows(region: str, data: RegionFlowResult) -> List[DataQueryResult]:
+def net_flows(
+    region: str, data: List[RegionFlowResult]
+) -> Dict[str, List[DataQueryResult]]:
     """
     Calculates net region flows for a region from a RegionFlowResult
     """
@@ -21,14 +23,22 @@ def net_flows(region: str, data: RegionFlowResult) -> List[DataQueryResult]:
                 "exports": 0.0,
             }
 
-        output_set[k] = values
-        print(values)
+        for es in values:
+            if es.flow_from == region:
+                output_set[k]["exports"] += es.generated
+
+            if es.flow_to == region:
+                output_set[k]["imports"] += es.generated
 
     imports_list = []
+    exports_list = []
 
     for interval, data in output_set.items():
         imports_list.append(
             DataQueryResult(interval=interval, result=data["imports"])
         )
+        exports_list.append(
+            DataQueryResult(interval=interval, result=data["exports"])
+        )
 
-    return imports_list
+    return {"imports": imports_list, "exports": exports_list}
