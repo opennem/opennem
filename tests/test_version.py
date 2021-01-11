fb1b51ce965e345e8e9c548ec429c1cc770c197e
Nik Cubrilovic <git@nikcub.me>
2021-01-11 22:40:31 +1100

    FIX: version unit test type cast

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,7 @@ from opennem.utils.version import VersionPart, get_version
 
 
 # @TODO mock this because obviouuuuusly...
-def test_version():
+def test_version() -> None:
     version = get_version(VersionPart.MAJOR)
 
-    assert version == 3
+    assert version == "3"
