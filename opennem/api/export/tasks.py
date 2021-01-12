da44e1247158758edda5b9f721af04e4bb7959c7
Nik Cubrilovic <git@nikcub.me>
2021-01-12 19:36:48 +1100

    FIX: update manual tasks

--- opennem/api/export/tasks.py
+++ opennem/api/export/tasks.py
@@ -285,8 +285,8 @@ def export_metadata() -> bool:
 
 
 if __name__ == "__main__":
-    # export_all_daily()
-    # export_all_monthly()
+    export_all_daily()
+    export_all_monthly()
     export_energy()
     # export_all_daily()
 
