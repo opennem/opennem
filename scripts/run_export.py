5789c8c711c3a0b1aebb83ae03b5b244d707f52b
Nik Cubrilovic <git@nikcub.me>
2021-01-13 18:38:03 +1100

    FIX: run export

--- scripts/run_export.py
+++ scripts/run_export.py
@@ -2,26 +2,14 @@
 from typing import List
 
 from opennem.api.export.controllers import energy_fueltech_daily
-from opennem.api.export.map import (
-    PriorityType,
-    StatExport,
-    StatType,
-    get_export_map,
-)
+from opennem.api.export.map import PriorityType, StatExport, StatType, get_export_map
 from opennem.api.export.tasks import export_energy, export_power
 from opennem.api.export.utils import write_output
 from opennem.api.stats.controllers import get_scada_range
 from opennem.api.time import human_to_period
-from opennem.core.network_region_bom_station_map import (
-    get_network_region_weather_station,
-)
+from opennem.core.network_region_bom_station_map import get_network_region_weather_station
 from opennem.core.networks import network_from_network_code
-from opennem.schema.network import (
-    NetworkAPVI,
-    NetworkAU,
-    NetworkNEM,
-    NetworkWEM,
-)
+from opennem.schema.network import NetworkAPVI, NetworkAU, NetworkNEM, NetworkWEM
 
 
 def run_tests() -> None:
@@ -44,21 +32,21 @@ def run_tests() -> None:
     # for w in wem_all:
     #     print(w.path)
 
-    export = StatExport(
-        stat_type=StatType.power,
-        priority=PriorityType.live,
-        country="au",
-        date_range=scada_range,
-        # network_region="NSW1",
-        network=NetworkAU,
-        networks=[NetworkNEM, NetworkWEM],
-        period=human_to_period("7d"),
-    )
+    # export = StatExport(
+    #     stat_type=StatType.power,
+    #     priority=PriorityType.live,
+    #     country="au",
+    #     date_range=scada_range,
+    #     # network_region="NSW1",
+    #     network=NetworkAU,
+    #     networks=[NetworkNEM, NetworkWEM],
+    #     period=human_to_period("7d"),
+    # )
 
-    export_power(stats=[export])
+    # export_power(stats=[export])
 
-    # network = network_from_network_code("NEM")
-    # networks = None
+    network = network_from_network_code("NEM")
+    networks = None
 
     # stat_set = energy_fueltech_daily(
     #     interval_size="1d",
@@ -97,18 +85,18 @@ def run_tests() -> None:
 
     # export_power(stats=[export])
 
-    # export = StatExport(
-    #     stat_type=StatType.energy,
-    #     priority=PriorityType.monthly,
-    #     country="au",
-    #     date_range=scada_range,
-    #     network=network_schema,
-    #     network_region=None,
-    #     bom_station=bom_station,
-    #     period=human_to_period("all"),
-    # )
+    export = StatExport(
+        stat_type=StatType.energy,
+        priority=PriorityType.monthly,
+        country="au",
+        date_range=scada_range,
+        network=network_schema,
+        network_region="SA1",
+        bom_station=bom_station,
+        period=human_to_period("all"),
+    )
 
-    # export_energy(stats=[export])
+    export_energy(stats=[export])
 
     # power_stats = export_map.resources
 
