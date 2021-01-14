fdb4f8f3fe5b799b99807fdb466d5d7575a75fb6
Nik Cubrilovic <git@nikcub.me>
2021-01-15 01:05:39 +1100

    FEAT: bom import script

--- scripts/bom_import.py
+++ scripts/bom_import.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
     Script to import BOM observations from large tab-del
     CSV files
@@ -6,11 +7,12 @@
 
 import csv
 import logging
+import sys
 from pathlib import Path
 from typing import Dict
 
 import pytz
-from sqlalchemy.dialects.postgresql import insert
+from pydantic.types import FilePath
 
 from opennem.core.normalizers import is_number
 from opennem.db import SessionLocal, get_database_engine
@@ -28,9 +30,7 @@ LIMIT_REC = 0
 
 BOM_BACKUP_FILE = "bom.data"
 BOM_BACKUP_PATH = (
-    Path(__file__).resolve().parent.parent.parent
-    / Path("infra/backups")
-    / Path(BOM_BACKUP_FILE)
+    Path(__file__).resolve().parent.parent.parent / Path("infra/backups") / Path(BOM_BACKUP_FILE)
 )
 
 
@@ -38,9 +38,7 @@ def load_station_timezone_map() -> Dict:
     s = SessionLocal()
     stations = s.query(BomStation).all()
 
-    station_map = {
-        s.code: STATE_TO_TIMEZONE[s.state.upper()] for s in stations
-    }
+    station_map = {s.code: STATE_TO_TIMEZONE[s.state.upper()] for s in stations}
 
     return station_map
 
@@ -49,37 +47,52 @@ STATION_TIMEZONE_MAP = load_station_timezone_map()
 
 
 def parse_record(rec: Dict) -> Dict:
+    """Parse BoM records in database format"""
 
     timezone = pytz.timezone(STATION_TIMEZONE_MAP[rec["station_id"]])
 
-    observation_time = parse_date(
-        rec["observation_time"], is_utc=True, dayfirst=False
-    ).astimezone(timezone)
+    observation_time = parse_date(rec["observation_time"], is_utc=True, dayfirst=False).astimezone(
+        timezone
+    )
 
     return {
         "observation_time": observation_time,
         "station_id": rec["station_id"],
-        "temp_apparent": float(rec["temp_apparent"])
-        if is_number(rec["temp_apparent"])
-        else None,
-        "temp_air": float(rec["temp_air"])
-        if is_number(rec["temp_air"])
-        else None,
-        "press_qnh": float(rec["press_qnh"])
-        if is_number(rec["press_qnh"])
-        else None,
+        "temp_apparent": float(rec["temp_apparent"]) if is_number(rec["temp_apparent"]) else None,
+        "temp_air": float(rec["temp_air"]) if is_number(rec["temp_air"]) else None,
+        "press_qnh": float(rec["press_qnh"]) if is_number(rec["press_qnh"]) else None,
         "wind_dir": rec["wind_dir"],
-        "wind_spd": float(rec["wind_spd"])
-        if is_number(rec["wind_spd"])
-        else None,
+        "wind_spd": float(rec["wind_spd"]) if is_number(rec["wind_spd"]) else None,
         "cloud": rec["cloud"],
         "cloud_type": rec["cloud_type"],
-        "humidity": float(rec["humidity"])
-        if is_number(rec["humidity"])
+        "humidity": float(rec["humidity"]) if is_number(rec["humidity"]) else None,
+        "wind_gust": float(rec["wind_gust"]) if is_number(rec["wind_gust"]) else None,
+    }
+
+
+def parse_record_bom(rec: Dict) -> Dict:
+    """Parse CSV records in BOM format"""
+
+    timezone = pytz.timezone(STATION_TIMEZONE_MAP[rec["station_id"]])
+
+    observation_time = parse_date(rec["observation_time"], is_utc=True, dayfirst=False).astimezone(
+        timezone
+    )
+
+    return {
+        "observation_time": observation_time,
+        "station_id": rec["station_id"],
+        "temp_apparent": float(rec["temp_apparent"].strip())
+        if is_number(rec["temp_apparent"])
         else None,
-        "wind_gust": float(rec["wind_gust"])
-        if is_number(rec["wind_gust"])
-        else None,
+        "temp_air": float(rec["temp_air"].strip()) if is_number(rec["temp_air"]) else None,
+        "press_qnh": None,
+        "wind_dir": None,
+        "wind_spd": None,
+        "cloud": None,
+        "cloud_type": None,
+        "humidity": float(rec["humidity"].strip()) if is_number(rec["humidity"]) else None,
+        "wind_gust": None,
     }
 
 
@@ -132,5 +145,118 @@ def main():
     logger.info("Inserted {} records".format(len(records_to_store)))
 
 
+BOM_FORMAT_FIELDS = [
+    "hm",
+    "station_id",
+    "station_name",
+    "observation_time",
+    "temp_air",
+    "Quality of air temperature",
+    "temp_apparent",
+    "Quality of Wet bulb temperature",
+    "Dew point temperature in degrees C",
+    "Quality of dew point temperature",
+    "Relative humidity in percentage %",
+    "Quality of relative humidity",
+    "humidity",
+    "Quality of vapour pressure",
+    "Saturated vapour pressure in hPa",
+    "Quality of saturated vapour pressure",
+    "AWS Flag",
+    "#",
+]
+
+
+BOM_DB_UPDATE_FIELDS = [
+    "station_id",
+    "observation_time",
+    "temp_air",
+    "temp_apparent",
+    "humidity",
+]
+
+DELIMITER = chr(255)
+
+import re
+
+__bom_date_match = re.compile(r"(\d{4})\,(\d{2}),(\d{2}),(\d{2}),(\d{2})")
+
+
+def bom_pad_stationid(station_id: str) -> str:
+    station_id = station_id.strip()
+
+    if len(station_id) == 5:
+        station_id = f"0{station_id}"
+
+    return station_id
+
+
+def bom_parse_date(line: str) -> str:
+    """BoM has the worst. date. format. ever."""
+    return re.sub(__bom_date_match, r"\1-\2-\3 \4:\5:00", line)
+
+
+def parse_bom(file: FilePath) -> int:
+    num_inserts = 0
+    limit = 0
+    read_count = 0
+    records_to_store = []
+    observation_times = []
+
+    with file.open() as fh:
+        fh.readline()
+
+        csvreader = csv.DictReader(
+            [bom_parse_date(l) for l in fh],
+            delimiter=",",
+            fieldnames=BOM_FORMAT_FIELDS,
+        )
+        for row in csvreader:
+
+            row["station_id"] = bom_pad_stationid(row["station_id"])
+
+            record = parse_record_bom(row)
+
+            ot = record["observation_time"]
+
+            if ot in observation_times:
+                continue
+
+            observation_times.append(ot)
+
+            records_to_store.append(record)
+            read_count += 1
+
+            if limit and limit > 0 and read_count >= limit:
+                break
+
+    sql_query = build_insert_query(BomObservation, BOM_DB_UPDATE_FIELDS)
+
+    conn = get_database_engine().raw_connection()
+    cursor = conn.cursor()
+    csv_content = generate_csv_from_records(
+        BomObservation,
+        records_to_store,
+        column_names=records_to_store[0].keys(),
+    )
+
+    cursor.copy_expert(sql_query, csv_content)
+    conn.commit()
+
+    return num_inserts
+
+
+def cli(files) -> None:
+    for f in files:
+        fp = Path(f)
+
+        if not fp.is_file():
+            raise Exception("Bad file: {}".format(fp))
+
+        print("Parsing: {}".format(fp))
+
+        parse_bom(fp)
+
+
 if __name__ == "__main__":
-    main()
+    cli(sys.argv[1:])
