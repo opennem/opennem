3debcc4f4a0338913f639ce8993174b3521ace67
Nik Cubrilovic <git@nikcub.me>
2021-01-15 14:22:59 +1100

    FEAT: spider queue specify spiders to run as arg

--- scripts/spider_queue.py
+++ scripts/spider_queue.py
@@ -3,9 +3,9 @@
 Srapyd control script
 
 """
-
+import sys
 
 from opennem.utils.scrapyd import job_schedule_all
 
 if __name__ == "__main__":
-    job_schedule_all("latest")
+    job_schedule_all(sys.argv[1])
