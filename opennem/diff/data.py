6c896e1b3515e9e4dd0ece5155ac16c213995828
Nik Cubrilovic <git@nikcub.me>
2021-01-15 17:43:37 +1100

    FIX: weather queries year end on specific years

---
+++ opennem/diff/data.py
@@ -0,0 +1,19 @@
+"""
+OpenNEM Compare Data
+
+Compare JSON outputs between OpenNEM versions to check for consistency
+"""
+
+
+import json
+
+from opennem.utils.http import http
+
+
+def compare_data() -> None:
+
+    return None
+
+
+if __name__ == "__main__":
+    compare_data()
