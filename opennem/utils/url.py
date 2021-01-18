7a74c53948da73e735cdcc607bd31dedf9548499
Nik Cubrilovic <git@nikcub.me>
2021-01-18 19:14:31 +1100

    FIX: remove unused import and add documentation for method

--- opennem/utils/url.py
+++ opennem/utils/url.py
@@ -1,5 +1,5 @@
 import urllib
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urlparse
 
 # Support S3 URI's in urllib
 urllib.parse.uses_netloc.append("s3")
@@ -9,6 +9,8 @@ urllib.parse.uses_relative.append("s3")
 def bucket_to_website(bucket_path: str, to_scheme: str = "https") -> str:
     """
     Converts a bucket path to a website path
+
+    ex. "s3://data.test.org" -> "https://data.test.org"
     """
     bucket_path_parsed = urlparse(bucket_path)
     bucket_path_parsed = bucket_path_parsed._replace(scheme=to_scheme)
