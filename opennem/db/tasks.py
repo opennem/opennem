38fbae34f226be768e972a1533f65c357124cfd5
Nik Cubrilovic <git@nikcub.me>
2021-01-19 16:53:16 +1100

    FEAT: refresh timescale views task

--- opennem/db/tasks.py
+++ opennem/db/tasks.py
@@ -1,19 +1,42 @@
 from opennem.db import get_database_engine
+from opennem.utils.dates import subtract_days
 
-VIEWS = ["mv_facility_energy_hour", "mv_facility_all"]
+TIMESCALE_VIEWS = ["mv_facility_energy_hour"]
+
+MATERIAL_VIEWS = ["mv_facility_all"]
 
 
-def refresh_views() -> None:
+def refresh_timescale_views() -> None:
+    """refresh timescale views"""
+    __query = "CALL refresh_continuous_aggregate('{view}', '{date_from}'::timestamp with time zone, null);"
+
+    engine = get_database_engine()
+
+    date_from = subtract_days()
+
+    with engine.connect() as c:
+        for v in TIMESCALE_VIEWS:
+            query = __query.format(view=v, date_from=date_from.strftime("%Y-%m-%d %H:%M:%S+%Z"))
+            print(query)
+            c.execution_options(isolation_level="AUTOCOMMIT").execute(query)
+
+
+def refresh_material_views() -> None:
     """Refresh material views"""
     __query = "REFRESH MATERIALIZED VIEW CONCURRENTLY {view} with data"
 
     engine = get_database_engine()
 
     with engine.connect() as c:
-        for v in VIEWS:
+        for v in MATERIAL_VIEWS:
             query = __query.format(view=v)
             c.execution_options(isolation_level="AUTOCOMMIT").execute(query)
 
 
+def refresh_views() -> None:
+    refresh_timescale_views()
+    refresh_material_views()
+
+
 if __name__ == "__main__":
-    refresh_views()
+    refresh_timescale_views()
