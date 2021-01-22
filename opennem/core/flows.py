69c313b31455f9942cf295fc04b1f68c7aecec69
Nik Cubrilovic <git@nikcub.me>
2021-01-22 17:22:22 +1100

    FEAT: net_flows_emissions method

--- opennem/core/flows.py
+++ opennem/core/flows.py
@@ -2,7 +2,10 @@ from itertools import groupby
 from operator import attrgetter
 from typing import Dict, List
 
-from opennem.api.stats.schema import DataQueryResult, RegionFlowResult
+from datetime_truncate import truncate
+
+from opennem.api.stats.schema import DataQueryResult, RegionFlowEmissionsResult, RegionFlowResult
+from opennem.schema.time import TimeInterval
 
 
 def net_flows(region: str, data: List[RegionFlowResult]) -> Dict[str, List[DataQueryResult]]:
@@ -35,6 +38,58 @@ def net_flows(region: str, data: List[RegionFlowResult]) -> Dict[str, List[DataQ
 
             if es.flow_to == region:
                 flow_sum += -1 * es.generated
+
+        if flow_sum > 0:
+            flow_type = "exports"
+
+        output_set[k][flow_type] = flow_sum
+
+    imports_list = []
+    exports_list = []
+
+    for interval, data in output_set.items():
+        imports_list.append(
+            DataQueryResult(interval=interval, group_by="imports", result=data["imports"])
+        )
+        exports_list.append(
+            DataQueryResult(interval=interval, group_by="exports", result=data["exports"])
+        )
+
+    return {"imports": imports_list, "exports": exports_list}
+
+
+def net_flows_emissions(
+    interval: TimeInterval, region: str, data: List[RegionFlowEmissionsResult]
+) -> Dict[str, List[DataQueryResult]]:
+    """
+    Calculates net region flow emissions for a region from a RegionFlowResult
+    """
+
+    output_set = {}
+
+    for k, v in groupby(data, lambda x: truncate(x.interval, interval.trunc)):
+        values = list(v)
+
+        if k not in output_set:
+            output_set[k] = {
+                "imports": 0.0,
+                "exports": 0.0,
+            }
+
+        flow_sum = 0.0
+        flow_type = "imports"
+
+        # Sum up
+        for es in values:
+
+            if not es.flow_from:
+                continue
+
+            if es.flow_from == region:
+                flow_sum += es.flow_from_emissions
+
+            if es.flow_to == region:
+                flow_sum += es.flow_to_emissions
 
         if flow_sum > 0:
             flow_type = "exports"
