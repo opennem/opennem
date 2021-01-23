643e5eb7dd7558f11a34e135565c62a22062c357
Nik Cubrilovic <git@nikcub.me>
2021-01-23 16:53:32 +1100

    FEAT: opennem core compat module with translate_id_v3_to_v2

---
+++ opennem/core/compat.py
@@ -0,0 +1,26 @@
+"""
+OpenNEM v2 -> v3 compatability methods
+
+"""
+
+from .fueltechs import map_v2_fueltech
+
+
+def translate_id_v3_to_v2(v3_id: str) -> str:
+    """Translates v3 ids to v2 ids"""
+    idc = id2 = v3_id.split(".")
+
+    if idc[0].lower() == "nem":
+        id2 = id2[1:]
+
+    stat_type = idc[-1]
+
+    if stat_type in ["energy", "power"]:
+        fueltech_old = idc[-2]
+        fueltech = map_v2_fueltech(fueltech_old)
+        id2 = [idc[1], "fuel_tech", fueltech]
+
+    if stat_type in ["temperature"]:
+        id2 = [idc[1], idc[-1]]
+
+    return ".".join([i for i in id2 if i])
