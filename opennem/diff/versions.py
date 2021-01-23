7c5f28a75a7dc221acd4aa7f2826c7f40adbe17c
Nik Cubrilovic <git@nikcub.me>
2021-01-24 07:46:08 +1100

    FIX: update versions script

--- opennem/diff/versions.py
+++ opennem/diff/versions.py
@@ -8,11 +8,13 @@ from typing import Any, Dict, List, Optional
 from urllib.parse import urljoin
 
 from opennem.api.export.map import StatType
-from opennem.core.compat import translate_id_v3_to_v2
+from opennem.core.compat import translate_id_v2_to_v3, translate_id_v3_to_v2
+from opennem.core.fueltechs import map_v3_fueltech
 from opennem.db import SessionLocal
 from opennem.db.models.opennem import NetworkRegion
 from opennem.schema.core import BaseConfig
 from opennem.schema.network import NetworkNEM, NetworkSchema
+from opennem.utils.dates import is_valid_isodate
 from opennem.utils.http import http
 
 logger = logging.getLogger("opennem.diff.versions")
@@ -116,7 +118,9 @@ def get_url_map(regions: List[NetworkRegion]) -> List[DiffComparisonSet]:
     urls = []
 
     for region in regions:
-        a = DiffComparisonSet(stat_type=StatType.power, network_region=region.code, bucket_size="7d")
+        a = DiffComparisonSet(
+            stat_type=StatType.power, network_region=region.code, bucket_size="7d"
+        )
         urls.append(a)
 
         a = DiffComparisonSet(
@@ -147,14 +151,19 @@ def validate_url_map(url_map: List[DiffComparisonSet]) -> bool:
     return success
 
 
-def get_network_regions(network: NetworkSchema) -> List[NetworkRegion]:
+def get_network_regions(
+    network: NetworkSchema, network_region: Optional[str] = None
+) -> List[NetworkRegion]:
     """Return regions for a network"""
     s = SessionLocal()
-    regions = s.query(NetworkRegion).filter_by(network_id=network.code).all()
+    regions = s.query(NetworkRegion).filter_by(network_id=network.code)
 
-    codes = [r.code for r in regions]
+    if network_region:
+        regions = regions.filter_by(code=network_region)
 
-    return codes
+    regions = regions.all()
+
+    return regions
 
 
 def get_id_diff(seriesv2: Dict, seriesv3: Dict) -> Optional[List[str]]:
@@ -180,7 +189,7 @@ def get_data_by_id(id: str, series: List[Dict]) -> Optional[Dict]:
 
 
 def run_diff() -> None:
-    regions = get_network_regions(NetworkNEM)
+    regions = get_network_regions(NetworkNEM, "NSW1")
     statsetmap = get_url_map(regions)
 
     # validate all urls are valid
@@ -189,6 +198,14 @@ def run_diff() -> None:
     for statset in statsetmap:
         v2_power = http.get(statset.urlv2).json()
         v3_power = http.get(statset.urlv3).json()["data"]
+
+        logger.info(
+            "Comparing {} {} for {}".format(
+                statset.stat_type.value, statset.bucket_size, statset.network_region
+            )
+        )
+        logger.info(statset.urlv2)
+        logger.info(statset.urlv3)
 
         if len(v2_power) != len(v3_power):
             logger.info(
@@ -201,6 +218,17 @@ def run_diff() -> None:
                 )
             )
 
+        id2_diffs = sorted([i["id"] for i in v2_power])
+        id3_diffs = sorted([translate_id_v3_to_v2(i["id"]) for i in v3_power])
+
+        logger.info("v2 has ids:")
+        for i in id2_diffs:
+            logger.info("\t{}".format(i))
+
+        logger.info("v3 has ids:")
+        for i in id3_diffs:
+            logger.info("\t{}".format(i))
+
         id_diff = get_id_diff(v2_power, v3_power)
 
         if id_diff:
@@ -210,6 +238,87 @@ def run_diff() -> None:
                         statset.stat_type.value, statset.bucket_size, statset.network_region, i
                     )
                 )
+
+        logger.info("Comparing each id field")
+
+        for i in id2_diffs:
+            logger.info(" = comparing {}".format(i))
+            v2i = get_data_by_id(i, v2_power)
+            v3i = get_data_by_id(translate_id_v2_to_v3(i), v3_power)
+
+            if not v3i:
+                logger.error(" missing in v3: {}".format(translate_id_v2_to_v3(i)))
+                continue
+
+            for v2ikey in v2i:
+                if v2ikey == "id":
+                    continue
+
+                if v2ikey == "fuel_tech":
+                    if v2i[v2ikey] == map_v3_fueltech(v3i[v2ikey]):
+                        logger.info("  * fueltech matches")
+                    else:
+                        logger.error(
+                            "  * fueltech DOESNT MATCH: {} and {}".format(
+                                v2i[v2ikey], map_v3_fueltech(v3i[v2ikey])
+                            )
+                        )
+                    continue
+
+                if v2ikey == "history":
+                    logger.info("  * comparing history:")
+                    hv2 = v2i["history"]
+                    hv3 = v3i["history"]
+
+                    for hv2i in hv2.keys():
+                        if hv2i not in hv3:
+                            logger.error("    - key missing: {}".format(hv2i))
+                            continue
+
+                        if hv2i == "data":
+                            if len(hv2["data"]) != len(hv2["data"]):
+                                logger.error(
+                                    "    - data length mismatch v2 {} v3 {}".format(
+                                        len(hv2["data"]), len(hv2["data"])
+                                    )
+                                )
+                            continue
+
+                        if hv2i in ["start", "end"]:
+                            dt2 = hv2[hv2i]
+                            dt3 = hv3[hv2i]
+
+                            if not is_valid_isodate(dt2, check_timezone=True):
+                                logger.error(
+                                    "    - v2 has invalid datetime format for {}: {}".format(
+                                        hv2i, dt2
+                                    )
+                                )
+
+                            if not is_valid_isodate(dt3, check_timezone=True):
+                                logger.error(
+                                    "    - v3 has invalid datetime format for {}: {}".format(
+                                        hv2i, dt3
+                                    )
+                                )
+
+                    continue
+
+                if v2ikey not in v3i:
+                    logger.error("  * key {} is missing".format(v2ikey))
+                else:
+                    data_matches = v2i[v2ikey] == v3i[v2ikey]
+
+                    if data_matches:
+                        logger.info("  * key {} exists and matches ".format(v2ikey))
+                    else:
+                        logger.error(
+                            "  * key {} DOESNT MATCH. values: v2'{}' and v3'{}'".format(
+                                v2ikey, v2i[v2ikey], v3i[v2ikey]
+                            )
+                        )
+
+        logger.info("=" * 50)
 
 
 if __name__ == "__main__":
