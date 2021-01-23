15fa63d3db088309381d877b2ffb59449bf2a3cd
Nik Cubrilovic <git@nikcub.me>
2021-01-24 06:57:42 +1100

    FIX: get_network_region_weather_station network_Region upper

--- opennem/core/network_region_bom_station_map.py
+++ opennem/core/network_region_bom_station_map.py
@@ -11,9 +11,12 @@ NETWORK_REGION_BOM_STATION = load_data("network_region_bom_station_map.json")
 
 def get_network_region_weather_station(region_code: str) -> Optional[str]:
     """
-        Maps a network region to a weather station id
+    Maps a network region to a weather station id
 
     """
+
+    region_code = region_code.upper()
+
     if not type(NETWORK_REGION_BOM_STATION) is dict:
         raise Exception("NETWORK_REGION_BOM_STATION invalid data type")
 
