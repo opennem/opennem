907655ea8f8f3b97f41f9631bb40235e6cd79f84
Nik Cubrilovic <git@nikcub.me>
2021-01-23 18:48:38 +1100

    FEAT: unit test different regions

--- tests/test_compat_id_translate.py
+++ tests/test_compat_id_translate.py
@@ -17,6 +17,18 @@ from opennem.core.compat import translate_id_v3_to_v2
         ("nem.tas1.fuel_tech.solar_rooftop.energy", "tas1.fuel_tech.rooftop_solar.energy"),
         ("nem.tas1.fuel_tech.imports.emissions", "tas1.fuel_tech.imports.emissions"),
         ("nem.tas1.demand", "tas1.demand"),
+        # same but with region changed
+        ("nem.nsw1.fuel_tech.coal_black.energy", "nsw1.fuel_tech.black_coal.energy"),
+        ("nem.nsw1.fuel_tech.coal_black.power", "nsw1.fuel_tech.black_coal.power"),
+        ("nem.nsw1.fuel_tech.wind.power", "nsw1.fuel_tech.wind.power"),
+        ("nem.nsw1.price", "nsw1.price"),
+        ("nem.nsw1.temperature.094029.temperature", "nsw1.temperature"),
+        ("nem.nsw1.temperature.094029.temperature_mean", "nsw1.temperature_mean"),
+        ("nem.nsw1.fuel_tech.gas_ocgt.market_value", "nsw1.fuel_tech.gas_ocgt.market_value"),
+        ("nem.nsw1.fuel_tech.coal_black.market_value", "nsw1.fuel_tech.black_coal.market_value"),
+        ("nem.nsw1.fuel_tech.solar_rooftop.energy", "nsw1.fuel_tech.rooftop_solar.energy"),
+        ("nem.nsw1.fuel_tech.imports.emissions", "nsw1.fuel_tech.imports.emissions"),
+        ("nem.nsw1.demand", "nsw1.demand"),
     ],
 )
 def test_translate_id_v3_to_v2(id: str, id_expected: str) -> None:
