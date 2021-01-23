63dda3ba17f0addfad793b20b6d76d5d98192021
Nik Cubrilovic <git@nikcub.me>
2021-01-23 16:53:39 +1100

    FEAT: unit tests for translate_id_v3_to_v2

---
+++ tests/test_compat_id_translate.py
@@ -0,0 +1,17 @@
+import pytest
+
+from opennem.core.compat import translate_id_v3_to_v2
+
+
+@pytest.mark.parametrize(
+    ["id", "id_expected"],
+    [
+        ("nem.tas1.fuel_tech.black_coal.power", "tas1.fuel_tech.coal_black"),
+        ("nem.tas1.fuel_tech.wind.power", "tas1.fuel_tech.wind"),
+        ("nem.tas1.price", "tas1.price"),
+        ("nem.tas1.temperature.094029.temperature", "tas1.temperature"),
+    ],
+)
+def test_translate_id_v3_to_v2(id: str, id_expected: str) -> None:
+    id_translated = translate_id_v3_to_v2(id)
+    assert id_translated == id_expected
