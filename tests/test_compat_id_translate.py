fa22b3d4f6bbea19ea0a851276769d238be4e2ab
Nik Cubrilovic <git@nikcub.me>
2021-01-23 18:29:34 +1100

    FIX: update compat unit tests

--- tests/test_compat_id_translate.py
+++ tests/test_compat_id_translate.py
@@ -6,11 +6,13 @@ from opennem.core.compat import translate_id_v3_to_v2
 @pytest.mark.parametrize(
     ["id", "id_expected"],
     [
-        ("nem.tas1.fuel_tech.black_coal.energy", "tas1.fuel_tech.coal_black.energy"),
-        ("nem.tas1.fuel_tech.black_coal.power", "tas1.fuel_tech.coal_black.power"),
+        ("nem.tas1.fuel_tech.coal_black.energy", "tas1.fuel_tech.black_coal.energy"),
+        ("nem.tas1.fuel_tech.coal_black.power", "tas1.fuel_tech.black_coal.power"),
         ("nem.tas1.fuel_tech.wind.power", "tas1.fuel_tech.wind.power"),
         ("nem.tas1.price", "tas1.price"),
         ("nem.tas1.temperature.094029.temperature", "tas1.temperature"),
+        ("nem.tas1.temperature.094029.temperature_mean", "tas1.temperature_mean"),
+        ("nem.tas1.fuel_tech.gas_ocgt.market_value", "tas1.fuel_tech.gas_ocgt.market_value"),
     ],
 )
 def test_translate_id_v3_to_v2(id: str, id_expected: str) -> None:
