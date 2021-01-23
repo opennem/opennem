ac00efde84668493dc1701aa7687430f597399d3
Nik Cubrilovic <git@nikcub.me>
2021-01-24 06:59:41 +1100

    FEAT: translate_id_v2_to_v3 with unit tests

--- opennem/core/compat.py
+++ opennem/core/compat.py
@@ -3,7 +3,10 @@ OpenNEM v2 -> v3 compatability methods
 
 """
 
-from .fueltechs import map_v3_fueltech
+from opennem.core.network_region_bom_station_map import get_network_region_weather_station
+from opennem.schema.network import NetworkNEM
+
+from .fueltechs import map_v2_fueltech, map_v3_fueltech
 
 
 def translate_id_v3_to_v2(v3_id: str) -> str:
@@ -24,3 +27,32 @@ def translate_id_v3_to_v2(v3_id: str) -> str:
         id2 = [idc[1], idc[-1]]
 
     return ".".join([i for i in id2 if i])
+
+
+def translate_id_v2_to_v3(v2_id: str) -> str:
+    network = NetworkNEM
+
+    idc = v2_id.split(".")
+
+    if idc[0] != network.code:
+        idc.insert(0, network.code.lower())
+
+    stat_type = idc[-1]
+    network_region = idc[1]
+
+    if stat_type in ["energy", "power", "market_value", "emissions"]:
+        fueltech_old = idc[-2]
+        fueltech = map_v2_fueltech(fueltech_old)
+        # id2 = [idc[1], "fuel_tech", fueltech, stat_type]
+        idc[-2] = fueltech
+
+    if stat_type in ["temperature", "temperature_mean", "temperature_max"]:
+        idc.insert(2, "temperature")
+        bom_station = get_network_region_weather_station(network_region)
+
+        if not bom_station:
+            bom_station = "000000"
+
+        idc.insert(3, bom_station)
+
+    return ".".join([i for i in idc if i])
--- tests/test_compat_id_translate.py
+++ tests/test_compat_id_translate.py
@@ -1,6 +1,6 @@
 import pytest
 
-from opennem.core.compat import translate_id_v3_to_v2
+from opennem.core.compat import translate_id_v2_to_v3, translate_id_v3_to_v2
 
 
 @pytest.mark.parametrize(
@@ -33,4 +33,37 @@ from opennem.core.compat import translate_id_v3_to_v2
 )
 def test_translate_id_v3_to_v2(id: str, id_expected: str) -> None:
     id_translated = translate_id_v3_to_v2(id)
+    assert id_translated == id_expected
+
+
+@pytest.mark.parametrize(
+    ["id_expected", "id"],
+    [
+        ("nem.tas1.fuel_tech.coal_black.energy", "tas1.fuel_tech.black_coal.energy"),
+        ("nem.tas1.fuel_tech.coal_black.power", "tas1.fuel_tech.black_coal.power"),
+        ("nem.tas1.fuel_tech.wind.power", "tas1.fuel_tech.wind.power"),
+        ("nem.tas1.price", "tas1.price"),
+        ("nem.tas1.temperature.094029.temperature", "tas1.temperature"),
+        ("nem.tas1.temperature.094029.temperature_mean", "tas1.temperature_mean"),
+        ("nem.tas1.fuel_tech.gas_ocgt.market_value", "tas1.fuel_tech.gas_ocgt.market_value"),
+        ("nem.tas1.fuel_tech.coal_black.market_value", "tas1.fuel_tech.black_coal.market_value"),
+        ("nem.tas1.fuel_tech.solar_rooftop.energy", "tas1.fuel_tech.rooftop_solar.energy"),
+        ("nem.tas1.fuel_tech.imports.emissions", "tas1.fuel_tech.imports.emissions"),
+        ("nem.tas1.demand", "tas1.demand"),
+        # same but with region changed
+        ("nem.nsw1.fuel_tech.coal_black.energy", "nsw1.fuel_tech.black_coal.energy"),
+        ("nem.nsw1.fuel_tech.coal_black.power", "nsw1.fuel_tech.black_coal.power"),
+        ("nem.nsw1.fuel_tech.wind.power", "nsw1.fuel_tech.wind.power"),
+        ("nem.nsw1.price", "nsw1.price"),
+        ("nem.nsw1.temperature.066214.temperature", "nsw1.temperature"),
+        ("nem.nsw1.temperature.066214.temperature_mean", "nsw1.temperature_mean"),
+        ("nem.nsw1.fuel_tech.gas_ocgt.market_value", "nsw1.fuel_tech.gas_ocgt.market_value"),
+        ("nem.nsw1.fuel_tech.coal_black.market_value", "nsw1.fuel_tech.black_coal.market_value"),
+        ("nem.nsw1.fuel_tech.solar_rooftop.energy", "nsw1.fuel_tech.rooftop_solar.energy"),
+        ("nem.nsw1.fuel_tech.imports.emissions", "nsw1.fuel_tech.imports.emissions"),
+        ("nem.nsw1.demand", "nsw1.demand"),
+    ],
+)
+def test_translate_id_v3_to_v2(id_expected: str, id: str) -> None:
+    id_translated = translate_id_v2_to_v3(id)
     assert id_translated == id_expected
