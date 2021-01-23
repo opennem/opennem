b447f84ce4b461378173cbd85f436b3609de973e
Nik Cubrilovic <git@nikcub.me>
2021-01-23 12:56:07 +1100

    FIX: adjust schedules

--- opennem/scheduler/db.py
+++ opennem/scheduler/db.py
@@ -28,19 +28,19 @@ huey = PriorityRedisHuey("opennem.scheduler.db", host=redis_host)
 
 
 # database tasks
-@huey.periodic_task(crontab(minute="*/15"))
+@huey.periodic_task(crontab(hour="*/1"))
 @huey.lock_task("db_refresh_ts_views")
 def db_refresh_ts_views() -> None:
     refresh_timescale_views()
 
 
-@huey.periodic_task(crontab(hour="*/1"))
+@huey.periodic_task(crontab(hour="*/6"))
 @huey.lock_task("db_refresh_material_views")
 def db_refresh_material_views() -> None:
     refresh_material_views()
 
 
-@huey.periodic_task(crontab(hour="*/2"), priority=10)
+@huey.periodic_task(crontab(hour="*/6"), priority=10)
 @huey.lock_task("db_refresh_all")
 def db_refresh_all() -> None:
     refresh_timescale_views(True)
