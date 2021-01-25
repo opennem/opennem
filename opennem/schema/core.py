4e467894d9b1d7bc9e7ed29cda9c1f8ee26b2e3b
Nik Cubrilovic <git@nikcub.me>
2021-01-26 01:13:04 +1100

    FEAT: Add PropertyBaseModel to base Schema to serialize properties

--- opennem/schema/core.py
+++ opennem/schema/core.py
@@ -1,7 +1,28 @@
 from pydantic import BaseModel
 
 
-class BaseConfig(BaseModel):
+class PropertyBaseModel(BaseModel):
+    """
+    Workaround for serializing properties with pydantic until
+    https://github.com/samuelcolvin/pydantic/issues/935
+    is solved
+    """
+
+    @classmethod
+    def get_properties(cls):
+        return [
+            prop
+            for prop in dir(cls)
+            if isinstance(getattr(cls, prop), property) and prop not in ("__values__", "fields")
+        ]
+
+    def dict(self, *args, **kwargs) -> "DictStrAny":
+        self.__dict__.update({prop: getattr(self, prop) for prop in self.get_properties()})
+
+        return super().dict(*args, **kwargs)
+
+
+class BaseConfig(PropertyBaseModel):
     class Config:
         orm_mode = True
         anystr_strip_whitespace = True
