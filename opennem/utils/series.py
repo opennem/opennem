6b15d17659f390b85f42361eaf22c42a2968058d
Nik Cubrilovic <git@nikcub.me>
2021-01-28 01:01:28 +1100

    FEAT: series methods

--- opennem/utils/series.py
+++ opennem/utils/series.py
@@ -3,8 +3,10 @@ Utilities to work on series
 
 """
 
+from datetime import datetime
+from math import isclose
 from operator import add
-from typing import List
+from typing import Dict, List, Tuple
 
 
 def add_series(*args) -> List:
@@ -34,13 +36,81 @@ def series_diff_percentage(s1, s2):
     for k in s1d.keys():
         if k in s2d:
             if isinstance(s2d[k], float) and isinstance(s1d[k], float):
-                if s2d[k] != 0:
-                    d[k] = (s2d[k] - s1d[k]) / s2d[k] * 100
-                elif s1d[k] == 0:
-                    d[k] = 0.0
+                if s2d[k] == s1d[k]:
+                    d[k] = 0
+
+                if s1d[k] == 0 and s2d[k] > 0:
+                    d[k] = 100
+
+                if s1d[k] != 0:
+                    try:
+                        d[k] = (abs(s1d[k]) - abs(s2d[k])) / abs(s1d[k]) * 100 * -1
+                    except ZeroDivisionError:
+                        pass
+
             elif isinstance(s1d[k], float):
-                d[k] = -100
+                d[k] = 100
             elif isinstance(s2d[k], float):
                 d[k] = 100
 
     return list(d.keys()), list(d.values())
+
+
+def are_approx_equal(actual: float, desired: float) -> bool:
+    if actual == desired:
+        return True
+
+    if not desired or not actual:
+        return False
+
+    return isclose(actual, desired)
+
+
+def series_are_equal(
+    s1: List[Tuple[datetime, float]],
+    s2: List[Tuple[datetime, float]],
+    absolute_equal: bool = False,
+) -> Dict:
+    s2d = {i[0]: i[1] for i in s2}
+    s1d = {i[0]: i[1] for i in s1}
+    d = dict()
+    for k in s1d.keys():
+        if k in s2d:
+            if absolute_equal:
+                d[k] = s1d[k] == s2d[k]
+            else:
+                d[k] = are_approx_equal(s1d[k], s2d[k])
+
+    return d
+
+
+def series_not_close(
+    s1: List[Tuple[datetime, float]],
+    s2: List[Tuple[datetime, float]],
+) -> Dict:
+    s2d = {i[0]: i[1] for i in s2}
+    s1d = {i[0]: i[1] for i in s1}
+    d = dict()
+    for k in s1d.keys():
+        if k in s2d:
+            if not are_approx_equal(s1d[k], s2d[k]):
+                d[k.isoformat()] = (s1d[k], s2d[k])
+
+    return d
+
+
+def series_trim_to_date(
+    s1: List[Tuple[datetime, float]],
+    s2: List[Tuple[datetime, float]],
+) -> Tuple[List, List]:
+    s2d = {i[0]: i[1] for i in s2}
+    s1d = {i[0]: i[1] for i in s1}
+    d1 = []
+    d2 = []
+    for k in s1d.keys():
+        if k in s2d:
+            if s1d[k] and s2d[k]:
+                d1.append(s1d[k])
+                d2.append(s2d[k])
+
+    return d1, d2
