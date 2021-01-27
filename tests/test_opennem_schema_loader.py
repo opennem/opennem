60a2002ca984dd0bab412867adad1dee087b7e53
Nik Cubrilovic <git@nikcub.me>
2021-01-27 14:32:09 +1100

    FEAT: schema loader unit tests

---
+++ tests/test_opennem_schema_loader.py
@@ -0,0 +1,32 @@
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Dict
+
+from opennem.api.stats.loader import load_statset
+
+
+def get_fixture(name: str) -> Dict:
+
+    f = Path(__file__).parent / "fixtures" / name
+
+    if not f.is_file():
+        raise Exception(f"Fixture {name} not found")
+
+    with f.open() as fh:
+        json_return = json.load(fh)
+
+    return json_return
+
+
+def test_schema_loader() -> None:
+    fixture = get_fixture("power-nsw1.json")
+
+    a = load_statset(fixture)
+
+    print(a.version)
+
+    assert isinstance(a.version, str), "Has a version"
+    assert isinstance(a.created_at, datetime), "Has a created date"
+    assert len(a.data) == 2, "Has data"
+    assert isinstance(a.data[0].history.start, datetime), "Has history and a start date"
