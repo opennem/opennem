e6673890e32ec9bffddab2dfd16a1720151e7be8
Nik Cubrilovic <git@nikcub.me>
2021-01-27 16:39:10 +1100

    FEAT: series_diff

--- opennem/utils/series.py
+++ opennem/utils/series.py
@@ -9,3 +9,13 @@ from typing import List
 
 def add_series(*args) -> List:
     return list(map(add, *args))
+
+
+def series_diff(s1, s2):
+    s2d = {i[0]: i[1] for i in s2}
+    s1d = {i[0]: i[1] for i in s1}
+    d = dict()
+    for k, v in s1d.items():
+        if k in s2d:
+            d[k] = s2d[k] - s1d[k]
+    return list(d.keys()), list(d.values())
