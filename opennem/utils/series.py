4b7896a548b3446ea22c42d5d7151b3eaa954346
Nik Cubrilovic <git@nikcub.me>
2021-01-27 17:06:57 +1100

    FEAT: series_diff_percentage

--- opennem/utils/series.py
+++ opennem/utils/series.py
@@ -15,7 +15,32 @@ def series_diff(s1, s2):
     s2d = {i[0]: i[1] for i in s2}
     s1d = {i[0]: i[1] for i in s1}
     d = dict()
-    for k, v in s1d.items():
+    for k in s1d.keys():
         if k in s2d:
-            d[k] = s2d[k] - s1d[k]
+            if isinstance(s2d[k], float) and isinstance(s1d[k], float):
+                d[k] = s2d[k] - s1d[k]
+            elif isinstance(s1d[k], float):
+                d[k] = -1 * s1d[k]
+            elif isinstance(s2d[k], float):
+                d[k] = s2d[k]
+
+    return list(d.keys()), list(d.values())
+
+
+def series_diff_percentage(s1, s2):
+    s2d = {i[0]: i[1] for i in s2}
+    s1d = {i[0]: i[1] for i in s1}
+    d = dict()
+    for k in s1d.keys():
+        if k in s2d:
+            if isinstance(s2d[k], float) and isinstance(s1d[k], float):
+                if s2d[k] != 0:
+                    d[k] = (s2d[k] - s1d[k]) / s2d[k] * 100
+                elif s1d[k] == 0:
+                    d[k] = 0.0
+            elif isinstance(s1d[k], float):
+                d[k] = -100
+            elif isinstance(s2d[k], float):
+                d[k] = 100
+
     return list(d.keys()), list(d.values())
