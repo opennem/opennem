3ffd5f9b240f45e3f4ce1de065021f288efe1fb4
Nik Cubrilovic <git@nikcub.me>
2021-01-28 20:24:11 +1100

    FIX: Add param types

--- opennem/utils/timezone.py
+++ opennem/utils/timezone.py
@@ -55,7 +55,7 @@ def is_naive(value: datetime) -> bool:
     return value.utcoffset() is None
 
 
-def make_aware(value, timezone=None, is_dst=None):
+def make_aware(value: datetime, timezone=None, is_dst=None) -> datetime:
     """Make a naive datetime.datetime in a given time zone aware."""
 
     if timezone is None:
@@ -67,9 +67,7 @@ def make_aware(value, timezone=None, is_dst=None):
     else:
         # Check that we won't overwrite the timezone of an aware datetime.
         if is_aware(value):
-            raise ValueError(
-                "make_aware expects a naive datetime, got %s" % value
-            )
+            raise ValueError("make_aware expects a naive datetime, got %s" % value)
         # This may be wrong around DST changes!
         return value.replace(tzinfo=timezone)
 
