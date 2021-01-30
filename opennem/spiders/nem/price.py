f63d1b169484484a13dfbbafff2b95b24f742756
Nik Cubrilovic <git@nikcub.me>
2021-01-31 01:05:45 +1100

    FIX: disable spider

--- opennem/spiders/nem/price.py
+++ opennem/spiders/nem/price.py
@@ -3,30 +3,24 @@ from opennem.spiders.nemweb import NemwebSpider
 
 
 class NemwebLatestPriceSpider(NemwebSpider):
-    name = "au.nem.latest.price"
-    start_url = (
-        "http://www.nemweb.com.au/Reports/CURRENT/Dispatchprices_PRE_AP/"
-    )
+    # name = "au.nem.latest.price"
+    start_url = "http://www.nemweb.com.au/Reports/CURRENT/Dispatchprices_PRE_AP/"
     limit = 2
 
     pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
 
 
 class NemwebCurrentPriceSpider(NemwebSpider):
-    name = "au.nem.current.price"
-    start_url = (
-        "http://www.nemweb.com.au/Reports/CURRENT/Dispatchprices_PRE_AP/"
-    )
+    # name = "au.nem.current.price"
+    start_url = "http://www.nemweb.com.au/Reports/CURRENT/Dispatchprices_PRE_AP/"
     limit = 0
 
     pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
 
 
 class NemwebArchivePriceSpider(NemwebSpider):
-    name = "au.nem.archive.price"
-    start_url = (
-        "http://www.nemweb.com.au/Reports/ARCHIVE/Dispatchprices_PRE_AP/"
-    )
+    # name = "au.nem.archive.price"
+    start_url = "http://www.nemweb.com.au/Reports/ARCHIVE/Dispatchprices_PRE_AP/"
     limit = 0
 
     pipelines_extra = set([NemwebUnitScadaOpenNEMStorePipeline])
