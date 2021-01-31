8e934731cff95ff59fe9a6430fccc461164c77d5
Nik Cubrilovic <git@nikcub.me>
2021-01-31 17:19:30 +1100

    FEA:T update client

--- opennem/client/__init__.py
+++ opennem/client/__init__.py
@@ -1,8 +1,10 @@
 """
 OpenNEM API Client
 
-(c) 2020 - Opennem Inc.
+(c) 2021 - Opennem Inc.
 """
+
+from typing import Optional
 
 from opennem.api.stats.schema import (
     DataQueryResult,
@@ -10,6 +12,21 @@ from opennem.api.stats.schema import (
     RegionFlowResult,
     ScadaDateRange,
 )
+from opennem.schema.network import NetworkSchema
+from opennem.settings.schema import OpennemSettings
+from opennem.utils.http import http
+
+
+class OpennemClient:
+
+    _http = None
+
+    def __init__(self, settings: Optional[OpennemSettings] = None) -> None:
+        self._http = http
+        pass
+
+    def get_networks() -> None:
+        pass
 
 
 def main() -> None:
