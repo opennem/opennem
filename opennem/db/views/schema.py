7aed3fd9a85b8a4a010bb1626959f6280057dd99
Nik Cubrilovic <git@nikcub.me>
2021-02-04 11:19:36 +1100

    FEAT: split view schema into module

---
+++ opennem/db/views/schema.py
@@ -0,0 +1,53 @@
+"""
+
+
+"""
+from typing import Optional
+
+from opennem.schema.core import BaseConfig
+
+from .utils import format_offset
+
+
+class ContinuousAggregationPolicy(BaseConfig):
+    """Defines a continuous aggregation policy as used by
+    timescale db. The map will destroy/create these"""
+
+    interval: str
+    start_interval: Optional[str]
+    end_interval: Optional[str]
+
+    @property
+    def schedule_interval(self) -> str:
+        return format_offset(self.interval)
+
+    @property
+    def start_offset(self) -> str:
+        if self.start_interval:
+            return format_offset(self.start_interval)
+
+        return "NULL"
+
+    @property
+    def end_offset(self) -> str:
+        if self.end_interval:
+            return format_offset(self.end_interval)
+
+        return "NULL"
+
+
+class ViewDefinition(BaseConfig):
+    """Defines a view (very useful docstrings!)"""
+
+    # Priority determines order views
+    # are processed in. probably better
+    # done with a depends_on but eyyyy
+    priority: int = 0
+
+    name: str
+
+    filepath: str
+
+    view_content: Optional[str]
+
+    aggregation_policy: Optional[ContinuousAggregationPolicy]
