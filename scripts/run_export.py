1c4acba9f4477d09aa668b9e8d64730066415350
Nik Cubrilovic <git@nikcub.me>
2021-02-04 15:19:00 +1100

    FIX: test energy exp

--- scripts/run_export.py
+++ scripts/run_export.py
@@ -27,14 +27,14 @@ def run_tests() -> None:
 
     export_map = get_export_map()
 
-    # nem_energy = (
-    #     export_map.get_by_network_id("NEM")
-    #     .get_by_stat_type(StatType.energy)
-    #     .get_by_priority(PriorityType.monthly)
-    #     .get_by_network_region("NSW1")
-    # )
+    nem_energy = (
+        export_map.get_by_network_id("NEM")
+        .get_by_stat_type(StatType.energy)
+        .get_by_priority(PriorityType.monthly)
+        .get_by_network_region("NSW1")
+    )
 
-    # export_energy(nem_energy.resources, latest=True)
+    export_energy(nem_energy.resources, latest=True)
 
     nem_power = (
         export_map.get_by_network_id("NEM")
