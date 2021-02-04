e9c516343f6e400979237094839486eedf9e82ed
Nik Cubrilovic <git@nikcub.me>
2021-02-04 17:08:29 +1100

    FIX: remove unused imports

--- opennem/api/stats/controllers.py
+++ opennem/api/stats/controllers.py
@@ -8,14 +8,12 @@ import pytz
 from sqlalchemy.orm import Session
 
 from opennem.core.normalizers import normalize_duid
-from opennem.core.units import get_unit
 from opennem.db import get_database_engine
 from opennem.db.models.opennem import FacilityScada, Station
 from opennem.schema.network import NetworkSchema
 from opennem.schema.time import TimeInterval, TimePeriod
 from opennem.schema.units import UnitDefinition
 from opennem.utils.cache import cache_scada_result
-from opennem.utils.interval import get_human_interval
 from opennem.utils.numbers import cast_trailing_nulls
 from opennem.utils.time import human_to_timedelta
 from opennem.utils.timezone import is_aware, make_aware
