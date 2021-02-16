e6e5f5df51b93aa923d0768400510f9abc3f616f
Nik Cubrilovic <git@nikcub.me>
2021-02-17 03:41:23 +1100

    FIX: New version of energy_sum_compat

--- opennem/core/compat/energy.py
+++ opennem/core/compat/energy.py
@@ -4,7 +4,7 @@ OpenNEM v2 compatible version of energy sums
 Uses an average in 30 minute buckets
 """
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Dict, List, Optional, Union
 
 import pandas as pd
@@ -21,48 +21,10 @@ class ScadaResultCompat(BaseConfig):
     generated: Union[float, int, None]
 
 
-def __trading_energy_generator(df: pd.DataFrame, duids: List[str], sel_date: datetime) -> List:
-    df.sort_index(inplace=True)
-    t_start = datetime(sel_date.year, sel_date.month, sel_date.day, 0, 5)
-
-    ret_list = []
-    # 48 trading intervals in the day
-    # (could be better with groupby function)
-    for TI in range(48):
-        # t_i initial timestamp of trading_interval, t_f = final timestamp of trading interval
-        t_i = t_start + timedelta(0, 1800 * TI)
-        t_f = t_start + timedelta(0, 1800 * (TI + 1))
-
-        bucket_all = df[(df.index >= t_i) & (df.index <= t_f)]
-
-        if bucket_all.empty:
-            continue
-
-        for duid in duids:
-            d_ti = bucket_all[(bucket_all.facility_code == duid)]
-
-            if d_ti.empty or d_ti.size < 7:
-                continue
-
-            ret_list.append(
-                [
-                    d_ti.index[-2].replace(tzinfo=FixedOffset(600)),
-                    duid,
-                    d_ti.network_id.unique()[0],
-                    __trapezium_integration(d_ti.generated, duid, t_i, t_f),
-                ]
-            )
-
-    return ret_list
-
-
-def __trapezium_integration(d_ti: pd.Series, duid: str, t_i: datetime, t_f: datetime) -> float:
-
+def __trapezium_integration_gapfill(d_ti: pd.Series) -> float:
+    """Gapfill version of bucket averages - will fill out"""
     # Fall back on average but warn to check data
     if d_ti.count() <= 3:
-        logger.warn(
-            "For duid {} only {} values in range {} => {}".format(duid, d_ti.count(), t_i, t_f)
-        )
         return 0.5 * d_ti.sum() / d_ti.count()
 
     bucket_middle = d_ti.count() - 2
@@ -78,34 +40,26 @@ def __trapezium_integration(d_ti: pd.Series, duid: str, t_i: datetime, t_f: date
     return bucket_energy
 
 
+def __trapezium_integration(d_ti: pd.Series) -> Optional[float]:
+    """ Energy for a 30 minute bucket """
+    # Fall back on average but warn to check data
+    if d_ti.count() != 7:
+        return None
+
+    weights = d_ti.values * [1, 2, 2, 2, 2, 2, 1]
+
+    weights_sum = weights.sum()
+
+    bucket_energy = 0.5 * weights_sum / 12
+
+    return bucket_energy
+
+
 def energy_sum_compat(gen_series: List[Dict]) -> pd.DataFrame:
+    """Takes the energy sum for a series of raw duid intervals
+    and returns a fresh dataframe to be imported"""
     df = pd.DataFrame(
         gen_series,
-        columns=[
-            "trading_interval",
-            "facility_code",
-            "network_id",
-            "generated",
-        ],
-    )
-    df.trading_interval = pd.to_datetime(df.trading_interval)
-    df.generated = pd.to_numeric(df.generated)
-    df = df.set_index(df.trading_interval)
-
-    sel_date = df.index.min().date()
-    max_date = df.index.max().date()
-
-    return_list: List[Dict] = []
-
-    sel_date = df.trading_interval.min().date()
-    duids = list(df.facility_code.unique())
-
-    while sel_date <= max_date:
-        return_list += __trading_energy_generator(df, duids, sel_date)
-        sel_date = sel_date + timedelta(days=1)
-
-    return_frame = pd.DataFrame(
-        return_list,
         columns=[
             "trading_interval",
             "facility_code",
@@ -114,6 +68,28 @@ def energy_sum_compat(gen_series: List[Dict]) -> pd.DataFrame:
         ],
     )
 
-    return_frame = return_frame.set_index(return_frame.trading_interval)
+    # Clean up types
+    df.trading_interval = pd.to_datetime(df.trading_interval)
+    df.eoi_quantity = pd.to_numeric(df.eoi_quantity)
 
-    return return_frame
+    # Index by datetime
+    df = df.set_index(df.trading_interval)
+
+    # Multigroup by datetime and facility code
+    df = df.groupby([pd.Grouper(freq="30min", offset="5m"), "facility_code"]).eoi_quantity.apply(
+        __trapezium_integration_gapfill
+    )
+
+    # Reset back to a simple frame
+    df = df.reset_index()
+
+    # Shift back 5 minutes to even up into 30 minute bkocks
+    df.trading_interval = df.trading_interval - pd.Timedelta(minutes=5)
+
+    # Add back the timezone for NEM
+    # We use a fixed offset so need to loop
+    df.apply(lambda x: pd.Timestamp(x.trading_interval, tz=FixedOffset(600)), axis=1)
+
+    df["network_id"] = "NEM"
+
+    return df
