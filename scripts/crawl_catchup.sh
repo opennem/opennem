35035a9618542c24df04eedfee136f58322624e7
Nik Cubrilovic <git@nikcub.me>
2021-02-23 05:21:30 +1100

    FIX: Update catchup script

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -1,4 +1,6 @@
-./scripts/spider_queue.py au.nem.current.dispatch_is
-./scripts/spider_queue.py au.nem.current.dispatch_scada
-./scripts/spider_queue.py au.nem.current.rooftop
-./scripts/spider_queue.py au.nem.current.trading_is
+./scripts/spider_queue.py au.nem.day.dispatch_is
+./scripts/spider_queue.py au.nem.day.dispatch_scada
+./scripts/spider_queue.py au.nem.day.rooftop
+./scripts/spider_queue.py au.nem.day.trading_is
+./scripts/spider_queue.py au.nem.latest.dispatch
+./scripts/spider_queue.py au.nem.latest.dispatch_actual_gen
