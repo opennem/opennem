fe35eb6f263cb8b859e326d114ac7b548785488e
Nik Cubrilovic <git@nikcub.me>
2021-02-23 05:03:26 +1100

    FEAT: crawl_catchup script

---
+++ scripts/crawl_catchup.sh
@@ -0,0 +1,4 @@
+./scripts/spider_queue.py au.nem.current.dispatch_is
+./scripts/spider_queue.py au.nem.current.dispatch_scada
+./scripts/spider_queue.py au.nem.current.rooftop
+./scripts/spider_queue.py au.nem.current.trading_is
