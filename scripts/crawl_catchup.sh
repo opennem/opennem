c3165c1dcdd53053a6b3994f0c3b99aa1e61f4fd
Nik Cubrilovic <git@nikcub.me>
2021-02-23 12:31:20 +1100

    FIX: Add archive to script

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -4,3 +4,4 @@
 ./scripts/spider_queue.py au.nem.day.trading_is
 ./scripts/spider_queue.py au.nem.current.dispatch
 ./scripts/spider_queue.py au.nem.current.dispatch_actual_gen
+./scripts/spider_queue.py au.nem.archive.dispatch_actual_gen
