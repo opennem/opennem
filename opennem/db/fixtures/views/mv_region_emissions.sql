99a0447701c3b8bf05227266653af3b0cb305320
Nik Cubrilovic <git@nikcub.me>
2021-03-01 19:46:38 +1100

    FIX: update mv_region_emissions

--- opennem/db/fixtures/views/mv_region_emissions.sql
+++ opennem/db/fixtures/views/mv_region_emissions.sql
@@ -4,7 +4,7 @@ create materialized view mv_region_emissions as select
     f.network_region,
     case
         when sum(f.energy) > 0 then
-            sum(f.emissions) / sum(f.energy)
+            sum(f.emissions) / sum(f.energy) * 0.5
         else 0
     end as emissions_per_mw
 from mv_facility_30m_all f
