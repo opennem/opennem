44a90c963d76f0e883ad43cf22bf620577b8a95c
Nik Cubrilovic <git@nikcub.me>
2021-03-01 19:05:54 +1100

    FEAT: multiprocess worker support fueltech filtering

--- opennem/workers/energy_multiprocess.py
+++ opennem/workers/energy_multiprocess.py
@@ -5,8 +5,11 @@ Multiprocessor for energy workers
 import logging
 import multiprocessing
 from datetime import datetime
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
+import click
+
+from opennem.core.facility.fueltechs import load_fueltechs
 from opennem.diff.versions import CUR_YEAR, get_network_regions
 from opennem.schema.network import NetworkNEM
 from opennem.workers.energy import run_energy_update_archive
@@ -14,16 +17,17 @@ from opennem.workers.energy import run_energy_update_archive
 logger = logging.getLogger("opennem.worker.energy_multi")
 
 CPU_COUNT = multiprocessing.cpu_count()
-WORKER_COUNT = int(CPU_COUNT / 3)
+WORKER_COUNT = 2
 YEAR_EARLIEST = 2020
 CUR_MONTH = datetime.now().month
 
 
-def _build_args_list() -> List[Tuple[int, int, str]]:
+def _build_args_list() -> List[Tuple[int, int, str, str]]:
     args_list = []
 
     network = NetworkNEM
     regions = [i.code for i in get_network_regions(network)]
+    fueltechs = [i for i in load_fueltechs().keys()]
 
     for year in range(CUR_YEAR, YEAR_EARLIEST - 1, -1):
         for month in range(12, 0, -1):
@@ -31,15 +35,16 @@ def _build_args_list() -> List[Tuple[int, int, str]]:
                 if year == CUR_YEAR and month > CUR_MONTH:
                     continue
 
-                args_list.append((year, month, region))
+                for fueltech in fueltechs:
+                    args_list.append((year, month, region, fueltech))
 
     return args_list
 
 
-def _worker_wrap(year, months, region):
+def _worker_wrap(year, months, region, fueltech_id: Optional[str]):
     """Map to named args"""
     return run_energy_update_archive(
-        year=year, months=[months], regions=[region], fueltech_id="exports"
+        year=year, months=[months], regions=[region], fueltech_id=fueltech_id
     )
 
 
