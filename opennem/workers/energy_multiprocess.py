c67f4019da160ce7d9a8ab910ecfea8c258db374
Nik Cubrilovic <git@nikcub.me>
2021-03-01 19:31:46 +1100

    FEAT: cli and defaults around energy multiprocess

--- opennem/workers/energy_multiprocess.py
+++ opennem/workers/energy_multiprocess.py
@@ -22,21 +22,39 @@ YEAR_EARLIEST = 2020
 CUR_MONTH = datetime.now().month
 
 
-def _build_args_list() -> List[Tuple[int, int, str, str]]:
+def _build_args_list(
+    year: Optional[int], region: Optional[str], fueltech: Optional[str]
+) -> List[Tuple[int, int, Optional[str], Optional[str]]]:
     args_list = []
 
     network = NetworkNEM
-    regions = [i.code for i in get_network_regions(network)]
-    fueltechs = [i for i in load_fueltechs().keys()]
 
-    for year in range(CUR_YEAR, YEAR_EARLIEST - 1, -1):
+    regions = [region]
+
+    if not region:
+        regions = [i.code for i in get_network_regions(network)]
+
+    fueltechs = [fueltech]
+
+    if not fueltech:
+        fueltechs = [i for i in load_fueltechs().keys()]
+
+    years = [year]
+
+    if not year:
+        years = [i for i in range(CUR_YEAR, YEAR_EARLIEST - 1, -1)]
+
+    for year in years:
         for month in range(12, 0, -1):
+            if year == CUR_YEAR and month > CUR_MONTH:
+                continue
+
             for region in regions:
                 if year == CUR_YEAR and month > CUR_MONTH:
                     continue
 
-                for fueltech in fueltechs:
-                    args_list.append((year, month, region, fueltech))
+                for ft in fueltechs:
+                    args_list.append((year, month, region, ft))
 
     return args_list
 
@@ -48,13 +66,31 @@ def _worker_wrap(year: int, months: int, region: str, fueltech_id: Optional[str]
     )
 
 
-def energy_process() -> None:
-    args_list = _build_args_list()
+def energy_process(args_list: List, worker_count: Optional[int]) -> None:
     logger.debug("Starting with {} workers".format(WORKER_COUNT))
 
-    with multiprocessing.Pool(processes=WORKER_COUNT) as pool:
+    if not worker_count:
+        worker_count = WORKER_COUNT
+
+    with multiprocessing.Pool(processes=worker_count) as pool:
         pool.starmap(_worker_wrap, args_list)
 
 
+@click.command()
+@click.option("--year", required=False, type=int)
+@click.option("--workers", required=False, type=int)
+@click.option("--fueltech", required=False, type=str)
+@click.option("--region", required=False, type=str)
+def cli(
+    year: Optional[int] = None,
+    fueltech: Optional[str] = None,
+    region: Optional[str] = None,
+    workers: Optional[int] = None,
+) -> None:
+    args_list = _build_args_list(year=year, region=region, fueltech=fueltech)
+    click.echo("Running {} items".format(len(args_list)))
+    energy_process(args_list, worker_count=workers)
+
+
 if __name__ == "__main__":
-    energy_process()
+    cli()
