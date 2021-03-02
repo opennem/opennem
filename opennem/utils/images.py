8d8e72a44518fbf1fef9d499f4ecfd0125c8dcf3
Nik Cubrilovic <git@nikcub.me>
2021-03-03 04:23:27 +1100

    FEAT: image_get_hash

--- opennem/utils/images.py
+++ opennem/utils/images.py
@@ -19,3 +19,15 @@ def img_to_buffer(img: Image) -> memoryview:
     img.save(buf, format="JPEG")
 
     return buf.getbuffer()
+
+
+def image_get_hash(img: Image) -> str:
+    img = img.resize((10, 10), Image.ANTIALIAS)
+    img = img.convert("L")
+
+    pixel_data = list(img.getdata())
+    avg_pixel = sum(pixel_data) / len(pixel_data)
+    bits = "".join(["1" if (px >= avg_pixel) else "0" for px in pixel_data])
+    hex_representation = str(hex(int(bits, 2)))[2:][::-1].upper()
+
+    return hex_representation
