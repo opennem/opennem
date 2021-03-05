ecc31043651583f258dbe475afad693dce5419a8
Nik Cubrilovic <git@nikcub.me>
2021-03-05 13:29:09 +1100

    FEAT: run_export test units

--- scripts/run_export.py
+++ scripts/run_export.py
@@ -3,35 +3,42 @@
 Scratchpad to export JSON's for unit tests + testing
 """
 
-from opennem.api.export.map import PriorityType, StatExport, StatType, get_export_map
+from opennem.api.export.map import PriorityType, StatType, get_export_map
 from opennem.api.export.tasks import export_energy, export_power
 
 
 def run_tests() -> None:
-    # network_schema = network_from_network_code("NEM")
-    # scada_range = get_scada_range(network=network_schema)
-    # bom_station = get_network_region_weather_station("NEM")
-
     export_map = get_export_map()
 
-    # nem_energy = (
-    #     export_map.get_by_network_id("NEM")
-    #     .get_by_stat_type(StatType.energy)
-    #     .get_by_priority(PriorityType.daily)
-    #     .get_by_network_region("NSW1")
-    #     .get_by_years([2020, 2021])
-    # )
-
-    # if len(nem_energy.resources):
-    #     export_energy(nem_energy.resources)
-
-    nem_power = (
-        export_map.get_by_network_id("WEM")
+    power = (
+        export_map.get_by_network_id("NEM")
         .get_by_stat_type(StatType.power)
+        .get_by_network_region("VIC1")
         .get_by_priority(PriorityType.live)
     )
 
-    export_power(nem_power.resources)
+    export_power(power.resources)
+
+    energy_map = (
+        export_map.get_by_network_id("NEM")
+        .get_by_stat_type(StatType.energy)
+        .get_by_priority(PriorityType.daily)
+        .get_by_network_region("NSW1")
+        .get_by_years([2021])
+    )
+
+    if len(energy_map.resources):
+        export_energy(energy_map.resources)
+
+    energy_map = (
+        export_map.get_by_network_id("NEM")
+        .get_by_stat_type(StatType.energy)
+        .get_by_priority(PriorityType.monthly)
+        .get_by_network_region("NSW1")
+    )
+
+    if len(energy_map.resources):
+        export_energy(energy_map.resources)
 
 
 if __name__ == "__main__":
