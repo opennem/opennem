dc93f9bb7ec2dfb3487b128d0483bd1fed195bb8
Nik Cubrilovic <git@nikcub.me>
2021-03-10 18:56:55 +1100

    FIX: return type in schema

--- opennem/schema/core.py
+++ opennem/schema/core.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from pydantic import BaseModel
 
 
@@ -16,7 +18,7 @@ class PropertyBaseModel(BaseModel):
             if isinstance(getattr(cls, prop), property) and prop not in ("__values__", "fields")
         ]
 
-    def dict(self, *args, **kwargs) -> "DictStrAny":
+    def dict(self, *args, **kwargs) -> Dict:
         self.__dict__.update({prop: getattr(self, prop) for prop in self.get_properties()})
 
         return super().dict(*args, **kwargs)
