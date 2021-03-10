f6fb2a4b6064e94de0be32d22035708d84ca2a83
Nik Cubrilovic <git@nikcub.me>
2021-03-10 18:07:40 +1100

    FEAT: osm parser get_osm_geom returns WKT

--- opennem/core/parsers/osm.py
+++ opennem/core/parsers/osm.py
@@ -6,6 +6,9 @@ import logging
 from typing import Dict
 
 import osm2geojson
+from geoalchemy2.elements import WKBElement
+from geoalchemy2.shape import from_shape
+from shapely.geometry import asShape
 
 from opennem.utils.http import http
 
@@ -31,15 +34,38 @@ def get_osm_way(way_id: str) -> Dict:
 
     way_resp_content = way_resp.text
 
-    geojson = osm2geojson.xml2geojson(way_resp_content, filter_used_refs=False, log_level="INFO")
+    geojson_response = osm2geojson.xml2geojson(
+        way_resp_content, filter_used_refs=False, log_level="INFO"
+    )
 
-    if not isinstance(geojson, dict):
+    if not isinstance(geojson_response, dict):
         raise Exception("Did not get a valid server response from OSM API")
 
-    if not "type" in geojson:
+    if "type" not in geojson_response:
         raise Exception("Did not get a valid server response from OSM API")
 
-    if geojson["type"] != "FeatureCollection":
+    if geojson_response["type"] != "FeatureCollection":
         raise Exception("Did not get a valid FeatureCollection from OSM API")
 
-    return geojson
+    if "features" not in geojson_response:
+        raise Exception("GeoJSON has no features")
+
+    return geojson_response
+
+
+def get_osm_geom(way_id: str, srid: int = 4326) -> WKBElement:
+    """ Returns an WKT element from an osm way id """
+    poly = None
+
+    osm_way = get_osm_way(way_id)
+
+    for feature in osm_way["features"]:
+        if feature["geometry"]["type"] == "Polygon":
+            poly = feature["geometry"]
+
+    if not poly:
+        return None
+
+    geom = from_shape(asShape(poly), srid=srid)
+
+    return geom
