64494b9950d1d81a96e4992d2cc5809926c5670e
Nik Cubrilovic <git@nikcub.me>
2021-03-10 22:36:26 +1100

    FEAT: flow set invert unit tests

--- tests/test_core_flows.py
+++ tests/test_core_flows.py
@@ -1,10 +1,28 @@
+import json
+from pathlib import Path
+
 import pytest
 
-from opennem.core.flows import FlowDirection, fueltech_to_flow, generated_flow_station_id
+from opennem.api.stats.schema import OpennemData
+from opennem.core.flows import (
+    FlowDirection,
+    fueltech_to_flow,
+    generated_flow_station_id,
+    invert_flow_set,
+)
 from opennem.schema.network import NetworkNEM, NetworkRegionSchema, NetworkSchema
 
 NSW1 = NetworkRegionSchema(code="NSW1", network_id="NEM")
 VIC1 = NetworkRegionSchema(code="VIC1", network_id="NEM")
+
+
+def get_power_flow_fixture() -> OpennemData:
+    fixture_file = "power_flow_set.json"
+    fixture_path = Path(__file__).parent / "fixtures" / fixture_file
+    with fixture_path.open() as fh:
+        fixture_envelope = json.load(fh)
+
+    return OpennemData(**fixture_envelope)
 
 
 @pytest.mark.parametrize(
@@ -40,3 +58,13 @@ def test_fueltech_to_flow(fueltech_id: str, flow_expected: FlowDirection) -> Non
     flow_direction = fueltech_to_flow(fueltech_id)
 
     assert flow_direction == flow_expected, "Got the expected flow"
+
+
+def test_fixture_invert_flow() -> None:
+    fixture = get_power_flow_fixture()
+    inverted = invert_flow_set(fixture)
+
+    assert "SA1->VIC1" in inverted.id, "id changed correctly"
+    assert inverted.code == "SA1->VIC1", "code changed correctly"
+    assert inverted.history.data[0] == -157.3, "data point 1 inverted"
+    assert inverted.history.data[1] == 144.9, "data point 2 inverted"
