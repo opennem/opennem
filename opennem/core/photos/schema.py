1c3cdb804607f55e87c39814dbbdd5a008b6c037
Nik Cubrilovic <git@nikcub.me>
2021-03-10 13:50:50 +1100

    FEAT: PhotoImportSchema

---
+++ opennem/core/photos/schema.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from opennem.schema.core import BaseConfig
+
+
+class PhotoImportSchema(BaseConfig):
+    """ Defines a schema for photo imports """
+
+    network_id: str
+    station_code: str
+    photo_url: str
+
+    author: Optional[str]
+    author_link: Optional[str]
+    license: Optional[str]
+    license_link: Optional[str]
