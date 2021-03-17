a94b022e54a50cde612f79d23a1f10a19ce6aed5
Nik Cubrilovic <git@nikcub.me>
2021-03-17 13:11:19 +1100

    FIX: Manual energy run order

--- opennem/api/export/tasks.py
+++ opennem/api/export/tasks.py
@@ -527,6 +527,6 @@ def export_metadata() -> bool:
 
 
 if __name__ == "__main__":
-    # export_power(priority=PriorityType.live)
-    # export_energy(latest=True)
-    export_electricitymap()
+    export_power(priority=PriorityType.live)
+    export_energy(latest=True)
+    # export_electricitymap()
