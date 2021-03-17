38e112d5ad416105caa525a52af7c688ed4f1ba5
Nik Cubrilovic <git@nikcub.me>
2021-03-17 23:00:14 +1100

    FIX: View references

--- opennem/db/fixtures/views/mv_region_emissions.sql
+++ opennem/db/fixtures/views/mv_region_emissions.sql
@@ -7,6 +7,6 @@ create materialized view mv_region_emissions as select
             sum(f.emissions) / sum(f.energy) * 0.5
         else 0
     end as emissions_per_mw
-from mv_facility_30m_all f
+from mv_facility_all f
 group by 1, 2, f.network_region
 order by 1 desc;
