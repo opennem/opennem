b01d7ffa7c0050a8e10fdb9272d6538a17f28e7e
Nik Cubrilovic <git@nikcub.me>
2021-03-23 22:49:37 +1100

    FIX: Remove unused import

--- opennem/api/geo/router.py
+++ opennem/api/geo/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.param_functions import Query
-from geojson_pydantic.features import FeatureCollection
 from sqlalchemy.orm.session import Session
 from starlette import status
 
@@ -14,20 +13,18 @@ router = APIRouter()
 
 
 @router.get(
-    "/facilities", name="Facility Geo", response_model=FacilityGeo,
+    "/facilities",
+    name="Facility Geo",
+    response_model=FacilityGeo,
 )
 def geo_facilities_api(
-    only_approved: bool = Query(
-        True, description="Only show approved stations"
-    ),
+    only_approved: bool = Query(True, description="Only show approved stations"),
     session: Session = Depends(get_database_session),
 ) -> FacilityGeo:
     stations = get_stations(session, only_approved=only_approved)
 
     if not stations:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No stations found"
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No stations found")
 
     stations_geo = stations_to_geojson(stations)
 
