0092003b7254a325fb809eae1b4076467fe3751c
Nik Cubrilovic <git@nikcub.me>
2021-03-25 17:38:02 +1100

    FEAT: Update view to dynamically assign timezone from network and both price options

--- opennem/db/fixtures/views/mv_network_fueltech_days.sql
+++ opennem/db/fixtures/views/mv_network_fueltech_days.sql
@@ -1,56 +1,64 @@
 create materialized view mv_network_fueltech_days as
-select
-    date_trunc('day', fs.trading_interval at time zone 'AEST') as trading_day,
-    f.code,
-    f.fueltech_id,
-    f.network_id,
-    f.network_region,
-    f.interconnector,
-    f.interconnector_region_to,
-    max(fs.energy) as energy,
-    case when max(bs.price_dispatch) >= 0  and min(fs.energy) >= 0 then
-        coalesce(
-            max(fs.energy) * max(bs.price_dispatch),
-            0.0
-        )
-    else 0.0
-    end as market_value,
-    case when min(f.emissions_factor_co2) >= 0  and min(fs.energy) >= 0 then
-        coalesce(
-            max(fs.energy) * min(f.emissions_factor_co2),
-            0.0
-        )
-    else 0.0
-    end as emissions
-from (
   select
-      time_bucket('30 minutes', fs.trading_interval) as trading_interval,
-      fs.facility_code,
-      fs.network_id,
-      sum(fs.eoi_quantity) as energy
-  from facility_scada fs
-  where fs.is_forecast is False
+      date_trunc('day', fs.trading_interval at time zone n.timezone_database) as trading_day,
+      f.code,
+      f.fueltech_id,
+      f.network_id,
+      f.network_region,
+      f.interconnector,
+      f.interconnector_region_to,
+      sum(fs.energy) as energy,
+      case when max(bs.price_dispatch) >= 0  and min(fs.energy) >= 0 then
+          coalesce(
+              sum(fs.energy) * max(bs.price_dispatch),
+              0.0
+          )
+      else 0.0
+      end as market_value,
+      case when max(bs.price) >= 0  and min(fs.energy) >= 0 then
+          coalesce(
+              sum(fs.energy) * max(bs.price),
+              0.0
+          )
+      else 0.0
+      end as market_value_rrp,
+      case when min(f.emissions_factor_co2) >= 0  and min(fs.energy) >= 0 then
+          coalesce(
+              sum(fs.energy) * min(f.emissions_factor_co2),
+              0.0
+          )
+      else 0.0
+      end as emissions
+  from (
+    select
+        time_bucket('30 minutes', fs.trading_interval) as trading_interval,
+        fs.facility_code,
+        fs.network_id,
+        sum(fs.eoi_quantity) as energy
+    from facility_scada fs
+    where fs.is_forecast is False
+    group by
+        1, 2, 3
+  ) as fs
+      left join facility f on fs.facility_code = f.code
+      left join balancing_summary bs on
+          bs.trading_interval = fs.trading_interval
+          and bs.network_id=f.network_id
+          and bs.network_region = f.network_region
+        left join network n on f.network_id = n.code
+  where
+      f.fueltech_id is not null
   group by
-      1, 2, 3
-) as fs
-    left join facility f on fs.facility_code = f.code
-    left join balancing_summary bs on
-        bs.trading_interval = fs.trading_interval
-        and bs.network_id=f.network_id
-        and bs.network_region = f.network_region
-where
-    f.fueltech_id is not null
-group by
-    fs.trading_interval,
-    1,
-    f.code,
-    f.fueltech_id,
-    f.network_id,
-    f.network_region,
-    f.interconnector,
-    f.interconnector_region_to
+      1,
+      f.network_id,
+      f.code,
+      f.fueltech_id,
+      f.network_region,
+      f.interconnector,
+      f.interconnector_region_to
 order by 1 desc;
 
+create unique index idx_mv_network_fueltech_days_unique_trading_day_network_id_code on mv_network_fueltech_days (trading_day, network_id, code);
 create index idx_mv_network_fueltech_days_trading_day on mv_network_fueltech_days (trading_day desc);
 create index idx_mv_network_fueltech_days_trading_day_code on mv_network_fueltech_days (trading_day desc, code);
 create index idx_mv_network_fueltech_days_trading_day_fueltech_id on mv_network_fueltech_days (trading_day desc, fueltech_id);
