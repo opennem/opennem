031464fc181a093d55a0e1aa297706e7e3d0f97c
Nik Cubrilovic <git@nikcub.me>
2021-03-25 14:49:17 +1100

    FIX: export all energies in manual task

--- opennem/api/export/tasks.py
+++ opennem/api/export/tasks.py
@@ -531,7 +531,7 @@ def export_metadata() -> bool:
 
 if __name__ == "__main__":
     export_power(priority=PriorityType.live)
-    export_energy(latest=True)
+    export_energy(latest=False)
     export_all_monthly()
     export_all_daily()
     export_electricitymap()
