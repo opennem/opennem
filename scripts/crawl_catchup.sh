d31fba68c8c3514593d6671368c568601ae95b67
Nik Cubrilovic <git@nikcub.me>
2021-03-29 11:10:03 +1100

    FIX: APVI + BOM current in catchup

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -1,3 +1,5 @@
+./scripts/spider_queue.py au.bom.all
+./scripts/spider_queue.py au.apvi.current
 ./scripts/spider_queue.py au.nem.day.dispatch_is
 ./scripts/spider_queue.py au.nem.day.dispatch_scada
 ./scripts/spider_queue.py au.nem.day.rooftop
