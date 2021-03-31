66b7130b65721a12a4014d5be3ff7315ffa52534
Nik Cubrilovic <git@nikcub.me>
2021-03-31 19:13:00 +1100

    FIX: update timezone parsing unit test

--- tests/test_timezones.py
+++ tests/test_timezones.py
@@ -1,21 +1,14 @@
 from datetime import datetime
 
-from pytz import timezone
-
-from opennem.schema.network import NetworkNEM, NetworkWEM
+from opennem.schema.network import NetworkWEM
 from opennem.utils.dates import parse_date
 from opennem.utils.timezone import is_aware
 
 
 class TestTimezones(object):
-    def test_nem_settlementdate_tz(self):
-        subject = parse_date(
-            "2020/10/07 10:15:00", dayfirst=False, network=NetworkWEM
-        )
-        comparator = datetime(
-            2020, 10, 7, 10, 15, 0, tzinfo=NetworkWEM.get_timezone()
-        )
+    def test_wem_settlementdate_tz(self) -> None:
+        subject = parse_date("2020/10/07 10:15:00", dayfirst=False, network=NetworkWEM)
+        comparator = datetime(2020, 10, 7, 10, 15, 0, tzinfo=NetworkWEM.get_timezone())
 
         assert subject == comparator, "Parses date correctly"
         assert is_aware(subject) is True, "Date has timezone info"
-
