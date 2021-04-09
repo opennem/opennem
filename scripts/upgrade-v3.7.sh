74f0ce3f01be3bbce4c1f97e3b01be018298f3c1
Nik Cubrilovic <git@nikcub.me>
2021-04-09 11:25:37 +1000

    META: v3.7 upgrade script

---
+++ scripts/upgrade-v3.7.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+if [ -z "$PYTHONPATH" ]
+then
+  export PYTHONPATH="$PWD:${PYTHONPATH}"
+else
+  export PYTHONPATH="/app"
+fi
+
+if [ -z "$VIRTUAL_ENV" ]
+then
+  echo "Running in $VIRTUAL_ENV"
+else
+  source .venv/bin/activate
+fi
+
+alembic upgrade head
+
+python -m opennem.cli db fixtures
+
+python -m opennem.importer.rooftop
