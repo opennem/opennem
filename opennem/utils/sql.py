2b015eed956c244a996843e6b3625c2ab56ff683
Nik Cubrilovic <git@nikcub.me>
2021-04-10 12:49:51 +1000

    FEAT: Add timescale functions for use in SQLAlchemy ORM

--- opennem/utils/sql.py
+++ opennem/utils/sql.py
@@ -1,7 +1,35 @@
 from typing import List
 
+from sqlalchemy.sql.functions import GenericFunction
+
 from opennem.core.normalizers import normalize_duid
 
 
 def duid_in_case(facility_codes: List[str]) -> str:
     return ",".join(["'{}'".format(i) for i in map(normalize_duid, facility_codes)])
+
+
+class time_bucket(GenericFunction):
+    r"""Implement the ``CUBE`` grouping operation.
+    This function is used as part of the GROUP BY of a statement,
+    e.g. :meth:`_expression.Select.group_by`::
+        stmt = select(
+            func.sum(table.c.value), table.c.col_1, table.c.col_2
+        ).group_by(func.cube(table.c.col_1, table.c.col_2))
+    .. versionadded:: 1.2
+    """
+    _has_args = True
+    inherit_cache = True
+
+
+class time_bucket_gapfill(GenericFunction):
+    r"""Implement the ``CUBE`` grouping operation.
+    This function is used as part of the GROUP BY of a statement,
+    e.g. :meth:`_expression.Select.group_by`::
+        stmt = select(
+            func.sum(table.c.value), table.c.col_1, table.c.col_2
+        ).group_by(func.cube(table.c.col_1, table.c.col_2))
+    .. versionadded:: 1.2
+    """
+    _has_args = True
+    inherit_cache = True
