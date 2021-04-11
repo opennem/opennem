5324d49286f0068a75a125f75fa39af610df1772
Nik Cubrilovic <git@nikcub.me>
2021-04-11 22:13:38 +1000

    FEAT: network regions core module

---
+++ opennem/core/network_regions.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+from opennem.db import SessionLocal
+from opennem.db.models.opennem import NetworkRegion
+from opennem.schema.network import NetworkSchema
+
+
+def get_network_regions(
+    network: NetworkSchema, network_region: Optional[str] = None
+) -> List[NetworkRegion]:
+    """Return regions for a network"""
+    s = SessionLocal()
+    regions = s.query(NetworkRegion).filter_by(network_id=network.code)
+
+    if network_region:
+        regions = regions.filter_by(code=network_region)
+
+    regions = regions.all()
+
+    return regions
