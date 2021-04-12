c898ac838dfa6ed2668d1948a7a3a85202565fcf
Nik Cubrilovic <git@nikcub.me>
2021-04-12 17:07:23 +1000

    FEAT: Rooftop AEMO backfill data + load script

--- scripts/rooftop_fill.py
+++ scripts/rooftop_fill.py
@@ -1,34 +1,107 @@
 #!/usr/bin/env python
+"""
+Fill AEMO_ROOFTOP_BACKFILL network data with APVI derived data from v2
 
-from opennem.db import get_database_engine
 
-__query = """
-insert into facility_scada
-select
-    'opennem.importer.apvi' as created_by,
-    now() as created_at,
-    now() as updated_at,
-    'NEM' as network_id,
-    time_bucket_gapfill('30 minutes', fs.trading_interval) as trading_interval,
-    'ROOFTOP_NEM_' ||  rtrim(f.network_region, '1') as facility_code,
-    avg(fs.generated) as generated,
-    avg(fs.generated) / 2 as eoi_quantity,
-    FALSE as is_forecast,
-    0 as energy_quality_flag
-
-left join facility f on fs.facility_code = f.code
-where
-    fs.trading_interval < '2018-03-06 00:00:00+00' and
-    fs.trading_interval > '2015-03-19 00:00:00+00' and
-    fs.network_id='APVI' and
-    f.network_region != 'WEM'
-group by 5, 4, 3, 2, 1, 6
-order by 6 desc
-on conflict do nothing;
 """
 
-if __name__ == "__main__":
-    engine = get_database_engine()
+import csv
+import logging
+from datetime import date
+from pathlib import Path
+from typing import Optional
 
-    with engine.connect() as c:
-        c.execute(__query)
+from pydantic import validator
+
+from opennem.core.normalizers import clean_float
+from opennem.db import SessionLocal
+from opennem.db.models.opennem import AggregateFacilityDaily
+from opennem.schema.core import BaseConfig
+from opennem.settings import settings
+
+logger = logging.getLogger("opennem.scripts.rooftop_fill")
+
+
+def _get_aemo_backfill_facility_id(network_region: str) -> str:
+    return f"ROOFTOP_AEMO_ROOFTOP_BACKFILL_{network_region[:-1]}".upper()
+
+
+class BackfillRecord(BaseConfig):
+    trading_day: date
+    network_id: str = "AEMO_ROOFTOP_BACKFILL"
+    fueltech_id: str = "solar_rooftop"
+    network_region: str
+    market_value: Optional[float]
+    energy_v2: Optional[float]
+    energy_v3: Optional[float] = None
+
+    _validate_energyv3 = validator("energy_v3", pre=True)(clean_float)
+
+    @property
+    def energy(self) -> float:
+        _value = 0.0
+
+        if self.energy_v2:
+            _value = self.energy_v2
+
+        if self.energy_v3:
+            _value -= self.energy_v3
+
+            logger.debug(
+                "Settings energy to delta: {} {} : {}".format(
+                    self.energy_v2, self.energy_v3, _value
+                )
+            )
+
+        return round(_value * 1000, 0)
+
+    @property
+    def facility_code(self) -> str:
+        return _get_aemo_backfill_facility_id(self.network_region)
+
+
+def import_rooftop_aemo_backfills() -> None:
+    csvrecords = []
+    backfill_file = Path(__file__).parent / "aemo_rooftop_backfill.csv"
+
+    with backfill_file.open() as fh:
+        fieldnames = fh.readline().strip().split(",")
+        csvreader = csv.DictReader(fh, fieldnames=fieldnames)
+        csvrecords = [BackfillRecord(**i) for i in csvreader]
+
+    logger.debug("Loaded {} records".format(len(csvrecords)))
+
+    export_records = [
+        i.dict(exclude={"energy_v2", "energy_v3", "network_region"}) for i in csvrecords
+    ]
+
+    session = SessionLocal()
+
+    records_added = 0
+    records_updated = 0
+
+    for rec in export_records:
+        model = (
+            session.query(AggregateFacilityDaily)
+            .filter_by(network_id="AEMO_ROOFTOP_BACKFILL")
+            .filter_by(trading_day=rec["trading_day"])
+            .filter_by(facility_code=rec["facility_code"])
+        ).first()
+
+        if not model:
+            model = AggregateFacilityDaily(**rec)
+            records_added += 1
+        else:
+            records_updated += 1
+
+        model.energy = rec["energy"]
+        model.market_value = rec["market_value"]
+
+        session.add(model)
+
+    session.commit()
+    logger.debug("Added {} records and updated {}".format(records_added, records_updated))
+
+
+if __name__ == "__main__":
+    import_rooftop_aemo_backfills()
