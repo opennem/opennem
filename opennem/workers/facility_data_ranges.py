d56444ff465444bf62108db862dcb7d2f83c34c2
Nik Cubrilovic <git@nikcub.me>
2021-04-13 22:31:47 +1000

    FEAT: Support single, list of duids in facility seen range

--- opennem/workers/facility_data_ranges.py
+++ opennem/workers/facility_data_ranges.py
@@ -6,47 +6,60 @@ for most recent this is the last active date
 """
 import logging
 from textwrap import dedent
+from typing import List, Optional
 
+from opennem.api.stats.controllers import duid_in_case
 from opennem.db import get_database_engine
 from opennem.notifications.slack import slack_message
 
 logger = logging.getLogger("opennem.workers.facility_data_ranges")
 
 
-def get_update_seen_query(last_seen: bool = True) -> str:
+def get_update_seen_query(
+    last_seen: bool = True,
+    facility_codes: Optional[List[str]] = None,
+) -> str:
     __query = """
     update facility f set
-    {fs}data_first_seen = seen_query.data_first_seen,
+        {fs}data_first_seen = seen_query.data_first_seen,
         data_last_seen = seen_query.data_last_seen
     from (
         select
             f.code as code,
-    {fs}    min(fs.trading_interval) as data_first_seen,
+            {fs}min(fs.trading_interval) as data_first_seen,
             max(fs.trading_interval) as data_last_seen
             from facility_scada fs
         left join facility f on fs.facility_code = f.code
         where fs.generated > 0
+        {facility_codes_query}
         group by 1
     ) as seen_query
     where f.code = seen_query.code;
     """
 
     fs = ""
+    facility_codes_query = ""
 
     if not last_seen:
         fs = "--"
 
-    query = __query.format(fs=fs)
+    if facility_codes:
+        facility_codes_query = f"and f.code in ({duid_in_case(facility_codes)})"
+
+    query = __query.format(fs=fs, facility_codes_query=facility_codes_query)
 
     return dedent(query)
 
 
-def update_facility_seen_range(last_seen: bool = True) -> bool:
+def update_facility_seen_range(
+    last_seen: bool = True,
+    facility_codes: Optional[List[str]] = None,
+) -> bool:
     """Updates last seen and first seen"""
 
     engine = get_database_engine()
 
-    __query = get_update_seen_query(last_seen)
+    __query = get_update_seen_query(last_seen, facility_codes)
 
     with engine.connect() as c:
         logger.debug(__query)
