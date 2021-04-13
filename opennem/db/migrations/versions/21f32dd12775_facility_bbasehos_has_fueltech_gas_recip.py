50367697accfa8b976859335981e23c51a3ef774
Nik Cubrilovic <git@nikcub.me>
2021-04-14 00:28:36 +1000

    FIX: Facility BBASEHOS has fueltech gas_recip

---
+++ opennem/db/migrations/versions/21f32dd12775_facility_bbasehos_has_fueltech_gas_recip.py
@@ -0,0 +1,24 @@
+# pylint: disable=no-member
+"""
+Facility BBASEHOS has fueltech gas_recip
+
+Revision ID: 21f32dd12775
+Revises: 0995118b97ec
+Create Date: 2021-04-14 00:27:38.951016
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "21f32dd12775"
+down_revision = "0995118b97ec"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("update facility set fueltech_id='gas_recip' where code = 'gas_recip';")
+
+
+def downgrade() -> None:
+    pass
