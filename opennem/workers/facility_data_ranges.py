0d86b51bff3e3fdf7d617a9f3b2b4109d1710c7e
Nik Cubrilovic <git@nikcub.me>
2021-04-13 22:41:24 +1000

    FEAT: get_facility_seen_range method

--- opennem/workers/facility_data_ranges.py
+++ opennem/workers/facility_data_ranges.py
@@ -3,14 +3,18 @@ For each facility calculate the first and last data seen dates from facility_sca
 
 The min dates are run less regularly since data is less likely to grow in that direction
 for most recent this is the last active date
+
+@TODO move the utility functions into core/facility use this as only the worker
 """
 import logging
+from datetime import datetime
 from textwrap import dedent
 from typing import List, Optional
 
-from opennem.api.stats.controllers import duid_in_case
 from opennem.db import get_database_engine
 from opennem.notifications.slack import slack_message
+from opennem.schema.core import BaseConfig
+from opennem.utils.sql import duid_in_case
 
 logger = logging.getLogger("opennem.workers.facility_data_ranges")
 
@@ -68,6 +72,45 @@ def update_facility_seen_range(
     slack_message("Ran facility_seen_range")
 
     return True
+
+
+class FacilitySeenRange(BaseConfig):
+    date_min: datetime
+    date_max: datetime
+
+
+def get_facility_seen_range(facility_codes: List[str]) -> FacilitySeenRange:
+    __query = """
+        select
+            min(data_first_seen) as first_seen,
+            max(data_last_seen) as last_seen
+        from facility
+        where code in ({facilities});
+    """
+
+    engine = get_database_engine()
+    result = []
+
+    query = __query.format(facilities=duid_in_case(facility_codes))
+
+    with engine.connect() as c:
+        logger.debug(query)
+        result = list(c.execute(query))
+
+    if not result:
+        raise Exception("Could not get facility seen range: No results")
+
+    _record = result.pop()
+
+    if len(_record) != 2:
+        raise Exception("Coult not get facility seen range: Invalid result or unknown")
+
+    schema = FacilitySeenRange(
+        date_min=_record[0],
+        date_max=_record[1],
+    )
+
+    return schema
 
 
 if __name__ == "__main__":
