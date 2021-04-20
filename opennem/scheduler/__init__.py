b89b5f92843973fca412513556b2dc6f9a5f7fe0
Nik Cubrilovic <git@nikcub.me>
2021-04-20 10:54:19 +1000

    FIX: only run facility_first_seen check once and lock task

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -134,7 +134,8 @@ def monitor_emission_factors() -> None:
 
 
 # worker tasks
-@huey.periodic_task(crontab(hour="23"))
+@huey.periodic_task(crontab(hour="23", minute="1"))
+@huey.lock_task("schedule_facility_first_seen_check")
 def schedule_facility_first_seen_check() -> None:
     """ Check for new DUIDS """
     facility_first_seen_check()
