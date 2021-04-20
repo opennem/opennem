4c5c7d8b02f47028ea982ecea686fb1baad73b38
Nik Cubrilovic <git@nikcub.me>
2021-04-20 21:00:02 +1000

    FIX: parameter and return types in load_fixture

--- opennem/utils/tests.py
+++ opennem/utils/tests.py
@@ -1,12 +1,11 @@
 import json
 import os
+from typing import Any
 
-TEST_FIXTURE_PATH = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), "..", "..", "data")
-)
+TEST_FIXTURE_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "data"))
 
 
-def load_fixture(fixture_name):
+def load_fixture(fixture_name: str) -> Any:
     fixture_path = os.path.join(TEST_FIXTURE_PATH, fixture_name)
 
     if not os.path.isfile(fixture_path):
