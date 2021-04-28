a3be191e72fd32ddb8f7e9a401ce5f5ce134454b
Nik Cubrilovic <git@nikcub.me>
2021-04-28 12:53:38 +1000

    FIX: BoM request headers

--- opennem/spiders/bom/capital_observations.py
+++ opennem/spiders/bom/capital_observations.py
@@ -4,6 +4,12 @@ from opennem.spiders.bom.bom_base import BomJSONObservationSpider
 
 from .utils import get_stations, get_stations_priority
 
+REQUEST_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Cache-Control": "no-cache",
+}
+
 
 class BomCapitalsSpider(BomJSONObservationSpider):
     name = "bom.capitals"
@@ -12,7 +18,9 @@ class BomCapitalsSpider(BomJSONObservationSpider):
         priority_stations = get_stations_priority()
 
         for station in priority_stations:
-            yield scrapy.Request(station.feed_url, meta={"code": station.code})
+            yield scrapy.Request(
+                station.feed_url, meta={"code": station.code}, headers=REQUEST_HEADERS
+            )
 
 
 class BomAllSpider(BomJSONObservationSpider):
@@ -22,4 +30,6 @@ class BomAllSpider(BomJSONObservationSpider):
         station = get_stations()
 
         for station in station:
-            yield scrapy.Request(station.feed_url, meta={"code": station.code})
+            yield scrapy.Request(
+                station.feed_url, meta={"code": station.code}, headers=REQUEST_HEADERS
+            )
