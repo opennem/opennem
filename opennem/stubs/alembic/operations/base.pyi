a31175b7c2e7da7987eb28a0223da3db21d774d5
Nik Cubrilovic <git@nikcub.me>
2021-05-04 04:20:31 +1000

    FEAT: MigrationContext and base Operation type class

--- opennem/stubs/alembic/operations/base.pyi
+++ opennem/stubs/alembic/operations/base.pyi
@@ -1,2 +1,6 @@
-class Operations:
+class MigrationContext:
     pass
+
+class Operations:
+    def __init__(self, migration_context: MigrationContext) -> None: ...
+    def get_context(self) -> MigrationContext: ...
