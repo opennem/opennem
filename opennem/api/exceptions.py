fa29b277e6dc741b76c7a87373f64ac089d0a133
Nik Cubrilovic <git@nikcub.me>
2021-05-12 00:14:26 +1000

    FEAT: OpennemBaseHttpException

--- opennem/api/exceptions.py
+++ opennem/api/exceptions.py
@@ -1,10 +1,18 @@
+from typing import Any
+
 from fastapi import HTTPException
 from starlette import status
+from starlette.responses import Response
 
 
-class ItemNotFound(HTTPException):
+class OpennemBaseHttpException(HTTPException):
+    """ Base OpenNEM Exception """
+
     status_code = status.HTTP_404_NOT_FOUND
 
+    def __init__(self, status_code: int = 404):
+        if not self.status_code:
+            self.status_code = status_code
 
 class InvalidDateRange(HTTPException):
     status_code = status.HTTP_404_NOT_FOUND
