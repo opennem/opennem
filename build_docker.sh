6e425f96b0fd0fc451988446ca5d83602fd109dc
Nik Cubrilovic <git@nikcub.me>
2021-05-15 17:17:36 +1000

    FIX: remove orphans on up script

--- build_docker.sh
+++ build_docker.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 docker build -t opennem/database:dev -f infra/database/Dockerfile .
-docker-compose up -d --force-recreate database
+docker-compose up -d --force-recreate --remove-orphans database
 docker-compose logs -f database
