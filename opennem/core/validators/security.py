101544d2a1d1d273d6447efbc405afe80ccbe36e
Nik Cubrilovic <git@nikcub.me>
2021-05-27 03:42:03 +1000

    FEAT: validate api key now checks string is url safe

--- opennem/core/validators/security.py
+++ opennem/core/validators/security.py
@@ -5,15 +5,21 @@ Functions to validate security fields and schemas
 """
 
 
+from opennem.core.normalizers import string_is_urlsafe
+
+
 def validate_api_key(api_key: str) -> str:
     """Validate an API key"""
     if not isinstance(api_key, str):
-        raise ValueError("Invalid API key: wrong type")
+        raise TypeError("API Key invalid type")
 
     if len(api_key) < 10:
         raise ValueError("API Key too short")
 
     if len(api_key) > 32:
         raise ValueError("API Key too long")
+
+    if not string_is_urlsafe(api_key):
+        raise ValueError("API Key is not an URL-safe string")
 
     return api_key
