5fc049ce971428d4e81a2faf93349ad09e23e84d
Nik Cubrilovic <git@nikcub.me>
2021-05-27 15:30:52 +1000

    FEAT: urlsafe_str_validator method

---
+++ opennem/core/validators/strings.py
@@ -0,0 +1,26 @@
+"""
+OpenNEM String Validators
+
+Used in Pydantic schemas
+"""
+from typing import Union
+
+from pydantic.errors import PydanticTypeError, StrError
+
+from opennem.core.normalizers import string_is_urlsafe
+
+
+class UrlsafeStrError(PydanticTypeError):
+    msg_template = "url safe str type expected"
+
+
+def urlsafe_str_validator(v: str) -> Union[str]:
+    """Validate that a string is url safe. Assumes already passed through string validation"""
+
+    if not isinstance(v, str):
+        raise StrError()
+
+    if string_is_urlsafe(v):
+        return v
+
+    raise UrlsafeStrError()
