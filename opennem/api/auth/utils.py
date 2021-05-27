4f0e4af46b95d9d3785108cb2bcd574cb5e6db58
Nik Cubrilovic <git@nikcub.me>
2021-05-27 20:37:47 +1000

    FEAT: cookie_name_from_auth_name

--- opennem/api/auth/utils.py
+++ opennem/api/auth/utils.py
@@ -10,3 +10,15 @@ API_KEY_LENGTH = 16
 def generate_api_key(key_length: int = API_KEY_LENGTH) -> str:
     """Generates an URL-safe API key using the secrets library and settings"""
     return secrets.token_urlsafe(API_KEY_LENGTH)
+
+
+def cookie_name_from_auth_name(auth_name: str) -> str:
+    """Takes an auth name and returns what the name of the cookie will be.
+    At the moment we are just prepending _"""
+
+    cookie_auth_name = auth_name
+
+    if not cookie_auth_name.startswith("_"):
+        cookie_auth_name = f"_{auth_name}"
+
+    return cookie_auth_name
