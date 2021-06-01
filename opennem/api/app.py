2800753cab56c21a50e9a26195a442542ee0a784
Nik Cubrilovic <git@nikcub.me>
2021-06-01 18:24:56 +1000

    FIX: Expose headers CORS fix

--- opennem/api/app.py
+++ opennem/api/app.py
@@ -64,7 +64,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["X-Process-Time"],
+    expose_headers=["X-Process-Time", "X-ONAU", "X-ONAA"],
 )
 
 
