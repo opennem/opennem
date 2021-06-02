b09f0576e65c7524b04a54dc3f84d06998a14938
Nik Cubrilovic <git@nikcub.me>
2021-06-03 01:08:19 +1000

    FIX: WEM fix in scada range

--- opennem/api/stats/controllers.py
+++ opennem/api/stats/controllers.py
@@ -315,6 +315,9 @@ def get_scada_range(
     timezone = network.timezone_database or "UTC"
     field_name = "generated"
 
+    if network and network.code in ["WEM"]:
+        energy = True
+
     if energy is True:
         field_name = "eoi_quantity"
 
