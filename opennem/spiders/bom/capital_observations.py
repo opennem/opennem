20f4e51349177ff1accec9fa547ad58e1d74b816
Nik Cubrilovic <git@nikcub.me>
2021-06-08 05:30:57 +1000

    FIX: BoM required headers

--- opennem/spiders/bom/capital_observations.py
+++ opennem/spiders/bom/capital_observations.py
@@ -7,9 +7,12 @@ from opennem.spiders.bom.bom_base import BomJSONObservationSpider
 from .utils import get_stations, get_stations_priority
 
 REQUEST_HEADERS = {
+    "Host": "www.bom.gov.au",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
     "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
 }
 
 
