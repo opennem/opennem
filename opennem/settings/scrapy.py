2409f85f82e0efab6e2f6d150edb8955ea47f1c0
Nik Cubrilovic <git@nikcub.me>
2021-06-16 02:10:01 +1000

    FIX: adjust accept header

--- opennem/settings/scrapy.py
+++ opennem/settings/scrapy.py
@@ -21,7 +21,7 @@ COOKIES_ENABLED = True
 TELNETCONSOLE_ENABLED = False
 
 DEFAULT_REQUEST_HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;v=b3;q=0.9",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
     "Cache-Control": "no-cache",
 }
