598c49f4b7dad292f325f54b656b45b7c26b0d8c
Nik Cubrilovic <git@nikcub.me>
2021-07-19 12:38:37 +1000

    FEAT: SpiderStoreMeta supports _data serialization of pydantic types from any spider

--- opennem/middlewares/spider_store_meta.py
+++ opennem/middlewares/spider_store_meta.py
@@ -16,26 +16,32 @@ logger = logging.getLogger("opennem.extensions.spider_store_meta")
 
 
 class ExtensionSpiderStoreMeta:
-    def __init__(self):
+    def __init__(self):  # type: ignore
         self.items_scraped = 0
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler):  # type: ignore
         ext = cls()
 
         # connect the extension object to signals
         crawler.signals.connect(ext.spider_opened, signal=signals.spider_opened)
         crawler.signals.connect(ext.spider_closed, signal=signals.spider_closed)
+        crawler.signals.connect(ext.item_scraped, signal=signals.item_scraped)
 
         # return the extension object
         return ext
 
-    def spider_opened(self, spider):
+    def spider_opened(self, spider):  # type: ignore
         logger.info("opened spider %s", spider.name)
 
-        print(spider.met)
-
-    def spider_closed(self, spider):
+    def spider_closed(self, spider):  # type: ignore
         logger.info("closed spider %s", spider.name)
 
         crawler_set_meta(spider, CrawlStatTypes.last_crawled, datetime.now())
+
+        if hasattr(spider, "data"):
+            crawler_set_meta(spider, CrawlStatTypes.data, spider.data)
+
+    def item_scraped(self, item, spider):  # type: ignore
+        if isinstance(item, dict) and "_data" in item:
+            crawler_set_meta(spider, CrawlStatTypes.data, [i.json() for i in item["_data"]])
