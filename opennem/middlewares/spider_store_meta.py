7c775ab079f4a6e6debd85e822632e789636788d
Nik Cubrilovic <git@nikcub.me>
2021-07-19 12:23:39 +1000

    FEAT: ExtensionSpiderStoreMeta middleware storing meta in database

--- opennem/middlewares/spider_store_meta.py
+++ opennem/middlewares/spider_store_meta.py
@@ -16,27 +16,16 @@ logger = logging.getLogger("opennem.extensions.spider_store_meta")
 
 
 class ExtensionSpiderStoreMeta:
-    def __init__(self, item_count):
-        self.item_count = item_count
+    def __init__(self):
         self.items_scraped = 0
 
     @classmethod
     def from_crawler(cls, crawler):
-        # first check if the extension should be enabled and raise
-        # NotConfigured otherwise
-        if not crawler.settings.getbool("STOREMETA_ENABLED"):
-            raise NotConfigured
-
-        # get the number of items from settings
-        item_count = crawler.settings.getint("MYEXT_ITEMCOUNT", 1000)
-
-        # instantiate the extension object
-        ext = cls(item_count)
+        ext = cls()
 
         # connect the extension object to signals
         crawler.signals.connect(ext.spider_opened, signal=signals.spider_opened)
         crawler.signals.connect(ext.spider_closed, signal=signals.spider_closed)
-        crawler.signals.connect(ext.item_scraped, signal=signals.item_scraped)
 
         # return the extension object
         return ext
@@ -44,12 +33,9 @@ class ExtensionSpiderStoreMeta:
     def spider_opened(self, spider):
         logger.info("opened spider %s", spider.name)
 
+        print(spider.met)
+
     def spider_closed(self, spider):
         logger.info("closed spider %s", spider.name)
 
         crawler_set_meta(spider, CrawlStatTypes.last_crawled, datetime.now())
-
-    def item_scraped(self, item, spider):
-        self.items_scraped += 1
-        if self.items_scraped % self.item_count == 0:
-            logger.info("scraped %d items", self.items_scraped)
