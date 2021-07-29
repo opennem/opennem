59aa881ec830d3df70027f2654dd67c55c5e8bf2
Nik Cubrilovic <git@nikcub.me>
2021-07-29 21:58:31 +1000

    FEAT: get_dataset loader

--- opennem/core/compat/loader.py
+++ opennem/core/compat/loader.py
@@ -1,12 +1,38 @@
 """
 OpenNEM v2 schema loader
 """
+import logging
+from typing import Dict, List, Optional
 
-
-from typing import Dict, List
-
+from opennem.api.export.map import StatType
 from opennem.core.compat.schema import OpennemDataSetV2, OpennemDataV2
+from opennem.diff.versions import get_v2_url
+from opennem.utils.http import http
+
+logger = logging.getLogger("opennem.compat.loader")
 
 
-def load_statset_v2(statset: List[Dict]) -> List[OpennemDataV2]:
+def load_statset_v2(statset: List[Dict]) -> OpennemDataSetV2:
     return OpennemDataSetV2(data=[OpennemDataV2.parse_obj(i) for i in statset])
+
+
+def get_dataset(
+    stat_type: StatType,
+    network_region: str,
+    bucket_size: str = "daily",
+    year: Optional[int] = None,
+) -> OpennemDataSetV2:
+    req_url = get_v2_url(stat_type, network_region, bucket_size, year)
+
+    r = http.get(req_url)
+
+    logger.debug("Loading: {}".format(req_url))
+
+    if not r.ok:
+        raise Exception("Could not parse URL: {}".format(req_url))
+
+    json_data = r.json()
+
+    statset = load_statset_v2(json_data)
+
+    return statset
