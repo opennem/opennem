44ebb6838eb1df983176b284d907f39a7dfc58e6
Nik Cubrilovic <git@nikcub.me>
2021-08-24 23:32:48 +1000

    FIX: remove old build script

--- build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-docker build -t opennem/database:dev -f infra/database/Dockerfile .
-docker-compose up -d --force-recreate --remove-orphans database
-docker-compose logs -f database
