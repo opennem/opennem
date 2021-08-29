216e9454e49fce5b233e764a6c186eee8b258c5c
Nik Cubrilovic <git@nikcub.me>
2021-08-30 09:12:44 +1000

    FIX: Update excel parser unit test

--- tests/test_parser_excel.py
+++ tests/test_parser_excel.py
@@ -1,10 +1,11 @@
 from typing import IO, BinaryIO, Callable
 
+from openpyxl.workbook.workbook import Workbook
+
 from opennem.core.parsers.excel import parse_workbook
 
 
 def test_excel_parser(xls_file: BinaryIO) -> None:
     parsed_excel = parse_workbook(xls_file)
 
-    assert isinstance(parsed_excel, dict), "Parsed excel response is a dict"
-    assert len(parsed_excel.keys()), "Has at least one entry"
+    assert isinstance(parsed_excel, Workbook), "Parsed excel response is a workbook"
