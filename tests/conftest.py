41a889f41d923647809db380e3e38ff16fdbef63
Nik Cubrilovic <git@nikcub.me>
2021-09-02 13:40:45 +1000

    FEAT: scrapy_mock_response testing fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,44 @@ import io
 from typing import BinaryIO, Callable
 
 import pytest
+from scrapy.http import Request, Response
 
 from .utils import PATH_TESTS_FIXTURES
 
 
+def _read_fixture_file(filename: str, directory: str = "files") -> BinaryIO:
+    """Read a fixture file and return a file object"""
+    excel_file_path = PATH_TESTS_FIXTURES / directory / filename
+
+    if not excel_file_path.is_file():
+        raise Exception("Could not find excel fixture at {}".format(excel_file_path))
+
+    _fh = io.open(excel_file_path, mode="rb")
+
+    return _fh
+
+
 @pytest.fixture
 def load_file() -> Callable:
-    def _read_file(filename: str) -> BinaryIO:
-        excel_file_path = PATH_TESTS_FIXTURES / "files" / filename
+    """Load a static file pytest fixture"""
+    return _read_fixture_file
 
-        if not excel_file_path.is_file():
-            raise Exception("Could not find excel fixture at {}".format(excel_file_path))
 
-        _fh = io.open(excel_file_path, mode="rb")
+@pytest.fixture
+def scrapy_mock_response() -> Callable:
+    """Load a scrapy response pytest fixture"""
 
-        return _fh
+    def _scrapy_response_from_file(filename: str, url: str) -> Response:
+        request = Request(url=url)
+        response_content = _read_fixture_file(filename)
 
-    return _read_file
+        response = Response(url=url, request=request, body=response_content.read())
+
+        response.encoding = "utf-8"
+
+        return response
+
+    return _scrapy_response_from_file
 
 
 @pytest.fixture
@@ -39,3 +60,11 @@ def xlsx_file(load_file: Callable) -> BinaryIO:
 @pytest.fixture
 def aemo_nemweb_dispatch_scada(load_file: Callable) -> BinaryIO:
     return load_file("PUBLIC_DISPATCHSCADA_202109021255_0000000348376188.zip")
+
+
+@pytest.fixture
+def aemo_nemweb_dirlisting(scrapy_mock_response: Callable) -> BinaryIO:
+    return scrapy_mock_response(
+        filename="nemweb_dispatch_scada_dirlisting.html",
+        url="https://www.nemweb.com.au/Reports/CURRENT/Dispatch_SCADA/",
+    )
