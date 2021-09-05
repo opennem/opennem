a6d1841e12328db2c113ffdc8c94aa1c8f386c8d
Nik Cubrilovic <git@nikcub.me>
2021-09-06 04:10:50 +1000

    FEAT: update core module entries, version package loads

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -5,13 +5,7 @@ Setup main module entry point with sanity checks, settings init
 and sentry.
 """
 import sys
-
-# This will eventually be dynamically parsed
-v = "3.7.0"
-
-__env__ = "prod"
-
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+from os.path import realpath
 
 # Check minimum required Python version
 if sys.version_info < (3, 7):
@@ -19,7 +13,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 
-# setup sentry
+# Setup sentry
 from opennem.settings import settings  # noqa: E402
 from opennem.utils.sentry import setup_sentry  # noqa: E402
 
@@ -27,7 +21,29 @@ if settings.sentry_enabled:
     setup_sentry()
 
 
+import logging  # noqa: E402
+
+# Clean up default loggers so they're less noisy
+# Kill warnings from various modules
 import warnings  # noqa: E402
 
 # Ignore noisy twisted deprecation warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="twisted")
+
+# Core methods we reuire in loading the module
+from opennem.utils.version import get_version  # noqa: E402
+
+
+def get_module_path() -> None:
+    _path = __import__("pkgutil").extend_path(__path__, __name__)
+
+    # _path = list(set([realpath(i) for i in opennem.__path__])).pop()
+
+    return _path
+
+
+# Module variables
+v = "3.7.0"
+__env__ = "prod"
+__version__ = get_version()
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
