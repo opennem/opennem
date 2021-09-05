957b275435f7bf1753ecc82892cfe6067d242226
Nik Cubrilovic <git@nikcub.me>
2021-09-06 04:13:38 +1000

    FIX: test utils path

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,11 @@
-"""
-Test utilities including paths and helper functions
+"""Test utilities including paths and helper functions
 """
 from pathlib import Path
 
-PATH_TESTS_ROOT = Path(__file__).parent
+# from opennem import __path__
+
+PATH_TESTS_ROOT = Path(__file__).parent / "tests"
 PATH_TESTS_FIXTURES = PATH_TESTS_ROOT / "fixtures"
 
 if not PATH_TESTS_FIXTURES.is_dir():
-    raise Exception("Tets fixtures path not found: {}".format(PATH_TESTS_FIXTURES))
+    raise Exception("Test fixtures path not found: {}".format(PATH_TESTS_FIXTURES))
