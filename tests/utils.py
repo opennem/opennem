3cdde309bed77ce0e9d1cb6fab9e0eaf60bd885f
Nik Cubrilovic <git@nikcub.me>
2021-09-06 04:14:57 +1000

    FIX: test path

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # from opennem import __path__
 
-PATH_TESTS_ROOT = Path(__file__).parent / "tests"
+PATH_TESTS_ROOT = Path(__file__).parent
 PATH_TESTS_FIXTURES = PATH_TESTS_ROOT / "fixtures"
 
 if not PATH_TESTS_FIXTURES.is_dir():
