fff1ce4d9cae44d6b614684eed6cef68b7a849bc
Nik Cubrilovic <git@nikcub.me>
2021-09-06 14:13:47 +0000

    FEAT: project utils + meta methods

--- opennem/utils/project.py
+++ opennem/utils/project.py
@@ -1,19 +1,23 @@
 """Module with project meta details and paths. Used in the core import
 module, in settings and in loading resources"""
 
+import importlib.resources as impres
 from importlib.metadata import PackageNotFoundError, version
-from pathlib import Path
 
 
-def get_importlib_version() -> str:
+def get_version_from_importlib() -> str:
     try:
-        __version__ = version("package-name")
+        _version = version("package-name")
     except PackageNotFoundError:
         # package is not installed
         pass
 
-    return __version__
+    return _version
 
 
-def get_project_path() -> Path:
-    pass
+def get_module_path() -> str:
+    """Returns the core module path. In frozen packages this gives us the temp
+    directory"""
+    with impres.path("opennem", "__init__.py") as ph:
+        _path = str(ph)
+        return _path
