4636ae89a9385a49420dc99427eeff04b2c4c914
Nik Cubrilovic <git@nikcub.me>
2021-09-07 02:13:47 +0000

    FEAT: unit test core module

--- tests/test_opennem.py
+++ tests/test_opennem.py
@@ -1,9 +1,26 @@
-import opennem
+# noqa: F408, F401
+import pytest
 
 
 def test_opennem_imports() -> None:
-    pass
+    try:
+        import opennem
+
+        hasattr(opennem, "core")
+    except Exception:
+        pytest.fail("Could not import main module")
 
 
 def test_opennem_version() -> None:
+    import opennem
+
     assert isinstance(opennem.__version__, str), "Version is a string"
+    assert len(opennem.__version__) > 1, "Version is not empty"
+    assert "." in opennem.__version__, "Version is somewhat versiony"
+    assert len(opennem.__version__.split(".")) > 2, "Version is more versiony"
+
+
+def test_opennem_path() -> None:
+    import opennem
+
+    assert isinstance(opennem.__path__, str), "Path is a string"
