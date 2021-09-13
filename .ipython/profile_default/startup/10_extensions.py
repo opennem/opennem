07c3bd02b9ca262df7013451d1f3c8e226301061
Nik Cubrilovic <git@nikcub.me>
2021-09-14 01:19:59 +1000

    FEAT: iPython extensions and settings in default profile

--- .ipython/profile_default/startup/10_extensions.py
+++ .ipython/profile_default/startup/10_extensions.py
@@ -1,10 +1,14 @@
 """iPython startup file that loads extensions"""
-
 from IPython import get_ipython
 
 ipython = get_ipython()
 
-if "__IPYTHON__" in globals():
+# If in ipython, load autoreload extension
+if "ipython" in globals():
     ipython.magic("load_ext autoreload")
     ipython.magic("autoreload 2")
-    ipython.magic("%config IPCompleter.greedy=True")
+
+# Display all cell outputs in notebook
+from IPython.core.interactiveshell import InteractiveShell  # noqa: E402
+
+InteractiveShell.ast_node_interactivity = "all"
