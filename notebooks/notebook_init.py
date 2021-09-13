751d8172bff61143c2b65e2366867893b4cc3452
Nik Cubrilovic <git@nikcub.me>
2021-09-14 02:00:13 +1000

    FIX: sys path in notebooks before imports

--- notebooks/notebook_init.py
+++ notebooks/notebook_init.py
@@ -7,6 +7,12 @@ import sys
 import warnings
 from pathlib import Path
 
+on_path = str(Path(__file__).parent.parent)
+
+if on_path not in sys.path:
+    sys.path.append(on_path)
+
+
 from opennem.settings import settings  # noqa
 
 logger = logging.getLogger("opennem.notebook")
@@ -16,8 +22,3 @@ __all__ = ["settings", "logging", "logger"]
 warnings.filterwarnings("ignore")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
-
-on_path = str(Path.cwd().parent)
-
-if on_path not in sys.path:
-    sys.path.append(on_path)
