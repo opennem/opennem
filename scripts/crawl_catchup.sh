a0e6aeaea238495fe61f587c47e12c2f8284703a
Nik Cubrilovic <git@nikcub.me>
2021-09-27 19:44:07 +0000

    FIX: no env in crawl catchup

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -1,9 +1,9 @@
-ENV=staging scrapy crawl au.bom.capitals
-# ENV=staging scrapy crawl au.apvi.current
-ENV=staging scrapy crawl au.nem.day.dispatch_is
-ENV=staging scrapy crawl au.nem.day.dispatch_scada
-ENV=staging scrapy crawl au.nem.day.rooftop
-ENV=staging scrapy crawl au.nem.day.trading_is
-ENV=staging scrapy crawl au.nem.week.dispatch
-ENV=staging scrapy crawl au.nem.week.dispatch_actual_gen
+scrapy crawl au.bom.capitals
+#  scrapy crawl au.apvi.current
+scrapy crawl au.nem.current.dispatch_is
+scrapy crawl au.nem.current.dispatch_scada
+scrapy crawl au.nem.current.rooftop
+scrapy crawl au.nem.current.trading_is
+scrapy crawl au.nem.week.dispatch
+scrapy crawl au.nem.week.dispatch_actual_gen
 
