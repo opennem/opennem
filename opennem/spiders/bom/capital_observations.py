47240e1ad73f78c4b1c76659d2bc59e4d91a04d9
Nik Cubrilovic <git@nikcub.me>
2021-10-14 23:38:32 +1100

    FIX: move headers to utils

--- opennem/spiders/bom/capital_observations.py
+++ opennem/spiders/bom/capital_observations.py
@@ -4,16 +4,7 @@ import scrapy
 
 from opennem.spiders.bom.bom_base import BomJSONObservationSpider
 
-from .utils import get_stations, get_stations_priority
-
-REQUEST_HEADERS = {
-    "Host": "www.bom.gov.au",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-    "Cache-Control": "no-cache",
-    "Connection": "keep-alive",
-}
+from .utils import BOM_REQUEST_HEADERS, get_stations, get_stations_priority
 
 
 class BomCapitalsSpider(BomJSONObservationSpider):
@@ -22,7 +13,7 @@ class BomCapitalsSpider(BomJSONObservationSpider):
     def start_requests(self) -> Generator[scrapy.Request, None, None]:
         priority_stations = get_stations_priority()
 
-        _headers = REQUEST_HEADERS.copy()
+        _headers = BOM_REQUEST_HEADERS.copy()
 
         for station in priority_stations:
             yield scrapy.Request(station.feed_url, meta={"code": station.code}, headers=_headers)
@@ -34,7 +25,7 @@ class BomAllSpider(BomJSONObservationSpider):
     def start_requests(self) -> Generator[scrapy.Request, None, None]:
         stations = get_stations()
 
-        _headers = REQUEST_HEADERS.copy()
+        _headers = BOM_REQUEST_HEADERS.copy()
 
         for station in stations:
             yield scrapy.Request(station.feed_url, meta={"code": station.code}, headers=_headers)
--- opennem/spiders/bom/utils.py
+++ opennem/spiders/bom/utils.py
@@ -4,6 +4,15 @@ from typing import List
 from opennem.db import SessionLocal
 from opennem.db.models.opennem import BomStation, Station
 
+BOM_REQUEST_HEADERS = {
+    "Host": "www.bom.gov.au",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+}
+
 
 def get_stations_priority() -> List[Station]:
     """ This gets all the capital stations which are required for the linked regions """
