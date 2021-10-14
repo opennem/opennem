25f084065e38d2f321988bc104371628839d07a1
Nik Cubrilovic <git@nikcub.me>
2021-10-14 02:09:52 +0000

    FIX: BOM utils docs + types

--- opennem/spiders/bom/utils.py
+++ opennem/spiders/bom/utils.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from opennem.db import SessionLocal
-from opennem.db.models.opennem import BomStation
+from opennem.db.models.opennem import BomStation, Station
 
 
-def get_stations_priority():
+def get_stations_priority() -> List[Station]:
+    """ This gets all the capital stations which are required for the linked regions """
     session = SessionLocal()
 
     stations = session.query(BomStation).filter(BomStation.priority < 2).all()
@@ -10,7 +13,8 @@ def get_stations_priority():
     return stations
 
 
-def get_stations():
+def get_stations() -> List[Station]:
+    """ Get all weather stations """
     session = SessionLocal()
 
     stations = session.query(BomStation).filter(BomStation.priority >= 2).all()
