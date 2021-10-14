47c8f66c03117d979144e739069b478827f708c2
Nik Cubrilovic <git@nikcub.me>
2021-10-14 19:05:25 +1100

    FEAT: get_archive_page_for_station_code + unit tests

--- opennem/spiders/bom/utils.py
+++ opennem/spiders/bom/utils.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from typing import List
 
 from opennem.db import SessionLocal
@@ -20,3 +21,12 @@ def get_stations() -> List[Station]:
     stations = session.query(BomStation).filter(BomStation.priority >= 2).all()
 
     return stations
+
+
+def get_archive_page_for_station_code(web_code: str, archive_month: date = datetime.now()) -> str:
+    """ returns a station archive page for a web code and a month"""
+    url_format = "http://www.bom.gov.au/climate/dwo/{datestr}/html/IDCJDW{web_code}.{datestr}.shtml"
+
+    return url_format.format(datestr=archive_month.strftime("%Y%m"), web_code=web_code)
+
+
