c3fe313af3c1ed72b3ce5be6a0ec3bd16d9a3dc0
Nik Cubrilovic <git@nikcub.me>
2021-10-14 23:38:45 +1100

    FEAT: bom history spider + processor + pipeline

--- opennem/pipelines/bom.py
+++ opennem/pipelines/bom.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Dict
 
 import pytz
+import scrapy
 from sqlalchemy.dialects.postgresql import insert
 
 from opennem.db import SessionLocal, get_database_engine
@@ -27,7 +29,103 @@ class StoreBomObservation(object):
     """
 
     @check_spider_pipeline
-    def process_item(self, item, spider):
+    def process_item(self, item, spider: scrapy.Spider) -> int:
+
+        session = SessionLocal()
+        engine = get_database_engine()
+
+        records_to_store = []
+
+        if "records" not in item:
+            return 0
+
+        records = item["records"]
+
+        if "header" not in item:
+            logger.error("No header in bom observation")
+            return 0
+
+        header = item["header"]
+
+        if "state_time_zone" not in header:
+            print(header)
+            logger.error("No state timezone in header")
+            return 0
+
+        timezone_state: str = header["state_time_zone"].strip().upper()
+
+        if timezone_state not in STATE_TO_TIMEZONE.keys():
+            logger.error("No timezone for state: %s", timezone_state)
+
+        timezone = pytz.timezone(STATE_TO_TIMEZONE[timezone_state])
+
+        for obs in records:
+            observation_time = parse_date(
+                obs["aifstime_utc"], dayfirst=False, is_utc=True
+            ).astimezone(timezone)
+
+            code = obs["code"]
+
+            if not observation_time or not code:
+                continue
+
+            records_to_store.append(
+                {
+                    "station_id": code,
+                    "observation_time": observation_time,
+                    "temp_apparent": obs["apparent_t"],
+                    "temp_air": obs["air_temp"],
+                    "press_qnh": obs["press_qnh"],
+                    "wind_dir": obs["wind_dir"],
+                    "wind_spd": obs["wind_spd_kmh"],
+                    "wind_gust": obs["gust_kmh"],
+                    "cloud": obs["cloud"].replace("-", ""),
+                    "cloud_type": obs["cloud_type"].replace("-", ""),
+                    "humidity": obs["rel_hum"],
+                }
+            )
+
+        if not len(records_to_store):
+            return 0
+
+        stmt = insert(BomObservation).values(records_to_store)
+        stmt.bind = engine
+        stmt = stmt.on_conflict_do_update(
+            index_elements=["observation_time", "station_id"],
+            set_={
+                "temp_apparent": stmt.excluded.temp_apparent,
+                "temp_air": stmt.excluded.temp_air,
+                "press_qnh": stmt.excluded.press_qnh,
+                "wind_dir": stmt.excluded.wind_dir,
+                "wind_spd": stmt.excluded.wind_spd,
+                "wind_gust": stmt.excluded.wind_gust,
+                "cloud": stmt.excluded.cloud,
+                "cloud_type": stmt.excluded.cloud_type,
+                "humidity": stmt.excluded.humidity,
+            },
+        )
+
+        try:
+            session.execute(stmt)
+            session.commit()
+        except Exception as e:
+            logger.error("Error: {}".format(e))
+            return 0
+        finally:
+            session.close()
+
+        return len(records_to_store)
+
+
+
+
+class StoreBomHistoryObservation(object):
+    """
+        Pipeline to store BOM observations history
+    """
+
+    @check_spider_pipeline
+    def process_item(self, item: Dict, spider: scrapy.Spider) -> int:
 
         session = SessionLocal()
         engine = get_database_engine()
---
+++ opennem/spiders/bom/history.py
@@ -0,0 +1,90 @@
+"""BOM history spider. Imports historic highs/lows from the BOM website.
+
+"""
+from datetime import datetime
+from typing import Any, Dict, Generator, Optional
+
+import scrapy
+from scrapy.http import TextResponse
+
+from opennem.core.normalizers import is_number
+from opennem.pipelines.bom import StoreBomHistoryObservation
+from opennem.spiders.bom.utils import get_archive_page_for_station_code, get_stations_priority
+
+from .utils import BOM_REQUEST_HEADERS
+
+
+class BOMHistorySpider(scrapy.Spider):
+    name = "bom.history"
+
+    allowed_domains = ["bom.gov.au"]
+
+    # pipelines = set([StoreBomHistoryObservation])
+
+    station_id: Optional[str] = None
+
+    random_user_agent: bool = True
+
+    latest_month: bool = True
+
+    def start_requests(self) -> Generator[scrapy.Request, None, None]:
+        priority_stations = get_stations_priority()[:1]
+
+        _headers = BOM_REQUEST_HEADERS.copy()
+
+        months = []
+
+        if self.latest_month:
+            months = [datetime.now().date()]
+
+        for station in priority_stations:
+            for month in months:
+                req_url = get_archive_page_for_station_code(station.web_code, month)
+                yield scrapy.Request(
+                    req_url,
+                    meta={"code": station.code, "month": month},
+                    headers=_headers
+                )  # type: ignore
+
+    def parse(self, response: TextResponse) -> Generator[Dict[str, Any], None, None]:
+        code = None
+        month = None
+
+
+        if "month" not in response.meta:
+            yield None
+
+        month = response.meta["month"]
+
+        if "code" in response.meta:
+            code = response.meta["code"]
+
+        elif hasattr(self, "station_id") and self.station_id:
+            code = self.station_id
+
+        if not code:
+            raise Exception("No station id for this scrape can't join")
+
+        records = []
+
+        for trow in response.css("#columns tbody tr"):
+            first_value_sel = trow.css("th::text")
+
+            if not first_value_sel:
+                continue
+
+            first_col_val = first_value_sel[0].get()
+
+            if not is_number(first_col_val):
+                continue
+
+            day_of_month = int(first_col_val)
+
+            records.append({
+                "observation_date": month.replace(day=day_of_month),
+                "code": code,
+                "temp_max": trow.css("td.xb::text").get(),
+                "temp_min": trow.css("td.c::text").get()
+            })
+
+        yield {"records": records}
