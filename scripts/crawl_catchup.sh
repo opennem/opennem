1fd595c438422a4852e1f5496f4a28608b31b04f
Nik Cubrilovic <git@nikcub.me>
2021-10-27 14:25:47 +0000

    FIX: update crawl_catchup

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -4,6 +4,11 @@ scrapy crawl au.nem.current.dispatch_is
 scrapy crawl au.nem.current.dispatch_scada
 scrapy crawl au.nem.current.rooftop
 scrapy crawl au.nem.current.trading_is
-scrapy crawl au.nem.week.dispatch
-scrapy crawl au.nem.week.dispatch_actual_gen
+scrapy crawl au.nem.latest.dispatch
+scrapy crawl au.nem.latest.dispatch_actual_gen
+scrapy crawl au.wem.current.balancing_summary
+scrapy crawl au.wem.current.facility_scada
+scrapy crawl au.wem.live.facility_intervals
+scrapy crawl au.wem.facilities
+scrapy crawl au.wem.live.pulse
 
