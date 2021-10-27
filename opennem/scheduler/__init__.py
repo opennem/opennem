4b24d0cffe2563bf27d924d5dab593d470f4022d
Nik Cubrilovic <git@nikcub.me>
2021-10-27 14:25:47 +0000

    FIX: schedule tweak

--- opennem/scheduler/__init__.py
+++ opennem/scheduler/__init__.py
@@ -116,7 +116,7 @@ def schedule_energy_monthlies() -> None:
 
 
 # geojson maps
-@huey.periodic_task(crontab(minute="*/5"), priority=50)
+@huey.periodic_task(crontab(minute="*/30"), priority=50)
 @huey.lock_task("schedule_export_geojson")
 def schedule_export_geojson() -> None:
     if settings.workers_run:
