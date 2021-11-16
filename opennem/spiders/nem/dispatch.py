3abc5430f68a655e90fd854a607cdcb551712a35
Nik Cubrilovic <git@nikcub.me>
2021-11-17 09:50:11 +1100

    FIX: process latest fix

--- opennem/spiders/nem/dispatch.py
+++ opennem/spiders/nem/dispatch.py
@@ -5,7 +5,7 @@ from opennem.spiders.nemweb import NemwebSpider
 
 
 class NemwebLatestDispatch(NemwebSpider):
-    process_latest = True
+    process_latest = False
     name = "au.nem.latest.dispatch"
     start_url = "http://nemweb.com.au/Reports/Current/Next_Day_Dispatch/"
     limit = 1
