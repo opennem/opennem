d794368fc46dbbc0da5cfb5ae3c1287553914e49
Nik Cubrilovic <git@nikcub.me>
2021-11-16 18:05:14 +1100

    FEAT: Update core url_downloader

---
+++ opennem/core/downloader.py
@@ -0,0 +1,46 @@
+import logging
+from io import BytesIO
+from zipfile import ZipFile
+
+from opennem.utils.handlers import _handle_zip, chain_streams
+from opennem.utils.http import http
+from opennem.utils.mime import mime_from_content, mime_from_url
+
+logger = logging.getLogger("opennem.downloader")
+
+
+def url_downloader(url: str) -> bytes:
+    """Downloads a URL and returns content, handling embedded zips and other MIME's"""
+    r = http.get(url)
+
+    if not r.ok:
+        raise Exception("Bad link returned {}: {}".format(r.status_code, url))
+
+    content = BytesIO(r.content)
+
+    file_mime = mime_from_content(content)
+
+    if not file_mime:
+        file_mime = mime_from_url(url)
+
+    # @TODO handle all this in utils/archive.py
+    # and make it all generic to handle other
+    # mime types
+    if file_mime == "application/zip":
+        with ZipFile(content) as zf:
+            if len(zf.namelist()) == 1:
+                return zf.open(zf.namelist()[0]).read()
+
+            c = []
+            stream_count = 0
+
+            for filename in zf.namelist():
+                if filename.endswith(".zip"):
+                    c.append(_handle_zip(zf.open(filename), "r"))
+                    stream_count += 1
+                else:
+                    c.append(zf.open(filename))
+
+            return chain_streams(c).read()
+
+    return content.getvalue()
