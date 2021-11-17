2016378a0400a6063152e1861e91cec5c576a2f2
Nik Cubrilovic <git@nikcub.me>
2021-11-17 13:18:50 +1100

    FIX: craw catchup grabs days not current

--- scripts/crawl_catchup.sh
+++ scripts/crawl_catchup.sh
@@ -1,9 +1,10 @@
 scrapy crawl bom.capitals
 scrapy crawl au.apvi.latest.data
-scrapy crawl au.nem.current.dispatch_is
-scrapy crawl au.nem.current.dispatch_scada
-scrapy crawl au.nem.current.rooftop
-scrapy crawl au.nem.current.trading_is
+scrapy crawl au.nem.day.dispatch_is
+scrapy crawl au.nem.day.dispatch_scada
+scrapy crawl au.nem.day.rooftop
+scrapy crawl au.nem.day.rooftop_forecast
+scrapy crawl au.nem.day.trading_is
 scrapy crawl au.nem.latest.dispatch
 scrapy crawl au.nem.latest.dispatch_actual_gen
 scrapy crawl au.wem.current.balancing_summary
