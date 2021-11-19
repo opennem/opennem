9d68394d7fd10d93912c80fdbbe90cc1bf810fd2
Nik Cubrilovic <git@nikcub.me>
2021-11-19 18:14:55 +1100

    FIX: Update scheduler

--- opennem/scheduler/db.py
+++ opennem/scheduler/db.py
@@ -8,6 +8,7 @@ from huey import PriorityRedisHuey, crontab
 
 from opennem.api.export.map import PriorityType
 from opennem.api.export.tasks import export_energy
+from opennem.crawl import run_crawls_all
 from opennem.db.tasks import refresh_material_views
 from opennem.monitors.aemo_intervals import aemo_wem_live_interval
 from opennem.monitors.database import check_database_live
@@ -138,31 +139,13 @@ def spider_catchup_tasks() -> None:
 @huey.periodic_task(crontab(minute="*/5"))
 @huey.lock_task("spider_live_tasks")
 def spider_live_tasks() -> None:
-    latest_spiders = [
-        "au.nem.latest.dispatch_scada",
-        "au.nem.latest.dispatch_is",
-        "au.nem.latest.rooftop",
-        "au.nem.latest.rooftop_forecast",
-        "au.nem.latest.trading_is",
-    ]
-
-    for _spider_name in latest_spiders:
-        job_schedule_all(_spider_name)
+    run_crawls_all()
 
 
-@huey.periodic_task(crontab(hour="*/1", minute="10"))
+@huey.periodic_task(crontab(hour="*/4", minute="10"))
 @huey.lock_task("spider_nem_catchup")
 def spider_nem_catchup() -> None:
-    catchup_spiders = [
-        "au.nem.day.dispatch_is",
-        "au.nem.day.dispatch_scada",
-        "au.nem.day.rooftop",
-        "au.nem.day.rooftop_forecast",
-        "au.nem.day.trading_is",
-    ]
-
-    for _spider_name in catchup_spiders:
-        job_schedule_all(_spider_name)
+    run_crawls_all(last_crawled=False)
 
 
 # bom spiders
@@ -174,18 +157,6 @@ def spider_bom() -> None:
     ]
 
     for _spider_name in bom_spiders:
-        job_schedule_all(_spider_name)
-
-
-@huey.periodic_task(crontab(hour="4,5,6,13,16,17", minute="10"))
-@huey.lock_task("spider_next_day_dispatches")
-def spider_next_day_dispatches() -> None:
-    catchup_spiders = [
-        "au.nem.latest.dispatch",
-        "au.nem.latest.dispatch_actual_gen",
-    ]
-
-    for _spider_name in catchup_spiders:
         job_schedule_all(_spider_name)
 
 
