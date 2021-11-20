483959e579ba5c5b14b7da52eb5346b3bcf8a1cd
Nik Cubrilovic <git@nikcub.me>
2021-11-20 19:56:40 +1100

    FIX: return type in get bom stations

--- opennem/spiders/bom/utils.py
+++ opennem/spiders/bom/utils.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 from typing import List
 
 from opennem.db import SessionLocal
-from opennem.db.models.opennem import BomStation, Station
+from opennem.db.models.opennem import BomStation
 
 BOM_REQUEST_HEADERS = {
     "Host": "www.bom.gov.au",
@@ -14,8 +14,8 @@ BOM_REQUEST_HEADERS = {
 }
 
 
-def get_stations_priority() -> List[Station]:
-    """ This gets all the capital stations which are required for the linked regions """
+def get_stations_priority() -> List[BomStation]:
+    """This gets all the capital stations which are required for the linked regions"""
     session = SessionLocal()
 
     stations = session.query(BomStation).filter(BomStation.priority < 2).all()
@@ -23,8 +23,8 @@ def get_stations_priority() -> List[Station]:
     return stations
 
 
-def get_stations() -> List[Station]:
-    """ Get all weather stations """
+def get_stations() -> List[BomStation]:
+    """Get all weather stations"""
     session = SessionLocal()
 
     stations = session.query(BomStation).filter(BomStation.priority >= 2).all()
@@ -33,9 +33,9 @@ def get_stations() -> List[Station]:
 
 
 def get_archive_page_for_station_code(web_code: str, archive_month: date = datetime.now()) -> str:
-    """ returns a station archive page for a web code and a month"""
-    url_format = "http://www.bom.gov.au/climate/dwo/{datestr}/html/IDCJDW{web_code}.{datestr}.shtml"
+    """returns a station archive page for a web code and a month"""
+    url_format = (
+        "http://www.bom.gov.au/climate/dwo/{datestr}/html/IDCJDW{web_code}.{datestr}.shtml"
+    )
 
     return url_format.format(datestr=archive_month.strftime("%Y%m"), web_code=web_code)
-
-
