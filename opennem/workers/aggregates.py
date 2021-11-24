cf63c9d830672fa8c5723bb2e17bcf77cf86a8e4
Nik Cubrilovic <git@nikcub.me>
2021-11-25 00:38:27 +1100

    FIX: Remove aggregate rooftop fix

--- opennem/workers/aggregates.py
+++ opennem/workers/aggregates.py
@@ -106,8 +106,8 @@ def exec_aggregates_facility_daily_query(
     logger.debug(result)
 
     # @NOTE rooftop fix for double counts
-    if not DRY_RUN:
-        run_rooftop_fix()
+    # if not DRY_RUN:
+    # run_rooftop_fix()
 
     return resp_code
 
