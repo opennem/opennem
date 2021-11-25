2c41d849f873815fcc7c6e8e78c24df01e688b6c
Nik Cubrilovic <git@nikcub.me>
2021-11-25 16:20:47 +1100

    FEAT: Load fixture test util

--- tests/utils.py
+++ tests/utils.py
@@ -9,3 +9,18 @@ PATH_TESTS_FIXTURES = PATH_TESTS_ROOT / "fixtures"
 
 if not PATH_TESTS_FIXTURES.is_dir():
     raise Exception("Test fixtures path not found: {}".format(PATH_TESTS_FIXTURES))
+
+
+def load_fixture(filename: str) -> str:
+    """Load a test fixture and return its content. Assumes relative path"""
+    fixture_path = PATH_TESTS_FIXTURES / Path(filename)
+
+    if not fixture_path.is_file():
+        raise Exception("Not a file: {}".format(filename))
+
+    fixture_content = ""
+
+    with fixture_path.open() as fh:
+        fixture_content = fh.read()
+
+    return fixture_content
