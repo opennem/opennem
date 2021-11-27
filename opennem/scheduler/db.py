893a7d9431a4869bb28060fccc13ba8c362f6c26
Nik Cubrilovic <git@nikcub.me>
2021-11-27 16:54:19 +1100

    FEAT: run_aggregates_all_days(2)

--- opennem/scheduler/db.py
+++ opennem/scheduler/db.py
@@ -73,6 +73,12 @@ def db_run_energy_gapfil() -> None:
     run_energy_gapfill()
 
 
+@huey.periodic_task(crontab(hour="*/3", minute=45))
+@huey.lock_task("db_run_aggregates")
+def db_run_aggregates() -> None:
+    run_aggregates_all_days(2)
+
+
 @huey.periodic_task(crontab(hour="6", minute="45"))
 @huey.lock_task("db_run_emission_tasks")
 def db_run_emission_tasks() -> None:
