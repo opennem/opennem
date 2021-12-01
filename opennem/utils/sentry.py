bf30e3680fb429dd1a4aeba0235eab69bb141ab8
Nik Cubrilovic <git@nikcub.me>
2021-12-01 12:58:05 +1100

    FEAT: _sentry_before_send to filter certain exception types

--- opennem/utils/sentry.py
+++ opennem/utils/sentry.py
@@ -2,7 +2,19 @@ import sentry_sdk
 from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
+from opennem.clients.bom import BOMParsingException
 from opennem.settings import settings
+
+_SENTRY_IGNORE_EXCEPTION_TYPES = BOMParsingException
+
+
+def _sentry_before_send(event, hint):
+    """Hook to sentry sending and excelude some exception types"""
+    if "exc_info" in hint:
+        exc_type, exc_value, tb = hint["exc_info"]
+        if isinstance(exc_value, _SENTRY_IGNORE_EXCEPTION_TYPES):
+            return None
+    return event
 
 
 def setup_sentry() -> None:
@@ -11,5 +23,6 @@ def setup_sentry() -> None:
             settings.sentry_url,
             traces_sample_rate=1.0,
             environment=settings.env,
+            before_send=_sentry_before_send,
             integrations=[RedisIntegration(), SqlalchemyIntegration()],
         )
