a03d0997d5b8e64afe762c7372f6dffd7cf5ce5c
Nik Cubrilovic <git@nikcub.me>
2021-12-02 14:52:34 +1100

    FIX: update make_release to not push master

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -22,7 +22,7 @@ git add pyproject.toml requirements.txt
 git ci -m "v$VERSION"
 
 git tag v$VERSION
-git push -u origin master v$VERSION
+git push -u origin v$VERSION
 
 if [[ $# -eq 0 ]] ; then
   echo 'Dev release'
