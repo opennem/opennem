913b6a27a68c613f05d63f7068ee8ab1d045829d
Nik Cubrilovic <git@nikcub.me>
2022-01-28 14:17:54 +0000

    FEAT: Support backoff parameter in crawl controller

--- opennem/crawlers/bom.py
+++ opennem/crawlers/bom.py
@@ -3,6 +3,7 @@
 """
 import logging
 from datetime import datetime
+from time import sleep
 from typing import Optional
 
 from opennem.clients.bom import get_bom_observations
@@ -17,7 +18,7 @@ logger = logging.getLogger("opennem.crawler.bom")
 def crawl_bom_capitals(
     crawler: CrawlerDefinition, last_crawled: bool = True, limit: bool = False
 ) -> ControllerReturn:
-    bom_stations = get_stations_priority()
+    bom_stations = get_stations_priority(limit=crawler.limit)
     cr: Optional[ControllerReturn] = None
 
     for bom_station in bom_stations:
@@ -28,6 +29,10 @@ def crawl_bom_capitals(
 
             bom_observations = get_bom_observations(bom_station.feed_url, bom_station.code)
             cr = store_bom_observation_intervals(bom_observations)
+
+            if crawler.backoff and crawler.backoff > 0:
+                logger.info("Backing off for {}".format(crawler.backoff))
+                sleep(crawler.backoff)
         except Exception as e:
             logger.info("Bom error for station {}: {}".format(bom_station.name, e))
 
