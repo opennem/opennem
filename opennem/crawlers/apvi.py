eb0023349199823c7b68eb69022361129dffa9c3
Nik Cubrilovic <git@nikcub.me>
2022-02-01 13:25:03 +0000

    FIX: APVI crawlers run on updated endpoint

--- opennem/crawlers/apvi.py
+++ opennem/crawlers/apvi.py
@@ -4,10 +4,11 @@
 
 import logging
 from datetime import datetime
+from typing import Optional
 
 import pytz
 
-from opennem.clients.apvi import get_apvi_rooftop_data
+from opennem.clients.apvi import APVIForecastSet, get_apvi_rooftop_data, get_apvi_rooftop_today
 from opennem.controllers.apvi import store_apvi_forecastset, update_apvi_facility_capacities
 from opennem.controllers.schema import ControllerReturn
 from opennem.crawlers.schema import CrawlerDefinition
@@ -20,14 +21,14 @@ def crawl_apvi_forecasts(
     crawler: CrawlerDefinition, last_crawled: bool = True, limit: bool = False
 ) -> ControllerReturn:
     """Runs the APVI crawl definition"""
-    if not crawler.limit or crawler.limit == 1:
-        apvi_forecast_return = run_apvi_crawl(get_today_nem())
-        return apvi_forecast_return
+    # if not crawler.limit or crawler.limit == 1:
+    #     apvi_forecast_return = run_apvi_crawl(get_today_nem())
+    #     return apvi_forecast_return
 
     apvi_return = ControllerReturn()
 
-    for date in date_series(get_today_nem().date(), length=crawler.limit, reverse=True):
-        apvi_forecast_return = run_apvi_crawl(date)
+    if crawler.latest:
+        apvi_forecast_return = run_apvi_crawl()
         apvi_return.processed_records += apvi_forecast_return.processed_records
         apvi_return.total_records += apvi_forecast_return.total_records
         apvi_return.inserted_records += apvi_forecast_return.inserted_records
@@ -39,14 +40,33 @@ def crawl_apvi_forecasts(
         ):
             apvi_return.last_modified = apvi_forecast_return.last_modified
 
+    # run the entire date range
+    else:
+        for date in date_series(get_today_nem().date(), length=crawler.limit, reverse=True):
+            apvi_forecast_return = run_apvi_crawl(date)
+            apvi_return.processed_records += apvi_forecast_return.processed_records
+            apvi_return.total_records += apvi_forecast_return.total_records
+            apvi_return.inserted_records += apvi_forecast_return.inserted_records
+            apvi_return.errors += apvi_forecast_return.errors
+
+            if (
+                not apvi_return.last_modified
+                or apvi_return.last_modified < apvi_forecast_return.last_modified
+            ):
+                apvi_return.last_modified = apvi_forecast_return.last_modified
+
     return apvi_return
 
 
-def run_apvi_crawl(day: datetime) -> ControllerReturn:
+def run_apvi_crawl(day: Optional[datetime] = None) -> ControllerReturn:
+    apvi_forecast_set: Optional[APVIForecastSet] = None
 
-    apvi_forecast_set = get_apvi_rooftop_data(day)
-
-    logger.debug("Getting APVI data for day {}".format(day))
+    if day:
+        logger.info("Getting APVI data for day {}".format(day))
+        apvi_forecast_set = get_apvi_rooftop_data(day)
+    else:
+        logger.info("Getting APVI data from today")
+        apvi_forecast_set = get_apvi_rooftop_today()
 
     if not apvi_forecast_set:
         raise Exception("Could not get APVI forecast set")
