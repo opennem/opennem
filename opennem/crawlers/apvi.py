6638b783b7fb21daad7c5e72a166b5f355cb60d0
Nik Cubrilovic <git@nikcub.me>
2022-02-03 17:00:26 +1100

    FEAT: refactor apvi to support backfilling and date ranges and limits

--- opennem/crawlers/apvi.py
+++ opennem/crawlers/apvi.py
@@ -4,6 +4,7 @@
 
 import logging
 from datetime import datetime
+from typing import Optional
 
 import pytz
 
@@ -11,7 +12,7 @@ from opennem.clients.apvi import get_apvi_rooftop_data
 from opennem.controllers.apvi import store_apvi_forecastset, update_apvi_facility_capacities
 from opennem.controllers.schema import ControllerReturn
 from opennem.crawlers.schema import CrawlerDefinition
-from opennem.utils.dates import chop_datetime_microseconds
+from opennem.utils.dates import TODAY_NEM, chop_datetime_microseconds, date_series, get_today_nem
 
 logger = logging.getLogger("opennem.crawlers.apvi")
 
@@ -19,9 +20,34 @@ logger = logging.getLogger("opennem.crawlers.apvi")
 def crawl_apvi_forecasts(
     crawler: CrawlerDefinition, last_crawled: bool = True, limit: bool = False
 ) -> ControllerReturn:
-
+    """Runs the APVI crawl definition"""
     if not crawler.limit or crawler.limit == 1:
-        apvi_forecast_set = get_apvi_rooftop_data()
+        apvi_forecast_return = run_apvi_crawl(get_today_nem())
+        return apvi_forecast_return
+
+    apvi_return = ControllerReturn()
+
+    for date in date_series(get_today_nem().date(), length=crawler.limit, reverse=True):
+        apvi_forecast_return = run_apvi_crawl(date)
+        apvi_return.processed_records += apvi_forecast_return.processed_records
+        apvi_return.total_records += apvi_forecast_return.total_records
+        apvi_return.inserted_records += apvi_forecast_return.inserted_records
+        apvi_return.errors += apvi_forecast_return.errors
+
+        if (
+            not apvi_return.last_modified
+            or apvi_return.last_modified < apvi_forecast_return.last_modified
+        ):
+            apvi_return.last_modified = apvi_forecast_return.last_modified
+
+    return apvi_return
+
+
+def run_apvi_crawl(day: datetime) -> ControllerReturn:
+
+    apvi_forecast_set = get_apvi_rooftop_data(day)
+
+    logger.debug("Getting APVI data for day {}".format(day))
 
     if not apvi_forecast_set:
         raise Exception("Could not get APVI forecast set")
@@ -31,7 +57,7 @@ def crawl_apvi_forecasts(
     update_apvi_facility_capacities(apvi_forecast_set)
 
     cr.last_modified = chop_datetime_microseconds(
-        datetime.now().astimezone(pytz.timezone("Australia/Sydney"))
+        datetime.now().astimezone(pytz.timezone("Australia/Brisbane"))
     )
 
     return cr
