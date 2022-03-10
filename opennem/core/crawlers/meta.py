5b29fac072656b91cd393977d9048dcf817ef055
Nik Cubrilovic <git@nikcub.me>
2022-03-10 11:19:25 +1100

    FIX: scope meta methods for crawlers to close sessions correctly

--- opennem/core/crawlers/meta.py
+++ opennem/core/crawlers/meta.py
@@ -38,75 +38,73 @@ class CrawlStatTypes(Enum):
 
 def crawler_get_all_meta(crawler_name: str) -> Optional[Dict[str, Any]]:
     """Get crawler metadata by crawler name"""
-    session = get_scoped_session()
+    with get_scoped_session() as session:
+        spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
 
-    spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
+        if not spider_meta:
+            return None
 
-    if not spider_meta:
-        return None
+        if not spider_meta.data:
+            return None
 
-    if not spider_meta.data:
-        return None
-
-    return spider_meta.data
+        return spider_meta.data
 
 
 def crawler_get_meta(crawler_name: str, key: CrawlStatTypes) -> Optional[Union[str, datetime]]:
     """Crawler get specific stat type from metadata for crawler name"""
-    session = get_scoped_session()
+    with get_scoped_session() as session:
 
-    spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
+        spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
 
-    if not spider_meta:
-        return None
+        if not spider_meta:
+            return None
 
-    if not spider_meta.data:
-        return None
+        if not spider_meta.data:
+            return None
 
-    if key.value not in spider_meta.data:
-        return None
+        if key.value not in spider_meta.data:
+            return None
 
-    _val = spider_meta.data[key.value]
+        _val = spider_meta.data[key.value]
 
-    if key in [
-        CrawlStatTypes.latest_processed,
-        CrawlStatTypes.last_crawled,
-        CrawlStatTypes.server_latest,
-    ]:
-        _val_processed = datetime.fromisoformat(_val)
-        return _val_processed
+        if key in [
+            CrawlStatTypes.latest_processed,
+            CrawlStatTypes.last_crawled,
+            CrawlStatTypes.server_latest,
+        ]:
+            _val_processed = datetime.fromisoformat(_val)
+            return _val_processed
 
-    return _val
+        return _val
 
 
 def crawler_set_meta(crawler_name: str, key: CrawlStatTypes, value: Any) -> None:
     """Set a crawler metadata stat type by name"""
-    session = get_scoped_session()
+    with get_scoped_session() as session:
+        if key == CrawlStatTypes.server_latest:
+            current_value = crawler_get_meta(crawler_name, key)
 
-    if key == CrawlStatTypes.server_latest:
-        current_value = crawler_get_meta(crawler_name, key)
-
-        try:
-            if current_value and current_value >= value:
-                return None
-        except TypeError:
-            logger.error(
-                "Error comparing {} ({}) and {} ({})".format(
-                    current_value, type(current_value), value, type(value)
+            try:
+                if current_value and current_value >= value:
+                    return None
+            except TypeError:
+                logger.error(
+                    "Error comparing {} ({}) and {} ({})".format(
+                        current_value, type(current_value), value, type(value)
+                    )
                 )
-            )
 
-    spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
+        spider_meta = session.query(CrawlMeta).filter_by(spider_name=crawler_name).one_or_none()
 
-    if not spider_meta:
-        spider_meta = CrawlMeta(spider_name=crawler_name, data={})
+        if not spider_meta:
+            spider_meta = CrawlMeta(spider_name=crawler_name, data={})
 
-    spider_meta.data[key.value] = value
+        spider_meta.data[key.value] = value
 
-    logger.debug("Spider {} meta: Set {} to {}".format(crawler_name, key.value, value))
+        logger.debug("Spider {} meta: Set {} to {}".format(crawler_name, key.value, value))
 
-    session.add(spider_meta)
-    session.commit()
+        session.add(spider_meta)
+        session.commit()
 
 
 if __name__ == "__main__":
