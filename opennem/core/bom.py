4328e5f5f9b4e788148d81ec57eb40cf25eda5b8
Nik Cubrilovic <git@nikcub.me>
2022-03-16 11:31:12 +1100

    FEAT: openne.core.bom for core bom methods

---
+++ opennem/core/bom.py
@@ -0,0 +1,54 @@
+"""OpenNEM Core BoM Station Utilities
+
+Get a list of stations from the local database
+"""
+from datetime import date, datetime
+from random import shuffle
+from typing import List, Optional
+
+from opennem.clients.bom import BOM_REQUEST_HEADERS
+from opennem.db import SessionLocal
+from opennem.db.models.opennem import BomStation
+from opennem.schema.bom import BomStationSchema
+
+
+def get_stations_priority(limit: Optional[int] = None) -> List[BomStationSchema]:
+    """This gets all the capital stations which are required for the linked regions"""
+    with SessionLocal() as session:
+        stations = session.query(BomStation).filter(BomStation.priority < 2).all()
+
+        all_models = [BomStationSchema.from_orm(i) for i in stations]
+        return_models = []
+
+        if limit:
+            if limit > len(all_models):
+                return all_models
+
+            # shuffle the list of stations and pick a random number out
+            shuffle(all_models)
+
+            return_models = all_models[:limit]
+
+    return return_models
+
+
+def get_stations() -> List[BomStationSchema]:
+    """Get all weather stations
+
+    @NOTE This is a bit redundant
+    """
+    with SessionLocal() as session:
+        stations = session.query(BomStation).filter(BomStation.priority >= 2).all()
+
+        all_models = [BomStationSchema.from_orm(i) for i in stations]
+
+    return all_models
+
+
+def get_archive_page_for_station_code(web_code: str, archive_month: date = datetime.now()) -> str:
+    """returns a station archive page for a web code and a month"""
+    url_format = (
+        "http://www.bom.gov.au/climate/dwo/{datestr}/html/IDCJDW{web_code}.{datestr}.shtml"
+    )
+
+    return url_format.format(datestr=archive_month.strftime("%Y%m"), web_code=web_code)
