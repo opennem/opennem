bec5cc8590a0444079c531cb5d47de5b464b3106
Nik Cubrilovic <git@nikcub.me>
2022-03-25 14:32:40 +0000

    FIX: type params in methods

--- opennem/api/photo/controllers.py
+++ opennem/api/photo/controllers.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from io import BytesIO
+from typing import Any
 
 import requests
 from PIL import Image
@@ -20,7 +21,7 @@ UPLOAD_ARGS = {
 }
 
 
-def write_photo_to_s3(file_path: str, data, overwrite: bool = False) -> int:
+def write_photo_to_s3(file_path: str, data: Any, overwrite: bool = False) -> int:
     # @TODO move this to aws.py
     s3_save_path = os.path.join(settings.photos_bucket_path, file_path)
     write_count = 0
