a7443d2c6cecd8b9236f729f8476d86387b366c8
Nik Cubrilovic <git@nikcub.me>
2022-04-13 14:36:16 +1000

    FEAT: parser benchmarks and tests

---
+++ tests/benchmark_mms_parser.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Any, Dict
+
+import pytest
+
+from opennem.core.downloader import file_opener
+from opennem.core.parsers.aemo.mms import parse_aemo_mms_csv
+
+NEM_FILE_PATH = Path("data/NEM_FACILITY_SCADA_DAY.zip")
+
+
+def load_nem_scada_records() -> Dict[str, Any]:
+    csv_content = file_opener(NEM_FILE_PATH).decode("utf-8")
+
+    ts = parse_aemo_mms_csv(csv_content)
+
+    if not ts:
+        raise Exception("no table set")
+
+    return ts.get_table("unit_scada").records
+
+
+@pytest.mark.benchmark(
+    group="load_nem_scada_records",
+    min_rounds=1,
+)
+def test_benchmark_generate_facility_scada_base(benchmark) -> None:
+    benchmark(load_nem_scada_records)
