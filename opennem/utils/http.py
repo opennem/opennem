197e25874db0689ca594c99dafbb7612bad90b14
Nik Cubrilovic <git@nikcub.me>
2022-04-15 13:58:50 +0000

    FEAT: Update http client module with better cachine and bypasses

--- opennem/utils/http.py
+++ opennem/utils/http.py
@@ -8,17 +8,43 @@
 
 """
 import logging
+from typing import Any
+from urllib.request import Request
 
 import requests
+import urllib3
 from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry  # type: ignore
+from urllib3.util.retry import Retry
 
 from opennem.settings import settings
+from opennem.utils.version import get_version
+
+urllib3.disable_warnings()
 
 logger = logging.getLogger("opennem.utils.http")
 
 DEFAULT_TIMEOUT = settings.http_timeout
 DEFAULT_RETRIES = settings.http_retries
+
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"  # noqa: 501
+
+CHROME_AGENT_HEADERS = {
+    "user-agent": USER_AGENT,
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*",
+    "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "accept-encoding": "gzip, deflate",
+    "scheme": "https",
+    "sec-ch-prefers-color-scheme": "light",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+}
+
+API_CLIENT_HEADERS = {"user-agent": f"OpenNEM (v {get_version()})", "accept": "*/*"}
 
 
 def setup_http_cache() -> bool:
@@ -33,7 +59,8 @@ def setup_http_cache() -> bool:
         logger.error("Request caching requires requests-cache library")
         return False
 
-    requests_cache.install_cache(".opennem_requests_cache", expire_after=300)
+    requests_cache.install_cache(".elecmon_requests_cache", expire_after=60 * 60 * 4)
+    logger.info("Setup HTTP cache at: {}".format(settings.http_cache_local))
     return True
 
 
@@ -45,7 +72,7 @@ class TimeoutHTTPAdapter(HTTPAdapter):
             del kwargs["timeout"]
         super().__init__(*args, **kwargs)
 
-    def send(self, request, **kwargs):
+    def send(self, request: Request, **kwargs) -> Any:
         timeout = kwargs.get("timeout")
         if timeout is None:
             kwargs["timeout"] = self.timeout
@@ -69,6 +96,8 @@ retry_strategy_on_permission_denied = Retry(
 )
 
 http = requests.Session()
+
+http.headers.update({"User-Agent": USER_AGENT})
 
 adapter_timeout = TimeoutHTTPAdapter()
 http.mount("https://", adapter_timeout)
