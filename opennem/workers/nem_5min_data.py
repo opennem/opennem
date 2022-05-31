94dc615a4350796d05316caebb75919139f0c338
Nik Cubrilovic <git@nikcub.me>
2022-05-31 09:56:00 +0000

    FIX: Aggregate edge data fixes and detection + data import (fixes #80)

---
+++ opennem/workers/nem_5min_data.py
@@ -0,0 +1,15 @@
+"""
+Workers to output NEM 5min data
+
+@NOTE Weekly 5min data and annual daily intervals
+
+Issue #65
+"""
+
+
+def run_nem_5min_export() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    run_nem_5min_export()
