8b5a64393ee48dafc3f283e84dd1e5958fb88cd3
Nik Cubrilovic <git@nikcub.me>
2022-07-05 14:33:05 +0000

    FEAT: proxy test script

---
+++ scripts/proxy.py
@@ -0,0 +1,15 @@
+from opennem.utils.http import attach_proxy, http
+
+
+def run_req() -> None:
+    """ """
+    req = attach_proxy(http)
+    url = "http://lumtest.com/myip.json"
+
+    resp = req.get(url)
+
+    print(resp.content)
+
+
+if __name__ == "__main__":
+    run_req()
