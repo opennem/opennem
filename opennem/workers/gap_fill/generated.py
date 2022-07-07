806cae0af2c518192a44ee1ab9d2628a98434745
Nik Cubrilovic <git@nikcub.me>
2022-07-07 15:44:41 +1000

    FEAT: Generated gap_fill worker, queries and check

---
+++ opennem/workers/gap_fill/generated.py
@@ -0,0 +1,95 @@
+"""OpenNEM Gapfill for all intervals
+"""
+
+import logging
+from datetime import datetime
+from textwrap import dedent
+
+from opennem import settings
+from opennem.db import get_database_engine
+from opennem.notifications.slack import slack_message
+from opennem.schema.core import BaseConfig
+from opennem.schema.network import NetworkNEM, NetworkSchema
+
+logger = logging.getLogger("opennem.workers.gap_fill")
+
+
+class GeneratedGap(BaseConfig):
+    interval: datetime
+    network_id: str
+    has_power: bool
+    total_generated: float | None
+
+
+def query_generated_gaps(network: NetworkSchema = NetworkNEM, days: int = 7) -> list[GeneratedGap]:
+    engine = get_database_engine()
+
+    query = """
+
+        select
+            t.interval at time zone '{tz}' as interval,
+            t.network_id,
+            t.has_power,
+            t.total_generated
+        from (
+            select
+                time_bucket_gapfill('{interval_size} min', fs.trading_interval ) as interval,
+                fs.network_id,
+                case when sum(fs.generated) is NULL then FALSE else TRUE end as has_power,
+                sum(fs.generated) as total_generated
+            from facility_scada fs
+            join facility f on fs.facility_code = f.code
+            where
+                fs.trading_interval >= now() - interval '{days} days 2 hours'
+                and fs.trading_interval < now() - interval '2 hours'
+                and fs.network_id='{network_id}'
+                and f.fueltech_id not in ('imports', 'exports', 'solar_rooftop')
+            group by 1, fs.network_id
+        ) as t
+        where
+            t.has_power is FALSE
+            order by t.interval desc;
+
+    """
+
+    _results = []
+
+    _query = query.format(
+        tz=network.timezone_database, network_id=network.code, days=days, interval_size=network.interval_size
+    )
+
+    logger.debug(dedent(_query))
+
+    with engine.connect() as c:
+        _results = c.execute(_query)
+
+    _result_models = [GeneratedGap(**i) for i in _results]
+
+    return _result_models
+
+
+def run_generated_gapfill_for_network(network: NetworkSchema = NetworkNEM, days: int = 7) -> list[GeneratedGap]:
+    generated_gaps = query_generated_gaps(network, days=days)
+
+    logger.info("Found {} generated gaps".format(len(generated_gaps)))
+
+    for gap in generated_gaps:
+        logger.info("{} Running for interval {} with power: {}".format(gap.network_id, gap.interval, gap.has_power))
+
+        if settings.dry_run:
+            continue
+
+    return generated_gaps
+
+
+def check_generated_gaps() -> None:
+    """Process for checking how many generation gaps there might be"""
+    gaps = run_generated_gapfill_for_network(days=3)
+
+    if gaps:
+        slack_message(f"Found {len(gaps)} generation gaps @nik")
+
+
+# debug entry point
+if __name__ == "__main__":
+    run_generated_gapfill_for_network(days=365)
