9d31acf38804fd8dfd3383a982e7c795f00d959c
Nik Cubrilovic <git@nikcub.me>
2022-07-07 16:11:37 +1000

    FEAT: fallback runner

---
+++ opennem/workers/fallback.py
@@ -0,0 +1,19 @@
+""" """
+
+
+import logging
+from datetime import datetime
+
+from opennem.api.export.tasks import export_energy
+from opennem.workers.aggregates import run_aggregates_all_days
+from opennem.workers.emissions import run_flow_updates_all_per_year
+from opennem.workers.energy import run_energy_update_days
+
+logger = logging.getLogger("opennem.worker.fallback")
+
+
+def fallback_runner(days: int = 7) -> None:
+    run_energy_update_days(days=days)
+    run_flow_updates_all_per_year(datetime.now().year, 1)
+    run_aggregates_all_days(days=days)
+    export_energy(latest=False)
