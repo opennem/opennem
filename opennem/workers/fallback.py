3a5f6ba4445b3fa3cfdcef95b889917b0620a18e
Nik Cubrilovic <git@nikcub.me>
2022-07-27 14:10:50 +1000

    FIX: Adjust aggregate that runs in fallback_runner

--- opennem/workers/fallback.py
+++ opennem/workers/fallback.py
@@ -5,19 +5,19 @@ import logging
 from datetime import datetime
 
 from opennem.api.export.tasks import export_energy
-from opennem.workers.aggregates import run_aggregates_all_days
+from opennem.workers.aggregates import run_aggregates_all
 from opennem.workers.emissions import run_flow_updates_all_per_year
-from opennem.workers.energy import run_energy_update_days
+from opennem.workers.gap_fill.energy import run_energy_gapfill
 
 logger = logging.getLogger("opennem.worker.fallback")
 
 
 def fallback_runner(days: int = 7) -> None:
-    run_energy_update_days(days=days)
+    run_energy_gapfill(days=days)
     run_flow_updates_all_per_year(datetime.now().year, 1)
-    run_aggregates_all_days(days=days)
+    run_aggregates_all()
     export_energy(latest=False)
 
 
 if __name__ == "__main__":
-    fallback_runner()
+    fallback_runner(days=365 + 40)
