20675d660c2504e954d3ad8e8aafb96f99ff66c7
Nik Cubrilovic <git@nikcub.me>
2022-08-08 12:21:06 +1000

    FEAT: slack client supports markdown, tagging users and blocks

--- opennem/notifications/slack.py
+++ opennem/notifications/slack.py
@@ -1,7 +1,8 @@
 """ Module to send slack messages """
+import dataclasses
 import logging
 import sys
-from typing import List, Optional
+from typing import List
 
 import requests
 from validators import ValidationFailure
@@ -13,6 +14,18 @@ from opennem.utils.random_agent import get_random_agent
 logger = logging.getLogger(__name__)
 
 REQ_HEADERS = {"User-Agent": get_random_agent(), "Content-type": "application/json"}
+
+
+@dataclasses.dataclass
+class SlackMessageBlockMarkdown:
+    text: str
+    type: str = dataclasses.field(default="mrkdwn")
+
+
+@dataclasses.dataclass
+class SlackMessageBlock:
+    text: SlackMessageBlockMarkdown
+    type: str = dataclasses.field(default="section")
 
 
 def _slack_tag_list(user_list: List[str]) -> str:
@@ -29,9 +42,18 @@ def _slack_tag_list(user_list: List[str]) -> str:
     return _tag_list
 
 
+def slack_create_markdown_block(message: str) -> SlackMessageBlock:
+    """Creates a slack block"""
+    markdown_block = SlackMessageBlockMarkdown(text=message)
+    msg_block = SlackMessageBlock(text=markdown_block)
+    return msg_block
+
+
 def slack_message(msg: str, tag_users: list[str] = None) -> bool:
     """
     Post a slack message to the watchdog channel
+
+    supports markdown and tagging users
 
     """
     if not settings.slack_notifications:
@@ -47,16 +69,22 @@ def slack_message(msg: str, tag_users: list[str] = None) -> bool:
         return False
 
     alert_url = settings.slack_hook_url
-    tag_list: Optional[str] = ""
+    tag_list: str | None = ""
 
     if tag_users:
         tag_list = _slack_tag_list(tag_users)
 
     composed_message = f"{msg} {tag_list}"
 
+    blocks = []
+
+    msg_block = slack_create_markdown_block(message=composed_message)
+
+    blocks.append(msg_block)
+
     logger.info("Sending message: {}".format(composed_message))
 
-    resp = requests.post(alert_url, json={"text": composed_message})
+    resp = requests.post(alert_url, json={"text": "", "blocks": [dataclasses.asdict(i) for i in blocks]})
 
     if resp.status_code != 200:
         logger.error("Error sending slack message")
