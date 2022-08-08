fc8913a147cc9d4580f827189f9f3ff36b7b6904
Nik Cubrilovic <git@nikcub.me>
2022-08-08 17:03:53 +1000

    FIX: aggregates_network_demand_query

--- opennem/workers/aggregates.py
+++ opennem/workers/aggregates.py
@@ -25,32 +25,24 @@ def aggregates_network_demand_query(date_max: datetime, date_min: datetime, netw
                 date_trunc('day', fs.trading_interval at time zone n.timezone_database) as trading_day,
                 fs.network_id,
                 fs.network_region,
-                round(sum(fs.energy), 2) as demand_energy,
-                round(sum(fs.vwp), 2) as demand_market_value
+                round(sum(fs.energy), 2) as energy,
+                round(sum(fs.vwp), 2) as vwp,
+                round(sum(fs.vwp) / sum(fs.energy), 2) as price
             from (
                 select
-                    time_bucket_gapfill('5 minutes', fs.trading_interval) as trading_interval,
-                    fs.network_id,
-                    f.network_region,
+                    time_bucket_gapfill('5 minutes', bs.trading_interval) as trading_interval,
+                    bs.network_id,
+                    bs.network_region,
                     (sum(bs.demand_total) / 12000) as energy,
                     (sum(bs.demand_total) / 12000) * max(bs.price) as vwp
-                from facility_scada fs
-                left join facility f on fs.facility_code = f.code
-                left join network n on fs.network_id = n.code
-                left join balancing_summary bs on
-                    bs.trading_interval - INTERVAL '5 minutes' = fs.trading_interval
-                    and bs.network_id = n.network_price
-                    and bs.network_region = f.network_region
-                    and fs.network_id = 'NEM'
+                from balancing_summary bs
                 where
-                    fs.is_forecast is False
-                    and fs.network_id = 'NEM'
-                    and fs.trading_interval >= '{date_min_offset}'
-                    and fs.trading_interval < '{date_max_offset}'
+                    bs.network_id = 'NEM'
+                    and bs.trading_interval >= '{date_min}'
+                    and bs.trading_interval < '{date_max}'
                 group by
                     1, 2, 3
             ) as fs
-            left join network n on fs.network_id = n.code
             group by
                 1, 2, 3
         on conflict (trading_day, network_id, network_region) DO UPDATE set
@@ -161,6 +153,29 @@ def aggregates_facility_daily_query(date_max: datetime, date_min: datetime, netw
     )
 
     return dedent(query)
+
+
+def exec_aggregates_network_demand_query(date_min: datetime, date_max: datetime, network: NetworkSchema) -> bool:
+    resp_code: bool = False
+    engine = get_database_engine()
+    result = None
+
+    if date_max < date_min:
+        raise Exception(
+            "exec_aggregates_network_demand_query: date_max ({}) is prior to date_min ({})".format(date_max, date_min)
+        )
+
+    query = aggregates_network_demand_query(date_min=date_min, date_max=date_max, network=network)
+
+    with engine.connect() as c:
+        logger.debug(query)
+
+        if not DRY_RUN:
+            result = c.execute(query)
+
+    logger.debug(result)
+
+    return resp_code
 
 
 def exec_aggregates_facility_daily_query(date_min: datetime, date_max: datetime, network: NetworkSchema) -> bool:
