791d1dea780e35fed96c7157c1b5461f6e781f11
Nik Cubrilovic <git@nikcub.me>
2022-08-08 20:17:55 +1000

    FIX: variable referenced before assignment (RESOLVES #BACKEND-362)

--- opennem/api/export/queries.py
+++ opennem/api/export/queries.py
@@ -499,6 +499,7 @@ def demand_network_region_query(time_series: TimeSeries, network: NetworkSchema,
     """
 
     network_region_query = ""
+    network_region_select = ""
     group_by = ""
 
     if network_region:
