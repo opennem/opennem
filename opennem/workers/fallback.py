77fab44b0be48215ffdc3feb49ab82030dbae395
Nik Cubrilovic <git@nikcub.me>
2022-08-23 07:56:07 +1000

    FEAT: daily_runner method

--- opennem/workers/fallback.py
+++ opennem/workers/fallback.py
@@ -5,7 +5,7 @@ import logging
 from datetime import datetime
 
 from opennem.api.export.tasks import export_energy
-from opennem.workers.aggregates import run_aggregates_all
+from opennem.workers.aggregates import run_aggregates_all_days
 from opennem.workers.emissions import run_flow_updates_all_per_year
 from opennem.workers.gap_fill.energy import run_energy_gapfill
 
@@ -15,8 +15,15 @@ logger = logging.getLogger("opennem.worker.fallback")
 def fallback_runner(days: int = 7) -> None:
     run_energy_gapfill(days=days)
     run_flow_updates_all_per_year(datetime.now().year, 1)
-    run_aggregates_all()
+    run_aggregates_all_days(days=days)
     export_energy(latest=False)
+
+
+def daily_runner(days: int = 7) -> None:
+    run_energy_gapfill(days=days)
+    run_flow_updates_all_per_year(datetime.now().year, 1)
+    run_aggregates_all_days(days=days)
+    export_energy(latest=True)
 
 
 if __name__ == "__main__":
