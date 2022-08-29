0ef86c1e3d541769f63a2b9f3cddf2e8e47ad64e
Nik Cubrilovic <git@nikcub.me>
2022-08-29 22:28:17 +1000

    FIX: slack message on daily runner

--- opennem/workers/fallback.py
+++ opennem/workers/fallback.py
@@ -4,7 +4,9 @@
 import logging
 from datetime import datetime
 
+from opennem import settings
 from opennem.api.export.tasks import export_energy
+from opennem.notifications.slack import slack_message
 from opennem.workers.aggregates import run_aggregates_all_days
 from opennem.workers.emissions import run_emission_update_day, run_flow_updates_all_per_year
 from opennem.workers.gap_fill.energy import run_energy_gapfill
@@ -25,6 +27,7 @@ def daily_runner(days: int = 2) -> None:
     run_emission_update_day(days=days)
     run_aggregates_all_days(days=days)
     export_energy(latest=True)
+    slack_message(f"ran daily_runner on {settings.env}")
 
 
 if __name__ == "__main__":
