d151af757cc25a2b8f1db55c8d15f1770e0764af
Nik Cubrilovic <git@nikcub.me>
2022-08-29 22:23:07 +1000

    FIX: run run_emission_update_day in daily

--- opennem/workers/fallback.py
+++ opennem/workers/fallback.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 from opennem.api.export.tasks import export_energy
 from opennem.workers.aggregates import run_aggregates_all_days
-from opennem.workers.emissions import run_flow_updates_all_per_year
+from opennem.workers.emissions import run_emission_update_day, run_flow_updates_all_per_year
 from opennem.workers.gap_fill.energy import run_energy_gapfill
 
 logger = logging.getLogger("opennem.worker.fallback")
@@ -22,6 +22,7 @@ def fallback_runner(days: int = 7) -> None:
 def daily_runner(days: int = 2) -> None:
     run_energy_gapfill(days=days)
     run_flow_updates_all_per_year(datetime.now().year, 1)
+    run_emission_update_day(days=days)
     run_aggregates_all_days(days=days)
     export_energy(latest=True)
 
