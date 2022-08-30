7b79a3178ab9a1c4d1bd9afbde5badd31ae23cd4
Nik Cubrilovic <git@nikcub.me>
2022-08-31 09:26:06 +1000

    FEAT: load_fixture updated method to support pathlib and loading json or generic

--- opennem/utils/tests.py
+++ opennem/utils/tests.py
@@ -1,19 +1,28 @@
+""" Utilities used in unit tests """
 import json
-import os
+from pathlib import Path
 from typing import Any
 
-TEST_FIXTURE_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "data"))
+TEST_FIXTURE_PATH = Path(__file__).parent.parent.parent / "tests" / "fixtures"
+
+
+if not TEST_FIXTURE_PATH.is_dir():
+    raise Exception("Not a directory: {}".format(TEST_FIXTURE_PATH))
 
 
 def load_fixture(fixture_name: str) -> Any:
-    fixture_path = os.path.join(TEST_FIXTURE_PATH, fixture_name)
+    """Loads a tests file fixture from tests/fixtures"""
+    fixture_path = TEST_FIXTURE_PATH / fixture_name
 
-    if not os.path.isfile(fixture_path):
+    if not fixture_path.is_file():
         raise Exception("Not a file: {}".format(fixture_path))
 
     fixture = None
 
-    with open(fixture_path) as fh:
-        fixture = json.load(fh)
+    with fixture_path.open() as fh:
+        if fixture_path.suffix == ".json":
+            fixture = json.load(fh)
+        else:
+            fixture = fh.read()
 
     return fixture
