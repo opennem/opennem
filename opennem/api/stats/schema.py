a18c55c19ee6f177ce9959b048bf5f835b1c0595
Nik Cubrilovic <git@nikcub.me>
2022-09-09 17:06:18 +1000

    FEAT: sort all output sets in OpennemDataSet by id alphabetically

--- opennem/api/stats/schema.py
+++ opennem/api/stats/schema.py
@@ -246,10 +246,10 @@ class OpennemDataSet(BaseConfig):
 
     # Validators
     # pylint: disable=no-self-argument
-    # @validator("data", pre=True, allow_reuse=True)
-    def validate_data_unique(cls, value: list[dict[str, Any]]) -> list[OpennemData]:
+    @validator("data", pre=True, allow_reuse=True)
+    def validate_data_unique(cls, value: list[dict[str, Any]], **kwargs) -> list[OpennemData]:
         """Validate the data being set to make sure there are no duplicate ids"""
-        _id_values = [i["id"] for i in value]
+        _id_values = [i.id for i in value]
 
         if len(_id_values) != len(set(_id_values)):
             # find the ids that are not unique
@@ -264,7 +264,7 @@ class OpennemDataSet(BaseConfig):
 
             raise ValueError(f"OpennemDataSet has duplicate id{'s' if len(_id_duplicates) > 1 else ''}: {_msg}")
 
-        return value
+        return sorted(value, key=lambda x: x.id)  # type: ignore
 
     _version_fromstr = validator("created_at", allow_reuse=True, pre=True)(optionally_parse_string_datetime)
 
