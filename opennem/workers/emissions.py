4f4c7197875714360fd070bbbc9d55967dd4289b
Nik Cubrilovic <git@nikcub.me>
2022-09-29 09:48:38 +1000

    FIX: offset and network params in emissions worker

--- opennem/workers/emissions.py
+++ opennem/workers/emissions.py
@@ -22,7 +22,7 @@ from opennem import settings
 from opennem.db import get_database_engine
 from opennem.db.bulk_insert_csv import build_insert_query, generate_csv_from_records
 from opennem.db.models.opennem import AggregateNetworkFlows
-from opennem.schema.network import NetworkNEM
+from opennem.schema.network import NetworkNEM, NetworkSchema
 from opennem.utils.dates import get_last_complete_day_for_network, get_today_nem
 
 logger = logging.getLogger("opennem.workers.flows")
@@ -32,15 +32,18 @@ class FlowWorkerException(Exception):
     pass
 
 
-def load_interconnector_intervals(date_start: datetime, date_end: datetime) -> pd.DataFrame:
+def load_interconnector_intervals(
+    date_start: datetime, date_end: datetime, network: NetworkSchema | None = None
+) -> pd.DataFrame:
     """Load interconnector flows for a date range"""
     engine = get_database_engine()
-    network = NetworkNEM
+
+    # default to NEM
+    if not network:
+        network = NetworkNEM
 
     if date_start >= date_end:
-        raise FlowWorkerException(
-            "load_interconnector_intervals: date_start is more recent than date_end"
-        )
+        raise FlowWorkerException("load_interconnector_intervals: date_start is more recent than date_end")
 
     query = """
         select
@@ -52,16 +55,19 @@ def load_interconnector_intervals(date_start: datetime, date_end: datetime) -> p
         left join facility f
             on fs.facility_code = f.code
         where
-            fs.trading_interval >= '{date_start}T00:00:00+10:00'
-            and fs.trading_interval < '{date_end}T00:00:00+10:10'
+            fs.trading_interval >= '{date_start}T00:00:00{offset}'
+            and fs.trading_interval < '{date_end}T00:00:00{offset}'
             and f.interconnector is True
-            and f.network_id = 'NEM'
+            and f.network_id = '{network_id}'
         group by 1, 2, 3
         order by
             1 asc;
 
     """.format(
-        date_start=date_start.date(), date_end=date_end.date()
+        date_start=date_start.date(),
+        date_end=date_end.date(),
+        offset=network.get_offset_string(),
+        network_id=network.code,
     )
 
     logger.debug(query)
@@ -107,27 +113,30 @@ def load_energy_emission_mv_intervals(date_start: datetime, date_end: datetime) 
                     bs.network_region,
                     locf(bs.price) as price
                 from balancing_summary bs
-                    where bs.network_id='NEM'
+                    where bs.network_id='NE{network_id}M'
             ) as  bsn on
                 bsn.trading_interval = fs.trading_interval
                 and bsn.network_id = n.network_price
                 and bsn.network_region = f.network_region
-                and f.network_id = 'NEM'
+                and f.network_id = '{network_id}'
             where
                 fs.is_forecast is False and
                 f.interconnector = False and
-                f.network_id = 'NEM' and
+                f.network_id = '{network_id}' and
                 fs.generated > 0
             group by
                 1, f.code, 2
         ) as t
         where
-            t.trading_interval >= '{date_start}T00:00:00+10:00' and
-            t.trading_interval < '{date_end}T00:00:00+10:00'
+            t.trading_interval >= '{date_start}T00:00:00{offset}' and
+            t.trading_interval < '{date_end}T00:00:00{offset}'
         group by 1, 2
         order by 1 asc, 2;
     """.format(
-        date_start=date_start.date(), date_end=date_end.date()
+        date_start=date_start.date(),
+        date_end=date_end.date(),
+        network_id=network.code,
+        offset=network.get_offset_string(),
     )
 
     logger.debug(query)
@@ -145,9 +154,7 @@ def load_energy_emission_mv_intervals(date_start: datetime, date_end: datetime) 
     return df_gen
 
 
-def merge_interconnector_and_energy_data(
-    df_energy: pd.DataFrame, df_inter: pd.DataFrame
-) -> pd.DataFrame:
+def merge_interconnector_and_energy_data(df_energy: pd.DataFrame, df_inter: pd.DataFrame) -> pd.DataFrame:
     """Merge the dataframes and break down into simple frame"""
 
     region_from = pd.merge(
@@ -179,12 +186,8 @@ def merge_interconnector_and_energy_data(
 
     f = pd.concat([df_merged, df_merged_inverted])
 
-    f["energy_exports"] = f.apply(
-        lambda x: x.generated if x.generated and x.generated > 0 else 0, axis=1
-    )
-    f["energy_imports"] = f.apply(
-        lambda x: abs(x.generated) if x.generated and x.generated < 0 else 0, axis=1
-    )
+    f["energy_exports"] = f.apply(lambda x: x.generated if x.generated and x.generated > 0 else 0, axis=1)
+    f["energy_imports"] = f.apply(lambda x: abs(x.generated) if x.generated and x.generated < 0 else 0, axis=1)
 
     f["emission_exports"] = f.apply(
         lambda x: x.generated * x.emission_factor_to if x.generated and x.generated > 0 else 0,
@@ -205,21 +208,11 @@ def merge_interconnector_and_energy_data(
 
     energy_flows = pd.DataFrame(
         {
-            "energy_imports": f.groupby(
-                ["trading_interval", "interconnector_region_from"]
-            ).energy_imports.sum()
+            "energy_imports": f.groupby(["trading_interval", "interconnector_region_from"]).energy_imports.sum() / 12,
+            "energy_exports": f.groupby(["trading_interval", "interconnector_region_from"]).energy_exports.sum() / 12,
+            "emissions_imports": f.groupby(["trading_interval", "interconnector_region_from"]).emission_imports.sum()
             / 12,
-            "energy_exports": f.groupby(
-                ["trading_interval", "interconnector_region_from"]
-            ).energy_exports.sum()
-            / 12,
-            "emissions_imports": f.groupby(
-                ["trading_interval", "interconnector_region_from"]
-            ).emission_imports.sum()
-            / 12,
-            "emissions_exports": f.groupby(
-                ["trading_interval", "interconnector_region_from"]
-            ).emission_exports.sum()
+            "emissions_exports": f.groupby(["trading_interval", "interconnector_region_from"]).emission_exports.sum()
             / 12,
             "market_value_imports": f.groupby(
                 ["trading_interval", "interconnector_region_from"]
@@ -249,9 +242,7 @@ def calc_flow_for_day(day: datetime) -> pd.DataFrame:
 
     df_energy = load_energy_emission_mv_intervals(date_start=day, date_end=day_next)
 
-    df_flows = merge_interconnector_and_energy_data(df_energy=df_energy, df_inter=df_inter)
-
-    return df_flows
+    return merge_interconnector_and_energy_data(df_energy=df_energy, df_inter=df_inter)
 
 
 def calc_flows_for_range(date_start: datetime, date_end: datetime) -> pd.DataFrame:
@@ -261,9 +252,7 @@ def calc_flows_for_range(date_start: datetime, date_end: datetime) -> pd.DataFra
 
     df_energy = load_energy_emission_mv_intervals(date_start=date_start, date_end=date_end)
 
-    df_flows = merge_interconnector_and_energy_data(df_energy=df_energy, df_inter=df_inter)
-
-    return df_flows
+    return merge_interconnector_and_energy_data(df_energy=df_energy, df_inter=df_inter)
 
 
 def insert_flows(flow_results: pd.DataFrame) -> int:
@@ -296,7 +285,7 @@ def insert_flows(flow_results: pd.DataFrame) -> int:
 
     records_to_store: List[Dict] = flow_results.to_dict("records")
 
-    if len(records_to_store) < 1:
+    if not records_to_store:
         logger.error("No records returned from energy sum")
         return 0
 
@@ -325,10 +314,10 @@ def insert_flows(flow_results: pd.DataFrame) -> int:
         cursor.copy_expert(sql_query, csv_content)
         conn.commit()
     except Exception as e:
-        logger.error("Error inserting records: {}".format(e))
+        logger.error(f"Error inserting records: {e}")
         return 0
 
-    logger.info("Inserted {} records".format(len(records_to_store)))
+    logger.info(f"Inserted {len(records_to_store)} records")
 
     return len(records_to_store)
 
@@ -339,16 +328,16 @@ def run_and_store_emission_flows(day: datetime) -> None:
     try:
         emissions_day = calc_flow_for_day(day)
     except Exception as e:
-        logger.error("Flow storage error: {}".format(e))
+        logger.error(f"Flow storage error: {e}")
         return None
 
     if emissions_day.empty:
-        logger.warning("No results for {}".format(day))
+        logger.warning(f"No results for {day}")
         return None
 
     records_to_store: List[Dict] = emissions_day.to_dict("records")
 
-    logger.debug("Got {} records".format(len(records_to_store)))
+    logger.debug(f"Got {len(records_to_store)} records")
 
     insert_flows(emissions_day)
 
@@ -359,23 +348,21 @@ def run_and_store_flows_for_range(date_start: datetime, date_end: datetime) -> N
     try:
         emissions_day = calc_flows_for_range(date_start, date_end)
     except Exception as e:
-        logger.error("Flow storage error: {}".format(e))
+        logger.error(f"Flow storage error: {e}")
         return None
 
     if emissions_day.empty:
-        logger.warning("No results for {} => {}".format(date_start, date_end))
+        logger.warning(f"No results for {date_start} => {date_end}")
         return None
 
     records_to_store: List[Dict] = emissions_day.to_dict("records")
 
-    logger.debug("Got {} records".format(len(records_to_store)))
+    logger.debug(f"Got {len(records_to_store)} records")
 
     insert_flows(emissions_day)
 
 
-def run_emission_update_day(
-    days: int = 1, day: Optional[datetime] = None, offset_days: int = 1
-) -> None:
+def run_emission_update_day(days: int = 1, day: Optional[datetime] = None, offset_days: int = 1) -> None:
     """Run emission calcs for number of days"""
     # This is Sydney time as the data is published in local time
 
@@ -405,6 +392,7 @@ def run_flow_updates_for_date_range(date_start: datetime, date_end: datetime) ->
 
 
 def run_flow_updates_all_per_year(year_start: int, years: int = 1) -> None:
+    """Run emission flow updates by year"""
     for year in range(year_start, year_start - years, -1):
         date_start = datetime.fromisoformat(f"{year}-01-01T00:00:00+10:00")
         date_end = datetime.fromisoformat(f"{year}-12-31T00:00:00+10:00") + timedelta(days=1)
@@ -418,16 +406,11 @@ def run_flow_updates_all_per_year(year_start: int, years: int = 1) -> None:
             date_start = NetworkNEM.data_first_seen
 
         if NetworkNEM.data_first_seen and year < NetworkNEM.data_first_seen.year:
-            logger.info(
-                "Skipping year {} since it is earler than earliest date {}".format(
-                    year, NetworkNEM.data_first_seen
-                )
-            )
+            logger.info(f"Skipping year {year} since it is earler than earliest date {NetworkNEM.data_first_seen}")
+
             continue
 
-        logger.info(
-            "Running flow_updates_per_year for {} ({} => {})".format(year, date_start, date_end)
-        )
+        logger.info(f"Running flow_updates_per_year for {year} ({date_start} => {date_end})")
 
         if not settings.dry_run:
             run_and_store_flows_for_range(
@@ -443,6 +426,6 @@ def run_flow_updates_all_for_nem() -> None:
 # debug entry point
 if __name__ == "__main__":
     logger.info("starting")
-    # run_flow_updates_all_for_nem()
+    run_flow_updates_all_for_nem()
     # run_emission_update_day(days=12)
-    run_flow_updates_all_per_year(datetime.now().year, 1)
+    # run_flow_updates_all_per_year(datetime.now().year, 1)
