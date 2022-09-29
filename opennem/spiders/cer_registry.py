a6923f40efe6b65ede52cc860b97acc485ca679c
Nik Cubrilovic <git@nikcub.me>
2022-09-29 10:19:43 +1000

    FIX: remove old module

--- opennem/spiders/cer_registry.py
+++
@@ -1,6 +0,0 @@
-from scrapy import Spider
-
-
-class CERRegistrySpider(Spider):
-    # name = "au.cer.registry"
-    start_urls = ["https://www.rec-registry.gov.au/rec-registry/app/public/power-station-register"]
