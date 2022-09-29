5d5569b2d9e1bb35c840899507fa7e1b0e0ed824
Nik Cubrilovic <git@nikcub.me>
2022-09-29 13:14:22 +1000

    FEAT: fixtures module, update refresh_fixtures

---
+++ scripts/fixtures.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+Script for methods to run for fixtures
+"""
+
+
+import logging
+from datetime import datetime
+
+from opennem import settings
+from opennem.api.export.map import PriorityType, StatType, get_export_map
+from opennem.api.export.tasks import export_energy, export_power
+from opennem.exporter.historic import export_historic_for_year_and_week_no
+from opennem.schema.network import NetworkNEM
+from opennem.utils.dates import get_current_week_for_network
+from opennem.workers.aggregates import run_aggregates_all, run_aggregates_all_days
+from opennem.workers.daily_summary import run_daily_fueltech_summary
+from opennem.workers.emissions import (
+    run_emission_update_day,
+    run_flow_updates_all_for_network,
+    run_flow_updates_all_per_year,
+)
+from opennem.workers.gap_fill.energy import run_energy_gapfill
+
+logger = logging.getLogger("opennem.scripts.fixtures")
+
+
+# get week number for last week
+CURRENT_WEEK = get_current_week_for_network(network=NetworkNEM)
+CURRENT_YEAR = datetime.now().year
+
+WEEK_TO_FETCH = CURRENT_WEEK - 1 if CURRENT_WEEK > 1 else 52
+
+
+def export_historic_weekly() -> None:
+    export_historic_for_year_and_week_no(2022, WEEK_TO_FETCH, [NetworkNEM])
+
+
+def export_energy_all() -> None:
+    """Export all energy data"""
+    # run exports for all
+    export_map = get_export_map()
+    energy_exports = export_map.get_by_stat_type(StatType.energy).get_by_priority(PriorityType.monthly)
+    export_energy(energy_exports.resources)
+
+
+def export_energy_current_year() -> None:
+    """Export energy data for current year"""
+    # run exports for all
+    export_map = get_export_map()
+    energy_exports = (
+        export_map.get_by_stat_type(StatType.energy).get_by_priority(PriorityType.daily).get_by_year(CURRENT_YEAR)
+    )
+    export_energy(energy_exports.resources)
+
+
+def refresh_fixtures() -> None:
+    """this is triggered from ./scripts/refresh_fixtures.sh"""
+    export_historic_weekly()
+    export_power(latest=True)
+    export_energy_all()
+    export_energy_current_year()
+
+
+if __name__ == "__main__":
+    refresh_fixtures()
--- scripts/refresh_fixtures.sh
+++ scripts/refresh_fixtures.sh
@@ -10,7 +10,7 @@ REGIONS_ALL=("NSW1" "QLD1" "VIC1" "TAS1" "SA1")
 WEEK_TO_FETCH=$(($(date +%U) - 1))
 
 # refresh exports
-python -c "from opennem.schema.network import NetworkNEM; from opennem.exporter.historic import export_historic_for_year_and_week_no; export_historic_for_year_and_week_no(2022, ${WEEK_TO_FETCH}, [NetworkNEM])"
+./scripts/fixtures.py
 
 for REGION_TO_FETCH in "${REGIONS_ALL[@]}"
 do
@@ -20,13 +20,17 @@ do
   git add tests/fixtures/nem_${REGION_TO_FETCH:l}_week.json
 
   print "Fetching https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/power/7d.json"
-  curl https://data.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/power/7d.json --output - --silent | jq . > tests/fixtures/nem_${REGION_TO_FETCH:l}_7d.json
+  curl https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/power/7d.json --output - --silent | jq . > tests/fixtures/nem_${REGION_TO_FETCH:l}_7d.json
   git add tests/fixtures/nem_${REGION_TO_FETCH:l}_7d.json
 
   print "Fetching https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/energy/${CURRENT_YEAR}.json"
-  curl https://data.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/energy/${CURRENT_YEAR}.json --output - --silent | jq . > tests/fixtures/nem_${REGION_TO_FETCH:l}_1y.json
+  curl https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/energy/${CURRENT_YEAR}.json --output - --silent | jq . > tests/fixtures/nem_${REGION_TO_FETCH:l}_1y.json
   git add tests/fixtures/nem_${REGION_TO_FETCH:l}_1y.json
+
+  print "Fetching https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/energy/all.json"
+  curl https://data.dev.opennem.org.au/v3/stats/au/NEM/${REGION_TO_FETCH:u}/energy/all.json --output - --silent | jq . > tests/fixtures/nem_${REGION_TO_FETCH:l}_all.json
+  git add tests/fixtures/nem_${REGION_TO_FETCH:l}_all.json
 
 done
 
-git commit -m "Refreshed fixtures for ${REGION_TO_FETCH:l}"
+git commit -m "Refreshed fixtures all regions"
