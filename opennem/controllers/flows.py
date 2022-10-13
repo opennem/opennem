43f262a611d0eb6a59d3cd62c05cedaf6752aeb2
Nik Cubrilovic <git@nikcub.me>
2022-10-13 15:46:19 +0000

    FEAT: Flows controller module

---
+++ opennem/controllers/flows.py
@@ -0,0 +1,22 @@
+""" Flow controllers """
+from datetime import datetime
+
+import pandas as pd
+
+from opennem.db import get_database_engine
+from opennem.queries.flows import get_interconnector_intervals_query
+from opennem.schema.network import NetworkSchema
+
+
+def get_network_interconnector_intervals(
+    date_start: datetime, date_end: datetime, network: NetworkSchema, as_dataframe: bool = False
+) -> list[dict]:
+    """Get interconnector intervals for a network"""
+    engine = get_database_engine()
+
+    query = get_interconnector_intervals_query(date_start, date_end, network)
+
+    with engine.connect() as conn:
+        results = conn.execute(query)
+
+    return results
