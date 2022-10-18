ddcaa5f6090344e29f1c2d66524464894ec30331
Nik Cubrilovic <git@nikcub.me>
2022-10-19 09:51:05 +1100

    FEAT: Charting utils module

---
+++ opennem/utils/chart.py
@@ -0,0 +1,85 @@
+""" Utility to generate simple charts """
+
+import logging
+from dataclasses import dataclass, field
+from datetime import datetime
+from decimal import Decimal
+
+import matplotlib.pyplot as plt
+
+from opennem.api.stats.schema import ValidNumber
+
+ValidNumber = float | int | Decimal
+
+
+logger = logging.getLogger("opennem.utils.chart")
+
+
+@dataclass
+class PlotValues:
+    interval: datetime
+    value: ValidNumber
+
+
+@dataclass
+class PlotIntegerValues:
+    interval: int
+    value: ValidNumber
+
+
+@dataclass
+class PlotSeries:
+    values: list[PlotValues | PlotIntegerValues]
+    label: str | None
+    color: str = field(default="blue")
+
+    def x(self) -> list[datetime]:
+        return [i.interval for i in self.values]
+
+    def y(self) -> list[ValidNumber]:
+        return [i.value for i in self.values]
+
+
+@dataclass
+class Plot:
+    series: list[PlotSeries]
+    destination_file: str | None = field(default=None)
+    title: str | None = field(default=None)
+    legend: bool = field(default=True)
+
+
+def chart_line(plot: Plot, show: bool = False) -> None:
+    """Creates a line chart from the series and returns"""
+    _, ax = plt.subplots()
+
+    if plot.title:
+        ax.set_title(plot.title)
+
+    for s in plot.series:
+        ax.plot(s.x(), s.y(), s.color, label=s.label)
+        ax.axhline(0, color="grey", linewidth=0.8)
+        ax.set_ylabel(s.label)
+
+    if plot.legend:
+        ax.legend()
+
+    if show:
+        plt.show()
+
+    if plot.destination_file:
+        plt.savefig(plot.destination_file)
+        logger.info(f"Saved figure to {plot.destination_file}")
+
+    return plot
+
+
+if __name__ == "__main__":
+    p = Plot(
+        series=[
+            PlotSeries(values=[PlotValues(interval=datetime.now(), value=1)], label="test"),
+            PlotSeries(values=[PlotValues(interval=datetime.now(), value=3)], label="test 2"),
+        ],
+        title="test",
+        legend=True,
+    )
+    chart_line(p, show=True)
