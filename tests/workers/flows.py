f947fc0254807a69b46181bd81851838e7a8e542
Nik Cubrilovic <git@nikcub.me>
2022-10-19 14:23:09 +1100

    FIX: flows unit test utility: get_flow_fixture_dataframe

---
+++ tests/workers/flows.py
@@ -0,0 +1,25 @@
+""" Unit tests for the flows worker """
+from pathlib import Path
+
+import pandas as pd
+
+from opennem.schema.network import NetworkNEM
+from opennem.utils.tests import TEST_FIXTURE_PATH
+from tests.conftest import TestException
+
+
+def get_flow_fixture_dataframe(filename: str) -> pd.DataFrame:
+    """Get interconnector intervals for a time series"""
+    fixture_path = TEST_FIXTURE_PATH / "workers" / filename
+
+    if not fixture_path.is_file():
+        raise TestException(f"Not a file {fixture_path}")
+
+    df_gen = pd.read_csv(fixture_path, parse_dates=["trading_interval"], index_col=["trading_interval"])
+
+    if df_gen.empty:
+        raise TestException("No results from load_interconnector_intervals")
+
+    df_gen.index = df_gen.index.tz_convert(tz=NetworkNEM.get_fixed_offset())
+
+    return df_gen
