552bf85833b4be3d0a56f3bb8294f4660904f117
Nik Cubrilovic <git@nikcub.me>
2022-10-20 17:59:37 +0000

    FEAT: flow comparison + charting + outputs

--- scripts/flows.py
+++ scripts/flows.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from textwrap import dedent, indent
 from typing import Dict
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import pydantic
 
@@ -23,39 +22,21 @@ from opennem.queries.flows import get_interconnector_intervals_query
 from opennem.schema.dates import DatetimeRange, TimeSeries
 from opennem.schema.network import NetworkNEM, NetworkSchema
 from opennem.schema.time import TimeInterval
+from opennem.utils.chart import Plot, PlotIntegerValues, PlotSeries, PlotValues, chart_line
 from opennem.utils.dates import date_series, get_last_complete_day_for_network
 from opennem.utils.interval import get_human_interval
-from opennem.workers.emissions import load_interconnector_intervals
+from opennem.workers.emissions import (
+    calc_flows_for_range,
+    load_interconnector_intervals,
+    merge_interconnector_and_energy_data,
+)
+from tests.workers.flows import get_flow_fixture_dataframe
 
 logger = logging.getLogger("opennem.flows")
 
 COMPARISON_START_DATE = datetime.fromisoformat("2022-01-01T00:00:00")
 
 COMPARISON_END_DATE = datetime.fromisoformat("2022-08-01:00:00")
-
-
-@dataclass
-class PlotSeries:
-    interval: datetime
-    value: ValidNumber
-
-
-def plot_flows(flows: list[PlotSeries], destination_file: str = "flows.png") -> None:
-    _, ax = plt.subplots()
-
-    x = [i.interval for i in flows]
-    y = [i.value for i in flows]
-
-    vic1 = ax.bar(x, y, label="vic1")
-    ax.axhline(0, color="grey", linewidth=0.8)
-    ax.set_ylabel("value")
-    ax.set_title("OpenNEM Flows")
-    # ax.bar_label(vic1, label_type="center")
-
-    ax.legend()
-    plt.show()
-    plt.savefig(destination_file)
-    logger.info(f"Saved figure to {destination_file}")
 
 
 def get_aemo_tableset(tableset_path: Path) -> AEMOTableSet:
@@ -120,10 +101,12 @@ def get_network_flows_emissions_market_value_query(time_series: TimeSeries, netw
     )
 
 
-def flow_plot() -> None:
+from collections import namedtuple
+
+
+def flow_plot(date_start: datetime, date_end: datetime) -> None:
     """Plot flows"""
-    date_end = get_last_complete_day_for_network(NetworkNEM)
-    date_start = date_end - timedelta(days=1)
+    engine = get_database_engine()
 
     # interconnector_results = get_network_interconnector_intervals(date_start, date_end, network=NetworkNEM)
     ts = TimeSeries(
@@ -133,9 +116,128 @@ def flow_plot() -> None:
     )
     derived_table = get_network_flows_emissions_market_value_query(time_series=ts, network_region_code="NSW1")
 
-    print(derived_table)
+    with engine.begin() as conn:
+        results = db_extract_result_records(conn, derived_table)
+
+    return results
+
+
+# TODO Rename this here and in `flow_plot`
+def db_extract_result_records(conn, derived_table) -> list[Dict[str, ValidNumber]]:
+    result_proxy = conn.execute(derived_table)
+    first_result = result_proxy.fetchone()
+
+    ResultRecord = namedtuple("ResultRecord", first_result.keys())
+    record = ResultRecord(*first_result)
+
+    results = [record._asdict()]
+
+    for row in result_proxy.fetchall():
+        record = ResultRecord(*row)
+        results.append(record._asdict())
+
+    return results
+
+
+def plot_emissions_from_fixtures() -> Dict:
+    """ """
+    data = {}
+
+    for year in [2022, 2015]:
+        data[year] = {}
+
+        # interconnector data
+        df_inter = get_flow_fixture_dataframe(f"interconnector_intervals_{year}.csv")
+
+        # energy data
+        df_gen = get_flow_fixture_dataframe(f"energy_set_{year}.csv")
+        df_gen["price"] = df_gen["market_value"] / df_gen["generated"]
+        df_gen["emission_factor"] = df_gen["emissions"] / df_gen["generated"]
+
+        # assign objects
+        data[year]["interconnector"] = df_inter
+        data[year]["energy"] = df_gen
+
+        edf = merge_interconnector_and_energy_data(
+            df_energy=data[year]["energy"], df_inter=data[year]["interconnector"], scale=NetworkNEM.intervals_per_hour
+        ).reset_index()
+
+        data[year]["emissions"] = edf
+
+    return data
+
+
+def part_date(ts: pd.Timestamp) -> datetime:
+    """Convert pandas timestamp to parted datetime"""
+    dt = ts.to_pydatetime()
+    dt = dt.replace(year=2022)
+    return dt
+
+
+def run_fixture_test(
+    set_name: str = "emissions", field_name: str = "energy_imports", filter_field: str = "network_region"
+) -> None:
+    """ """
+    data = plot_emissions_from_fixtures()
+
+    plot_series = []
+
+    colors = {2015: "blue", 2022: "red"}
+
+    for year in [2022, 2015]:
+        edf = data[year][set_name]
+        series = edf[edf[filter_field] == "NSW1"][field_name]
+        series = series.reset_index()
+
+        plot_series.append(
+            PlotSeries(
+                values=[
+                    PlotIntegerValues(interval=part_date(r["trading_interval"]), value=r[field_name])
+                    for r in series.to_dict("records")
+                ],
+                label=f"{year} {set_name} {field_name}",
+                color=colors.get(year, "grey"),
+            )
+        )
+
+    p = Plot(series=plot_series, title="Data comparison", legend=True)
+    chart_line(plot=p, show=True)
+
+
+def run_test() -> None:
+    date_start = datetime.fromisoformat("2022-08-01T00:00:00+10:00")
+    date_end = datetime.fromisoformat("2022-08-30T00:00:00+10:00")
+
+    flows_result = calc_flows_for_range(date_start, date_end, network=NetworkNEM)
+    records_new = flows_result[flows_result.index.get_level_values(2) == "NSW1"].reset_index().to_dict("records")
+
+    date_start = date_start.replace(year=2015)
+    date_end = date_end.replace(year=2015)
+
+    flows_result = calc_flows_for_range(date_start, date_end, network=NetworkNEM)
+    records_old = flows_result[flows_result.index.get_level_values(2) == "NSW1"].reset_index().to_dict("records")
+    # records_old = flow_plot(date_start, date_end)
+
+    series = [
+        PlotSeries(
+            values=[
+                PlotIntegerValues(interval=r["trading_interval"].day, value=r["energy_imports"]) for r in records_new
+            ],
+            label="Imports 2022",
+        ),
+        PlotSeries(
+            values=[
+                PlotIntegerValues(interval=r["trading_interval"].day, value=r["energy_imports"]) for r in records_old
+            ],
+            label="Imports 2015",
+            color="g",
+        ),
+    ]
+    p = Plot(series=series, title="Flow comparison", legend=True)
+    chart_line(plot=p, show=True)
 
 
 if __name__ == "__main__":
     # get_aemo_raw_values()
-    flow_plot()
+    run_fixture_test("interconnector", "generated", "interconnector_region_from")
+    # run_fixture_test("energy", "generated")
