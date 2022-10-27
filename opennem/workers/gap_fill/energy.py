c5d49ea8de975adac491052e9cbf86d876a582b8
Nik Cubrilovic <git@nikcub.me>
2022-10-27 23:56:42 +1100

    FIX: gap_fill.energy - arbitary date range, separate out day runner, format strings

--- opennem/workers/gap_fill/energy.py
+++ opennem/workers/gap_fill/energy.py
@@ -4,13 +4,16 @@
 import logging
 from datetime import datetime, timedelta
 from textwrap import dedent
-from typing import List, Optional
+
+from datetime_truncate import truncate as date_trunc
 
 from opennem import settings
 from opennem.core.networks import network_from_network_code
 from opennem.db import get_database_engine
 from opennem.schema.core import BaseConfig
 from opennem.schema.network import NetworkAEMORooftop, NetworkAPVI, NetworkNEM, NetworkSchema, NetworkWEM
+from opennem.utils.dates import get_last_complete_day_for_network
+from opennem.utils.timezone import is_aware
 from opennem.workers.energy import run_energy_calc
 
 logger = logging.getLogger("opennem.workers.gap_fill")
@@ -21,14 +24,16 @@ class EnergyGap(BaseConfig):
     network_id: str
     has_power: bool
     has_energy: bool
-    total_generated: Optional[float]
-    total_energy: Optional[float]
+    total_generated: float | None
+    total_energy: float | None
 
 
-def query_energy_gaps(network: NetworkSchema = NetworkNEM, days: int = 7) -> List[EnergyGap]:
+def query_energy_gaps(date_min: datetime, date_max: datetime, network: NetworkSchema = NetworkNEM) -> list[EnergyGap]:
+    """Query gaps in energy and power. Minimum date is inclusive, maximum date is exclusive"""
+
     engine = get_database_engine()
 
-    query = """
+    __query = """
 
         select
             t.interval at time zone '{tz}' as interval,
@@ -48,8 +53,8 @@ def query_energy_gaps(network: NetworkSchema = NetworkNEM, days: int = 7) -> Lis
             from facility_scada fs
             join facility f on fs.facility_code = f.code
             where
-                fs.trading_interval >= now() - interval '{days} days 2 hours'
-                and fs.trading_interval < now() - interval '2 hours'
+                fs.trading_interval >= '{date_min}'
+                and fs.trading_interval < '{date_max}'
                 and fs.network_id='{network_id}'
             group by 1, fs.network_id
         ) as t
@@ -59,35 +64,53 @@ def query_energy_gaps(network: NetworkSchema = NetworkNEM, days: int = 7) -> Lis
 
     """
 
-    _results = []
+    results = []
 
-    _query = query.format(tz=network.timezone_database, network_id=network.code, days=days)
+    # Set the timezone on dates if they haven't been already
+    for dt in {date_min, date_max}:
+        if not is_aware(dt):
+            dt = dt.replace(tzinfo=network.get_fixed_offset())
 
-    logger.debug(dedent(_query))
+    # Truncate dates down to each hour
+    # @NOTE we can make this a parm and adjust both the bucket size and the trunc
+    for dt in {date_min, date_max}:
+        dt = date_trunc(dt, "hour")
+
+    query = __query.format(tz=network.timezone_database, date_min=date_min, date_max=date_max, network_id=network.code)
+
+    logger.debug(dedent(query))
 
     with engine.connect() as c:
-        _results = c.execute(_query)
+        results = c.execute(query)
 
-    _result_models = [EnergyGap(**i) for i in _results]
-
-    return _result_models
+    return [EnergyGap(**i) for i in results]
 
 
-def run_energy_gapfill_for_network(network: NetworkSchema = NetworkNEM, days: int = 7, run_all: bool = False) -> None:
-    energy_gaps = query_energy_gaps(network, days=days)
+def run_energy_gapfill_for_network_by_days(
+    network: NetworkSchema = NetworkNEM, days: int = 7, fill_all: bool = False, fill_gaps: bool = True
+) -> None:
+    """Run energy gapfilling. If fill all it will ignore if it has energy. If fill gaps it will only fill gaps"""
+
+    # calculate
+    date_max = get_last_complete_day_for_network(network)
+    date_min = date_max - timedelta(days=days)
+
+    energy_gaps = query_energy_gaps(date_min=date_min, date_max=date_max, network=network)
 
     energy_gaps_filtered = list(filter(lambda x: x.has_power is True and x.has_energy is False, energy_gaps))
 
-    if run_all:
+    if fill_all:
         energy_gaps_filtered = energy_gaps
 
-    logger.info("Found {} energy gaps interval hours".format(len(energy_gaps_filtered)))
+    logger.info(f"Found {len(energy_gaps_filtered)} energy gaps interval hours")
+
+    if not fill_gaps:
+        return energy_gaps
 
     for gap in energy_gaps_filtered:
         logger.info(
-            "{} Running for interval {} with power: {} energy: {} energy_value: {}".format(
-                gap.network_id, gap.interval, gap.has_power, gap.has_energy, gap.total_energy
-            )
+            f"{gap.network_id} Running for interval {gap.interval} with power: {gap.has_power} \
+                energy: {gap.has_energy} energy_value: {gap.total_energy}"
         )
 
         if settings.dry_run:
@@ -99,27 +122,37 @@ def run_energy_gapfill_for_network(network: NetworkSchema = NetworkNEM, days: in
         try:
             run_energy_calc(dmin, dmax, network=network_from_network_code(gap.network_id))
         except Exception:
-            logger.error("Error running {} energy gapfill for {} => {}".format(gap.network_id, dmin, dmax))
+            logger.error(f"Error running {gap.network_id} energy gapfill for {dmin} => {dmax}")
 
 
-def run_energy_gapfill(
+def run_energy_gapfill_previous_days(
     days: int = 14,
-    networks: List[NetworkSchema] = [NetworkNEM, NetworkWEM, NetworkAPVI, NetworkAEMORooftop],
+    networks: list[NetworkSchema] = [NetworkNEM, NetworkWEM, NetworkAPVI, NetworkAEMORooftop],
     run_all: bool = False,
 ) -> None:
     """Run gapfill of energy values - will find gaps in energy values and run energy_sum"""
     for network in networks:
-        logger.info("Running energy gapfill for {}".format(network.code))
+        logger.info(f"Running energy gapfill for {network.code}")
 
         try:
-            run_energy_gapfill_for_network(network, days=days, run_all=run_all)
+            run_energy_gapfill_for_network_by_days(network, days=days, fill_all=run_all)
         except Exception as e:
-            logger.error("gap_fill run error: {}".format(e))
+            logger.error(f"gap_fill run error: {e}")
 
 
 # debug entry point
 if __name__ == "__main__":
     # run_energy_gapfill(days=365/2)
-    dmin = datetime.fromisoformat("2022-02-28T00:00:00+10:00")
-    dmax = dmin + timedelta(days=3)
-    run_energy_calc(dmin, dmax, network=network_from_network_code("NEM"))
+    # dmin = datetime.fromisoformat("2022-02-28T00:00:00+10:00")
+    # dmax = dmin + timedelta(days=3)
+    # run_energy_calc(dmin, dmax, network=network_from_network_code("NEM"))
+
+    # energy_intervals = query_energy_gaps(network=network_from_network_code("NEM"), days=301)
+    # energy_gaps = list(filter(lambda x: x.has_energy is False, energy_intervals))
+
+    energy_gaps = run_energy_gapfill_for_network_by_days(network=NetworkNEM, fill_gaps=False)
+
+    print(f"{len(energy_gaps)} gaps")
+
+    for i in energy_gaps:
+        print(i.interval, i.has_power, i.has_energy, i.total_energy)
