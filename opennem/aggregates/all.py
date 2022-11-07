d541c8c87750b5473672194841a9ab85d7794a53
Nik Cubrilovic <git@nikcub.me>
2022-11-07 12:18:04 +1100

    FEAT: all runner in aggregates.all will also flush/run exports

--- opennem/aggregates/all.py
+++ opennem/aggregates/all.py
@@ -2,6 +2,7 @@
 
 from opennem.aggregates.facility_daily import run_aggregate_facility_all_by_year, run_aggregate_facility_days
 from opennem.aggregates.network_demand import run_aggregates_demand_network, run_aggregates_demand_network_days
+from opennem.api.export.tasks import export_all_daily, export_all_monthly, export_energy, export_power
 from opennem.schema.network import NetworkAEMORooftop, NetworkAPVI, NetworkNEM, NetworkWEM
 from opennem.workers.emissions import run_emission_update_day, run_flow_updates_all_for_network
 from opennem.workers.energy import run_energy_update_all
@@ -28,3 +29,10 @@ def run_aggregates_all() -> None:
             run_flow_updates_all_for_network(network=network)
 
     run_aggregates_demand_network()
+
+    # run the exports for all
+    export_power(latest=False)
+    export_energy(latest=False)
+
+    export_all_daily()
+    export_all_monthly()
