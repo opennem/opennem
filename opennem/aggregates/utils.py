7843f0a48cdc1fb65d5a0847df75058fe0e79ea3
Nik Cubrilovic <git@nikcub.me>
2023-01-01 09:52:26 +1100

    FIX: get_aggregate_year_range debug entry point

--- opennem/aggregates/utils.py
+++ opennem/aggregates/utils.py
@@ -8,11 +8,16 @@ from opennem.utils.dates import get_today_opennem
 def get_aggregate_year_range(year: int, network: NetworkSchema = NetworkNEM) -> tuple[datetime, datetime]:
     """Get a date range for a year with end exclusive"""
     tz = network.get_fixed_offset()
+    today = get_today_opennem()
 
     date_min = datetime(year, 1, 1, 0, 0, 0, 0, tzinfo=tz)
     date_max = datetime(year + 1, 1, 1, 0, 0, 0, 0, tzinfo=tz)
 
-    if year == get_today_opennem().year:
+    if year == today.year:
         date_max = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=tz)
 
     return date_min, date_max
+
+
+if __name__ == "__main__":
+    print(get_aggregate_year_range(2023))
