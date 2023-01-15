e0c54824ae9e2380be13d17a624e7941badba1f3
Nik Cubrilovic <git@nikcub.me>
2023-01-16 10:31:21 +1100

    FIX: Incorrect interval time and parsing in query for power_network_flow_query (#160)

--- opennem/queries/flows.py
+++ opennem/queries/flows.py
@@ -45,7 +45,7 @@ def power_network_flow_query(time_series: OpennemExportSeries, network_region: s
 
     ___query = """
     select
-        time_bucket_gapfill({interval}, nf.trading_interval) as trading_interval,
+        time_bucket_gapfill('{interval}', nf.trading_interval) as trading_interval,
         nf.network_id,
         nf.network_region,
         avg(nf.energy_imports) as energy_imports,
@@ -71,7 +71,7 @@ def power_network_flow_query(time_series: OpennemExportSeries, network_region: s
     date_min = time_series_range.start
 
     query = ___query.format(
-        interval=time_series.interval.trunc,
+        interval=time_series.interval.interval_sql,
         network_id=time_series.network.code,
         network_region=network_region,
         date_start=date_min,
