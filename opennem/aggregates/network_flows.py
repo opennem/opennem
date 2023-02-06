060073efb2354967828e71da112d79d1db190e18
Nik Cubrilovic <git@nikcub.me>
2023-02-06 15:31:18 +0000

    FIX: network flow aggregate profile messages neater (#167)

--- opennem/aggregates/network_flows.py
+++ opennem/aggregates/network_flows.py
@@ -370,7 +370,14 @@ def run_flow_update_for_interval(interval: datetime, network: NetworkSchema | No
     return run_and_store_flows_for_range(date_start, date_end, network=network)
 
 
-@profile_task(send_slack=True, include_args=True, level=ProfilerLevel.INFO, retention_period=ProfilerRetentionTime.FOREVER)
+@profile_task(
+    send_slack=True,
+    message_fmt="Ran emission update for day {day}",
+    message_prepend=True,
+    include_args=True,
+    level=ProfilerLevel.INFO,
+    retention_period=ProfilerRetentionTime.FOREVER,
+)
 def run_emission_update_day(days: int = 1, day: datetime | None = None, offset_days: int = 1) -> None:
     """Run emission calcs for number of days"""
     # This is Sydney time as the data is published in local time
@@ -400,7 +407,14 @@ def run_flow_updates_for_date_range(date_start: datetime, date_end: datetime) ->
         current_day -= timedelta(days=1)
 
 
-@profile_task(send_slack=True, include_args=True, level=ProfilerLevel.INFO, retention_period=ProfilerRetentionTime.FOREVER)
+@profile_task(
+    send_slack=True,
+    message_fmt="Ran flow updated all for last year",
+    message_prepend=True,
+    include_args=True,
+    level=ProfilerLevel.INFO,
+    retention_period=ProfilerRetentionTime.FOREVER,
+)
 def run_flow_updates_all_per_year(year_start: int, years: int = 1, network: NetworkSchema | None = None) -> None:
     """Run emission flow updates by year"""
 
