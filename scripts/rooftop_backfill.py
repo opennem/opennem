da5b51f417ea261ad6a0a3b33b6ab2d6bb43e0ae
Nik Cubrilovic <git@nikcub.me>
2023-02-28 19:01:25 +1100

    FEAT: rooftop backfill network fill script (#182)

---
+++ scripts/rooftop_backfill.py
@@ -0,0 +1,57 @@
+""" Crates the OPENNEM ROOFTOP BACKFILL network"""
+
+import logging
+
+from sqlalchemy import text as sql
+
+from opennem.db import get_database_engine
+
+logger = logging.getLogger("opennem.rooftop_backfill")
+
+
+def run_import_opennem_rooftop_backfill() -> None:
+    """Runs the query that creates OPENNEM ROOFTOP BACKFILL"""
+
+    query = sql(
+        """
+        insert into facility_scada
+        (
+            created_by,
+            created_at,
+            network_id,
+            trading_interval,
+            facility_code,
+            generated,
+            eoi_quantity,
+            is_forecast,
+            energy_quality_flag
+        )
+        select
+            'opennem.backfill' as created_by,
+            now() as created_at,
+            'OPENNEM_ROOFTOP_BACKFILL' as network_id,
+            fs.trading_interval as trading_interval,
+            facility_code,
+            generated,
+            generated / 4 as eoi_quantity,
+            is_forecast,
+            0
+        from facility_scada fs
+        where
+            fs.network_id = 'APVI' and
+            fs.trading_interval < '2016-07-31 14:30:00+00'
+        ;
+
+
+    """
+    )
+
+    engine = get_database_engine()
+
+    with engine.begin() as conn:
+        logger.debug(query)
+        conn.execute(query)
+
+
+if __name__ == "__main__":
+    run_import_opennem_rooftop_backfill()
