417c33ccf34c57db151b2ac8920c9b042c63a0e0
Nik Cubrilovic <git@nikcub.me>
2023-03-14 21:22:02 +1100

    FIX: versioning and utils.version

--- opennem/utils/version.py
+++ opennem/utils/version.py
@@ -2,18 +2,10 @@
 
 @NOTE updated to dynamically update with build scripts
 """
-import logging
 
-from opennem import __version__
-
-logger = logging.getLogger(__name__)
-
-version = __version__
-
-release = version
+version = "3.13.0-beta0"
 
 
 def get_version() -> str:
     """@TODO remove this and use poetry versioning"""
-
     return version
