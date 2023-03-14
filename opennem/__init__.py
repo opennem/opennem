d46e5e18ece0b0e31526bed40d8cb1b21501470c
Nik Cubrilovic <git@nikcub.me>
2023-03-14 19:10:51 +1100

    bump version 3.13.0-beta1 -> 3.13.0-beta2

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -36,7 +36,7 @@ warnings.filterwarnings("ignore", module="openpyxl")
 
 
 # Module variables
-__version__ = "3.13.0-beta1"
+__version__ = "3.13.0-beta2"
 __env__ = "prod"
 __package__ = "opennem"
 
