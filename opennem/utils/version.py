d9fa3ec238c7dc60a9fff362f43ff3c42b31394e
Nik Cubrilovic <git@nikcub.me>
2023-03-14 20:47:55 +1100

    FIX: get_version uses opennem.__version__ and doesn't parse toml

--- opennem/utils/version.py
+++ opennem/utils/version.py
@@ -1,68 +1,14 @@
-import logging
-from datetime import datetime
-from pathlib import Path
+""" Reads the project version
 
-try:
-    import tomlkit
-except ModuleNotFoundError:
-    print("Could not import external package. virtualenv likely not activated.")
+@NOTE updated to dynamically update with build scripts
+"""
+import logging
+
+from opennem import __version__
 
 logger = logging.getLogger(__name__)
 
-
-# get metadata from pyproject.toml
-def get_project_meta() -> dict | None:
-    pyproject = Path(__file__).parent / "../../pyproject.toml"
-
-    if not pyproject.is_file():
-        return None
-
-    with pyproject.open() as pp_file:
-        file_contents = pp_file.read()
-
-    project_meta = None
-
-    try:
-        project_meta = tomlkit.parse(file_contents)
-    except Exception as e:
-        logger.error(f"Error parsing project metadata: {e}")
-        return None
-
-    return dict(project_meta)
-
-
-# get version from VERSION file
-def get_pkg_version() -> str | None:
-    import pkgutil
-
-    pkg_data = None
-
-    try:
-        pkg_data = pkgutil.get_data(__package__, "VERSION")
-    except Exception:
-        pass
-
-    if not pkg_data:
-        return None
-
-    version = pkg_data.decode("utf-8").strip()
-
-    del pkgutil
-
-    return version
-
-
-pkg_meta = get_project_meta()
-version = None
-
-if pkg_meta:
-    CUR_YEAR = datetime.now().year
-    poetry_meta = pkg_meta["tool"]["poetry"]
-    project = str(poetry_meta["name"])
-    copyright = f"{CUR_YEAR}, OpenNEM"
-    version = str(poetry_meta["version"])
-else:
-    raise Exception("Could not read project meta. Likely virtualenv not activated")
+version = __version__
 
 release = version
 
