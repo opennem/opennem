ed701f92ab969f134999384f0a653b54423fdaec
Nik Cubrilovic <git@nikcub.me>
2023-03-14 18:58:00 +1100

    DEV: Update Makefile and make_release script

--- Makefile
+++ Makefile
@@ -19,12 +19,24 @@ format:
 pyright:
 	$(pyright)
 
+.PHONY: install
 install:
 	pip install -r ./requirements.txt
 
+.PHONY: build
 build:
 	pip install wheel
 	python setup.py sdist bdist_wheel
+
+.PHONY: bump
+bump:
+	bumpver update --${1-patch}
+
+.PHONY: requirements
+requirements:
+	poetry export --format requirements.txt -E postgres --without-hashes > requirements.txt
+	poetry export --with dev --format requirements.txt --without-hashes > requirements_dev.txt
+	git add requirements.txt requirements_dev.txt
 
 pyclean:
 	find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
@@ -35,5 +47,4 @@ cleandist:
 codecov:
 	pytest --cov=./opennem
 
-
-.PHONY: install build release pyright
+release: format requirements bump
--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,29 +1,26 @@
 set -euxo pipefail
 
+# @TODO migrate this file to Makefile
+
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 #pytest
 
-# run ruff linter
-# ruff . --select=E9,F63,F7,F82 --show-source --statistics
-ruff . --exit-zero
+make format
 
-# @TODO run mypy
+# run pyright
+# pyright --stats opennem/
 
-poetry version ${1-prerelease}
+bumpver update --${1-patch}
 
-VERSION=$(poetry version | sed 's/opennem-backend\ //g')
-
+VERSION=$(poetry version -s)
 echo "Building version $VERSION"
 
 poetry export --format requirements.txt -E postgres --without-hashes > requirements.txt
-poetry export --dev --format requirements.txt --without-hashes > requirements_dev.txt
-
-git add pyproject.toml requirements.txt requirements_dev.txt
+poetry export --with dev --format requirements.txt --without-hashes > requirements_dev.txt
+git add requirements.txt requirements_dev.txt
 
 git ci -m "v$VERSION"
 
 git tag v$VERSION
 git push -u origin master v$VERSION
-
-# end of file
