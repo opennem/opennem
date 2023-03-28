8d588d0eba7e307bd4d3940b132772191e6c3534
Nik Cubrilovic <git@nikcub.me>
2023-03-28 17:05:14 +1100

    FEAT: get_version_components method in utils.version

--- opennem/utils/version.py
+++ opennem/utils/version.py
@@ -2,6 +2,7 @@
 
 @NOTE updated to dynamically update with build scripts
 """
+import semantic_version
 
 version = "3.13.2-alpha.7"
 
@@ -9,3 +10,8 @@ version = "3.13.2-alpha.7"
 def get_version() -> str:
     """@TODO remove this and use poetry versioning"""
     return version
+
+
+def get_version_components() -> semantic_version.Version:
+    """Gets the version components"""
+    return semantic_version.Version(version)
