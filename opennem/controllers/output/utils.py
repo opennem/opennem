0338d99bdc1a1d92e483ed76df834c7721e4492b
Nik Cubrilovic <git@nikcub.me>
2023-03-28 18:11:02 +1100

    FEAT: output utilities and get_export_path (#221)

---
+++ opennem/controllers/output/utils.py
@@ -0,0 +1,47 @@
+""" Utilities for exporters """
+
+from opennem.controllers.output.schema import StatType
+from opennem.schema.network import NetworkSchema
+from opennem.utils.version import get_version_components
+
+VERSION_MAJOR = get_version_components().major
+STATS_FOLDER = "stats"
+
+
+def get_export_path(
+    network: NetworkSchema,
+    stat_type: StatType,
+    network_region: str | None = None,
+    period=None,
+    year: int | None = None,
+    week_number: int | None = None,
+) -> str:
+    """Takes the attributes of an export and returns the path they should be saved to"""
+    _path_components = [
+        f"v{VERSION_MAJOR}",
+        STATS_FOLDER,
+        network.country,
+        network.code,
+    ]
+
+    if network_region:
+        _path_components.append(network_region)
+
+    _path_components.append(stat_type.value)
+
+    # only show the period when it's not explicitly a year
+    if period and not year:
+        _path_components.append(period.period_human)
+
+    if week_number:
+        _path_components.append("week")
+
+    if year:
+        _path_components.append(str(year))
+
+    if week_number:
+        _path_components.append(str(week_number))
+
+    dir_path = "/".join([str(i) for i in _path_components])
+
+    return f"{dir_path}.json"
