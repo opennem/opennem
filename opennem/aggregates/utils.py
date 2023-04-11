fc2fc2337e6c1889fcce969437969a23cac03b40
Nik Cubrilovic <git@nikcub.me>
2023-04-11 12:11:33 +0000

    FEAT: get_aggregate_month_range - gets a range to run aggregates by month

--- opennem/aggregates/utils.py
+++ opennem/aggregates/utils.py
@@ -19,5 +19,19 @@ def get_aggregate_year_range(year: int, network: NetworkSchema = NetworkNEM) -> 
     return date_min, date_max
 
 
+def get_aggregate_month_range(year: int, month: int, network: NetworkSchema = NetworkNEM) -> tuple[datetime, datetime]:
+    """Get a date range for a month with end exclusive"""
+    tz = network.get_fixed_offset()
+    today = get_today_opennem()
+
+    date_min = datetime(year, month, 1, 0, 0, 0, 0, tzinfo=tz)
+    date_max = datetime(year, month + 1, 1, 0, 0, 0, 0, tzinfo=tz)
+
+    if year == today.year and month == today.month:
+        date_max = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=tz)
+
+    return date_min, date_max
+
+
 if __name__ == "__main__":
     print(get_aggregate_year_range(2023))
