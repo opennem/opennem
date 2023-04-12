5fae91065cec77b93377d421d47a8a12302c487b
Nik Cubrilovic <git@nikcub.me>
2023-04-12 13:00:13 +0000

    FEAT: export controller methods for exporting latest for per-interval processing

--- opennem/pipelines/export.py
+++ opennem/pipelines/export.py
@@ -10,8 +10,9 @@ a bit too abstract.
 import logging
 
 from opennem.api.export.map import PriorityType, StatType, get_export_map
-from opennem.api.export.tasks import export_power
+from opennem.api.export.tasks import export_energy, export_power
 from opennem.schema.network import NetworkSchema
+from opennem.utils.dates import get_today_opennem
 
 logger = logging.getLogger("opennem.pipelines.export")
 
@@ -29,3 +30,37 @@ def run_export_power_latest_for_network(network: NetworkSchema, network_region_c
     logger.info(f"Running {len(latest_power_exports.resources)} exports")
 
     export_power(stats=latest_power_exports.resources)
+
+
+def run_export_all(network_region_code: str | None = None) -> None:
+    # run exports for all
+    export_map = get_export_map()
+    energy_exports = export_map.get_by_stat_type(StatType.energy).get_by_priority(PriorityType.monthly)
+
+    if network_region_code:
+        energy_exports = energy_exports.get_by_network_region(network_region_code)
+
+    export_energy(energy_exports.resources)
+
+
+def run_export_power_for_region(region_code: str) -> None:
+    # run exports for all
+    export_map = get_export_map()
+    power_exports = (
+        export_map.get_by_stat_type(StatType.power).get_by_priority(PriorityType.live).get_by_network_region(region_code)
+    )
+    export_power(power_exports.resources)
+
+
+def run_export_current_year(network_region: str | None = None) -> None:
+    """Run export for latest year"""
+    export_map = get_export_map()
+    current_year = get_today_opennem().year
+
+    energy_exports = export_map.get_by_stat_type(StatType.energy).get_by_priority(PriorityType.daily).get_by_year(current_year)
+
+    if network_region:
+        energy_exports = energy_exports.get_by_network_region(network_region)
+
+    logger.info(f"Running {len(energy_exports.resources)} exports")
+    export_energy(energy_exports.resources)
