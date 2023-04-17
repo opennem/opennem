845a409a746efcc56817684abefa12a85195a3bd
Nik Cubrilovic <git@nikcub.me>
2023-04-17 20:41:30 +1000

    FIX: Clean up main module init

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -3,6 +3,27 @@ OpenNEM main module entry
 
 Setup main module entry point with sanity checks, settings init
 and sentry.
+
+Settings files - read settings from env
+
+Process:
+
+ * Loads dotenv to read environment from .env files
+ * Setup logging - root logger, read logging config, etc.
+ * Settings init - read all env and init settings module
+
+Will load environment in order:
+
+ * `.env`
+ * `.env.{environment}`
+ * system env
+ * pydantic settings
+
+Environments:
+  * local (default)
+  * development
+  * staging
+  * production
 """
 import logging
 import logging.config
@@ -14,13 +35,13 @@ from pathlib import Path
 from platform import platform
 
 from dotenv import load_dotenv
-from pydantic import ValidationError
-from pydantic.error_wrappers import _display_error_loc, _display_error_type_and_ctx
 from rich.console import Console
 from rich.prompt import Prompt
 
 from opennem.settings_schema import OpennemSettings
 from opennem.utils.log_config import LOGGING_CONFIG
+from opennem.utils.logging import setup_root_logger
+from opennem.utils.project_path import get_project_path
 from opennem.utils.security import obfuscate_dsn_password
 from opennem.utils.sentry import setup_sentry
 from opennem.utils.settings import load_env_file
@@ -42,80 +63,31 @@ if sys.version_info < (3, 10):
 console = Console()
 
 # Setup logging - root logger and handlers
-__root_logger = logging.getLogger()
-__root_logger.setLevel(logging.INFO)
-__root_logger_formatter = logging.Formatter(fmt=" * %(message)s")
-num_handlers = len(__root_logger.handlers)
-
-if num_handlers == 0:
-    __root_logger.addHandler(logging.StreamHandler())
-
-__root_logger.handlers[0].setFormatter(__root_logger_formatter)
+setup_root_logger()
 
 # module constants
 PYTHON_VERSION = ".".join([str(i) for i in (sys.version_info.major, sys.version_info.minor, sys.version_info.micro)])
 SYSTEM_STRING = platform()
 ENV = os.getenv("ENV", default="local")
-VERSION = __version__
 
 console.print(f" * Loading OpenNEM ENV: [b magenta]{ENV}[/b magenta]")
 console.print(
-    f" * OpenNEM Version: [b magenta]{VERSION}[/]. Python version: [b magenta]{PYTHON_VERSION}[/]."
+    f" * OpenNEM Version: [b magenta]{__version__}[/]. Python version: [b magenta]{PYTHON_VERSION}[/]."
     f" System: [b magenta]{SYSTEM_STRING}[/]"
 )
 
 env_files = load_env_file(ENV)
 
-# Load the env files
-# @TODO add logging
-"""
-Settings files - read settings from env
-
-Process:
-
- * Loads dotenv to read environment from .env files
- * Setup logging - root logger, read logging config, etc.
- * Settings init - read all env and init settings module
-
-Will load environment in order:
-
- * `.env`
- * `.env.{environment}`
- * system env
- * pydantic settings
-
-Environments:
-  * local (default)
-  * development
-  * staging
-  * production
-
-"""
 for _env_file in env_files:
     _env_full_path = Path(_env_file).resolve()
     console.print(f" * Loading env file: {_env_full_path}")
     load_dotenv(dotenv_path=_env_file, override=True)
 
-# setup settings
-# @NOTE don't use pydantics env file support since it doesn't support multiple
-try:
-    settings: OpennemSettings = OpennemSettings()
-except ValidationError as e:
-    logging.error(f"{len(e.errors())} validation errors in settings schema")
-
-    for _err_no, _validation_error in enumerate(e.errors()):
-        logging.error(
-            f'{_display_error_loc(_validation_error)}: {_validation_error["msg"]} \
-                ({_display_error_type_and_ctx(_validation_error)})'
-        )
-
-    logging.info("Exiting")
-    sys.exit(-1)
-
+settings: OpennemSettings = OpennemSettings()
 
 if settings.dry_run:
     console.print(" * Dry run (no database actions)")
-else:
+elif settings.db_url:
     console.print(f" * Using database connection: [red bold encircle]{obfuscate_dsn_password(settings.db_url)}[/]")
 
 # skip if logging not configed
@@ -133,23 +105,12 @@ if LOGGING_CONFIG:
     # other misc loggers
     logging.getLogger("PIL").setLevel(logging.ERROR)
 
-IS_DEV = not settings.is_prod
-
 # Setup sentry
 if settings.sentry_url:
     setup_sentry(sentry_url=settings.sentry_url, environment=settings.env)
 
 
-if __package__ not in sys.modules:
-    raise Exception(f"Could not find {__package__} module")
-
-if sys.modules[__package__].__file__:
-    MODULE_DIR_PATH = Path(sys.modules[__package__].__file__).parent  # type: ignore
-else:
-    MODULE_DIR_PATH = Path(__file__).parent
-
-DATA_DIR_PATH = MODULE_DIR_PATH / "data"
-PROJECT_PATH = MODULE_DIR_PATH.parent
+PROJECT_PATH = get_project_path()
 
 
 # Log current timezone to console
