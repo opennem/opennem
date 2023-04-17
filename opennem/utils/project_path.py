de2759205ad5818cb889ac88ae0397146cf3960e
Nik Cubrilovic <git@nikcub.me>
2023-04-17 20:33:35 +1000

    FEAT: project_path module

---
+++ opennem/utils/project_path.py
@@ -0,0 +1,14 @@
+""" Utilities for the project path """
+import sys
+from pathlib import Path
+
+
+def get_project_path() -> Path:
+    """Gets the project path from sys.modules"""
+    if __package__ not in sys.modules:
+        raise Exception(f"Could not find {__package__} module")
+
+    if sys.modules[__package__].__file__:
+        return Path(sys.modules[__package__].__file__).parent  # type: ignore
+
+    return Path(__file__).parent
