8f253bc70de7d8c40bc5dde4812efbd0fd3dce0e
Nik Cubrilovic <git@nikcub.me>
2023-04-28 11:55:06 +1000

    bump version 3.15.1-alpha.13 -> 3.15.1

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -50,7 +50,7 @@ logger = logging.getLogger("opennem")
 warnings.filterwarnings("ignore", module="openpyxl")
 
 # Module variables
-__version__ = "3.15.1-alpha.13"
+__version__ = "3.15.1"
 __env__ = "prod"
 __package__ = "opennem"
 
