da71e0a04ba16dd2d7a301d027cc4445fca23762
Nik Cubrilovic <git@nikcub.me>
2023-05-08 13:10:12 +1000

    META: manually set version

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -50,7 +50,7 @@ logger = logging.getLogger("opennem")
 warnings.filterwarnings("ignore", module="openpyxl")
 
 # Module variables
-__version__ = "3.16.0-alpha.1"
+__version__ = "3.15.2-alpha.0"
 __env__ = "prod"
 __package__ = "opennem"
 
