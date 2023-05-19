afce71db69cb1ba0759ecb2ff9b6fd96c308506c
Nik Cubrilovic <git@nikcub.me>
2023-05-19 16:27:35 +1000

    FIX: add comments in unit tests showing dataframe shape (#227)

--- tests/core/flow_solver.py
+++ tests/core/flow_solver.py
@@ -10,8 +10,16 @@ from opennem.schema.network import NetworkNEM, NetworkSchema
 
 # fixtures for this test adapted from Simons spreadsheet at
 # https://docs.google.com/spreadsheets/d/12eAnLYSdXJ55I06m0sRfrJyVd-1gGsSr/edit#gid=1210585319
-def load_energy_and_emissions(interval: datetime, network: NetworkSchema = NetworkNEM) -> pd.DataFrame:
-    """Load generation and emissions for an interval and the interval prior for each region of the network"""
+def load_energy_and_emissions_spreadsheet(interval: datetime, network: NetworkSchema = NetworkNEM) -> pd.DataFrame:
+    """Load generation and emissions for an interval and the interval prior for each region of the network
+
+    trading_interval network_id network_region  energy  emissions  emissions_intensity
+          2023-01-01        NEM           QLD1     500        325                 0.65
+          2023-01-01        NEM           NSW1     600        330                 0.55
+          2023-01-01        NEM           VIC1     300        180                 0.60
+          2023-01-01        NEM            SA1     100         15                 0.15
+          2023-01-01        NEM           TAS1      80          4                 0.05
+    """
     df = pd.DataFrame(
         {
             "trading_interval": [interval] * 5,
@@ -27,8 +35,16 @@ def load_energy_and_emissions(interval: datetime, network: NetworkSchema = Netwo
     return df
 
 
-def load_interconnector_interval(interval: datetime, network: NetworkSchema = NetworkNEM) -> pd.DataFrame:
-    """Load interval for each interconnector for a given interval"""
+def load_interconnector_interval_spreadsheet(interval: datetime, network: NetworkSchema = NetworkNEM) -> pd.DataFrame:
+    """Load interval for each interconnector for a given interval
+
+
+    trading_interval network_id interconnector_region_from interconnector_region_to  energy
+          2023-01-01        NEM                       VIC1                      SA1    22.0
+          2023-01-01        NEM                       NSW1                     QLD1   -55.0
+          2023-01-01        NEM                       TAS1                     VIC1    11.0
+          2023-01-01        NEM                       VIC1                     NSW1    27.5
+    """
 
     df = pd.DataFrame(
         {
@@ -43,25 +59,47 @@ def load_interconnector_interval(interval: datetime, network: NetworkSchema = Ne
     return df
 
 
-def flow_solver_test_output() -> pd.DataFrame:
-    """This is the test output to compare against"""
+def flow_solver_test_output_spreadsheet() -> pd.DataFrame:
+    """This is the test output to compare against
+
+    network_region  energy_exported  emissions_exported  energy_imported emissions_imported
+              TAS1             11.0                0.55              0.0                0.0
+              VIC1             49.5               28.70             11.0                0.6
+               SA1              0.0                0.00             22.0               12.8
+              QLD1             55.0               35.75              0.0                0.0
+              NSW1              0.0                0.00             82.5               51.7
+
+    """
     df = pd.DataFrame(
         {
             "network_region": ["TAS1", "VIC1", "SA1", "QLD1", "NSW1"],
             "energy_exported": [11, 49.5, 0, 55, 0],
             "emissions_exported": [0.55, 28.7, 0, 35.75, 0],
             "energy_imported": [0, 11, 22, 0, 82.5],
-            "emissions_imported": [0, 0.6, 12.8, 0, 51.7],
+            "emissions_imported": [0, 0.55, 12.8, 0, 51.7],
         },
     )
 
     return df
 
 
+# Secondary test of raw values from dicts into solver function
+
+
 @pytest.mark.parametrize(
     "emissions_dict, power_dict, demand_dict, expected_output",
     [
         (
+            {
+                "NSW1": 63576.32150005468,
+                "QLD1": 72424.89831325083,
+                "SA1": 1207.01427219245,
+                "TAS1": 0,
+                "VIC1": 55621.219853363276,
+                ("QLD1", "NSW1"): 0.0002745705239764611,
+                ("SA1", "VIC1"): 0.12867995814508557,
+                ("TAS1", "VIC1"): 0,
+            },
             {
                 "NSW1": 99876.21579870833,
                 "QLD1": 92799.05102995834,
@@ -76,16 +114,6 @@ def flow_solver_test_output() -> pd.DataFrame:
                 ("TAS1", "VIC1"): 9345725.368333332,
                 ("VIC1", "NSW1"): 2928083.5116666667,
                 ("VIC1", "SA1"): 2263190.0225,
-            },
-            {
-                "NSW1": 63576.32150005468,
-                "QLD1": 72424.89831325083,
-                "SA1": 1207.01427219245,
-                "TAS1": 0,
-                "VIC1": 55621.219853363276,
-                ("QLD1", "NSW1"): 0.0002745705239764611,
-                ("SA1", "VIC1"): 0.12867995814508557,
-                ("TAS1", "VIC1"): 0,
             },
             {
                 "NSW1": 99876.21579870884,
@@ -144,10 +172,10 @@ def test_calculate_flow_for_interval() -> None:
     """
     interval = datetime.fromisoformat("2023-04-09T10:15:00+10:00")
 
-    df_energy_and_emissions = load_energy_and_emissions(interval)
-    df_interconnector = load_interconnector_interval(interval)
+    df_energy_and_emissions = load_energy_and_emissions_spreadsheet(interval)
+    df_interconnector = load_interconnector_interval_spreadsheet(interval)
 
-    expected_output = flow_solver_test_output()
+    expected_output = flow_solver_test_output_spreadsheet()
 
     # Call the function with the test data
     actual_output = calculate_flow_for_interval(df_energy_and_emissions, df_interconnector)
