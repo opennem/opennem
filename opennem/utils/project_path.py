7108366d007a451e7f2b0a8c03ba9b68436e1c35
Nik Cubrilovic <git@nikcub.me>
2023-06-26 09:31:54 +1000

    FIX: number of parent directories in path

--- opennem/utils/project_path.py
+++ opennem/utils/project_path.py
@@ -5,10 +5,4 @@ from pathlib import Path
 
 def get_project_path() -> Path:
     """Gets the project path from sys.modules"""
-    if __package__ not in sys.modules:
-        raise Exception(f"Could not find {__package__} module")
-
-    if sys.modules[__package__].__file__:
-        return Path(sys.modules[__package__].__file__).parent.parent  # type: ignore
-
-    return Path(__file__).parent
+    return Path(sys.modules["opennem"].__file__).parent.parent
