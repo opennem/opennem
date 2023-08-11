2f8d99e8b2cd56f5d4073f33c5b1a567b483e805
Daniel Ribeiro <daniel.ribeiro@pathzero.com>
2023-08-11 18:08:09 +1000

    Remove tests for dead code

--- tests/test_series.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from opennem.utils.series import add_series
-
-
-@pytest.mark.parametrize("expected,first,second", [([2, 4, 6], [1, 2, 3], [1, 2, 3])])
-def test_add_series(expected, first, second):
-    sum_value = add_series(first, second)
-    assert sum_value == expected, "Values add"
