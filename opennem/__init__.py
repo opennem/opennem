3341e502a1d6033454546e8b964b1dd1cbd61ed6
Nik Cubrilovic <git@nikcub.me>
2023-09-26 07:34:54 +1000

    META: update poetry version

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -50,7 +50,7 @@ logger = logging.getLogger("opennem")
 warnings.filterwarnings("ignore", module="openpyxl")
 
 # Module variables
-__version__ = "3.16.0-alpha.64"
+__version__ = "4.0.0a0"
 __env__ = "prod"
 __package__ = "opennem"
 
