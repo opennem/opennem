f6f261029bd596a22c1e53006a1b46d8c20df290
Nik Cubrilovic <git@nikcub.me>
2023-09-26 07:36:29 +1000

    fix: version number parsing

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -50,7 +50,7 @@ logger = logging.getLogger("opennem")
 warnings.filterwarnings("ignore", module="openpyxl")
 
 # Module variables
-__version__ = "4.0.0a0"
+__version__ = "4.0.0-alpha.0"
 __env__ = "prod"
 __package__ = "opennem"
 
