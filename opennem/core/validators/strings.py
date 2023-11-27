e8e5438b860452ec0905c4f790f15a8331449479
Nik Cubrilovic <git@nikcub.me>
2023-11-28 08:40:54 +1100

    FIX: remove old string type validation

--- opennem/core/validators/strings.py
+++ opennem/core/validators/strings.py
@@ -4,22 +4,17 @@ OpenNEM String Validators
 Used in Pydantic schemas
 """
 
-from pydantic.errors import PydanticTypeError, StrError
 
 from opennem.core.normalizers import string_is_urlsafe
-
-
-class UrlsafeStrError(PydanticTypeError):
-    msg_template = "url safe str type expected"
 
 
 def urlsafe_str_validator(v: str) -> str:
     """Validate that a string is url safe. Assumes already passed through string validation"""
 
     if not isinstance(v, str):
-        raise StrError()
+        raise ValueError()
 
     if string_is_urlsafe(v):
         return v
 
-    raise UrlsafeStrError()
+    raise ValueError()
