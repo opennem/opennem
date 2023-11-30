d494877f92808874206ca2ba59e71c6fbe99e95f
Nik Cubrilovic <git@nikcub.me>
2023-11-30 13:09:45 +0000

    FIX: milestone reactor ai generator fixes

--- opennem/recordreactor/ai/client.py
+++ opennem/recordreactor/ai/client.py
@@ -4,28 +4,25 @@ in return
 """
 import logging
 import sys
-from datetime import datetime
 from pathlib import Path
-from zoneinfo import ZoneInfo
+from textwrap import dedent
 
-import openai
-from energychat.clients.opennem import (
+from openai import OpenAI
+
+from opennem import settings
+from opennem.recordreactor.ai.train import generate_prompt_set
+from opennem.recordreactor.ai.utils import (
     BadQuery,
+    NotReadOnlyException,
     OpennemResultSet,
-    insert_query_to_database,
+    is_read_only_query,
     run_opennem_query,
 )
-from energychat.train import generate_prompt_set
 
-logger = logging.getLogger("energychat.process")
-
-
-def get_today() -> str:
-    return datetime.now().astimezone(ZoneInfo("Australia/Sydney")).strftime("%Y-%m-%d")
+logger = logging.getLogger("opennem.recordreactor.ai")
 
 
 SYSTEM_PROMPT = """You are an agent designed to interact with a SQL database.
-Current date: {today}
 Given an input question, create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
 Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {limit} results.
 You can order the results by a relevant column to return the most interesting examples in the database.
@@ -168,42 +165,50 @@ MESSAGES = [{"role": "system", "content": SYSTEM_PROMPT}]
 
 ERROR_TEMPLATE = "Try again. " "The SQL query you just generated resulted in the following error message:\n" "{error_message}"
 
-prompts = generate_prompt_set(Path(__file__).parent / "train" / "prompts.txt")
+OPENNEM_PROMPTS = generate_prompt_set(Path(__file__).parent / "data" / "prompts.txt")
+
+openai_client = OpenAI(
+    # This is the default and can be omitted
+    api_key=settings.openai_api_key,
+)
 
 
-def get_openai_chat_prompt(query: str, error_detail: list[dict] | None = None):
+def get_openai_chat_prompt(query: str, error_detail: list[dict] | None = None) -> str:
     """
     Function that takes a natural language query and asks OpenAI for a sql
     statement in return
     """
 
-    system_prompt_formatted = SYSTEM_PROMPT.format(today=get_today(), limit=10)
+    system_prompt_formatted = SYSTEM_PROMPT.format(limit=100)
 
     messages = (
         [{"role": "system", "content": system_prompt_formatted}]
-        + prompts
+        + OPENNEM_PROMPTS
         + [{"role": "user", "content": USER_PROMPT.format(query=query)}]
     )
 
     if error_detail:
-        messages += error_detail
+        # messages += error_detail
+        logger.error("Error detail not yet supported")
+        pass
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
+    response = openai_client.chat.completions.create(
+        model="gpt-4-1106-preview",
         messages=messages,
         temperature=0,
-        max_tokens=1024,
     )
 
     logger.info(response)
 
-    if "choices" not in response:
+    if not response.choices:
         raise Exception("No result")
 
-    if not response["choices"]:
+    if not response.choices[0].message.content:
         raise Exception("No result")
 
-    return response["choices"][0]["message"]["content"]
+    response_content = response.choices[0].message.content
+
+    return response_content
 
 
 def run_human_query_with_retries(query: str, error_detail: list[dict] | None = None, retry_number: int = 0) -> OpennemResultSet:
@@ -217,12 +222,6 @@ def run_human_query_with_retries(query: str, error_detail: list[dict] | None = N
     try:
         results = run_opennem_query(sql_query)
 
-        try:
-            insert_query_to_database(sql_query, query, "", competion_tokens=0, prompt_tokens=0)
-        except Exception as e:
-            logger.error(e)
-            pass
-
     except BadQuery as e:
         logger.info("Bad query: %s", e)
 
@@ -232,12 +231,6 @@ def run_human_query_with_retries(query: str, error_detail: list[dict] | None = N
         error_detail.append({"role": "assistant", "content": sql_query})
         error_detail.append({"role": "user", "content": ERROR_TEMPLATE.format(error_message=str(e))})
 
-        try:
-            insert_query_to_database(sql_query, query, str(e), competion_tokens=0, prompt_tokens=0)
-        except Exception as e:
-            logger.error(e)
-            pass
-
         return run_human_query_with_retries(query, retry_number=retry_number + 1)
 
     if not results:
@@ -246,7 +239,22 @@ def run_human_query_with_retries(query: str, error_detail: list[dict] | None = N
     return results
 
 
+def get_opennem_query_from_prompt(prompt: str) -> str:
+    """Function that takes a natural language query and asks OpenAPI for a sql statement in return"""
+
+    query = get_openai_chat_prompt(prompt)
+
+    if not is_read_only_query(query):
+        raise NotReadOnlyException("Only read-only queries are allowed.")
+
+    logger.debug(dedent(query))
+
+    query = query.lower()
+
+    return query
+
+
 if __name__ == "__main__":
-    response = get_openai_chat_prompt(sys.argv[1])
+    response = get_opennem_query_from_prompt(sys.argv[1])
 
     print(response)
