abf1d1a889cfd356335aea10b8cde89b04420e71
Nik Cubrilovic <git@nikcub.me>
2023-11-30 13:09:45 +0000

    FEAT: Custom JSON encoder supporting dataclasses and new pydantic models

---
+++ opennem/utils/encoder.py
@@ -0,0 +1,20 @@
+""" Utility functions for encoding and decoding data."""
+
+
+import dataclasses
+import json
+
+from pydantic import BaseModel
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    """Json Encoder that can handle dataclasses and pydantic models"""
+
+    def default(self, o: dataclasses.dataclass | BaseModel) -> dict:
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)  # type: ignore
+
+        if isinstance(o, BaseModel):
+            return dict(o)
+
+        return super().default(o)
