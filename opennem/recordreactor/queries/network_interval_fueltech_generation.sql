564f484ca645d0b2e4775d4d981f1cc196adb2a1
Nik Cubrilovic <git@nikcub.me>
2023-12-01 16:09:45 +0000

    FIX: milestone fueltech group query

--- opennem/recordreactor/queries/network_interval_fueltech_generation.sql
+++ opennem/recordreactor/queries/network_interval_fueltech_generation.sql
@@ -1,26 +1,31 @@
-select
+SELECT
+  f.network_id,
   f.network_region,
-  ft.label as fueltech,
+  ftg.code as fueltech_id,
+  ftg.label AS fueltech_label,
   aggr.highest_output,
   aggr.lowest_output,
-  max(fs.trading_interval) filter (where fs.generated = aggr.highest_output) as highest_output_interval,
-  max(fs.trading_interval) filter (where fs.generated = aggr.lowest_output) as lowest_output_interval
-from facility_scada fs
-join facility f on fs.facility_code = f.code
-join fueltech ft on f.fueltech_id = ft.code
-join (
-  select
+  MAX(fs.trading_interval) FILTER (WHERE fs.generated = aggr.highest_output) AS highest_output_interval,
+  MAX(fs.trading_interval) FILTER (WHERE fs.generated = aggr.lowest_output) AS lowest_output_interval
+FROM facility_scada fs
+JOIN facility f ON fs.facility_code = f.code
+JOIN fueltech ft ON f.fueltech_id = ft.code
+JOIN fueltech_group ftg on ftg.code = ft.fueltech_group_id
+JOIN (
+  SELECT
     f_inner.network_region,
-    ft_inner.label as fueltech_label,
-    max(fs_inner.generated) as highest_output,
-    min(fs_inner.generated) as lowest_output
-  from facility_scada fs_inner
-  join facility f_inner on fs_inner.facility_code = f_inner.code
-  join fueltech ft_inner on f_inner.fueltech_id = ft_inner.code
-  group by f_inner.network_region, ft_inner.label
+    ft_inner.label AS fueltech_label,
+    MAX(fs_inner.generated) AS highest_output,
+    MIN(fs_inner.generated) AS lowest_output
+  FROM facility_scada fs_inner
+  JOIN facility f_inner ON fs_inner.facility_code = f_inner.code
+  JOIN fueltech ft_inner ON f_inner.fueltech_id = ft_inner.code
   where
-    fs_inner.trading_interval >= now() at time zone 'aest' - interval '1 year'
-) as aggr on f.network_region = aggr.network_region and ft.label = aggr.fueltech_label
+    fs_inner.trading_interval >= now() at time zone 'AEST' - interval '180 days' and
+    fs_inner.generated >= 0
+  GROUP BY f_inner.network_region, ft_inner.code
+  ) AS aggr ON f.network_region = aggr.network_region AND ft.label = aggr.fueltech_label
 where
-    fs.trading_interval >= now() at time zone 'aest' - interval '1 year'
-group by f.network_region, ft.label, aggr.highest_output, aggr.lowest_output;
+    fs.trading_interval >= now() at time zone 'AEST' - interval '180 days' and
+    f.network_id in ('AEMO_ROOFTOP', 'NEM', 'WEM')
+GROUP BY f.network_id, f.network_region, ftg.code, ftg.label, aggr.highest_output, aggr.lowest_output;
