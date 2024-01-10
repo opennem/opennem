7c79b4b1d18f64a90aa06a45d0599d38110ebc8c
Nik Cubrilovic <git@nikcub.me>
2024-01-11 07:43:12 +1100

    FIX: remove old station mapping code

--- opennem/core/stations/station_name_code_map.py
+++
@@ -1,9 +0,0 @@
-from opennem.core.loader import load_data
-
-STATION_NAME_CODE_MAP = load_data("station_name_code_map.json")
-
-
-def station_name_code_map(station_name: str) -> str | None:
-    if station_name.strip() in STATION_NAME_CODE_MAP:
-        return STATION_NAME_CODE_MAP[station_name]
-    return None
