48e65bfab7d74bce02b8605ed2e6e42bb4505947
Nik Cubrilovic <git@nikcub.me>
2024-01-11 07:40:28 +1100

    FIX: remove old photos code

--- opennem/settings_schema.py
+++ opennem/settings_schema.py
@@ -61,7 +61,6 @@ class OpennemSettings(BaseSettings):
 
     s3_bucket_path: str = Field("data.opennem.org.au", validation_alias=AliasChoices("S3_DATA_BUCKET_PATH"))
     backup_bucket_path: str = Field("backups.opennem.org.au")
-    photos_bucket_path: str = "photos.opennem.org.au"
 
     # opennem output settings
     interval_default: str = "15m"
