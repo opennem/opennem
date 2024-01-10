4d00254fe8c831800bfaccdac9305b15b6d559b2
Nik Cubrilovic <git@nikcub.me>
2024-01-11 06:50:19 +1100

    FIX: remove underscore_attrs_are_private in pydantic 2 port (#250)

--- opennem/core/parsers/aemo/mms.py
+++ opennem/core/parsers/aemo/mms.py
@@ -171,9 +171,7 @@ class AEMOTableSchema(BaseConfig):
 
         logger.info(f"Wrote records to {self.full_name}")
 
-    # TODO[pydantic]: The following keys were removed: `underscore_attrs_are_private`.
-    # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-config for more information.
-    model_config = ConfigDict(underscore_attrs_are_private=True)
+    model_config = ConfigDict()
 
 
 class AEMOTableSet(BaseModel):
