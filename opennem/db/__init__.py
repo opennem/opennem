6300a25050ccf450d86187051292550ce1314cd8
Nik Cubrilovic <git@nikcub.me>
2024-02-12 14:18:17 +0000

    FEAT: Upgrade sqlalchemy 2, core async engine, session maker and return generator

--- opennem/db/__init__.py
+++ opennem/db/__init__.py
@@ -7,10 +7,11 @@ project
 
 """
 import logging
-from collections.abc import Generator
+from collections.abc import AsyncGenerator, Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
@@ -77,7 +78,61 @@ def db_connect(db_conn_str: str | None = None, debug: bool = False, timeout: int
         raise exc
 
 
+async def db_connect_async(db_conn_str: str | None = None, debug: bool = False, timeout: int = 10) -> Engine:
+    """
+    Returns async sqlalchemy engine instance
+
+    :param db_conn_str: Database connection string
+    :param debug: Debug mode will render queries and info to terminal
+    :param timeout: Database connection timeout
+    """
+    if not db_conn_str:
+        db_conn_str = str(settings.db_url)
+
+    connect_args = {}
+
+    if not db_conn_str.startswith("postgresql"):
+        raise Exception("Only postgresql supported")
+
+    if settings.db_debug:
+        debug = True
+
+    keepalive_kwargs = {
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 5,
+        "keepalives_count": 5,
+    }
+
+    if settings.db_debug:
+        debug = True
+        logger.info("Database debug mode enabled")
+
+    try:
+        return create_async_engine(
+            db_conn_str,
+            json_serializer=opennem_serialize,
+            json_deserializer=opennem_deserialize,
+            query_cache_size=1200,
+            echo=debug,
+            pool_size=30,
+            max_overflow=20,
+            pool_recycle=100,
+            pool_timeout=timeout,
+            pool_pre_ping=True,
+            pool_use_lifo=True,
+            connect_args={
+                **connect_args,
+                **keepalive_kwargs,
+            },
+        )
+    except Exception as exc:
+        logger.error("Could not connect to database: %s", exc)
+        raise exc
+
+
 engine = db_connect()
+engine_async = db_connect_async()
 
 
 def get_database_engine() -> Engine:
@@ -92,6 +147,7 @@ def get_database_engine() -> Engine:
 
 
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+SessionLocalAsync = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine_async))
 
 
 def get_scoped_session() -> Session:
@@ -99,6 +155,23 @@ def get_scoped_session() -> Session:
 
 
 def get_database_session() -> Generator[Session, None, None]:
+    """
+    Gets a database session
+
+    """
+    s = None
+
+    try:
+        s = get_scoped_session()
+        yield s
+    except Exception as e:
+        raise e
+    finally:
+        if s:
+            s.close()
+
+
+async def get_database_session_async() -> AsyncGenerator[Session, None, None]:
     """
     Gets a database session
 
