45f5a5b0a6d9d50ce16cc82b42c87cb4c710aeed
Nik Cubrilovic <git@nikcub.me>
2024-02-16 14:29:53 +1100

    FEAT: milestones initstate

--- opennem/recordreactor/initstate.py
+++ opennem/recordreactor/initstate.py
@@ -9,11 +9,12 @@ run queries for all records back to a date defined as a parameter.
 
 import csv
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
 from pydantic import BaseModel
+from sqlalchemy import text as sql
 
 from opennem.db import SessionLocal
 from opennem.db.models.opennem import Milestones, MilestoneType
@@ -82,13 +83,74 @@ def get_aware_interval_for_record(record: ReactorBackfillRecord, record_type: Mi
         raise Exception(f"Record does not have attribute {milestone_type_attribute}")
 
     if record.network_id != "WEM":
-        interval = getattr(record, milestone_type_attribute).astimezone(ZoneInfo("Australia/Sydney"))
-        interval += timedelta(hours=10)
+        interval = getattr(record, milestone_type_attribute).astimezone(ZoneInfo("Australia/Brisbane"))
+        # interval += timedelta(hours=10)
     else:
         interval = getattr(record, milestone_type_attribute).astimezone(ZoneInfo("Australia/Perth"))
-        interval += timedelta(hours=8)
+        # interval += timedelta(hours=8)
 
     return interval
+
+
+def get_network_id_map(network_id: str) -> str:
+    """Get a network id map"""
+    network_id_map = {
+        "AEMO_ROOFTOP": "NEM",
+        "APVI": "WEM",
+    }
+
+    if network_id not in network_id_map:
+        return network_id
+
+    return network_id_map[network_id]
+
+
+def get_record_id(
+    record: ReactorBackfillRecord,
+    network_id: str,
+    record_type: MilestoneType,
+    record_unit: str | None = None,
+    record_period: str | None = None,
+) -> str:
+    """Get a record id"""
+    record_id_components = [
+        "ms",
+        network_id,
+        record.network_region,
+        record.fueltech_id,
+        record_unit,
+        record_period,
+        record_type.value,
+    ]
+
+    # remove empty items from record id components list and join with a period
+    record_id = ".".join(filter(None, record_id_components)).lower()
+
+    return record_id
+
+
+def get_record_description(
+    record: ReactorBackfillRecord,
+    network_id: str,
+    record_type: MilestoneType,
+    record_unit: str | None = None,
+    record_period: str | None = None,
+) -> str:
+    """get a record description"""
+    record_description_components = [
+        f"{record_type.value.capitalize()} MW" if record_type else None,
+        f"{record_unit}" if record_unit else None,
+        f"for {record.fueltech_label}" if record.fueltech_label else None,
+        record_period.lower() if record_period else None,
+        "record for",
+        network_id,
+        f"in {record.network_region}" if record.network_region else None,
+    ]
+
+    # remove empty items from record id components list and join with a period
+    record_description = " ".join(filter(None, record_description_components))
+
+    return record_description
 
 
 def backfill_records_to_milestones(backfill_records: list[ReactorBackfillRecord]) -> list[Milestones]:
@@ -97,8 +159,20 @@ def backfill_records_to_milestones(backfill_records: list[ReactorBackfillRecord]
     milestones: list[Milestones] = []
 
     for record in backfill_records:
+        if record.network_region:
+            if record.network_region.lower() == record.network_id.lower():
+                record.network_region = None
+
+        network_id = get_network_id_map(record.network_id)
+
         if record.highest_output:
-            record_id = f"{record.network_id}.{record.network_region}.{record.fueltech_id}.high".lower()
+            record_id = get_record_id(
+                record=record, network_id=network_id, record_type=MilestoneType.high, record_unit="power", record_period="all"
+            )
+
+            description = get_record_description(
+                record=record, network_id=network_id, record_type=MilestoneType.high, record_unit="power", record_period="all"
+            )
 
             if not record.highest_output_interval:
                 continue
@@ -112,14 +186,22 @@ def backfill_records_to_milestones(backfill_records: list[ReactorBackfillRecord]
                     record_type=MilestoneType.high,
                     significance=8,
                     value=record.highest_output,
-                    network_id=record.network_id,
+                    network_id=network_id,
                     network_region=record.network_region,
                     fueltech_group_id=record.fueltech_id,
+                    period="all",
+                    description=description,
                 )  #  type: ignore
             )
 
         if record.lowest_output:
-            record_id = f"{record.network_id}.{record.network_region}.{record.fueltech_id}.low".lower()
+            record_id = get_record_id(
+                record=record, network_id=network_id, record_type=MilestoneType.low, record_unit="power", record_period="all"
+            )
+
+            description = get_record_description(
+                record=record, network_id=network_id, record_type=MilestoneType.low, record_unit="power", record_period="all"
+            )
 
             if not record.lowest_output_interval:
                 continue
@@ -133,9 +215,11 @@ def backfill_records_to_milestones(backfill_records: list[ReactorBackfillRecord]
                     record_type=MilestoneType.low,
                     significance=1,
                     value=record.highest_output,
-                    network_id=record.network_id,
+                    network_id=network_id,
                     network_region=record.network_region,
                     fueltech_group_id=record.fueltech_id,
+                    period="all",
+                    description=description,
                 )  #  type: ignore
             )
 
@@ -151,8 +235,12 @@ if __name__ == "__main__":
     milestone_records = backfill_records_to_milestones(backfill_records)
 
     with SessionLocal() as session:
-        for record in milestone_records:
-            logger.info(record)
-            session.add(record)
+        session.execute(sql("DELETE FROM milestones"))
 
-        session.commit()
+        for record in milestone_records:
+            try:
+                session.add(record)
+                session.commit()
+            except Exception as e:
+                logger.error(f"Error adding record: {e}")
+                session.rollback()
