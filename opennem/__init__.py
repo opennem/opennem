7671c1b1aead0ca1d140c1598450350a8ea978ff
Nik Cubrilovic <git@nikcub.me>
2024-06-07 23:09:16 +1000

    Bump version: 3.16.12a2

--- opennem/__init__.py
+++ opennem/__init__.py
@@ -58,7 +58,7 @@ logger = logging.getLogger("opennem")
 warnings.filterwarnings("ignore", module="openpyxl")
 
 # Module variables
-__version__ = "3.16.12a1"
+__version__ = "3.16.12a2"
 __env__ = "prod"
 __package__ = "opennem"
 
