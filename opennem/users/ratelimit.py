5f1ebc1f3ae77b5f56ed9df90d7b43c858f23e79
Nik Cubrilovic <git@nikcub.me>
2024-07-25 14:30:20 +1000

    FEAT: user ratelimit definitions

---
+++ opennem/users/ratelimit.py
@@ -0,0 +1,25 @@
+import unkey
+
+# 10 requests a second
+OPENNEM_RATELIMIT_ADMIN = unkey.Ratelimit(
+    unkey.RatelimitType.Fast,
+    limit=10,
+    refill_rate=10,
+    refill_interval=1000,
+)
+
+# 1 request per second
+OPENNEM_RATELIMIT_PRO = unkey.Ratelimit(
+    unkey.RatelimitType.Fast,
+    limit=1,
+    refill_rate=1,
+    refill_interval=1000,
+)
+
+# 100 requests per day
+OPENNEM_RATELIMIT_USER = unkey.Ratelimit(
+    unkey.RatelimitType.Fast,
+    limit=100,
+    refill_rate=100,
+    refill_interval=1000 * 60 * 60 * 24,
+)
