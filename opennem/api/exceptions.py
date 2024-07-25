361e67cb6cc8e205d4aaa5d941dedfa26b0d6892
Nik Cubrilovic <git@nikcub.me>
2024-07-25 22:11:54 +1000

    FEAT: API exceptions

--- opennem/api/exceptions.py
+++ opennem/api/exceptions.py
@@ -53,6 +53,33 @@ class OpenNEMThrottleMigrateResponse(OpennemBaseHttpException):
     )
 
 
+class UnauthorizedRequest(OpennemBaseHttpException):
+    """Base 401 return for unauthorized"""
+
+    detail = "Unauthorized request"
+    status_code = 401
+
+
+class BadCredentials(OpennemBaseHttpException):
+    """Base 403 return for permission denied"""
+
+    detail = "Bad credentials"
+    status_code = 403
+
+
+class BadCredentialsKeyNotFound(BadCredentials):
+    """API Key not found exception"""
+
+    detail = "Bad credentials: Key not found"
+
+
+class RevokedCredentials(OpennemBaseHttpException):
+    """401 for revoked credentials"""
+
+    detail = "Credentials revoked or expires"
+    status_code = 401
+
+
 class OpennemExceptionResponse(Response):
     media_type = "application/json"
     response_class: OpennemErrorSchema
