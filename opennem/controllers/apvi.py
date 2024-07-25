05a641a624e878b9bc715d730118c64b1befb981
Nik Cubrilovic <git@nikcub.me>
2024-07-25 19:30:12 +1000

    FIX: APVI update queries async

--- opennem/controllers/apvi.py
+++ opennem/controllers/apvi.py
@@ -6,11 +6,12 @@ and various other methods for persisting APVI data
 
 import logging
 
+from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
 
 from opennem.clients.apvi import APVIForecastSet
 from opennem.controllers.schema import ControllerReturn
-from opennem.db import SessionLocal, get_database_engine
+from opennem.db import SessionLocal, db_connect
 from opennem.db.models.opennem import Facility, FacilityScada
 
 logger = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 async def store_apvi_forecastset(forecast_set: APVIForecastSet) -> ControllerReturn:
     """Persist an APVI forecast set to the database"""
-    engine = get_database_engine()
+    engine = db_connect()
     cr = ControllerReturn()
 
     records_to_store = []
@@ -47,8 +48,8 @@ async def store_apvi_forecastset(forecast_set: APVIForecastSet) -> ControllerRet
         )
 
         try:
-            session.execute(stmt)
-            session.commit()
+            await session.execute(stmt)
+            await session.commit()
             cr.inserted_records = len(records_to_store)
         except Exception as e:
             logger.error(f"Error: {e}")
@@ -66,7 +67,9 @@ async def update_apvi_facility_capacities(forecast_set: APVIForecastSet) -> None
 
     async with SessionLocal() as session:
         for state_capacity in forecast_set.capacities:
-            state_facility: Facility = session.query(Facility).filter_by(code=state_capacity.facility_code).one_or_none()
+            stmt = select(Facility).filter_by(code=state_capacity.facility_code)
+            result = await session.execute(stmt)
+            state_facility: Facility = result.scalar_one_or_none()
 
             if not state_facility:
                 raise Exception("Could not find rooftop facility for %s", state_capacity.facility_code)
@@ -75,6 +78,7 @@ async def update_apvi_facility_capacities(forecast_set: APVIForecastSet) -> None
             state_facility.unit_number = state_capacity.unit_number
 
             session.add(state_facility)
-            session.commit()
+
+        await session.commit()
 
     logger.info(f"Updated {len(forecast_set.capacities)} facility capacities")
