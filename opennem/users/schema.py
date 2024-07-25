7e4c37f6fefb339d927bb82a39910f1eeab5d81a
Nik Cubrilovic <git@nikcub.me>
2024-07-25 14:21:45 +1000

    FIX: convert reset field to datetime in user schema output

--- opennem/users/schema.py
+++ opennem/users/schema.py
@@ -1,6 +1,9 @@
+from datetime import UTC, datetime
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+
+from opennem.utils.dates import chop_datetime_microseconds
 
 
 class OpenNEMRoles(Enum):
@@ -12,14 +15,25 @@ class OpenNEMRoles(Enum):
 class OpenNEMUserRateLimit(BaseModel):
     limit: int
     remaining: int
-    reset: int
+    reset: datetime | int
+
+    # convert reset unix timestamp to datetime in UTC time
+    @field_validator("reset", mode="before")
+    def reset_utc(cls, v) -> datetime:
+        # convert milliseconds to seconds
+        dt = chop_datetime_microseconds(datetime.fromtimestamp(v / 1000, tz=UTC))
+
+        if not dt:
+            raise ValueError("Invalid reset")
+
+        return dt
 
 
 class OpenNEMUser(BaseModel):
     valid: bool
     id: str
     owner_id: str | None = None
-    meta: dict | None = None
+    meta: dict | None = Field(exclude=True, default={})
     error: str | None = None
     rate_limit: OpenNEMUserRateLimit | None = None
     roles: list[OpenNEMRoles] = [OpenNEMRoles.anonymous]
