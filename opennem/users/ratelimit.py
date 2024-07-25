11f315e9c164620e36159b0e1f09e701a684a39c
Nik Cubrilovic <git@nikcub.me>
2024-07-25 15:12:56 +1000

    FEAT: academic rate limit and user role

--- opennem/users/ratelimit.py
+++ opennem/users/ratelimit.py
@@ -16,6 +16,14 @@ OPENNEM_RATELIMIT_PRO = unkey.Ratelimit(
     refill_interval=1000,
 )
 
+# 1000 requests per day
+OPENNEM_RATELIMIT_ACADEMIC = unkey.Ratelimit(
+    unkey.RatelimitType.Fast,
+    limit=1000,
+    refill_rate=1000,
+    refill_interval=1000 * 60 * 60 * 24,
+)
+
 # 100 requests per day
 OPENNEM_RATELIMIT_USER = unkey.Ratelimit(
     unkey.RatelimitType.Fast,
