5b6806a27eb1cb713ef2ab7d5dc6c6881d2babe8
Nik Cubrilovic <git@nikcub.me>
2024-08-02 18:49:11 +1000

    FIX: remove old tasks from cli

--- opennem/cli.py
+++ opennem/cli.py
@@ -11,8 +11,6 @@ import logging
 import click
 
 from opennem import settings
-from opennem.api.export.map import PriorityType
-from opennem.api.export.tasks import export_all_monthly, export_energy, export_power
 from opennem.core.crawlers.cli import cmd_crawl_cli
 from opennem.db.load_fixtures import load_bom_stations_json, load_fixtures, load_fueltechs
 from opennem.exporter.historic import export_historic_intervals
@@ -20,7 +18,7 @@ from opennem.importer.db import import_all_facilities
 from opennem.importer.db import init as db_init
 from opennem.parsers.aemo.cli import cmd_data_cli
 from opennem.workers.daily import all_runner, daily_runner
-from opennem.workers.energy import run_energy_update_archive, run_energy_update_days
+from opennem.workers.energy import run_energy_update_archive
 
 logger = logging.getLogger("opennem.cli")
 
@@ -69,33 +67,6 @@ def cmd_import_fueltechs() -> None:
 @click.command()
 def cmd_import_bom_stations() -> None:
     load_bom_stations_json()
-
-
-@click.command()
-def cmd_export_power() -> None:
-    export_power(priority=PriorityType.live)
-
-
-@click.command()
-def cmd_export_energy() -> None:
-    export_energy()
-
-
-@click.command()
-def cmd_export_energy_monthly() -> None:
-    export_all_monthly()
-
-
-@click.command()
-def cmd_export_all() -> None:
-    run_energy_update_days(days=5)
-    export_energy(latest=True)
-    export_energy(priority=PriorityType.monthly)
-
-
-@click.group()
-def cmd_weather() -> None:
-    pass
 
 
 @click.command()
@@ -149,22 +120,16 @@ main.add_command(cmd_crawl_cli, name="crawl")
 main.add_command(cmd_db, name="db")
 main.add_command(cmd_import, name="import")
 main.add_command(cmd_export, name="export")
-main.add_command(cmd_weather, name="weather")
 main.add_command(cmd_task, name="task")
 
 cmd_import.add_command(cmd_import_facilities, name="facilities")
 cmd_import.add_command(cmd_import_fueltechs, name="fueltechs")
 cmd_import.add_command(cmd_import_bom_stations, name="bom")
 
-cmd_export.add_command(cmd_export_all, name="all")
-cmd_export.add_command(cmd_export_power, name="power")
-cmd_export.add_command(cmd_export_energy, name="energy")
-cmd_export.add_command(cmd_export_energy_monthly, name="energy_monthly")
 
 cmd_db.add_command(cmd_db_init, name="init")
 cmd_db.add_command(cmd_db_fixtures, name="fixtures")
 
-cmd_weather.add_command(cmd_weather_init, name="init")
 
 cmd_task.add_command(cmd_task_energy, name="energy")
 cmd_task.add_command(cmd_task_daily, name="daily")
