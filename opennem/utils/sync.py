6fde5357cda233aef5069e4ffbd505e97c249caa
Nik Cubrilovic <git@nikcub.me>
2024-08-08 16:34:15 +1000

    FIX: better run_async_task_reusable implementation

--- opennem/utils/sync.py
+++ opennem/utils/sync.py
@@ -1,11 +1,25 @@
 import asyncio
 import functools
 import logging
+from collections.abc import Callable, Coroutine
+from typing import Any
+
+from asgiref.sync import AsyncToSync
 
 logger = logging.getLogger("opennem.utils.async")
 
 
-def run_async_task_reusable(func):
+def run_async_task_reusable[**P, R](coroutine: Callable[P, Coroutine[Any, Any, R]]):
+    sync_call = AsyncToSync(coroutine)
+
+    @functools.wraps(coroutine)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        return sync_call(*args, **kwargs)
+
+    return wrapper
+
+
+def run_async_task_reusable_old(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
