c28e2ae59b2e7e0d33b88744a7bd70865722d7a1
Nik Cubrilovic <git@nikcub.me>
2024-09-18 20:24:30 +1000

    feat: update renewable proportion query to support grouping by fueltech, renewable etc.

--- opennem/queries/renewable_proportion.py
+++ opennem/queries/renewable_proportion.py
@@ -5,14 +5,15 @@ OpenNEM queries for renewable proportion
 
 from datetime import datetime
 
-from sqlalchemy import case, func, literal_column, select, text
+from sqlalchemy import case, func, select, text
 from sqlalchemy.sql import expression as sql
 
 from opennem.db import get_read_session
-from opennem.db.models.opennem import BalancingSummary, Facility, FacilityScada, FuelTech
+from opennem.db.models.opennem import BalancingSummary, Facility, FacilityScada, FuelTech, FuelTechGroup
 from opennem.recordreactor.buckets import get_bucket_interval
 from opennem.recordreactor.schema import MilestonePeriod
-from opennem.schema.network import NetworkNEM, NetworkSchema
+from opennem.schema.network import NetworkNEM, NetworkSchema, NetworkWEM, NetworkWEMDE
+from opennem.utils.datatable import datatable_print
 
 
 async def get_renewable_energy_proportion(
@@ -22,6 +23,8 @@ async def get_renewable_energy_proportion(
     end_date: datetime,
     network_region: str | None = None,
     group_by_region: bool = True,
+    group_by_fueltech: bool = False,
+    group_by_renewable: bool = False,
 ) -> list[dict]:
     """
     Get the renewable energy proportion for a given network region and date range
@@ -30,11 +33,18 @@ async def get_renewable_energy_proportion(
     """
     rooftop_networks = ["AEMO_ROOFTOP", "AEMO_ROOFTOP_BACKFILL"]
 
+    if network in [NetworkWEM, NetworkWEMDE]:
+        rooftop_networks = ["APVI"]
+
+    # one of group_by_fueltech or group_by_renewable must be true
+    assert group_by_fueltech or group_by_renewable, "one of group_by_fueltech or group_by_renewable must be true"
+
     # Subquery for generation_rooftop
     generation_rooftop = (
         select(
             func.time_bucket_gapfill(text("'5 min'"), FacilityScada.interval).label("interval"),
             Facility.network_region,
+            case((group_by_fueltech, text("'solar'")), else_=text("NULL")).label("fueltech_id"),
             func.locf(func.sum(FacilityScada.generated)).label("generation"),
         )
         .select_from(FacilityScada)
@@ -46,30 +56,36 @@ async def get_renewable_energy_proportion(
             FacilityScada.interval >= start_date,
             FacilityScada.interval < end_date,
         )
-        .group_by(text("1"), text("2"))
+        .group_by(text("1"), text("2"), text("3"))
         .alias("generation_rooftop")
     )
 
     # Subquery for generation_renewable
-    generation_renewable = (
+    generation = (
         select(
             func.time_bucket_gapfill(text("'5 min'"), FacilityScada.interval).label("interval"),
             Facility.network_region,
+            case(
+                (group_by_fueltech, FuelTechGroup.code), else_=text("'renewables'") if group_by_renewable else text("NULL")
+            ).label("fueltech_id"),
             func.sum(FacilityScada.generated).label("generation"),
         )
         .select_from(FacilityScada)
         .join(Facility, Facility.code == FacilityScada.facility_code)
         .join(FuelTech, FuelTech.code == Facility.fueltech_id)
+        .join(FuelTechGroup, FuelTechGroup.code == FuelTech.fueltech_group_id)
         .where(
             Facility.network_id.in_(["NEM", "AEMO_ROOFTOP", "AEMO_ROOFTOP_BACKFILL"]),
             Facility.fueltech_id != "solar_rooftop",
-            FuelTech.renewable.is_(True),
             FacilityScada.interval >= start_date,
             FacilityScada.interval < end_date,
         )
-        .group_by(text("1"), text("2"))
-        .alias("generation_renewable")
     )
+
+    if group_by_renewable:
+        generation = generation.where(FuelTech.renewable.is_(True))
+
+    generation = generation.group_by(text("1"), text("2"), text("3")).alias("generation_renewable")
 
     # Subquery for demand
     demand = (
@@ -95,11 +111,14 @@ async def get_renewable_energy_proportion(
 
     query = (
         select(
-            func.time_bucket(text(f"'{bucket_size_sql}'"), generation_renewable.c.interval).label("interval"),
-            case((group_by_region, generation_renewable.c.network_region), else_=literal_column(f"'{network.code}'")).label(
-                "network_region"
-            ),
-            func.round(func.max(generation_renewable.c.generation), 2).label("generation_renewable"),
+            func.time_bucket(text(f"'{bucket_size_sql}'"), generation.c.interval).label("interval"),
+            case((group_by_region, generation.c.network_region), else_=sql.null()).label("network_region"),
+            case(
+                (group_by_fueltech, generation.c.fueltech_id),
+                (group_by_renewable, text("'renewables'")),
+                else_=sql.null(),
+            ).label("fueltech_id"),
+            func.round(func.max(generation.c.generation), 2).label("generation"),
             func.round(func.max(generation_rooftop.c.generation), 2).label("generation_rooftop"),
             func.round(func.avg(demand.c.demand_total), 2).label("demand_total"),
             case(
@@ -109,7 +128,8 @@ async def get_renewable_energy_proportion(
                         (
                             (
                                 func.sum(generation_rooftop.c.generation)
-                                + func.coalesce(func.sum(generation_renewable.c.generation), 0)
+                                if not group_by_fueltech
+                                else 0 + func.coalesce(func.sum(generation.c.generation), 0)
                             )
                             / (func.sum(demand.c.demand_total) + func.coalesce(func.sum(generation_rooftop.c.generation), 0))
                         )
@@ -118,25 +138,24 @@ async def get_renewable_energy_proportion(
                     ),
                 ),
                 else_=sql.null(),
-            ).label("renewable_proportion"),
+            ).label("proportion"),
         )
-        .select_from(generation_renewable)
+        .select_from(generation)
         .join(
             generation_rooftop,
-            (generation_rooftop.c.interval == generation_renewable.c.interval)
-            & (generation_rooftop.c.network_region == generation_renewable.c.network_region),
+            (generation_rooftop.c.interval == generation.c.interval)
+            & (generation_rooftop.c.network_region == generation.c.network_region),
         )
         .join(
             demand,
-            (demand.c.interval == generation_renewable.c.interval)
-            & (demand.c.network_region == generation_renewable.c.network_region),
+            (demand.c.interval == generation.c.interval) & (demand.c.network_region == generation.c.network_region),
         )
     )
 
     if network_region:
         query = query.where(demand.c.network_region == network_region)
 
-    query = query.group_by(text("1"), text("2")).order_by(text("1"), text("2"))
+    query = query.group_by(text("1"), text("2"), text("3")).order_by(text("1"), text("2"), text("3"))
 
     async with get_read_session() as session:
         result = await session.execute(query)
@@ -151,9 +170,15 @@ if __name__ == "__main__":
     import asyncio
 
     async def main():
-        result = await get_renewable_energy_proportion(
-            network=NetworkNEM, bucket_size=MilestonePeriod.month, start_date=datetime(2024, 1, 1), end_date=datetime(2024, 1, 31)
+        results = await get_renewable_energy_proportion(
+            network=NetworkNEM,
+            bucket_size=MilestonePeriod.interval,
+            start_date=datetime.fromisoformat("2024-08-01 12:00:00"),
+            end_date=datetime.fromisoformat("2024-08-01 12:05:00"),
+            group_by_region=True,
+            group_by_fueltech=False,
+            group_by_renewable=True,
         )
-        print(result)
+        datatable_print(results)
 
     asyncio.run(main())
