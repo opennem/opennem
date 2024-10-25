1a790bd6e675d640990ab2be6b64a9e5fba51301
Nik Cubrilovic <git@nikcub.me>
2024-10-25 08:26:28 +0000

    feat: get_unit_by_code method in queries

--- opennem/queries/facilities.py
+++ opennem/queries/facilities.py
@@ -6,7 +6,7 @@ Facility queries
 from sqlalchemy import select
 
 from opennem.db import get_read_session
-from opennem.db.models.opennem import Facility
+from opennem.db.models.opennem import Facility, Unit
 
 
 async def get_facility_by_code(facility_code: str) -> Facility:
@@ -14,5 +14,14 @@ async def get_facility_by_code(facility_code: str) -> Facility:
 
     async with get_read_session() as session:
         query = select(Facility).where(Facility.code == facility_code)
+        result = await session.execute(query)
+        return result.scalars().one()
+
+
+async def get_unit_by_code(unit_code: str) -> Unit:
+    """Get a unit by its code"""
+
+    async with get_read_session() as session:
+        query = select(Unit).where(Unit.code == unit_code)
         result = await session.execute(query)
         return result.scalars().one()
