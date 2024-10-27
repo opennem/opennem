cd6fab05879f01566ea5743f193dfd780c329107
Nik Cubrilovic <git@nikcub.me>
2024-10-27 04:26:28 +0000

    feat: update cms importer to update all facility and unit fields

--- opennem/cms/importer.py
+++ opennem/cms/importer.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import json
 import logging
-from datetime import datetime
 from pathlib import Path
 
-from rich.prompt import Confirm, Prompt
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
-from opennem.cms.fieldmap import cms_field_to_database_field
+from opennem import settings
+from opennem.clients.slack import slack_message
 from opennem.cms.queries import get_cms_facilities
 from opennem.db import get_read_session, get_write_session
 from opennem.db.models.opennem import Facility, Location, Unit
-from opennem.queries.facilities import get_facility_by_code
 from opennem.schema.facility import CMSFacilitySchema
 
 logger = logging.getLogger("sanity.importer")
@@ -32,53 +31,99 @@ async def get_database_facilities():
     """Get all facilities with their associated station information from the database"""
 
     async with get_read_session() as session:
-        query = select(Facility).where(Facility.approved == True)  # noqa: E712
+        query = select(Facility).join(Unit).where(Facility.approved == True)  # noqa: E712
         result = await session.execute(query)
         facilities = result.scalars().all()
 
     return facilities
 
 
-async def create_database_facility_from_cms(facility: CMSFacilitySchema) -> None:
+async def create_or_update_database_facility(facility: CMSFacilitySchema) -> None:
     """Create new database facilities from the CMS"""
 
-    async with get_write_session() as session:
-        station = Facility(
-            code=facility.code,
-            name=facility.name,
-            network_code=facility.network_id,
-            description=facility.description,
-            wikipedia_link=facility.wikipedia,
-            website_url=facility.website,
-            approved=True,
-            created_at=datetime.now(),
-        )
+    # created_date = get_today_opennem()
+    facility_new = False
 
-        if facility.location and (facility.location.lat and facility.location.lng):
-            station.location = Location(geom=f"SRID=4326;POINT({facility.location.lng} {facility.location.lat})")
+    async with get_write_session() as session:
+        facility_query = select(Facility).options(selectinload(Facility.units)).where(Facility.code == facility.code)
+        facility_db = (await session.execute(facility_query)).scalars().one_or_none()
+
+        if not facility_db:
+            facility_db = Facility(
+                code=facility.code,
+                name=facility.name,
+                network_id=facility.network_id,
+                network_region=facility.network_region,
+                description=facility.description,
+                wikipedia_link=facility.wikipedia,
+                website_url=facility.website,
+                approved=True,
+                # created_at=created_date,
+            )
+            facility_new = True
+
+        if facility.network_id and facility.network_id != facility_db.network_id:
+            facility_db.network_id = facility.network_id
+
+        if facility.name and facility.name != facility_db.name:
+            facility_db.name = facility.name.strip()
+
+        if facility.network_region and facility.network_region != facility_db.network_region:
+            facility_db.network_region = facility.network_region.strip() if facility.network_region else None
+
+        if facility.wikipedia and facility.wikipedia != facility_db.wikipedia_link:
+            facility_db.wikipedia_link = facility.wikipedia.strip()
+
+        if facility.website and facility.website != facility_db.website_url:
+            facility_db.website_url = facility.website.strip()
+
+        if facility_db.location and (facility_db.location.lat and facility_db.location.lng):
+            facility_db.location = Location(geom=f"SRID=4326;POINT({facility_db.location.lng} {facility_db.location.lat})")
+
+        facility_db_unit_codes = [unit.code for unit in facility_db.units]
+        print(facility_db_unit_codes)
 
         for unit in facility.units:
-            db_unit = Unit(
-                code=unit.code,
-                network_id=station.network_code,
-                fueltech_id=unit.fueltech_id,
-                status_id=unit.status_id,
-                dispatch_type=unit.dispatch_type.value,
-                capacity_registered=round(unit.capacity_registered, 2) if unit.capacity_registered else None,
-                emissions_factor_co2=round(unit.emissions_factor_co2, 4) if unit.emissions_factor_co2 else None,
-                expected_closure_date=unit.expected_closure_date,
-                registered=unit.commencement_date,
-                deregistered=unit.closure_date,
-            )
-            station.units.append(db_unit)
+            unit_db = next((x for x in facility_db.units if x.code == unit.code), None)
 
-        session.add(station)
+            if not unit_db:
+                unit_db = Unit(
+                    code=unit.code,
+                    fueltech_id=unit.fueltech_id,
+                    status_id=unit.status_id,
+                    dispatch_type=unit.dispatch_type.value,
+                    capacity_registered=round(unit.capacity_registered, 2) if unit.capacity_registered else None,
+                    emissions_factor_co2=round(unit.emissions_factor_co2, 4) if unit.emissions_factor_co2 else None,
+                    expected_closure_date=unit.expected_closure_date,
+                    registered=unit.commencement_date,
+                    deregistered=unit.closure_date,
+                    # created_at=created_date,
+                )
+                facility_db.units.append(unit_db)
+
+                logger.info(f"Created unit {unit.code} for facility {facility.code}")
+
+            if unit.fueltech_id and unit.fueltech_id != unit_db.fueltech_id:
+                unit_db.fueltech_id = unit.fueltech_id
+
+            if unit.status_id and unit.status_id != unit_db.status_id:
+                unit_db.status_id = unit.status_id
+
+            if unit.dispatch_type and unit.dispatch_type != unit_db.dispatch_type:
+                unit_db.dispatch_type = unit.dispatch_type.value
+
+        session.add(facility_db)
 
         try:
             await session.commit()
-            logger.info(f"Created facility {facility.code} - {facility.name}")
+            logger.info(f"Created facility {facility_db.code} - {facility_db.name}")
+            update_type = "New" if facility_new else "Updated"
+            await slack_message(
+                webhook_url=settings.slack_hook_new_facilities,
+                message=f"{update_type} facility {facility_db.code} - {facility_db.name}",
+            )
         except Exception as e:
-            logger.error(f"Error creating facility {facility.code} - {facility.name}: {e}")
+            logger.error(f"Error creating facility {facility_db.code} - {facility_db.name}: {e}")
 
 
 def check_cms_duplicate_ids(facilities: list[CMSFacilitySchema]) -> tuple[list[str], list[str]]:
@@ -134,51 +179,7 @@ async def update_database_facilities_from_cms(run_updates: bool = True) -> None:
 
     # loop through each facility and add to database if not present or update if present
     for facility in sanity_facilities:
-        if facility.code not in database_facility_codes:
-            logger.info(f"New facility {facility.code} - {facility.name}")
-            add_facility_prompt = Confirm.ask("Add new facility?", default=False)
-
-            if not add_facility_prompt:
-                continue
-
-            await create_database_facility_from_cms(facility)
-            continue
-
-        if not run_updates:
-            logger.info(f"Skipping update for {facility.code} - {facility.name}")
-            continue
-
-        # else get the facility from the database
-        database_facility = await get_facility_by_code(facility.code)
-
-        if not database_facility:
-            logger.error(f"Facility {facility.code} not found in database .. somehow")
-            continue
-
-        logger.info(f"Updating facility {facility.code} - {facility.name}")
-
-        # loop through each field and each unit and update the database
-        for field in ["name", "website", "description", "wikipedia"]:
-            database_field = cms_field_to_database_field(field)
-
-            cms_value = getattr(facility, field)
-            database_value = getattr(database_facility, database_field)
-
-            if cms_value != database_value:
-                logger.info(f" => {field}: {database_value} needs to be updated to {cms_value}")
-
-                confirm = Prompt.ask(f"Update {field}?", choices=["y", "n", "c"], default="n")
-
-                if confirm == "n":
-                    continue
-
-                # update the CMS record
-                elif confirm == "c":
-                    setattr(facility, field, database_value)
-
-                # update the database record
-                else:
-                    setattr(database_facility, database_field, cms_value)
+        await create_or_update_database_facility(facility)
 
 
 if __name__ == "__main__":
