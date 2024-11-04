a02698f7b5785d7bcb80e9bfe8f99572587a3a8e
Nik Cubrilovic <git@nikcub.me>
2024-11-04 08:29:11 +0000

    fix: update sanity webhook router to validate postback

--- opennem/api/webhooks/router.py
+++ opennem/api/webhooks/router.py
@@ -5,7 +5,7 @@ from fastapi_versionizer.versionizer import api_version
 from starlette.exceptions import HTTPException
 
 from opennem import settings
-from opennem.controllers.sanity import parse_sanity_webhook
+from opennem.controllers.sanity import parse_sanity_webhook_request
 
 logger = logging.getLogger("opennem.api.webhooks.router")
 
@@ -15,30 +15,40 @@ router = APIRouter(tags=["Webhooks"], include_in_schema=False)
 @api_version(4)
 @router.post(
     "/sanity/{webhook_secret}",
-    # response_model=APIV4ResponseSchema,
     response_model_exclude_unset=True,
     description="Webhooks",
 )
 @api_version(4)
 async def webhook_sanity_update(webhook_secret: str, request: Request) -> str:
-    """Get a list of milestones"""
+    """
+    Sanity webhook endpoint
 
+    """
     if webhook_secret != settings.webhook_secret:
         raise HTTPException(status_code=404, detail="Not Found")
 
+    if "Content-Type" not in request.headers:
+        raise HTTPException(status_code=400, detail="Invalid request no Content-Type header present")
+
+    if request.headers["Content-Type"] != "application/json":
+        raise HTTPException(status_code=400, detail="Invalid request Content-Type is not application/json")
+
+    if request.headers["sanity-project-id"] != settings.sanity_project_id:
+        raise HTTPException(status_code=400, detail="Invalid request sanity-project-id does not match")
+
     request_json = await request.json()
 
-    logger.debug(request_json)
+    from pprint import pprint
+
+    pprint(request_json)
 
     if "_type" not in request_json:
         raise HTTPException(status_code=400, detail="Invalid request no _type field present")
 
-    await parse_sanity_webhook(request_json)
-
     try:
-        pass
+        await parse_sanity_webhook_request(request_json)
     except Exception as e:
-        logger.error(f"Error parsing webhook: {e}")
+        logger.error(f"Error parsing sanity webhook: {e}")
         raise HTTPException(status_code=500, detail="Server Error") from e
 
     return "OK"
