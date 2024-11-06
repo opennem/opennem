d32eb098a0e08b8cbaaa718daa0c1e85a4ddf75a
Nik Cubrilovic <git@nikcub.me>
2024-11-06 07:58:04 +0000

    fix: simplify battery core module with just DUIDs

--- opennem/core/battery.py
+++ opennem/core/battery.py
@@ -8,25 +8,25 @@ It is called from the NEM crawl controller.
 """
 
 import logging
-from dataclasses import dataclass
 
+from pydantic import BaseModel
 from sqlalchemy import select, text
 
 from opennem.db import get_read_session, get_write_session
 from opennem.db.models.opennem import Facility, Unit
+from opennem.schema.field_types import DUIDType
 from opennem.schema.unit import UnitDispatchType, UnitSchema
 
 logger = logging.getLogger("opennem.core.battery")
 
 
-@dataclass
-class BatteryUnitMap:
-    unit: UnitSchema
-    charge_unit: UnitSchema
-    discharge_unit: UnitSchema
+class BatteryUnitMap(BaseModel):
+    unit: DUIDType
+    charge_unit: DUIDType
+    discharge_unit: DUIDType
 
 
-UNIT_MAP: dict[str, BatteryUnitMap] = {}
+UNIT_MAP: dict[DUIDType, BatteryUnitMap] = {}
 
 
 async def get_battery_unit_map() -> dict[str, BatteryUnitMap]:
@@ -44,6 +44,8 @@ async def get_battery_unit_map() -> dict[str, BatteryUnitMap]:
         logger.debug(f"Found {len(facilities)} bidirectional units")
 
         for facility in facilities:
+            logger.info(f"Processing {facility.code} {facility.name}")
+
             # find the bidirectional unit
             bidirectional_unit = next(
                 (unit for unit in facility.units if unit.dispatch_type == UnitDispatchType.BIDIRECTIONAL.value), None
@@ -52,10 +54,6 @@ async def get_battery_unit_map() -> dict[str, BatteryUnitMap]:
             if not bidirectional_unit:
                 logger.warning(f"No bidirectional unit found for {facility.code} {facility.name}")
                 continue
-
-            unit_schema = UnitSchema(
-                **bidirectional_unit.__dict__,
-            )
 
             # find the charge and discharge units
             charge_unit = list(filter(lambda x: x.fueltech_id == "battery_charging", facility.units))
@@ -70,22 +68,20 @@ async def get_battery_unit_map() -> dict[str, BatteryUnitMap]:
                 continue
 
             if len(charge_unit) > 1:
-                logger.warning(
-                    f"Multiple charge units found for {facility.code} {facility.name}: {",".join([i.code for i in charge_unit])}"
-                )
+                logger.warning(f"Multiple charge units found for {facility.code} {facility.name}")
                 continue
 
             if len(discharge_unit) > 1:
-                logger.warning(f"Multiple discharge units found for {facility.code} {facility.name}: {",".join(discharge_unit)}")
+                logger.warning(f"Multiple discharge units found for {facility.code} {facility.name}")
                 continue
 
             charge_unit_schema = UnitSchema(**charge_unit[0].__dict__)
             discharge_unit_schema = UnitSchema(**discharge_unit[0].__dict__)
 
-            UNIT_MAP[bidirectional_unit.code] = BatteryUnitMap(
-                unit=unit_schema,
-                charge_unit=charge_unit_schema,
-                discharge_unit=discharge_unit_schema,
+            UNIT_MAP[str(bidirectional_unit.code)] = BatteryUnitMap(
+                unit=str(bidirectional_unit.code),
+                charge_unit=charge_unit_schema.code,
+                discharge_unit=discharge_unit_schema.code,
             )
 
             # create the charge and discharge units
@@ -112,7 +108,7 @@ async def process_battery_history():
     )
 
     async with get_write_session() as session:
-        for unit_map in unit_map.values():  # type: ignore  # noqa: B020
+        for unit in unit_map.values():  # type: ignore  # noqa: B020
             # clear out old records
             result = await session.execute(
                 text(
@@ -120,29 +116,28 @@ async def process_battery_history():
                     "and interval >= (select min(interval) from facility_scada where facility_code=:bidirectional_facility_code);"
                 ),
                 {
-                    "discharge_facility_code": unit_map.discharge_unit.code,
-                    "charge_facility_code": unit_map.charge_unit.code,
-                    "bidirectional_facility_code": unit_map.unit.code,
+                    "discharge_facility_code": unit.discharge_unit,
+                    "charge_facility_code": unit.charge_unit,
+                    "bidirectional_facility_code": unit.unit,
                 },
             )
 
             logger.info(
-                f"Deleted {unit_map.discharge_unit.code} and {unit_map.charge_unit.code} records newer than "
-                f"{unit_map.unit.code}: {result.rowcount}"  # type: ignore
+                f"Deleted {unit.discharge_unit} and {unit.charge_unit} records newer than " f"{unit.unit}: {result.rowcount}"  # type: ignore
             )
 
             await session.execute(
                 query_load_template,
-                {"new_facility_code": unit_map.charge_unit.code, "old_facility_code": unit_map.unit.code},
+                {"new_facility_code": unit.charge_unit, "old_facility_code": unit.unit},
             )
 
-            logger.info(f"Updated {unit_map.unit.code} charge to {unit_map.charge_unit.code}")
+            logger.info(f"Updated {unit.unit} charge to {unit.charge_unit}")
             await session.execute(
                 query_generator,
-                {"new_facility_code": unit_map.discharge_unit.code, "old_facility_code": unit_map.unit.code},
+                {"new_facility_code": unit.discharge_unit, "old_facility_code": unit.unit},
             )
 
-            logger.info(f"Updated {unit_map.unit.code} discharge to {unit_map.discharge_unit.code}")
+            logger.info(f"Updated {unit.unit} discharge to {unit.discharge_unit}")
 
             await session.commit()
 
@@ -150,8 +145,7 @@ async def process_battery_history():
 if __name__ == "__main__":
     import asyncio
 
-    unit_map = asyncio.run(process_battery_history())
+    unit_map = asyncio.run(get_battery_unit_map())
 
-    from pprint import pprint
-
-    pprint(unit_map)
+    for unit in unit_map.values():
+        print(f"{unit.unit} -> {unit.charge_unit} | {unit.discharge_unit}")
