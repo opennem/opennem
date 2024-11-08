85ed7b6a546b8b0ea34d4e2f37e284d215aa0427
Nik Cubrilovic <git@nikcub.me>
2024-11-08 11:14:08 +0000

    fix: DUID field type needs to accept forward slash

--- opennem/schema/field_types.py
+++ opennem/schema/field_types.py
@@ -31,4 +31,4 @@ RoundedFloat4 = Annotated[float, BeforeValidator(round_float_4)]
 RoundedFloat8 = Annotated[float, BeforeValidator(round_float_8)]  # used for lat, lng and polygons
 
 # valid DUID
-DUIDType = Annotated[str, StringConstraints(pattern=r"^[A-Z0-9_.\-#]{3,32}$")]
+DUIDType = Annotated[str, StringConstraints(pattern=r"^[A-Z0-9_./\-#]{3,32}$")]
