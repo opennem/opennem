584063043d5ed479a289ddcbb73a5d6f6b537a76
Nik Cubrilovic <git@nikcub.me>
2024-11-13 20:10:21 +1100

    fix: facilities exporter handle bidirectional batteries correctly

--- opennem/exporter/facilities.py
+++ opennem/exporter/facilities.py
@@ -9,6 +9,8 @@ import asyncio
 from opennem.api.schema import APIV4ResponseSchema
 from opennem.cms.importer import get_cms_facilities
 from opennem.exporter.storage_bucket import cloudflare_uploader
+from opennem.utils.dates import get_today_opennem
+from opennem.utils.version import get_version
 
 
 async def export_facilities_static() -> None:
@@ -17,7 +19,21 @@ async def export_facilities_static() -> None:
     # get all facilities
     facilities = get_cms_facilities()
 
-    model_output = APIV4ResponseSchema(success=True, data=facilities, total_records=len(facilities))
+    # remove 'battery' units
+    facilities_clean = []
+
+    for facility in facilities:
+        units = [u for u in facility.units if u.fueltech_id != "battery"]
+        facility.units = units
+        facilities_clean.append(facility)
+
+    model_output = APIV4ResponseSchema(
+        version=get_version(),
+        created_at=get_today_opennem(),
+        success=True,
+        total_records=len(facilities_clean),
+        data=facilities_clean,
+    )
 
     await cloudflare_uploader.upload_content(
         model_output.model_dump_json(exclude_unset=True, exclude_none=True),
