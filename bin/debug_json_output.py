cb1dcb7bb4167d869e90d33cab10847ad170b92f
Nik Cubrilovic <git@nikcub.me>
2024-11-16 10:50:51 +1100

    feat: debug json outputs script

---
+++ bin/debug_json_output.py
@@ -0,0 +1,59 @@
+""" "
+Takes an OpenNEM JSON output and debugs it
+
+"""
+
+import logging
+
+from opennem.utils.datatable import datatable_print
+from opennem.utils.http import http
+
+logger = logging.getLogger(__name__)
+
+
+def main(url: str):
+    logger.info(f"Getting {url}")
+    response = http.get(url)
+
+    if not response.ok:
+        logger.error(f"Failed to get {url}")
+        return
+
+    data = response.json()
+
+    if "data" not in data:
+        logger.error(f"No data in {url}")
+        return
+
+    opennem_data = data["data"]
+    data_items = []
+
+    for item in opennem_data:
+        if "history" not in item:
+            logger.error(f"No history in {item.get('id')}")
+            continue
+
+        data_items.append(
+            {
+                "id": item["id"],
+                "interval": item["history"]["interval"],
+                "date_start": item["history"]["start"],
+                "date_end": item["history"]["last"],
+                "items": len(item["history"]["data"]),
+            }
+        )
+
+    print(f"Version: {data['version']}")
+    print(f"Updated: {data['created_at']}")
+    datatable_print(data_items)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        test_url = sys.argv[1]
+    else:
+        test_url = "https://data.dev.opennem.org.au/v4/stats/au/NEM/NSW1/energy/2024.json"
+
+    main(test_url)
