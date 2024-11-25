d9ad289c760466e46b1cc7db846dc528441cbb54
Nik Cubrilovic <git@nikcub.me>
2024-11-25 16:16:33 +0000

    fix: facility API endpoint output schema and battery filter

--- opennem/api/station/router.py
+++ opennem/api/station/router.py
@@ -9,6 +9,9 @@ from opennem.api import throttle
 from opennem.api.exceptions import OpennemBaseHttpException
 from opennem.api.schema import APIV4ResponseSchema
 from opennem.cms.importer import get_cms_facilities
+from opennem.schema.unit import UnitFueltechType
+from opennem.utils.dates import get_today_opennem
+from opennem.utils.version import get_version
 
 logger = logging.getLogger("opennem.api.station")
 
@@ -61,6 +64,14 @@ async def get_facility(
 
     facilities = get_cms_facilities(facility_code=station_code)
 
+    # remove 'battery' units
+    facilities_clean = []
+
+    for facility in facilities:
+        units = [u for u in facility.units if u.fueltech_id != UnitFueltechType.battery]
+        facility.units = units
+        facilities_clean.append(facility)
+
     if not facilities:
         raise StationNotFound()
 
@@ -73,7 +84,13 @@ async def get_facility(
         )
 
     try:
-        model = APIV4ResponseSchema(success=True, data=[facility], total_records=1)
+        model = APIV4ResponseSchema(
+            version=get_version(),
+            created_at=get_today_opennem(),
+            success=True,
+            data=[facility],
+            total_records=1,
+        )
     except Exception as e:
         logger.error(f"Error creating APIV4ResponseSchema: {e}")
         raise e
