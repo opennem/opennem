d1fbe2f0083c77ad2a54d1271445ad17c37beef6
Nik Cubrilovic <git@nikcub.me>
2024-12-28 22:07:38 +1100

    fix: break up field migration into smaller parts

--- opennem/db/migrations/versions/c0c337f7211a_table_float_columns_convert_to_numeric.py
+++ opennem/db/migrations/versions/c0c337f7211a_table_float_columns_convert_to_numeric.py
@@ -40,92 +40,92 @@ def upgrade() -> None:
                existing_nullable=True)
 
     # Balancing Summary table
-    op.alter_column('balancing_summary', 'forecast_load',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'generation_scheduled',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'generation_non_scheduled',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'generation_total',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'net_interchange',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'demand',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'demand_total',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'price',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'price_dispatch',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('balancing_summary', 'net_interchange_trading',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
+    # op.alter_column('balancing_summary', 'forecast_load',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'generation_scheduled',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'generation_non_scheduled',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'generation_total',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'net_interchange',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'demand',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'demand_total',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'price',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'price_dispatch',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('balancing_summary', 'net_interchange_trading',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
 
     # Facility Aggregate table
-    op.alter_column('at_facility_intervals', 'generated',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'energy',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'emissions',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'emissions_intensity',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'market_value',
-               existing_type=sa.DOUBLE_PRECISION(precision=53),
-               type_=sa.Numeric(precision=20, scale=6),
-               existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'generated',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'energy',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'emissions',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'emissions_intensity',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'market_value',
+    #            existing_type=sa.DOUBLE_PRECISION(precision=53),
+    #            type_=sa.Numeric(precision=20, scale=6),
+    #            existing_nullable=True)
 
 
 def downgrade() -> None:
     # Facility Aggregate table
-    op.alter_column('at_facility_intervals', 'market_value',
-               existing_type=sa.Numeric(precision=20, scale=6),
-               type_=sa.DOUBLE_PRECISION(precision=53),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'emissions_intensity',
-               existing_type=sa.Numeric(precision=20, scale=6),
-               type_=sa.DOUBLE_PRECISION(precision=53),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'emissions',
-               existing_type=sa.Numeric(precision=20, scale=6),
-               type_=sa.DOUBLE_PRECISION(precision=53),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'energy',
-               existing_type=sa.Numeric(precision=20, scale=6),
-               type_=sa.DOUBLE_PRECISION(precision=53),
-               existing_nullable=True)
-    op.alter_column('at_facility_intervals', 'generated',
-               existing_type=sa.Numeric(precision=20, scale=6),
-               type_=sa.DOUBLE_PRECISION(precision=53),
-               existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'market_value',
+    #            existing_type=sa.Numeric(precision=20, scale=6),
+    #            type_=sa.DOUBLE_PRECISION(precision=53),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'emissions_intensity',
+    #            existing_type=sa.Numeric(precision=20, scale=6),
+    #            type_=sa.DOUBLE_PRECISION(precision=53),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'emissions',
+    #            existing_type=sa.Numeric(precision=20, scale=6),
+    #            type_=sa.DOUBLE_PRECISION(precision=53),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'energy',
+    #            existing_type=sa.Numeric(precision=20, scale=6),
+    #            type_=sa.DOUBLE_PRECISION(precision=53),
+    #            existing_nullable=True)
+    # op.alter_column('at_facility_intervals', 'generated',
+    #            existing_type=sa.Numeric(precision=20, scale=6),
+    #            type_=sa.DOUBLE_PRECISION(precision=53),
+    #            existing_nullable=True)
 
     # Balancing Summary table
     op.alter_column('balancing_summary', 'net_interchange_trading',
