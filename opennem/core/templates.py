a6b93787220738d28ea03b93d0a624705e36ad63
Nik Cubrilovic <git@nikcub.me>
2024-12-29 20:38:17 +1100

    fix: template rendering always returns string

--- opennem/core/templates.py
+++ opennem/core/templates.py
@@ -19,10 +19,18 @@ def _get_template_dir() -> str:
 template_lookup = TemplateLookup(directories=[_get_template_dir()], module_directory="/tmp/.mako_modules")
 
 
-def serve_template(template_name: str, **kwargs) -> bytes | str:
+def serve_template(template_name: str, **kwargs) -> str:
     """
     Render a template from the templates directory
     """
     _template = template_lookup.get_template(template_name)
 
-    return _template.render(**kwargs)
+    rendered = _template.render(**kwargs)
+
+    if isinstance(rendered, bytes):
+        return rendered.decode("utf-8")
+
+    elif isinstance(rendered, bytearray | memoryview):
+        return str(rendered)
+
+    return str(rendered)
