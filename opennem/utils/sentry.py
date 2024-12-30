eff1f10699b1d2dfc822acc4269b90bad7079f94
Nik Cubrilovic <git@nikcub.me>
2024-12-31 06:59:06 +1100

    fix: sentry explicit integrations, reduce sampling in prod

--- opennem/utils/sentry.py
+++ opennem/utils/sentry.py
@@ -2,10 +2,23 @@ import logging
 
 import sentry_sdk
 from fastapi import HTTPException
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 logger = logging.getLogger("opennem.utils.sentry")
 
 _SENTRY_IGNORE_EXCEPTION_TYPES = [HTTPException]
+
+_INTEGRATIONS = [
+    RedisIntegration(),
+    SqlalchemyIntegration(),
+    FastApiIntegration(
+        transaction_style="endpoint",
+        failed_request_status_codes={401, 403, *range(500, 599)},
+        http_methods_to_capture=("GET", "POST"),
+    ),
+]
 
 
 def _sentry_before_send(event, hint):
@@ -20,31 +33,26 @@ def _sentry_before_send(event, hint):
 def setup_sentry(sentry_url: str, environment: str) -> None:
     """
     Setup sentry for the application
+
+    @NOTE: default_integrations=False is used to disable the default integrations since there is an error in the arq integration
     """
     if environment == "local":
         logger.info("Sentry not enabled in local mode")
         return
 
-    elif environment == "development":
-        sentry_sdk.init(
-            sentry_url,
-            environment="development",
-        )
-        logger.info("Sentry enabled in dev mode")
-        return
+    sentry_options = {
+        "environment": environment,
+        "traces_sample_rate": 1.0,
+        "profiles_sample_rate": 1.0,
+        "integrations": _INTEGRATIONS,
+        "default_integrations": False,
+    }
 
-    elif environment == "production":
-        sentry_sdk.init(
-            sentry_url,
-            traces_sample_rate=0.1,
-            environment="production",
-            before_send=_sentry_before_send,
-            # integrations=[RedisIntegration(), SqlalchemyIntegration()],  # @NOTE: default integrations
-            profiles_sample_rate=0.1,
-        )
-        logger.info("Sentry enabled in prod mode")
-        return
+    if environment == "production":
+        # in production we reduce the sample rate and ignore HTTP exceptions
+        sentry_options["before_send"] = _sentry_before_send
+        sentry_options["traces_sample_rate"] = 0.1
+        sentry_options["profiles_sample_rate"] = 0.1
 
-    else:
-        logger.error("Sentry not enabled in unknown mode")
-        return
+    sentry_sdk.init(**{**sentry_options, "dsn": sentry_url})
+    logger.info(f"Sentry enabled in {environment} mode")
