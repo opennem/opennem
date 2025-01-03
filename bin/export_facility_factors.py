2416980064696770eb5d1116fbd75447533a33f8
Nik Cubrilovic <git@nikcub.me>
2025-01-03 14:42:16 +1100

    feat: join facility factors with v3 factors and show generation

--- bin/export_facility_factors.py
+++ bin/export_facility_factors.py
@@ -4,8 +4,28 @@ Query sanity and get a list of each facility and unit and their emission factors
 """
 
 import csv
+from dataclasses import dataclass
 
 from opennem.cms.queries import get_unit_factors
+
+
+@dataclass
+class V3UnitFactor:
+    facility_code: str
+    emissions_factor_co2: float
+    emissions_factor_source: str
+    y_generated_gwh: float
+
+
+def read_v3_facility_factors():
+    with open("data/v3_emission_factors.csv") as f:
+        reader = csv.reader(f)
+        return {
+            row[0]: V3UnitFactor(
+                facility_code=row[0], emissions_factor_co2=row[2], emissions_factor_source=row[3], y_generated_gwh=row[4]
+            )
+            for row in reader
+        }
 
 
 def main():
@@ -13,6 +33,7 @@ def main():
 
     # sort by station code
     factors = sorted(factors, key=lambda x: x["code"])
+    v3_factors = read_v3_facility_factors()
 
     # parse into a csv file as one row for each unit containing facility into
     with open("facility_factors.csv", "w") as f:
@@ -25,8 +46,11 @@ def main():
                 "network_region",
                 "unit_code",
                 "fueltech_id",
+                "v3_emission_factor_co2",
+                "v3_emission_factor_source",
                 "emissions_factor_co2",
                 "emissions_factor_source",
+                "y_generated_gwh",
             ]
         )
         for facility in factors:
@@ -42,6 +66,8 @@ def main():
                 ]:
                     continue
 
+                v3_factor = v3_factors.get(unit["code"])
+
                 writer.writerow(
                     [
                         facility["code"],
@@ -50,8 +76,11 @@ def main():
                         facility["network_region"],
                         unit["code"],
                         unit["fueltech_id"],
+                        round(float(v3_factor.emissions_factor_co2), 4) if v3_factor and v3_factor.emissions_factor_co2 else None,
+                        v3_factor.emissions_factor_source if v3_factor and v3_factor.emissions_factor_source else None,
                         unit["emissions_factor_co2"],
                         unit["emissions_factor_source"],
+                        round(float(v3_factor.y_generated_gwh), 4) if v3_factor and v3_factor.y_generated_gwh else None,
                     ]
                 )
 
