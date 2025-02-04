986efda57ea8ec4b342e80c12c4e47224883cbe0
Nik Cubrilovic <git@nikcub.me>
2025-02-04 18:09:06 +1100

    fix: bidirectional battery fix for WEM and multiple bidirectional units on a facility (#404)

--- opennem/core/battery.py
+++ opennem/core/battery.py
@@ -41,14 +41,19 @@ async def get_battery_unit_map() -> dict[str, BatteryUnitMap]:
     return UNIT_MAP
 
 
-async def _generate_battery_unit_map(facility_code: str | None = None) -> dict[str, BatteryUnitMap]:
+def _generate_manual_battery_unit_map() -> dict[str, BatteryUnitMap]:
+    """This is a manual map of bidirectional units to their charge/discharge units"""
+    return {"COLLIE_ESR1": BatteryUnitMap(unit="COLLIE_ESR1", charge_unit="COLLIE_ESRL1", discharge_unit="COLLIE_ESRG1")}
+
+
+async def _generate_battery_unit_map() -> dict[str, BatteryUnitMap]:
     """This queries the database for all the bidirectional battery units and returns what each should be mapped to"""
+
+    # if we have a manual map start with that
+    unit_map: dict[str, BatteryUnitMap] = _generate_manual_battery_unit_map()
 
     async with get_read_session() as session:
         facility_query = select(Facility).join(Unit).where(Unit.dispatch_type == UnitDispatchType.BIDIRECTIONAL.value)
-
-        if facility_code:
-            facility_query = facility_query.where(Facility.code == facility_code)
 
         facility_query = facility_query.order_by(Facility.code, Unit.code)
 
@@ -64,6 +69,10 @@ async def _generate_battery_unit_map(facility_code: str | None = None) -> dict[s
 
             if not bidirectional_unit:
                 logger.warning(f"No bidirectional unit found for {facility.code} {facility.name}")
+                continue
+
+            if unit_map.get(str(bidirectional_unit.code)):
+                logger.info(f"Skipping {facility.code} {facility.name} as it has a manual or existing map")
                 continue
 
             # find the charge and discharge units
@@ -89,7 +98,7 @@ async def _generate_battery_unit_map(facility_code: str | None = None) -> dict[s
             charge_unit_schema = UnitSchema(**charge_unit[0].__dict__)
             discharge_unit_schema = UnitSchema(**discharge_unit[0].__dict__)
 
-            UNIT_MAP[str(bidirectional_unit.code)] = BatteryUnitMap(
+            unit_map[str(bidirectional_unit.code)] = BatteryUnitMap(
                 unit=str(bidirectional_unit.code),
                 charge_unit=charge_unit_schema.code,
                 discharge_unit=discharge_unit_schema.code,
@@ -97,25 +106,31 @@ async def _generate_battery_unit_map(facility_code: str | None = None) -> dict[s
 
             # create the charge and discharge units
 
-    return UNIT_MAP
+    return unit_map
 
 
 async def process_battery_history(facility_code: str | None = None):
     """This processes the history of facility_scada and updates the battery units so that
     bidirectional units are split into two separate units
     """
-    unit_map = await _generate_battery_unit_map(facility_code=facility_code)
+    unit_map = await get_battery_unit_map()
+
+    if not unit_map.get(facility_code):
+        logger.warning(f"No unit map found for {facility_code}")
+        return
+
+    if facility_code:
+        unit_map = {facility_code: unit_map[facility_code]}
 
     query_load_template = text(
-        "update facility_scada set facility_code = :new_facility_code where facility_code = :old_facility_code "
-        "and generated < 0 "
-        "on conflict (network_id, interval, facility_code, is_forecast) do nothing"
+        "update facility_scada set facility_code = :new_facility_code where facility_code = :old_facility_code and generated < 0 "
+        # "on conflict (network_id, interval, facility_code, is_forecast) do nothing"
     )
 
     query_generator = text(
         "update facility_scada set facility_code = :new_facility_code where facility_code = :old_facility_code "
         "and generated >= 0 "
-        "on conflict (network_id, interval, facility_code, is_forecast) do nothing "
+        # "on conflict (network_id, interval, facility_code, is_forecast) do nothing "
     )
 
     async with get_write_session() as session:
@@ -134,7 +149,7 @@ async def process_battery_history(facility_code: str | None = None):
             )
 
             logger.info(
-                f"Deleted {unit.discharge_unit} and {unit.charge_unit} records newer than " f"{unit.unit}: {result.rowcount}"  # type: ignore
+                f"Deleted {unit.discharge_unit} and {unit.charge_unit} records newer than {unit.unit}: {result.rowcount}"  # type: ignore
             )
 
             await session.execute(
@@ -157,11 +172,11 @@ if __name__ == "__main__":
     import asyncio
 
     async def backlog_battery_history():
-        for facility_code in ["RESS", "RIVNB", "TB2SF", "WDBESS"]:
+        for facility_code in ["COLLIE_ESR1", "KWINANA_ESR2"]:
             logger.info(f"Processing {facility_code}")
             await process_battery_history(facility_code=facility_code)
 
     unit_map = asyncio.run(backlog_battery_history())
 
     # for unit in unit_map.values():
-    #     print(f"{unit.unit} -> {unit.charge_unit} | {unit.discharge_unit}")
+    # print(f"{unit.unit} -> {unit.charge_unit} | {unit.discharge_unit}")
