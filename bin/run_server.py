b4faa00e1c8c4d3f3e5bdf50980590c941243c0a
Nik Cubrilovic <git@nikcub.me>
2025-02-20 21:19:51 +1100

    fix: dev server number of workers and logging on startup

--- bin/run_server.py
+++ bin/run_server.py
@@ -19,14 +19,14 @@ logger = logging.getLogger("opennem.server")
 RELOAD_PATH = Path(__file__).parent.parent.resolve() / "opennem"
 
 
-def run_server(host: str = "0.0.0.0", port: int = 8000, server_ssl: bool = False, workers: int | None = 2) -> None:
+def run_server(host: str = "0.0.0.0", port: int = 8000, server_ssl: bool = False, workers: int | None = 1) -> None:
     log_level = settings.log_level.lower()
     reload_enabled = False
     reload_dirs = None
     ssl_options = {}
 
     # @TODO move this into opennem.settings
-    if settings.debug or settings.is_dev:
+    if settings.debug or settings.is_dev or settings.is_local:
         reload_enabled = True
         log_level = "debug"
         reload_dirs = [str(RELOAD_PATH)]
@@ -37,6 +37,8 @@ def run_server(host: str = "0.0.0.0", port: int = 8000, server_ssl: bool = False
             "ssl_keyfile": "./var/_wildcard.opennem.localhost-key.pem",
             "ssl_certfile": "./var/_wildcard.opennem.localhost.pem",
         }
+
+    logger.info(f"Running server on {host} {port} with {workers} workers")
 
     uvicorn.run(
         "opennem.api.app:app",
