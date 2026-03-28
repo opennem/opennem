aee131e3fd79812c10853d0bfc81439b4eac1e23
Nik Cubrilovic <git@nikcub.me>
2026-03-29 08:20:10 +1100

    perf(db): switch SA engine to NullPool, let pgbouncer handle pooling

--- opennem/db/__init__.py
+++ opennem/db/__init__.py
@@ -54,12 +54,7 @@ def db_connect(db_conn_str: str | None = None, debug: bool = False, timeout: int
             query_cache_size=1200,
             echo=settings.db_debug,
             future=True,
-            pool_size=30,
-            max_overflow=20,
-            pool_recycle=1800,
-            pool_timeout=timeout,
-            pool_pre_ping=True,
-            pool_use_lifo=True,
+            poolclass=NullPool,
         )
 
         return _engine
